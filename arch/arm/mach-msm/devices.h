bc075a448a9f1f1c291a5bec20f7df18fdb38251
Dima Zavin <dima@android.com>
2009-03-30 18:36:59 -0700

    [ARM] msm: Add touchscreen device definition
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/devices.h
+++ arch/arm/mach-msm/devices.h
@@ -40,6 +40,7 @@ extern struct platform_device msm_device_nand;
 extern struct platform_device msm_device_mddi0;
 extern struct platform_device msm_device_mddi1;
 extern struct platform_device msm_device_mdp;
+extern struct platform_device msm_device_touchscreen;
 
 extern struct clk msm_clocks_7x01a[];
 extern unsigned msm_num_clocks_7x01a;
