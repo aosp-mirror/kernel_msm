cab9940dff4e4035465010bfafb5a79e57560b41
Iliyan Malchev <malchev@google.com>
2009-04-01 17:29:08 -0700

    [ARM] msm: AMSS >= 6350 swaps VFE_CLK and VFE_MDC_CLK
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- arch/arm/mach-msm/clock-pcom.h
+++ arch/arm/mach-msm/clock-pcom.h
@@ -72,8 +72,13 @@
 #define P_USB_HS_P_CLK	37  /* High speed USB pbus clock */
 #define P_USB_OTG_CLK	38  /* Full speed USB clock */
 #define P_VDC_CLK	39  /* Video controller clock */
-#define P_VFE_MDC_CLK	40  /* Camera / Video Front End clock */
-#define P_VFE_CLK	41  /* VFE MDDI client clock */
+#if CONFIG_MSM_AMSS_VERSION >= 6350
+#define VFE_MDC_CLK	40  /* VFE MDDI client clock */
+#define VFE_CLK		41  /* Camera / Video Front End clock */
+#else/* For radio code base others */
+#define VFE_MDC_CLK	41  /* VFE MDDI client clock */
+#define VFE_CLK		40  /* Camera / Video Front End clock */
+#endif
 #define P_MDP_LCDC_PCLK_CLK	42
 #define P_MDP_LCDC_PAD_PCLK_CLK 43
 #define P_MDP_VSYNC_CLK	44
