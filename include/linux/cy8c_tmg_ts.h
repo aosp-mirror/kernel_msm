6c89a541d5b15e04bc3769a4e20f9f08a1e82a23
Dima Zavin <dima@android.com>
2009-05-03 03:11:52 -0700

    input: support for Cypress TMG i2c capacitive touchscreen
    Based on a driver from HTC.
     - lots of cleanups.
     - don't do averaging/snapping for x/y coordinates
       add a fuzz value instead, and report the raw events to userspace.
    
    Signed-off-by: Dima Zavin <dima@android.com>

---
+++ include/linux/cy8c_tmg_ts.h
@@ -0,0 +1,37 @@
+/* include/linux/cy8c_tmg_ts.c
+ *
+ * Copyright (C) 2007-2008 HTC Corporation.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef CY8C_I2C_H
+#define CY8C_I2C_H
+
+#include <linux/types.h>
+
+#define CYPRESS_TMG_NAME "cy8c-tmg-ts"
+
+struct cy8c_i2c_platform_data {
+	uint16_t version;
+	int abs_x_min;
+	int abs_x_max;
+	int abs_y_min;
+	int abs_y_max;
+	int abs_pressure_min;
+	int abs_pressure_max;
+	int abs_width_min;
+	int abs_width_max;
+	int (*power)(int on);
+};
+
+#endif
+
