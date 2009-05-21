7ea8a8ec3a53957208dcbb45e792ecd24fce6e21
Dima Zavin <dima@android.com>
2009-05-20 22:55:58 -0700

    [ARM] msm: Hack the RTC driver to match either msm7k AMSS version or arch_qsd8k
    Signed-off-by: Dima Zavin <dima@android.com>

--- drivers/rtc/rtc-msm7x00a.c
+++ drivers/rtc/rtc-msm7x00a.c
@@ -30,7 +30,7 @@
 
 extern void msm_pm_set_max_sleep_time(int64_t sleep_time_ns);
 
-#if CONFIG_MSM_AMSS_VERSION >= 6350
+#if CONFIG_MSM_AMSS_VERSION >= 6350 || defined(CONFIG_ARCH_QSD8X50)
 #define APP_TIMEREMOTE_PDEV_NAME "rs30000048:00010000"
 #else
 #define APP_TIMEREMOTE_PDEV_NAME "rs30000048:0da5b528"
