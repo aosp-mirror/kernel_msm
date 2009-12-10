1dac4331fb747efed7296100dc52aa50a8582705
Iliyan Malchev <malchev@google.com>
2009-12-10 00:28:51 -0800

    [ARM] qsd8k: map in TCSR_SPARE2
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- arch/arm/mach-msm/io.c
+++ arch/arm/mach-msm/io.c
@@ -86,6 +86,7 @@ static struct map_desc qsd8x50_io_desc[] __initdata = {
 	MSM_DEVICE(SCPLL),
 	MSM_DEVICE(AD5),
 	MSM_DEVICE(MDC),
+	MSM_DEVICE(TCSR),
 #ifdef CONFIG_MSM_DEBUG_UART
 	MSM_DEVICE(DEBUG_UART),
 #endif
