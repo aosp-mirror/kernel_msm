ac5ee56cfbb3c0ef3ef22d31a338bccf01544dfd
HK Chen <hk_chen@htc.com>
2010-02-02 11:09:51 -0800

    [ARM] qsd8k: mahimahi: register as a misc. device for dynamic configuration
    -- Provide the following ioctls, which can override the defaults and retrieve
    current configuration:
    
    TPA2018_SET_CONFIG
    TPA2018_READ_CONFIG
    TPA2018_SET_MODE
    TPA2018_SET_PARAM
    
    -- Add a public header tpa2018d1.h for the ioctl()s
    
    Change-Id: I49b3bf4c88fa441d282ee6f19c2307a30a07a87f
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- include/linux/tpa2018d1.h
+++ include/linux/tpa2018d1.h
@@ -13,23 +13,24 @@
  *
  */
 
+#ifndef _LINUX_TPA2018D1_H
+#define _LINUX_TPA2018D1_H
 
-#ifndef __ASM_ARM_ARCH_TPA2018D1_H
-#define __ASM_ARM_ARCH_TPA2018D1_H
+#include <linux/ioctl.h>
 
-#define TPA2018D1_I2C_NAME "tpa2018d1"
-#define TPA2018D1_CMD_LEN 8
-
-struct tpa2018d1_platform_data {
-	uint32_t gpio_tpa2018_spk_en;
+enum tpa2018d1_mode {
+	TPA2018_MODE_OFF,
+	TPA2018_MODE_PLAYBACK,
+	TPA2018_MODE_RINGTONE,
+	TPA2018_MODE_VOICE_CALL,
+	TPA2018_NUM_MODES,
 };
 
-struct tpa2018d1_config_data {
-	unsigned char *cmd_data;  /* [mode][cmd_len][cmds..] */
-	unsigned int mode_num;
-	unsigned int data_len;
-};
+#define TPA2018_IOCTL_MAGIC	'a'
+#define TPA2018_SET_CONFIG	_IOW(TPA2018_IOCTL_MAGIC, 1, unsigned)
+#define TPA2018_READ_CONFIG	_IOR(TPA2018_IOCTL_MAGIC, 2, unsigned)
+#define TPA2018_SET_PARAM	_IOW(TPA2018_IOCTL_MAGIC, 3, unsigned)
+#define TPA2018_SET_MODE	_IOW(TPA2018_IOCTL_MAGIC, 4, unsigned)
 
-extern void tpa2018d1_set_speaker_amp(int on);
+#endif
 
-#endif /* __ASM_ARM_ARCH_TPA2018D1_H */
