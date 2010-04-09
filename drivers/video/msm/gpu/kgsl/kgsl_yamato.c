9f2ac21ba3effd1f0c3083c9db150283cdf7960a
Shubhraprakash Das <sadas@quicinc.com>
2010-04-08 17:11:57 -0700

    [ARM] msm: kgsl: Invalidate base addrs when setting page tables or flushing TLBs
    Change-Id: I17917aac393bec76950cbdc1978f11ee734b836b
    Signed-off-by: Shubhraprakash Das <sadas@quicinc.com>
    Signed-off-by: Dima Zavin <dima@android.com>

--- drivers/video/msm/gpu/kgsl/kgsl_yamato.c
+++ drivers/video/msm/gpu/kgsl/kgsl_yamato.c
@@ -398,6 +398,12 @@ int kgsl_yamato_setstate(struct kgsl_device *device, uint32_t flags)
 			sizedwords += 21;
 		}
 
+		if (flags & (KGSL_MMUFLAGS_PTUPDATE | KGSL_MMUFLAGS_TLBFLUSH)) {
+			*cmds++ = pm4_type3_packet(PM4_INVALIDATE_STATE, 1);
+			*cmds++ = 0x7fff; /* invalidate all base pointers */
+			sizedwords += 2;
+		}
+
 		kgsl_ringbuffer_issuecmds(device, KGSL_CMD_FLAGS_PMODE,
 					  &link[0], sizedwords);
 	} else {
