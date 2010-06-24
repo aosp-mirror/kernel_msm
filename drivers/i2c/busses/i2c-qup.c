0a2fe3ada4db9edde5969605b76c0b2227c8494d
Dima Zavin <dima@android.com>
2010-06-23 21:12:30 -0700

    qup_i2c: make compile for 2.6.35
    Change-Id: Id583e78494c0501bcbff78d86412fdba34a267ab
    Signed-off-by: Dima Zavin <dima@android.com>

--- drivers/i2c/busses/i2c-qup.c
+++ drivers/i2c/busses/i2c-qup.c
@@ -67,6 +67,7 @@
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
+#include <linux/slab.h>
 #include <linux/delay.h>
 #include <linux/io.h>
 #include <mach/msm_qup.h>
