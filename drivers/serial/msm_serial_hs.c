6c298fcbadd1d2bff43cb907f765c2c2f5e6e269
Dmitry Shmidt <dimitrysh@google.com>
2010-07-02 15:18:23 -0700

    serial_hs: msm: Fix section mismatch warning
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/serial/msm_serial_hs.c
+++ drivers/serial/msm_serial_hs.c
@@ -1339,7 +1339,7 @@ static int uartdm_init_port(struct uart_port *uport)
 	return 0;
 }
 
-static int __init msm_hs_probe(struct platform_device *pdev)
+static int msm_hs_probe(struct platform_device *pdev)
 {
 	int ret;
 	struct uart_port *uport;
