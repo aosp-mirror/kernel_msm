cf5a7b56019dda70bebfcdc39f9012567404ec94
Laura Abbott <lauraa@codeaurora.org>
2011-11-14 15:27:02 -0800

    gpu: ion: Add call back for unmapping user mappings
    When mmap is called to remove a mapping, the mmap code
    takes care of removing all the entries so it isn't
    necessary to call unmap versions. However, there may be
    cases where heaps need to do other buffer clean up when
    a userspace buffer is unmapped. Add an unmap_user function
    to the ion heap ops. This callback needs to happen in
    vma_close, so move the decrementing of the overall user map
    call back as well.
    
    Change-Id: I8e5716774dd973828f76e03ec43e8e8ecf8c7936
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -1052,6 +1052,9 @@ static void ion_vma_close(struct vm_area_struct *vma)
 	mutex_lock(&buffer->lock);
 	buffer->umap_cnt--;
 	mutex_unlock(&buffer->lock);
+
+	if (buffer->heap->ops->unmap_user)
+		buffer->heap->ops->unmap_user(buffer->heap, buffer);
 }
 
 static struct vm_operations_struct ion_vm_ops = {
