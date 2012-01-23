8fa10ad0b7289637c8b296f17178ae148cae68f6
Laura Abbott <lauraa@codeaurora.org>
2012-01-23 13:06:20 -0800

    printk: Log printk events
    Log printk events to the uncached buffer. If a reset occurs
    and printk data is still present in the cache, information will
    be lost. This doesn't store the actual printk, but it at least
    gives information on who the last caller was.
    
    Change-Id: If1bd414a44b19b45c157d23a7ddde8f16b30e780
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Trevor Bourget <tbourget@codeaurora.org>
    (cherry picked from commit aa9b60bb87a69ba2e0472fbfb04a8c056690936f)
    
    Conflicts:
    
    	arch/arm/mach-msm/include/mach/msm_rtb.h
    	arch/arm/mach-msm/msm_rtb.c

--- kernel/printk.c
+++ kernel/printk.c
@@ -46,6 +46,7 @@
 
 #include <asm/uaccess.h>
 
+#include <mach/msm_rtb.h>
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 
@@ -1677,6 +1678,11 @@ asmlinkage int printk(const char *fmt, ...)
 {
 	va_list args;
 	int r;
+#ifdef CONFIG_MSM_RTB
+	void *caller = __builtin_return_address(0);
+
+	uncached_logk_pc(LOGK_LOGBUF, caller, (void *)log_next_idx);
+#endif
 
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
