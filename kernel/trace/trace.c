c10ea662e18da3e91d3b8e381b5c592e73c64544
Pratik Patel <pratikp@codeaurora.org>
2012-06-29 12:24:49 -0700

    coresight: enable stm logging for ftrace events and printk
    Dup ftrace event traffic (including writes to trace_marker file from
    userspace) to STM. Also dup printk traffic to STM. This allows Linux
    tracing and log data to be correlated with other data transported over
    STM.
    
    Change-Id: Ieb0b856447f7667eb0005a6a884211dc46f50217
    Signed-off-by: Pratik Patel <pratikp@codeaurora.org>
    (cherry picked from commit 8e1e6b65fe92a0fa7bdb787fc7d9c5c0eae3d654)
    
    Conflicts:
    
    	include/linux/coresight-stm.h
    	kernel/printk.c

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -40,6 +40,7 @@
 #include <linux/nmi.h>
 #include <linux/fs.h>
 #include <linux/sched/rt.h>
+#include <linux/coresight-stm.h>
 
 #include "trace.h"
 #include "trace_output.h"
@@ -1783,6 +1784,7 @@ int trace_array_vprintk(struct trace_array *tr,
 	memcpy(&entry->buf, tbuffer, len);
 	entry->buf[len] = '\0';
 	if (!filter_check_discard(call, entry, buffer, event)) {
+		stm_log(OST_ENTITY_TRACE_PRINTK, entry->buf, len + 1);
 		__buffer_unlock_commit(buffer, event);
 		ftrace_trace_stack(buffer, flags, 6, pc);
 	}
@@ -4140,8 +4142,11 @@ tracing_mark_write(struct file *filp, const char __user *ubuf,
 	if (entry->buf[cnt - 1] != '\n') {
 		entry->buf[cnt] = '\n';
 		entry->buf[cnt + 1] = '\0';
-	} else
+		stm_log(OST_ENTITY_TRACE_MARKER, entry->buf, cnt + 2);
+	} else {
 		entry->buf[cnt] = '\0';
+		stm_log(OST_ENTITY_TRACE_MARKER, entry->buf, cnt + 1);
+	}
 
 	__buffer_unlock_commit(buffer, event);
 
