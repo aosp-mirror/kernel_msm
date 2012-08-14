ddec4d6fb39df3d5ca3d3ef15d3ccb0c0cb16386
Shubhraprakash Das <sadas@codeaurora.org>
2012-08-14 00:25:43 -0700

    lib: genalloc: Change chunk allocation to vmalloc
    Change the chunk allocation from kmalloc to vmalloc for
    allocations greater than a page. This allows large
    chunks to be allocated from physically non-contiguous
    memory and increases the chance of the allocation
    succeeding.
    
    CRs-fixed: 387655
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    (cherry picked from commit 0614b96dc8e4c8a0dd0db41aec5a79daa3bdf091)
    
    Conflicts:
    
    	lib/genalloc.c
    
    Change-Id: Ic0dedbad9d277d0842591b1f075ce3b80c8c05de
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- lib/genalloc.c
+++ lib/genalloc.c
@@ -14,7 +14,7 @@
 #include <linux/module.h>
 #include <linux/bitmap.h>
 #include <linux/genalloc.h>
-
+#include <linux/vmalloc.h>
 
 /* General purpose special memory pool descriptor. */
 struct gen_pool {
@@ -88,9 +88,17 @@ int __must_check gen_pool_add_virt(struct gen_pool *pool, unsigned long virt, ph
 		return -EINVAL;
 
 	nbytes = sizeof *chunk + BITS_TO_LONGS(size) * sizeof *chunk->bits;
-	chunk = kzalloc_node(nbytes, GFP_KERNEL, nid);
+
+	if (nbytes <= PAGE_SIZE)
+		chunk = kzalloc_node(nbytes, GFP_KERNEL, nid);
+	else
+		chunk = vmalloc(nbytes);
+
 	if (!chunk)
 		return -ENOMEM;
+
+	if (nbytes > PAGE_SIZE)
+		memset(chunk, 0, nbytes);
 
 	spin_lock_init(&chunk->lock);
 	chunk->phys_addr = phys;
@@ -142,6 +150,7 @@ void gen_pool_destroy(struct gen_pool *pool)
 {
 	struct gen_pool_chunk *chunk;
 	int bit;
+	size_t nbytes;
 
 	while (!list_empty(&pool->chunks)) {
 		chunk = list_entry(pool->chunks.next, struct gen_pool_chunk,
@@ -151,7 +160,13 @@ void gen_pool_destroy(struct gen_pool *pool)
 		bit = find_next_bit(chunk->bits, chunk->size, 0);
 		BUG_ON(bit < chunk->size);
 
-		kfree(chunk);
+		nbytes = sizeof *chunk + BITS_TO_LONGS(chunk->size) *
+			sizeof *chunk->bits;
+
+		if (nbytes <= PAGE_SIZE)
+			kfree(chunk);
+		else
+			vfree(chunk);
 	}
 	kfree(pool);
 }
