MiCode/Xiaomi_Kernel_OpenSource

usb: dwc3-msm: Fix system suspend race with USB connection
DWC3 driver releases/acquires wake_lock as part of its
runtime suspend/resume routines. This allows DWC3's system
suspend routine to be called only after its wake_lock has been
released as part of runtime suspend. But, currently there exists
a race in code where DWC3 may receive a wakeup event just before
its system suspend is kicked-in. Though USB is brought out of LPM
as part of wakeup event processing but it may be put in LPM again
by DWC3's system suspend, resulting in missed USB wakeup event.

Fix this by aborting system suspend if USB is not in LPM. And also
flush resume_work from system suspend to make sure any pending
wakeup event has already been processed before the safeguard check.

Change-Id: Ida57c7aca17065c671b446fc8a66b83d3a356f36
CRs-Fixed: 441974
Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

drivers/usb/dwc3/dwc3-msm.c
@@ -2490,6 +2490,12 @@ static int dwc3_msm_pm_suspend(struct device *dev)
 
 	dev_dbg(dev, "dwc3-msm PM suspend\n");
 
+	flush_delayed_work_sync(&mdwc->resume_work);
+	if (!atomic_read(&mdwc->in_lpm)) {
+		dev_err(mdwc->dev, "Abort PM suspend!! (USB is outside LPM)\n");
+		return -EBUSY;
+	}
+
 	ret = dwc3_msm_suspend(mdwc);
 	if (!ret)
 		atomic_set(&mdwc->pm_suspended, 1);

aosp-mirror/kernel_msm

drivers/usb/dwc3/dwc3-msm.c
@@ -2490,6 +2490,12 @@ static int dwc3_msm_pm_suspend(struct device *dev)
 
 	dev_dbg(dev, "dwc3-msm PM suspend\n");
 
+	flush_delayed_work(&mdwc->resume_work);
+	if (!atomic_read(&mdwc->in_lpm)) {
+		dev_err(mdwc->dev, "Abort PM suspend!! (USB is outside LPM)\n");
+		return -EBUSY;
+	}
+
 	ret = dwc3_msm_suspend(mdwc);
 	if (!ret)
 		atomic_set(&mdwc->pm_suspended, 1);