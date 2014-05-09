b4eb7bc45f5edac586ef93a25c504486d2e99c0d
choongryeol.lee <choongryeol.lee@lge.com>
2014-05-08 18:21:35 -0700

    power: max17050: report instantaneous battery current
    Some driver needs an instantaneous current measurement
    rather than average current. Since there is no need
    for the average current, report instantaneous current
    instead of average current.
    
    Change-Id: I84b489bf87cf8f7a23e4551494cc998c0cd24e50
    Signed-off-by: choongryeol.lee <choongryeol.lee@lge.com>

--- drivers/power/max17050_battery.c
+++ drivers/power/max17050_battery.c
@@ -496,7 +496,7 @@ static void max17050_update(struct max17050_chip *chip)
 	chip->repcap = max17050_read_reg(client, MAX17050_REPCAP);
 	chip->repsoc = max17050_read_reg(client, MAX17050_REPSOC);
 	chip->tte = max17050_read_reg(client, MAX17050_TTE);
-	chip->current_now = max17050_read_reg(client, MAX17050_AVGCURRENT);
+	chip->current_now = max17050_read_reg(client, MAX17050_CURRENT);
 	chip->temp = max17050_read_reg(client, MAX17050_TEMP);
 	chip->vcell = max17050_read_reg(client, MAX17050_VCELL);
 	chip->learned.cycles = max17050_read_reg(client, MAX17050_CYCLES);
