b25d226a49c7625de16b42cd09712430a0f76250
Eason <eason_fan@htc.com>
2014-08-26 20:34:44 +0800

    [restart][debug][arm64] show saved_command_line when system restart
    Symptom:   N/A
    Root Cause:N/A
    Solution:  N/A
    Project:   8996
    Note:      N/A
    
    Change-Id: I1dc12b6fdac839703e9f8aadb57a9623f483c827
    Reviewed-on: http://git.htc.com:8081/420518
    Reviewed-by: woody_lin <woody_lin@htc.com>
    Tested-by: woody_lin <woody_lin@htc.com>
    Reviewed-on: http://git.htc.com:8081/694699
    Tested-by: Jason.CS_Chiu <jason.cs_chiu@htc.com>
    Reviewed-on: http://git.htc.com:8081/714714
    Reviewed-by: Jason.CS_Chiu <jason.cs_chiu@htc.com>
    Reviewed-by: Midas_Chieh <midas_chieh@htc.com>
    Tested-by: Midas_Chieh <midas_chieh@htc.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -156,9 +156,13 @@ void machine_power_off(void)
  */
 void machine_restart(char *cmd)
 {
+	extern char *saved_command_line;
+
 	/* Disable interrupts first */
 	local_irq_disable();
 	smp_send_stop();
+
+	pr_emerg("Kernel command line: %s\n", saved_command_line);
 
 	/* Now call the architecture specific reboot code. */
 	if (arm_pm_restart)
