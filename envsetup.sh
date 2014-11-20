a78b04c1e6428cfaeb17b7bfcc6b0ab794579dce
Artem Borisov <dedsa2002@gmail.com>
2014-11-20 23:25:34 +0300

    Envsetup: use symlinked linaro binary instead of full name

--- envsetup.sh
+++ envsetup.sh
@@ -2,6 +2,6 @@
 
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=~/arm-cortex_a15-linux-gnueabihf-linaro_4.7.4-2014.06/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=~/arm-cortex_a15-linux-gnueabihf-linaro_4.7.4-2014.06/bin/arm-eabi-
 
 export TARGET_PREBUILT_KERNEL=$(pwd)/arch/arm/boot/zImage
