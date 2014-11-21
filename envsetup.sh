eff32a1cc937927f74ea0613881ce4f4109b3dcb
Artem Borisov <dedsa2002@gmail.com>
2014-11-21 20:42:20 +0300

    Fixed build for GCC newer than 4.7(memset bug)

--- envsetup.sh
+++ envsetup.sh
@@ -2,6 +2,6 @@
 
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=~/arm-cortex_a15-linux-gnueabihf-linaro_4.7.4-2014.06/bin/arm-eabi-
+export CROSS_COMPILE=~/arm-cortex_a15-linux-gnueabihf-linaro_4.9.2-2014.10/bin/arm-eabi-
 
 export TARGET_PREBUILT_KERNEL=$(pwd)/arch/arm/boot/zImage
