702bb6170a2cb1195fdaf9c3fc357e7d7b48e78c
Nikhilesh Reddy <reddyn@codeaurora.org>
2015-05-15 16:03:01 -0700

    fuse: Add support for shortcircuited read/write for files
    Add support for shortcircuited read/write for files
    when enabled through a userspace init option of
    FUSE_SHORTCIRCUIT.
    
    When FUSE_SHORTCIRCUIT is enabled all the reads and writes
    to the fuse mount point go directly to the native filesystem
    rather than through the fuse daemon. All requsts that aren't
    read/write still go thought the userspace code.
    
    This allows for significantly better performance on read and writes
    and the difference between fuse and the native lower filesystem is
    negligible.
    
    Bug: 30222859
    Change-Id: I49e21b77813595c2faec6fcba38a74e8f686d020
    Signed-off-by: Nikhilesh Reddy <reddyn@codeaurora.org>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -7,6 +7,7 @@
 */
 
 #include "fuse_i.h"
+#include "fuse_shortcircuit.h"
 
 #include <linux/init.h>
 #include <linux/module.h>
@@ -1880,6 +1881,8 @@ static ssize_t fuse_dev_do_write(struct fuse_conn *fc,
 							req->canonical_path);
 	}
 	fuse_copy_finish(cs);
+
+	fuse_setup_shortcircuit(fc, req);
 
 	spin_lock(&fc->lock);
 	req->locked = 0;
