dd05c74b618f47a1ae4a6684e7d655d42ab901f8
Nikhilesh Reddy <reddyn@codeaurora.org>
2015-05-15 16:03:01 -0700

    fuse: Add support for shortcircuited read/write for files
    Add support for shortcircuited read/write for files
    when enabled through a userspace init option of
    FUSE_SHORTCIRCUIT.
    
    When FUSE_SHORTCIRCUIT is enabled all the reads and writes
    to the fuse mount point go directly to the native filesystem
    rather than through the fuse daemon. All requsts that aren't
    read/write still go thought the userspace code.
    
    This allows for significantly better performance on read and writes
    and the difference between fuse and the native lower filesystem is
    negligible.
    
    Bug: 30222859
    Change-Id: I49e21b77813595c2faec6fcba38a74e8f686d020
    Signed-off-by: Nikhilesh Reddy <reddyn@codeaurora.org>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -904,6 +904,10 @@ static void process_init_reply(struct fuse_conn *fc, struct fuse_req *req)
 				fc->async_dio = 1;
 			if (arg->flags & FUSE_WRITEBACK_CACHE)
 				fc->writeback_cache = 1;
+			if (arg->flags & FUSE_SHORTCIRCUIT) {
+				fc->writeback_cache = 0;
+				fc->shortcircuit_io = 1;
+			}
 		} else {
 			ra_pages = fc->max_read / PAGE_CACHE_SIZE;
 			fc->no_lock = 1;
