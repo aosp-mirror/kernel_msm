5a4290579aa6a0175594ce44a5ef4ec208721be3
Ricky Liang <jcliang@chromium.org>
2015-09-03 11:15:27 +0800

    CHROMIUM: Enable ENERGY_AWARE sched feature
    Signed-off-by: Ricky Liang <jcliang@chromium.org>
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I8b6feaca9ca65aa76f4fa585621ae82ed7be839c

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -83,4 +83,4 @@ SCHED_FEAT(NUMA_RESIST_LOWER, false)
  * Energy aware scheduling. Use platform energy model to guide scheduling
  * decisions optimizing for energy efficiency.
  */
-SCHED_FEAT(ENERGY_AWARE, false)
+SCHED_FEAT(ENERGY_AWARE, true)
