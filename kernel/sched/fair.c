10cd9acce79e730beef4a7a5ddc8bbd57ef61004
Waiman Long <Waiman.Long@hpe.com>
2015-11-25 14:09:38 -0500

    UPSTREAM: sched/fair: Avoid redundant idle_cpu() call in update_sg_lb_stats()
    Part of the responsibility of the update_sg_lb_stats() function is to
    update the idle_cpus statistical counter in struct sg_lb_stats. This
    check is done by calling idle_cpu(). The idle_cpu() function, in
    turn, checks a number of fields within the run queue structure such
    as rq->curr and rq->nr_running.
    
    With the current layout of the run queue structure, rq->curr and
    rq->nr_running are in separate cachelines. The rq->curr variable is
    checked first followed by nr_running. As nr_running is also accessed
    by update_sg_lb_stats() earlier, it makes no sense to load another
    cacheline when nr_running is not 0 as idle_cpu() will always return
    false in this case.
    
    This patch eliminates this redundant cacheline load by checking the
    cached nr_running before calling idle_cpu().
    
    Signed-off-by: Waiman Long <Waiman.Long@hpe.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Douglas Hatch <doug.hatch@hpe.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Scott J Norton <scott.norton@hpe.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1448478580-26467-2-git-send-email-Waiman.Long@hpe.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit a426f99c91d1036767a7819aaaba6bd3191b7f06)
    Signed-off-by: Javi Merino <javi.merino@arm.com>
    
    BUG=chrome-os-partner:47839
    TEST=Boot kernel on Oak.
    TEST=smaug-release and strago-release trybots.
    
    Change-Id: Idf994aff669975190af7c72406cda6d712b70f0c
    Reviewed-on: https://chromium-review.googlesource.com/327025
    Commit-Ready: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6945,7 +6945,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 			bool *overload, bool *overutilized)
 {
 	unsigned long load;
-	int i;
+	int i, nr_running;
 
 	memset(sgs, 0, sizeof(*sgs));
 
@@ -6962,7 +6962,8 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		sgs->group_util += cpu_util(i);
 		sgs->sum_nr_running += rq->cfs.h_nr_running;
 
-		if (rq->nr_running > 1)
+		nr_running = rq->nr_running;
+		if (nr_running > 1)
 			*overload = true;
 
 #ifdef CONFIG_NUMA_BALANCING
@@ -6970,7 +6971,10 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		sgs->nr_preferred_running += rq->nr_preferred_running;
 #endif
 		sgs->sum_weighted_load += weighted_cpuload(i);
-		if (idle_cpu(i))
+		/*
+		 * No need to call idle_cpu() if nr_running is not 0
+		 */
+		if (!nr_running && idle_cpu(i))
 			sgs->idle_cpus++;
 
 		if (cpu_overutilized(i)) {
