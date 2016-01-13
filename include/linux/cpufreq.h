1043b3e0f7444101f459f13179450e52840b632f
Guenter Roeck <linux@roeck-us.net>
2016-01-13 02:44:40 -0800

    CHROMIUM: cpufreq: Declare acct_update_power as static inline if needed
    If CONFIG_CPU_FREQ_STAT is not configured, builds fail with
    
    kernel/built-in.o: In function `account_user_time':
    kernel/sched/cputime.c:155: undefined reference to `acct_update_power'
    kernel/sched/cputime.c:155: undefined reference to `acct_update_power'
    kernel/built-in.o: In function `__account_system_time':
    kernel/sched/cputime.c:208: undefined reference to `acct_update_power'
    kernel/sched/cputime.c:208: undefined reference to `acct_update_power'
    
    Declare acct_update_power() as static inline if CONFIG_CPU_FREQ_STAT
    is not configured.
    
    Fixes: bf550d0b70dc ("ANDROID: sched: cpufreq: Update cpu_power field in the task_struct")
    BUG=chromium:583462
    TEST=None (build)
    
    Change-Id: I64f5d0087504034a239014c31ee9cb9a12ad8611
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/325374
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -641,6 +641,16 @@ int cpufreq_generic_init(struct cpufreq_policy *policy,
  *                         CPUFREQ STATS                             *
  *********************************************************************/
 
+#ifdef CONFIG_CPU_FREQ_STAT
+
 void acct_update_power(struct task_struct *p, cputime_t cputime);
+
+#else
+
+static inline void acct_update_power(struct task_struct *p, cputime_t cputime)
+{
+}
+
+#endif
 
 #endif /* _LINUX_CPUFREQ_H */
