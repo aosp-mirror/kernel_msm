a3637dd9b78ecaa2f5306480a534b042892428d3
Patrick Tjin <pattjin@google.com>
2016-01-16 02:32:46 -0800

    video: msm: Add ioctl for low persistence display mode
    1) Add IOCTL MSMFB_SET_PERSISTENCE_MODE
    2) Parse low persistence mode commands from dt nodes:
          qcom,mdss-dsi-lp-mode-on
          qcom,mdss-dsi-lp-mode-off
    
      Based on:
        commit 2fc4b8aa55dd753be11a47161239f7408fd17a61
        Author: Ruben Brunk <rubenbrunk@google.com>
        Date:   Sat Jan 16 02:32:46 2016 -0800
    
        Add ioctl for low persistence display mode to MDSS driver.
    
        Change-Id: I996756f6d31b5f31fd02141a2d8130e313ff0833
    
    Change-Id: I996756f6d31b5f31fd02141a2d8130e313ff0833
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -387,6 +387,8 @@ struct dsi_err_container {
 #define MDSS_DSI_COMMAND_COMPRESSION_MODE_CTRL2	0x02ac
 #define MDSS_DSI_COMMAND_COMPRESSION_MODE_CTRL3	0x02b0
 
+#define DISPLAY_LOW_PERSISTENCE_MASK    1
+
 struct mdss_dsi_ctrl_pdata {
 	int ndx;	/* panel_num */
 	int (*on) (struct mdss_panel_data *pdata);
@@ -460,6 +462,8 @@ struct mdss_dsi_ctrl_pdata {
 	struct dsi_panel_cmds post_dms_on_cmds;
 	struct dsi_panel_cmds post_panel_on_cmds;
 	struct dsi_panel_cmds off_cmds;
+	struct dsi_panel_cmds lp_on_cmds;
+	struct dsi_panel_cmds lp_off_cmds;
 	struct dsi_panel_cmds status_cmds;
 	u32 *status_valid_params;
 	u32 *status_cmds_rlen;
--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -703,6 +703,7 @@ struct mdss_panel_timing {
 struct mdss_panel_data {
 	struct mdss_panel_info panel_info;
 	void (*set_backlight) (struct mdss_panel_data *pdata, u32 bl_level);
+	int (*apply_display_setting) (struct mdss_panel_data *pdata, u32 mode);
 	unsigned char *mmss_cc_base;
 
 	/**
