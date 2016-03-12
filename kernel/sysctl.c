760d20939c8870f4291a4e2eb75e6a9dffb7f8c5
Todd Kjos <tkjos@google.com>
2016-03-11 16:44:16 -0800

    sched: fair: add tunable to set initial task load

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -320,6 +320,13 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec,
 	},
 	{
+		.procname	= "sched_initial_task_util",
+		.data		= &sysctl_sched_initial_task_util,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+	{
 		.procname	= "sched_wakeup_granularity_ns",
 		.data		= &sysctl_sched_wakeup_granularity,
 		.maxlen		= sizeof(unsigned int),
