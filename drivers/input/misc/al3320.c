099f0c157517e6fed525e047e5747f51abd3586f
shunmin_wang <shunmin_wang@asus.com>
2016-04-01 16:24:06 +0800

    Swift: BMMI: for lightsensor checking
    Change-Id: I841565d9a93dca8e148bdb0bf204ed4a11b71c60
    Reviewed-on: http://mcrd1-22-pc.corpnet.asus/code-review/master/222921
    Reviewed-by: shunmin_wang <shunmin_wang@asus.com>
    Tested-by: shunmin_wang <shunmin_wang@asus.com>

--- drivers/input/misc/al3320.c
+++ drivers/input/misc/al3320.c
@@ -428,6 +428,10 @@ static ssize_t al3320_show_mode(struct device *dev,
 {
 	struct al3320_data *data = al3320_data_g;
 
+#if ASUS_FACTORY_BUILD
+	return snprintf(buf, 20, "1\n");
+#endif
+
 	return snprintf(buf, 20,
 		"%d\n", al3320_get_mode(data->client));
 }
