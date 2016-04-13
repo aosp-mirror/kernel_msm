3a614bc15ad06f7d11151fce281ee44e78ab9269
Eva Huang <eva_huang@htc.com>
2016-04-13 19:05:55 +0800

    drivers: rtb: Set default filter type for RTB.
    Bug: 28175949
    
    Change-Id: I6fa379dfbf44c5dcdcf0162e8c61cc2035189f9e
    Signed-off-by: Eva Huang <eva_huang@htc.com>

--- kernel/trace/msm_rtb.c
+++ kernel/trace/msm_rtb.c
@@ -72,7 +72,7 @@ static atomic_t msm_rtb_idx;
 #endif
 
 static struct msm_rtb_state msm_rtb = {
-	.filter = 1 << LOGK_LOGBUF,
+	.filter = (1 << LOGK_READL)|(1 << LOGK_WRITEL)|(1 << LOGK_LOGBUF)|(1 << LOGK_HOTPLUG)|(1 << LOGK_CTXID)|(1 << LOGK_IRQ)|(1 << LOGK_DIE)|(1 << LOGK_INITCALL)|(1 << LOGK_SOFTIRQ),
 	.enabled = 1,
 };
 
