c3e8df5bb0c6b5452442e2815cce3fa49a173c91
matt_huang <matt_huang@htc.com>
2016-04-19 12:00:00 +0800

    leds: qpnp: Fix qcom HW known issue
    Add 1ms delay to fix pwm abnormal HW known issue.
    
    Bug: 28128794
    Change-Id: I87d881786d80f22d751733362a869560f73720b0
    Signed-off-by: matt_huang <matt_huang@htc.com>

--- drivers/pwm/pwm-qpnp.c
+++ drivers/pwm/pwm-qpnp.c
@@ -25,6 +25,7 @@
 #include <linux/of_device.h>
 #include <linux/radix-tree.h>
 #include <linux/qpnp/pwm.h>
+#include <linux/delay.h>
 
 #define QPNP_LPG_DRIVER_NAME	"qcom,qpnp-pwm"
 #define QPNP_LPG_CHANNEL_BASE	"qpnp-lpg-channel-base"
@@ -1089,6 +1090,9 @@ static int qpnp_lpg_configure_lut_state(struct qpnp_pwm_chip *chip,
 
 	addr = SPMI_LPG_REG_ADDR(lpg_config->base_addr,
 				QPNP_ENABLE_CONTROL);
+
+	// Add 1mS delay to fix qcom known HW issue
+	mdelay(1);
 
 	if (chip->in_test_mode) {
 		test_enable = (state == QPNP_LUT_ENABLE) ? 1 : 0;
