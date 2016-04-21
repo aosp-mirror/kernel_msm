96f31f32641f5a652b0711d164b38aa34b1334d8
jason_sung <jason_sung@htc.com>
2016-04-21 17:41:46 +0800

    power: debug: support to read vddcx and vddmx retention voltage via cpuinfo
    Change-Id: Ie7ee8b67fa16d6d96b8beba726f4bead3e1d0813
    Signed-off-by: jason_sung <jason_sung@htc.com>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -40,6 +40,7 @@
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
 #include <linux/memblock.h>
+#include <linux/of_address.h>
 #include <linux/of_fdt.h>
 #include <linux/of_platform.h>
 #include <linux/efi.h>
@@ -512,6 +513,67 @@ static const char *compat_hwcap2_str[] = {
 };
 #endif /* CONFIG_COMPAT */
 
+static u32 cx_fuse_data = 0x0;
+static u32 mx_fuse_data = 0x0;
+
+static const u32 vddcx_pvs_retention_data[8] =
+{
+  /* 000 */ 600000,
+  /* 001 */ 550000,
+  /* 010 */ 500000,
+  /* 011 */ 450000,
+  /* 100 */ 400000,
+  /* 101 */ 400000, //limiting based on CR812560
+  /* 110 */ 400000, //limiting based on CR812560
+  /* 111 */ 600000
+};
+
+static const u32 vddmx_pvs_retention_data[8] =
+{
+  /* 000 */ 700000,
+  /* 001 */ 650000,
+  /* 010 */ 580000,
+  /* 011 */ 550000,
+  /* 100 */ 490000,
+  /* 101 */ 490000,
+  /* 110 */ 490000,
+  /* 111 */ 490000
+};
+
+static int read_cx_fuse_setting(void){
+	if(cx_fuse_data != 0x0)
+		/* 0x00070134[31:29] */
+		return ((cx_fuse_data & (0x7 << 29)) >> 29);
+	else
+		return -ENOMEM;
+}
+
+static int read_mx_fuse_setting(void){
+	if(mx_fuse_data != 0x0)
+		/* 0x00070148[4:2] */
+		return ((mx_fuse_data & (0x7 << 2)) >> 2);
+	else
+		return -ENOMEM;
+}
+
+static u32 Get_min_cx(void) {
+	u32 lookup_val = 0;
+	int mapping_data;
+	mapping_data = read_cx_fuse_setting();
+	if(mapping_data >= 0)
+		lookup_val = vddcx_pvs_retention_data[mapping_data];
+	return lookup_val;
+}
+
+static u32 Get_min_mx(void) {
+	u32 lookup_val = 0;
+	int mapping_data;
+	mapping_data = read_mx_fuse_setting();
+	if(mapping_data >= 0)
+		lookup_val = vddmx_pvs_retention_data[mapping_data];
+	return lookup_val;
+}
+
 static int c_show(struct seq_file *m, void *v)
 {
 	int i, j;
@@ -530,6 +592,9 @@ static int c_show(struct seq_file *m, void *v)
 #ifdef CONFIG_SMP
 		seq_printf(m, "processor\t: %d\n", i);
 #endif
+
+		seq_printf(m, "min_vddcx\t: %d\n", Get_min_cx());
+		seq_printf(m, "min_vddmx\t: %d\n", Get_min_mx());
 
 		/*
 		 * Dump out the common processor features in a single line.
@@ -598,3 +663,40 @@ void arch_setup_pdev_archdata(struct platform_device *pdev)
 	pdev->archdata.dma_mask = DMA_BIT_MASK(32);
 	pdev->dev.dma_mask = &pdev->archdata.dma_mask;
 }
+
+static int msm8996_read_cx_fuse(void){
+	void __iomem *addr;
+	struct device_node *dn = of_find_compatible_node(NULL,
+						NULL, "qcom,cpucx-8996");
+	if (dn && (cx_fuse_data == 0x0)) {
+		addr = of_iomap(dn, 0);
+		if (!addr)
+			return -ENOMEM;
+		cx_fuse_data = readl_relaxed(addr);
+		iounmap(addr);
+	}
+	else {
+		return -ENOMEM;
+	}
+	return 0;
+}
+arch_initcall_sync(msm8996_read_cx_fuse);
+
+static int msm8996_read_mx_fuse(void){
+	void __iomem *addr;
+	struct device_node *dn = of_find_compatible_node(NULL,
+						NULL, "qcom,cpumx-8996");
+	if (dn && (mx_fuse_data == 0x0)) {
+		addr = of_iomap(dn, 0);
+		if (!addr)
+			return -ENOMEM;
+		mx_fuse_data = readl_relaxed(addr);
+		iounmap(addr);
+	}
+	else {
+		return -ENOMEM;
+	}
+	return 0;
+}
+
+arch_initcall_sync(msm8996_read_mx_fuse);
