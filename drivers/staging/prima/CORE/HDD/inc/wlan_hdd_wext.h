93f20ce0f23c9008e86f16e080affec08259aae2
Tom Cherry <tomcherry@google.com>
2016-05-05 17:04:32 -0700

    prima: Update QC wlan driver to LW.BR.1.0-02510-8x09w.0
    Change-Id: I4cb0e21a4495506a4117c6bebc2506e1b44fedcd
    Signed-off-by: Tom Cherry <tomcherry@google.com>

--- drivers/staging/prima/CORE/HDD/inc/wlan_hdd_wext.h
+++ drivers/staging/prima/CORE/HDD/inc/wlan_hdd_wext.h
@@ -463,5 +463,6 @@ void* wlan_hdd_change_country_code_callback(void *pAdapter);
 int hdd_setBand(struct net_device *dev, u8 ui_band);
 int hdd_setBand_helper(struct net_device *dev, const char *command);
 VOS_STATUS wlan_hdd_get_frame_logs(hdd_adapter_t *pAdapter, v_U8_t flag);
+int wlan_hdd_set_proximity(int set_value);
 #endif // __WEXT_IW_H__
 
--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_dev_pwr.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_dev_pwr.c
@@ -133,7 +133,7 @@ static int wlan_suspend(hdd_context_t* pHddCtx)
    INIT_COMPLETION(pHddCtx->tx_sus_event_var);
 
    /* Indicate Tx Thread to Suspend */
-   set_bit(TX_SUSPEND_EVENT_MASK, &vosSchedContext->txEventFlag);
+   set_bit(TX_SUSPEND_EVENT, &vosSchedContext->txEventFlag);
 
    wake_up_interruptible(&vosSchedContext->txWaitQueue);
 
@@ -151,7 +151,7 @@ static int wlan_suspend(hdd_context_t* pHddCtx)
        * Thread then it means it is going to suspend, so do not return failure
        * from here.
        */
-      if (!test_and_clear_bit(TX_SUSPEND_EVENT_MASK,
+      if (!test_and_clear_bit(TX_SUSPEND_EVENT,
                               &vosSchedContext->txEventFlag))
       {
          VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
@@ -169,7 +169,7 @@ tx_suspend:
    INIT_COMPLETION(pHddCtx->rx_sus_event_var);
 
    /* Indicate Rx Thread to Suspend */
-   set_bit(RX_SUSPEND_EVENT_MASK, &vosSchedContext->rxEventFlag);
+   set_bit(RX_SUSPEND_EVENT, &vosSchedContext->rxEventFlag);
 
    wake_up_interruptible(&vosSchedContext->rxWaitQueue);
 
@@ -186,7 +186,7 @@ tx_suspend:
         * Thread then it means it is going to suspend, so do not return failure
         * from here.
         */
-       if (!test_and_clear_bit(RX_SUSPEND_EVENT_MASK,
+       if (!test_and_clear_bit(RX_SUSPEND_EVENT,
                                &vosSchedContext->rxEventFlag))
        {
            VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
@@ -210,7 +210,7 @@ rx_suspend:
    INIT_COMPLETION(pHddCtx->mc_sus_event_var);
 
    /* Indicate MC Thread to Suspend */
-   set_bit(MC_SUSPEND_EVENT_MASK, &vosSchedContext->mcEventFlag);
+   set_bit(MC_SUSPEND_EVENT, &vosSchedContext->mcEventFlag);
 
    wake_up_interruptible(&vosSchedContext->mcWaitQueue);
 
@@ -229,7 +229,7 @@ rx_suspend:
         * Thread then it means it is going to suspend, so do not return failure
         * from here.
         */
-       if (!test_and_clear_bit(MC_SUSPEND_EVENT_MASK,
+       if (!test_and_clear_bit(MC_SUSPEND_EVENT,
                                &vosSchedContext->mcEventFlag))
        {
            VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
