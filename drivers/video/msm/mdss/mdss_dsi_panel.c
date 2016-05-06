ad53fb8c4124c8612bc4dd51f1317fc0581e612f
lwx347737 <lwx347737@notesmail.huawei.com>
2016-05-06 15:16:19 +0800

    Display:OLED choose different panel using panel ID in kernel
    1.add an new 1.19inch panel config
    2.add enable pin in DTSI
    
    Change-Id: Iebeb920c7b7f3eb973397ce6777e8f52c4f0dd9c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -398,7 +398,7 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 			gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
 			gpio_free(ctrl_pdata->disp_en_gpio);
 		}
-		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		gpio_set_value((ctrl_pdata->rst_gpio), 1);
 		gpio_free(ctrl_pdata->rst_gpio);
 		if (gpio_is_valid(ctrl_pdata->mode_gpio))
 			gpio_free(ctrl_pdata->mode_gpio);
