ff42fcd3298952174508684f3d27f6f220a3df71
ben_chao <ben_chao@asus.com>
2016-05-09 11:06:16 +0800

    Swift: battery: Porting external gauge driver
    Change-Id: I03c7eaa2312a9543cd393f0fb30dc469f93a1fd3
    Reviewed-on: http://mcrd1-22-pc.corpnet.asus/code-review/master/231799
    Reviewed-by: Ben_Chao <ben_chao@asus.com>
    Tested-by: Ben_Chao <ben_chao@asus.com>
    Reviewed-by: Wu, Meng-Hui <menghui_wu@asus.com>

--- drivers/power/stc3117_platformdata_default.c
+++ drivers/power/stc3117_platformdata_default.c
@@ -1,6 +1,4 @@
-
 #if CONFIG_BATTERY_STC3117
-
 int null_fn(void)
 {
 	return 0;                // for discharging status
@@ -12,52 +10,70 @@ int Temperature_fn(void)
 }
 
 static struct stc311x_platform_data stc3117_data = {
-	.battery_online = NULL,
-	.charger_online = null_fn, 		// used in stc311x_get_status()
-	.charger_enable = null_fn,		// used in stc311x_get_status()
-	.power_supply_register = NULL,
-	.power_supply_unregister = NULL,
+		.battery_online = NULL,
+		.charger_online = null_fn, 		// used in stc311x_get_status()
+		.charger_enable = null_fn,		// used in stc311x_get_status()
+		.power_supply_register = NULL,
+		.power_supply_unregister = NULL,
 
-	.Vmode= 0,       /*REG_MODE, BIT_VMODE 1=Voltage mode, 0=mixed mode */
-	.Alm_SOC = 10,      /* SOC alm level %*/
-	.Alm_Vbat = 3600,   /* Vbat alm level mV*/
-	.CC_cnf = 525,      /* nominal CC_cnf, coming from battery characterisation*/
-	.VM_cnf = 558,      /* nominal VM cnf , coming from battery characterisation*/
-	.Rint = 200,			/* nominal internal impedance*/
-	.Cnom = 2600,       /* nominal capacity in mAh, coming from battery characterisation*/
-	.Rsense = 10,       /* sense resistor mOhms*/
-	.RelaxCurrent = 150, /* current for relaxation in mA (< C/20) */
-	.Adaptive = 1,     /* 1=Adaptive mode enabled, 0=Adaptive mode disabled */
+		.Vmode= 0,       /*REG_MODE, BIT_VMODE 1=Voltage mode, 0=mixed mode */
+		.Alm_SOC = 10,      /* SOC alm level %*/
+		.Alm_Vbat = 3600,   /* Vbat alm level mV*/
+		.CC_cnf = 59,      /* nominal CC_cnf, coming from battery characterisation*/
+		.VM_cnf = 271,      /* nominal VM cnf , coming from battery characterisation*/
+		.Rint = 330,			/* nominal internal impedance*/
+		.Cnom = 300,       /* nominal capacity in mAh, coming from battery characterisation*/
+		.Rsense = 10,       /* sense resistor mOhms*/
+		.RelaxCurrent = 150, /* current for relaxation in mA (< C/20) */
+		.Adaptive = 1,     /* 1=Adaptive mode enabled, 0=Adaptive mode disabled */
 
-	/* Elentec Co Ltd Battery pack - 80 means 8% */
-	.CapDerating[6] = 71,   /* capacity derating in 0.1%, for temp = -20°C */
-	.CapDerating[5] = 42,   /* capacity derating in 0.1%, for temp = -10°C */
-	.CapDerating[4] = 13,   /* capacity derating in 0.1%, for temp = 0°C */
-	.CapDerating[3] = 5,   /* capacity derating in 0.1%, for temp = 10°C */
-	.CapDerating[2] = 0,   /* capacity derating in 0.1%, for temp = 25°C */
-	.CapDerating[1] = 0,   /* capacity derating in 0.1%, for temp = 40°C */
-	.CapDerating[0] = 0,   /* capacity derating in 0.1%, for temp = 60°C */
+		/* Elentec Co Ltd Battery pack - 80 means 8% */
+	.CapDerating[6] = -5,   /* capacity derating in 0.1%, for temp = -20°C */
+	.CapDerating[5] = -5,   /* capacity derating in 0.1%, for temp = -10°C */
+	.CapDerating[4] = -5,    /* capacity derating in 0.1%, for temp = 0°C */
+	.CapDerating[3] = -5,  /* capacity derating in 0.1%, for temp = 10°C */
+	.CapDerating[2] = -5,  /* capacity derating in 0.1%, for temp = 25°C */
+	.CapDerating[1] = 0,  /* capacity derating in 0.1%, for temp = 40°C */
+	.CapDerating[0] = 0,  /* capacity derating in 0.1%, for temp = 60°C */
 
-	.OCVValue[15] = 0,   /* OCV curve adjustment */
-	.OCVValue[14] = 0,   /* OCV curve adjustment */
-	.OCVValue[13] = 0,   /* OCV curve adjustment */
-	.OCVValue[12] = 0,   /* OCV curve adjustment */
-	.OCVValue[11] = 0,   /* OCV curve adjustment */
-	.OCVValue[10] = 0,   /* OCV curve adjustment */
-	.OCVValue[9] = 0,    /* OCV curve adjustment */
-	.OCVValue[8] = 0,    /* OCV curve adjustment */
-	.OCVValue[7] = 0,    /* OCV curve adjustment */
-	.OCVValue[6] = 0,    /* OCV curve adjustment */
-	.OCVValue[5] = 0,    /* OCV curve adjustment */
-	.OCVValue[4] = 0,    /* OCV curve adjustment */
-	.OCVValue[3] = 0,    /* OCV curve adjustment */
-	.OCVValue[2] = 0,    /* OCV curve adjustment */
-	.OCVValue[1] = 0,    /* OCV curve adjustment */
-	.OCVValue[0] = 0,    /* OCV curve adjustment */
+		//OCV curve example for a 4.35V li-ion battery
+	.OCVValue[0] = 3300,             /* OCV curve adjustment */
+	.OCVValue[1] = 3559,             /* OCV curve adjustment */
+	.OCVValue[2] = 3671,             /* OCV curve adjustment */
+	.OCVValue[3] = 3684,             /* OCV curve adjustment */
+	.OCVValue[4] = 3702,             /* OCV curve adjustment */
+	.OCVValue[5] = 3730,             /* OCV curve adjustment */
+	.OCVValue[6] = 3747,             /* OCV curve adjustment */
+	.OCVValue[7] = 3765,             /* OCV curve adjustment */
+	.OCVValue[8] = 3795,             /* OCV curve adjustment */
+	.OCVValue[9] = 3832,             /* OCV curve adjustment */
+	.OCVValue[10] = 3899,            /* OCV curve adjustment */
+	.OCVValue[11] = 3941,            /* OCV curve adjustment */
+	.OCVValue[12] = 3967,            /* OCV curve adjustment */
+	.OCVValue[13] = 4087,            /* OCV curve adjustment */
+	.OCVValue[14] = 4181,            /* OCV curve adjustment */
+	.OCVValue[15] = 4297,            /* OCV curve adjustment */
 
-	/*if the application temperature data is preferred than the STC3117 temperature*/
-	.ExternalTemperature = Temperature_fn, /*External temperature fonction, return °C*/
-	.ForceExternalTemperature = 0, /* 1=External temperature, 0=STC3117 temperature */
+	.SOCValue[0]  = 0x0,
+	.SOCValue[1]  = 0x6,
+	.SOCValue[2]  = 0x0C,
+	.SOCValue[3]  = 0x14,
+	.SOCValue[4]  = 0x1E,
+	.SOCValue[5]  = 0x28,
+	.SOCValue[6]  = 0x32,
+	.SOCValue[7]  = 0x3C,
+	.SOCValue[8]  = 0x50,
+	.SOCValue[9]  = 0x64,
+	.SOCValue[10] = 0x78,
+	.SOCValue[11] = 0x82,
+	.SOCValue[12] = 0x8C,
+	.SOCValue[13] = 0xA0,
+	.SOCValue[14] = 0xB4,
+	.SOCValue[15] = 0xC8,
+
+		/*if the application temperature data is preferred than the STC3117 temperature*/
+		.ExternalTemperature = Temperature_fn, /*External temperature fonction, return °C*/
+		.ForceExternalTemperature = 0, /* 1=External temperature, 0=STC3117 temperature */
 
 };
 #endif
@@ -65,11 +81,11 @@ static struct stc311x_platform_data stc3117_data = {
 
 static struct i2c_board_info __initdata beagle_i2c2_boardinfo[] = {
 
-#if CONFIG_BATTERY_STC3117
+	#if CONFIG_BATTERY_STC3117
 	{
 		I2C_BOARD_INFO("stc3117", 0x70),
-			.platform_data = &stc3117_data,
+		.platform_data = &stc3117_data,
 	},
-#endif
+	#endif
 
 };
---
+++ include/linux/platform_data/stc3117_battery.h
@@ -0,0 +1,117 @@
+/*
+ *  Copyright (C) 2011 STMicroelectronics.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+int null_fn(void)
+{
+	return 0;                // for discharging status
+}
+
+int Temperature_fn(void)
+{
+	return (25);
+}
+
+#ifndef __STC3117_BATTERY_H_
+#define __STC3117_BATTERY_H_
+
+#define STC3117_DEV_NAME			"stc3117"
+
+struct stc311x_platform_data {
+	int (*battery_online)(void);
+	int (*charger_online)(void);
+	int (*charger_enable)(void);
+	int (*power_supply_register)(struct device *parent, struct power_supply *psy);
+	void (*power_supply_unregister)(struct power_supply *psy);
+	int (*ExternalTemperature) (void); /*External temperature fonction, return degrC*/
+
+	int Vmode;       /* 1=Voltage mode, 0=mixed mode */
+	int Alm_SOC;     /* SOC alm level %*/
+	int Alm_Vbat;    /* Vbat alm level mV*/
+	int CC_cnf;      /* nominal CC_cnf */
+	int VM_cnf;      /* nominal VM cnf */
+	int Rint;		 /*nominal Rint*/
+	int Cnom;        /* nominal capacity in mAh */
+	int Rsense;      /* sense resistor mOhms*/
+	int RelaxCurrent; /* current for relaxation in mA (< C/20) */
+	int Adaptive;     /* 1=Adaptive mode enabled, 0=Adaptive mode disabled */
+	int CapDerating[7];   /* capacity derating in 0.1%, for temp = 60, 40, 25, 10,  0, -10, -20 degrC */
+	int OCVValue[16];    /* OCV curve adjustment */
+	int SOCValue[16];
+
+	int ForceExternalTemperature; /* 1=External temperature, 0=STC3115 temperature */
+};
+
+
+static struct stc311x_platform_data default_stc3117_platform_data = {
+
+	.battery_online = NULL,
+	.charger_online = NULL, 		/* used in stc311x_get_status() */
+	.charger_enable = NULL, 		/* used in stc311x_get_status() */
+	.power_supply_register = NULL,
+	.power_supply_unregister = NULL,
+
+	//Gas gauge specific, Default:
+	.Alm_SOC = 10,      /* SOC alm level %*/
+	.Alm_Vbat = 3600,   /* Vbat alm level mV*/
+	.CC_cnf = 59,      /* nominal CC_cnf, coming from battery characterisation*/
+	.VM_cnf = 271,      /* nominal VM cnf , coming from battery characterisation*/
+	.Rint = 330,		/* nominal internal impedance*/
+	.Cnom = 300,       /* nominal capacity in mAh, coming from battery characterisation*/
+	.Rsense = 10,       /* sense resistor mOhms*/
+	.RelaxCurrent = 150, /* current for relaxation in mA (< C/20) */
+	.Adaptive = 1,     /* 1=Adaptive mode enabled, 0=Adaptive mode disabled */
+
+	/* Elentec Co Ltd Battery pack - 80 means 8% */
+	.CapDerating[6] = -5,   /* capacity derating in 0.1%, for temp = -20°C */
+	.CapDerating[5] = -5,   /* capacity derating in 0.1%, for temp = -10°C */
+	.CapDerating[4] = -5,    /* capacity derating in 0.1%, for temp = 0°C */
+	.CapDerating[3] = -5,  /* capacity derating in 0.1%, for temp = 10°C */
+	.CapDerating[2] = -5,  /* capacity derating in 0.1%, for temp = 25°C */
+	.CapDerating[1] = 0,  /* capacity derating in 0.1%, for temp = 40°C */
+	.CapDerating[0] = 0,  /* capacity derating in 0.1%, for temp = 60°C */
+
+	.OCVValue[0] = 3300,             /* OCV curve adjustment */
+	.OCVValue[1] = 3559,             /* OCV curve adjustment */
+	.OCVValue[2] = 3671,             /* OCV curve adjustment */
+	.OCVValue[3] = 3684,             /* OCV curve adjustment */
+	.OCVValue[4] = 3702,             /* OCV curve adjustment */
+	.OCVValue[5] = 3730,             /* OCV curve adjustment */
+	.OCVValue[6] = 3747,             /* OCV curve adjustment */
+	.OCVValue[7] = 3765,             /* OCV curve adjustment */
+	.OCVValue[8] = 3795,             /* OCV curve adjustment */
+	.OCVValue[9] = 3832,             /* OCV curve adjustment */
+	.OCVValue[10] = 3899,            /* OCV curve adjustment */
+	.OCVValue[11] = 3941,            /* OCV curve adjustment */
+	.OCVValue[12] = 3967,            /* OCV curve adjustment */
+	.OCVValue[13] = 4087,            /* OCV curve adjustment */
+	.OCVValue[14] = 4181,            /* OCV curve adjustment */
+	.OCVValue[15] = 4297,            /* OCV curve adjustment */
+
+	.SOCValue[0]  = 0x0,
+	.SOCValue[1]  = 0x6,
+	.SOCValue[2]  = 0x0C,
+	.SOCValue[3]  = 0x14,
+	.SOCValue[4]  = 0x1E,
+	.SOCValue[5]  = 0x28,
+	.SOCValue[6]  = 0x32,
+	.SOCValue[7]  = 0x3C,
+	.SOCValue[8]  = 0x50,
+	.SOCValue[9]  = 0x64,
+	.SOCValue[10] = 0x78,
+	.SOCValue[11] = 0x82,
+	.SOCValue[12] = 0x8C,
+	.SOCValue[13] = 0xA0,
+	.SOCValue[14] = 0xB4,
+	.SOCValue[15] = 0xC8,
+
+	/*if the application temperature data is preferred than the STC3117 temperature*/
+	.ExternalTemperature = Temperature_fn, /*External temperature fonction, return °C*/
+	.ForceExternalTemperature = 1, /* 1=External temperature, 0=STC3117 temperature */
+};
+
+#endif
