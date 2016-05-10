118771b6675868dd64524f7cf31bc1004784b8fa
redick_lin <redick_lin@htc.com>
2016-05-10 19:33:43 +0800

    power: battery: Set default power supply type as USB
    While device boot up without charger, the default power supply type is UNKNOWN.
    The healthd initialization will use the power supply type.
    If the type is UNKNOWN, it will get incorrectly initialized value.
    
    Bug: 28553955
    
    Change-Id: Icfc3671f516601d39c51f16ff2b19015246c563f
    Signed-off-by: redick_lin <redick_lin@htc.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2479,6 +2479,8 @@ static int dwc3_msm_power_set_property_usb(struct power_supply *psy,
 			break;
 		default:
 			mdwc->chg_type = DWC3_INVALID_CHARGER;
+			/* set default power supply type as USB */
+			psy->type = POWER_SUPPLY_TYPE_USB;
 			break;
 		}
 
