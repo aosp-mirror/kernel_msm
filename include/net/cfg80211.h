772fa67b5fb0205e5648cd104c6d9ad233d75d66
Srinivas Girigowda <sgirigow@codeaurora.org>
2016-05-19 12:52:11 +0530

    cfg80211: Define macro to indicate support for new cfg80211 disconnect api
    Define macro to indicate backport support for the new
    cfg80211 disconnect api.
    
    Change-Id: I4b9fd57ac7e30ffa967866d0e8b59eec4775e294
    CRs-Fixed: 1016871
    Signed-off-by: Mahesh A Saptasagar <msapta@codeaurora.org>
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -73,6 +73,7 @@ struct wiphy;
 #define CFG80211_SCAN_RANDOM_MAC_ADDR 1
 #define CFG80211_MULTI_SCAN_PLAN_BACKPORT 1
 #define CFG80211_UPDATE_CONNECT_PARAMS 1
+#define CFG80211_DISCONNECTED_V2 1
 
 /*
  * wireless hardware capability structures
