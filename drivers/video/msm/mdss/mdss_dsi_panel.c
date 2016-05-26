fc61b0071e905fcbf8ab8ac16efde986bac27257
davis_chen <davis_chen@asus.com>
2016-05-26 18:08:14 +0800

    Swift: mdss: prevent panel to dark completely when resume
    Change-Id: I34a8a560fc4f5c2ffd8c8c440351f08a9a548ad9
    Reviewed-on: http://mcrd1-22-pc.corpnet.asus/code-review/master/233131
    Reviewed-by: davis_chen <davis_chen@asus.com>
    Tested-by: davis_chen <davis_chen@asus.com>
    Reviewed-by: paul_fan <paul_fan@asus.com>
    Tested-by: paul_fan <paul_fan@asus.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -681,6 +681,13 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
+    /* avoid MURA effect */
+    if (bl_level > 0 && bl_level < ambient_bl_level)
+        bl_level = ambient_bl_level;
+
+    /* Lock backlight of ambient mode to 28nits */
+    backup_bl_level = bl_level;
+
 	/*
 	 * Some backlight controllers specify a minimum duty cycle
 	 * for the backlight brightness. If the brightness is less
