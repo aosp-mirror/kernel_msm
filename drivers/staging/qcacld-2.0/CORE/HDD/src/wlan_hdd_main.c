4d8df3652000c64505da4f46d9a7da506aad2dd7
Srinivas Girigowda <sgirigow@codeaurora.org>
2016-06-07 16:23:10 +0530

    qcacld-2.0: Clear MC filters when more than max available
    When number of link MC addresses configured are more than the
    maximum firmware MC address list size, host sets the firmware's
    MC filters up to the max and skipping the rest. As a result
    firmware drops the MC packets for the addresses whose MC filter
    is not set.
    
    To fix this clear the firmware MC filters when the number of kernel
    MC addresses list is greater than the firmware's MC filter list size.
    When no filters are configured firmware allows all the MC frames
    to the host.
    
    Change-Id: I6d8e8b2f5a646b914a7568ceae593b45b58f84d0
    CRs-Fixed: 1025279
    Bug: 31438853
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
@@ -9777,8 +9777,20 @@ static void __hdd_set_multicast_list(struct net_device *dev)
       hddLog(VOS_TRACE_LEVEL_INFO,
             "%s: mc_count = %u", __func__, mc_count);
 
+      if (mc_count > pHddCtx->max_mc_addr_list) {
+         hddLog(VOS_TRACE_LEVEL_INFO,
+                "%s: No free filter available; allow all multicast frames",
+                __func__);
+         pAdapter->mc_addr_list.mc_cnt = 0;
+         return;
+      }
+
       netdev_for_each_mc_addr(ha, dev) {
-         if (i == mc_count || i == pHddCtx->max_mc_addr_list)
+         hddLog(VOS_TRACE_LEVEL_INFO,
+                FL("ha_addr[%d] "MAC_ADDRESS_STR),
+                i, MAC_ADDR_ARRAY(ha->addr));
+
+         if (i == mc_count)
             break;
          /*
           * Skip following addresses:
