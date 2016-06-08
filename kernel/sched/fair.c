e8be5a3e3aa8b57166fb508e4b4361be775de197
Patrick Bellasi <patrick.bellasi@arm.com>
2016-06-08 15:37:24 +0100

    sched/tune: fix accounting for runnable tasks (1/5)
    The accounting for tasks into boost groups of different CPUs is currently
    broken mainly because:
    a) we do not properly track the change of boost group of a RUNNABLE task
    b) there are race conditions between migration code and acconting code
    
    This patch provides a first chunk of fixes to ensure enqueue/dequeue
    accounting also for throttled tasks.
    
    Without this patch is can happen that a task is enqueued into a throttled
    RQ thus not being accounted for the boosting of the corresponding RQ.
    We could argue that a throttled task should not boost a CPU, however:
    a) properly implementing CPU boosting considering throttled tasks will
       increase a lot the complexity of the solution
    b) it's not easy to quantify the benefits introduced by such a more
       complex solution
    
    Since task throttling requires the usage of the CFS bandwidth controller,
    which is not widely used on mobile systems (at least not by Android kernels
    so far), for the time being we go for the simple solution and boost also
    for throttled RQs.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4076,8 +4076,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 			trace_sched_overutilized(true);
 		}
 
-		schedtune_enqueue_task(p, cpu_of(rq));
-
 		/*
 		 * We want to potentially trigger a freq switch
 		 * request only for tasks that are waking up; this is
@@ -4094,6 +4092,10 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	/* Update RQ estimated utilization */
 	cfs_rq->avg.util_est += task_util_est(p);
+
+	/* Update SchedTune accouting */
+	schedtune_enqueue_task(p, cpu_of(rq));
+
 	hrtick_update(rq);
 }
 
@@ -4156,7 +4158,6 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	if (!se) {
 		sub_nr_running(rq, 1);
 		walt_dec_cumulative_runnable_avg(rq, p);
-		schedtune_dequeue_task(p, cpu_of(rq));
 
 		/*
 		 * We want to potentially trigger a freq switch
@@ -4187,6 +4188,9 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	/* Update estimated utilization */
 	if (task_sleep)
 		p->se.avg.util_est = p->se.avg.util_avg;
+
+	/* Update SchedTune accouting */
+	schedtune_dequeue_task(p, cpu_of(rq));
 
 	hrtick_update(rq);
 }
