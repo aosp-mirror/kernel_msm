d4595d475df897d7de9a9760e821a1328333c986
Wei Wang <wvw@google.com>
2016-06-15 15:19:55 -0700

    irq: Add wakeup reason logging
    log wakeup reason for irq
    
    Bug: 28852315
    Change-Id: Iad9cf7423e51441c81f52fd94dd6347de6709262

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -25,6 +25,7 @@
 #include <linux/percpu.h>
 #include <linux/slab.h>
 #include <linux/module.h>
+#include <linux/wakeup_reason.h>
 
 #include <linux/irqchip/arm-gic-v3.h>
 #include <linux/syscore_ops.h>
@@ -383,6 +384,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
 
+		log_base_wakeup_reason(irq);
 		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
 	}
 }
