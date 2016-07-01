0b85bde61fa935b2092d410b959d3e127f27592e
Srinivas Girigowda <sgirigow@codeaurora.org>
2016-07-01 14:10:18 -0700

    qcald-2.0: wlan host driver upgrade to 4.4.25.015
    wlan host driver upgrade to 4.4.25.015
    
    Change-Id: I8a50642f7ca45eab869423e7f3c3c7e70c27b22d
    Bug: 29941999
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
+++ drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            4
 #define QWLAN_VERSION_PATCH            25
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            14
+#define QWLAN_VERSION_BUILD            15
 
-#define QWLAN_VERSIONSTR               "4.4.25.014"
+#define QWLAN_VERSIONSTR               "4.4.25.015"
 
 
 #define AR6320_REV1_VERSION             0x5000000
