4bf50c44d80c9ed9608f904174624306a3533687
Srinivas Girigowda <sgirigow@codeaurora.org>
2016-07-07 15:06:34 -0700

    qcacld-2.0: Block interface down until pending scan request is aborted
    prima to qcacld-2.0 propagation
    
    From Kernel-3.18 onwards pending scan request is handled
    as part of NETDEV_DOWN event, by invoking the cfg80211_scan_done
    callback which frees the scan request structure. To support this,
    driver will not invoke scan_done_callback if interface is down.
    
    In case, if interface down and up are called back to back there
    is a chance that kernel frees scan request as part of NETDEV_DOWN
    and after which before driver triggers scan_done_callback if
    interface is made up, driver scan_done_callback is through its
    execution and accesses the freed scan request results in kernel
    panic.
    
    To mitigate this, don't return from hdd_stop until scan request
    is aborted. Though this fix is to avoid kernel panic due to 3.18
    kernel specific changes, it is acceptable across all the kernel
    versions.
    
    Change-Id: Iba8bd7a32fac33e8a0c3eea293aad682a1105397
    CRs-Fixed: 977264
    Bug: 30018115
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
@@ -11464,8 +11464,7 @@ VOS_STATUS hdd_stop_adapter( hdd_context_t *pHddCtx, hdd_adapter_t *pAdapter,
          }
          else
          {
-            hdd_abort_mac_scan(pHddCtx, pAdapter->sessionId,
-                               eCSR_SCAN_ABORT_DEFAULT);
+            wlan_hdd_scan_abort(pAdapter);
          }
          if ((pAdapter->device_mode == WLAN_HDD_P2P_CLIENT) ||
               (pAdapter->device_mode == WLAN_HDD_P2P_DEVICE)) {
