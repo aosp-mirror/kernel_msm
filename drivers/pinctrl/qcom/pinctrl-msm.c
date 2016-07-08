71a982015aa88d7979508313837d061e66e9f04b
Thierry Strudel <tstrudel@google.com>
2016-07-07 18:13:23 -0700

    pinctrl: qcom: log wakeup reason
    Bug: 29697729
    Change-Id: Id25487ba6ba4f141dff3653089a100931bd4fb71
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -31,6 +31,7 @@
 #include <linux/reboot.h>
 #include <linux/irqchip/msm-mpm-irq.h>
 #include <linux/spmi.h>
+#include <linux/wakeup_reason.h>
 #include "../core.h"
 #include "../pinconf.h"
 #include "pinctrl-msm.h"
@@ -938,7 +939,7 @@ static void msm_pinctrl_resume(void)
 				name = "stray irq";
 			else if (desc->action && desc->action->name)
 				name = desc->action->name;
-
+			log_base_wakeup_reason(irq);
 			pr_warning("%s: %d triggered %s\n",
 				__func__, irq, name);
 		}
