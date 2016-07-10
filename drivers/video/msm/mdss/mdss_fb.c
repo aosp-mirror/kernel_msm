e6d05db19671b898831ad75c856260a7dadd5856
Devin Kim <dojip.kim@lge.com>
2016-07-09 18:16:15 -0700

    msm: mdss: Control backlight level on unblank
    Android recovery/charger mode does not control backlight level.
    In this case, there is no way to turn the backlight on. So if
    the backlight level is not control by sysfs node, device will
    turn the backlight on itself when unblank
    
    Change-Id: I83b3352f1a65cf35d1646b3d24c10b838b53d5b2
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -263,6 +263,8 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 	struct msm_fb_data_type *mfd = dev_get_drvdata(led_cdev->dev->parent);
 	int bl_lvl;
 
+	mfd->bl_level_used = true;
+
 	if (mfd->boot_notification_led) {
 		led_trigger_event(mfd->boot_notification_led, 0);
 		mfd->boot_notification_led = NULL;
@@ -1812,10 +1814,14 @@ static int mdss_fb_blank_unblank(struct msm_fb_data_type *mfd)
 			 * the backlight would remain 0 (0 is set in blank).
 			 * Hence resetting back to calibration mode value
 			 */
-			if (!IS_CALIB_MODE_BL(mfd))
+			if (!mfd->bl_level_used) {
+				mdss_fb_set_backlight(mfd,
+					mfd->panel_info->brightness_default);
+			} else if (!IS_CALIB_MODE_BL(mfd)) {
 				mdss_fb_set_backlight(mfd, mfd->unset_bl_level);
-			else
+			} else {
 				mdss_fb_set_backlight(mfd, mfd->calib_mode_bl);
+			}
 
 			/*
 			 * it blocks the backlight update between unblank and
