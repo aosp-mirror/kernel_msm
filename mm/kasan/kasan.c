e813879fd22bd61141219d81e779cbb682497ecd
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-08-02 14:02:43 -0700

    mm/kasan: don't reduce quarantine in atomic contexts
    Currently we call quarantine_reduce() for ___GFP_KSWAPD_RECLAIM (implied
    by __GFP_RECLAIM) allocation.  So, basically we call it on almost every
    allocation.  quarantine_reduce() sometimes is heavy operation, and
    calling it with disabled interrupts may trigger hard LOCKUP:
    
     NMI watchdog: Watchdog detected hard LOCKUP on cpu 2irq event stamp: 1411258
     Call Trace:
      <NMI>   dump_stack+0x68/0x96
       watchdog_overflow_callback+0x15b/0x190
       __perf_event_overflow+0x1b1/0x540
       perf_event_overflow+0x14/0x20
       intel_pmu_handle_irq+0x36a/0xad0
       perf_event_nmi_handler+0x2c/0x50
       nmi_handle+0x128/0x480
       default_do_nmi+0xb2/0x210
       do_nmi+0x1aa/0x220
       end_repeat_nmi+0x1a/0x1e
      <<EOE>>   __kernel_text_address+0x86/0xb0
       print_context_stack+0x7b/0x100
       dump_trace+0x12b/0x350
       save_stack_trace+0x2b/0x50
       set_track+0x83/0x140
       free_debug_processing+0x1aa/0x420
       __slab_free+0x1d6/0x2e0
       ___cache_free+0xb6/0xd0
       qlist_free_all+0x83/0x100
       quarantine_reduce+0x177/0x1b0
       kasan_kmalloc+0xf3/0x100
    
    Reduce the quarantine_reduce iff direct reclaim is allowed.
    
    Fixes: 55834c59098d("mm: kasan: initial memory quarantine implementation")
    Link: http://lkml.kernel.org/r/1470062715-14077-2-git-send-email-aryabinin@virtuozzo.com
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reported-by: Dave Jones <davej@codemonkey.org.uk>
    Acked-by: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 4b3ec5a3f4b1d5c9d64b9ab704042400d050d432)
    Signed-off-by: Alexander Potapenko <glider@google.com>

--- mm/kasan/kasan.c
+++ mm/kasan/kasan.c
@@ -545,7 +545,7 @@ void kasan_kmalloc(struct kmem_cache *cache, const void *object, size_t size,
 	unsigned long redzone_start;
 	unsigned long redzone_end;
 
-	if (flags & __GFP_RECLAIM)
+	if (gfpflags_allow_blocking(flags))
 		quarantine_reduce();
 
 	if (unlikely(object == NULL))
@@ -576,7 +576,7 @@ void kasan_kmalloc_large(const void *ptr, size_t size, gfp_t flags)
 	unsigned long redzone_start;
 	unsigned long redzone_end;
 
-	if (flags & __GFP_RECLAIM)
+	if (gfpflags_allow_blocking(flags))
 		quarantine_reduce();
 
 	if (unlikely(ptr == NULL))
