a5c3e3ceb5aab7a7936b2d5f92db1fa93d56745b
lewis.chen <lewis.chen@quantatw.com>
2016-08-10 06:19:47 +0000

    [XU1][Feldspar] OEM - Add "android@gpe" into kernel version
    [Description]
    	- If device uses kernel binary built outside of Google's Android build servers, it's kernel version
              string (/proc/version) must contain substring "android@gpe"
    [Customer Impact]
    	- None
    [Affected Area]
    	- None
    [Side effect of the change]
    	- None
    
    Change-Id: Idb7cb375fbca878935d8dffceca2bff1996fca6c

--- init/version.c
+++ init/version.c
@@ -43,8 +43,7 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 const char linux_banner[] =
 	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
 	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
-
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
-	" (" LINUX_COMPILER ") %s\n";
+	" (" "android@gpe" ")"
+	" (" LINUX_COMPILER ") %s \n";
