488f7d14dda82be895f375794d9318a1efa716c7
Biswajit Paul <biswajitpaul@codeaurora.org>
2016-09-07 12:53:43 +0530

    msm: camera: Restructure data handling to be more robust
    Use dynamic array allocation instead of static array to
    prevent stack overflow.
    User-supplied number of bytes may result in integer overflow.
    To fix this we check that the num_byte isn't above 8K size.
    
    Bug: 30559423
    CRs-Fixed: 1060554
    Change-Id: I9b05b846e5cc3a62b1a0a67be529f09abc764796
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>
    Signed-off-by: Biswajit Paul <biswajitpaul@codeaurora.org>

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -10,6 +10,7 @@
 
 #define I2C_SEQ_REG_SETTING_MAX   5
 #define I2C_SEQ_REG_DATA_MAX      256
+#define I2C_REG_DATA_MAX          (8*1024)
 #define MAX_CID                   16
 
 #define MSM_SENSOR_MCLK_8HZ   8000000
