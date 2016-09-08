fd40bbf9d02b5d0fee388bc05353900469b71ae5
Charan Teja Reddy <charante@codeaurora.org>
2016-09-08 20:47:55 +0530

    coresight: fix the dangling pointer issues on coresight
    Fix the dangling pointer issues on CoreSight that can cause kernel
    panic.
    
    Change-Id: If3abe89bf0326230c29a49d293ab22ebcec93076
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>

--- drivers/soc/qcom/jtag-fuse.c
+++ drivers/soc/qcom/jtag-fuse.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -87,8 +87,6 @@ static int jtag_fuse_probe(struct platform_device *pdev)
 	drvdata = devm_kzalloc(dev, sizeof(*drvdata), GFP_KERNEL);
 	if (!drvdata)
 		return -ENOMEM;
-	/* Store the driver data pointer for use in exported functions */
-	fusedrvdata = drvdata;
 	drvdata->dev = &pdev->dev;
 	platform_set_drvdata(pdev, drvdata);
 
@@ -100,6 +98,8 @@ static int jtag_fuse_probe(struct platform_device *pdev)
 	if (!drvdata->base)
 		return -ENOMEM;
 
+	/* Store the driver data pointer for use in exported functions */
+	fusedrvdata = drvdata;
 	dev_info(dev, "JTag Fuse initialized\n");
 	return 0;
 }
