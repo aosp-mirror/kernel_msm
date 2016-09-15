080ea011fd9f47315e1fc53185872ef813b59d00
John Dias <joaodias@google.com>
2016-09-15 08:52:27 -0700

    sched: avoid scheduling RT threads on cores currently handling softirqs
    Bug: 31501544
    Change-Id: I99dd7aaa12c11270b28dbabea484bcc8fb8ba0c1

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1842,3 +1842,8 @@ static inline u64 irq_time_read(int cpu)
 }
 #endif /* CONFIG_64BIT */
 #endif /* CONFIG_IRQ_TIME_ACCOUNTING */
+
+/*
+ * task_may_not_preempt - check whether a task may not be preemptible soon
+ */
+extern bool task_may_not_preempt(struct task_struct *task, int cpu);
