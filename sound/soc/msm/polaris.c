3ee05f10147f79e0f230805927f23cf1a59903b5
John Muir <muirj@google.com>
2016-09-22 18:05:14 -0700

    sound: Polaris: Add loopback widgets for audio routing.
    The polaris board has a loopback device that will be used for
    latency testing. Define the DAPM widgets used for routing.
    
    Bug: 31665487
    Test: The routing can be setup correctly to include the loopback
          devices.
    
    Change-Id: I1c1fc66954695417c32570f33c8d3da429fc9313

--- sound/soc/msm/polaris.c
+++ sound/soc/msm/polaris.c
@@ -1384,7 +1384,6 @@ static int ext_disp_rx_format_put(struct snd_kcontrol *kcontrol,
 
 	return 0;
 }
-
 static int ext_disp_rx_ch_get(struct snd_kcontrol *kcontrol,
 			      struct snd_ctl_elem_value *ucontrol)
 {
@@ -2484,13 +2483,15 @@ static const struct snd_soc_dapm_widget msm_dapm_widgets[] = {
 	SND_SOC_DAPM_SPK("Lineout_3 amp", NULL),
 	SND_SOC_DAPM_SPK("Lineout_2 amp", NULL),
 	SND_SOC_DAPM_SPK("Lineout_4 amp", NULL),
+	SND_SOC_DAPM_SPK("Loopback Output", NULL),
+
 	SND_SOC_DAPM_MIC("Handset Mic", NULL),
 	SND_SOC_DAPM_MIC("Headset Mic", NULL),
 	SND_SOC_DAPM_MIC("ANCRight Headset Mic", NULL),
 	SND_SOC_DAPM_MIC("ANCLeft Headset Mic", NULL),
 	SND_SOC_DAPM_MIC("Analog Mic5", NULL),
 	SND_SOC_DAPM_MIC("Analog Mic6", NULL),
-
+	SND_SOC_DAPM_MIC("Loopback Input", NULL),
 	SND_SOC_DAPM_MIC("Digital Mic0", NULL),
 	SND_SOC_DAPM_MIC("Digital Mic1", NULL),
 	SND_SOC_DAPM_MIC("Digital Mic2", NULL),
@@ -2498,6 +2499,7 @@ static const struct snd_soc_dapm_widget msm_dapm_widgets[] = {
 	SND_SOC_DAPM_MIC("Digital Mic4", NULL),
 	SND_SOC_DAPM_MIC("Digital Mic5", NULL),
 };
+
 
 static inline int param_is_mask(int p)
 {
@@ -3117,6 +3119,8 @@ static int msm_audrx_init(struct snd_soc_pcm_runtime *rtd)
 	snd_soc_dapm_ignore_suspend(dapm, "ANC LINEOUT2");
 	snd_soc_dapm_ignore_suspend(dapm, "AIF4 VI");
 	snd_soc_dapm_ignore_suspend(dapm, "VIINPUT");
+	snd_soc_dapm_ignore_suspend(dapm, "Loopback Output");
+	snd_soc_dapm_ignore_suspend(dapm, "Loopback Input");
 
 	snd_soc_dapm_sync(dapm);
 
