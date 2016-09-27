5120222996aa65ca693a8d2ff2bdaa0a3e6b9791
Robb Glasser <rglasser@google.com>
2016-09-26 17:29:54 -0700

    Add padding field to fuse_open_out
    Bug: 30222859
    Change-Id: Iefc66a02a7692a6286dab9b30d4bad7d92afdd77

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -467,7 +467,10 @@ struct fuse_create_in {
 struct fuse_open_out {
 	uint64_t	fh;
 	uint32_t	open_flags;
-	int32_t         lower_fd;/* lower layer file descriptor */
+        union {
+	        int32_t  lower_fd;/* lower layer file descriptor */
+                uint32_t padding;
+        };
 };
 
 struct fuse_release_in {
