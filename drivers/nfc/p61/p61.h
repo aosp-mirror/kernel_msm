c59102276339b2a98649b21ed88a784cdae12de4
Lianchao Song <songlianchao@huawei.com>
2016-09-30 21:07:22 +0800

    nfc: add the nfc feature support
    1.add the nfc drvier in driver/nfc
    2.add nfc related dts config to nxp pn547 and p61 devices
    
    BUG:31847080
    Change-Id: Ia4291527ba84c33fe3c9588f4cd79b578c8d4571
    Signed-off-by: [h00383482] <[huanglizhe@huawei.com]>

---
+++ drivers/nfc/p61/p61.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2010 Trusted Logic S.A.
+ * modifications copyright (C) 2015 NXP B.V.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ */
+
+
+#define P61_MAGIC 0xEA
+#define P61_SET_PWR _IOW(P61_MAGIC, 0x01, unsigned int)
+#define P61_SET_DBG _IOW(P61_MAGIC, 0x02, unsigned int)
+#define P61_SET_POLL _IOW(P61_MAGIC, 0x03, unsigned int)
+/*
+ * SPI Request NFCC to enable p61 power, only in param
+ * Only for SPI
+ * level 1 = Enable power
+ * level 0 = Disable power
+ */
+#define P61_SET_SPM_PWR    _IOW(P61_MAGIC, 0x04, unsigned int)
+
+/* SPI or DWP can call this ioctl to get the current
+ * power state of P61
+ *
+*/
+#define P61_GET_SPM_STATUS    _IOR(P61_MAGIC, 0x05, unsigned int)
+
+#define P61_GET_ESE_ACCESS    _IOW(P61_MAGIC, 0x07, unsigned int)
+
+struct p61_spi_platform_data {
+    unsigned int irq_gpio;
+    unsigned int rst_gpio;
+};
