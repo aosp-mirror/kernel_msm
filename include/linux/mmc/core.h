b19bbcfdbba286e4eafe6429e11c5ac1f4512988
Mohan Srinivasan <srmohan@google.com>
2016-10-03 16:17:34 -0700

    Fix a build breakage in IO latency hist code.
    Fix a build breakage where MMC is enabled, but BLOCK is not.
    
    Change-Id: I0eb422d12264f0371f3368ae7c37342ef9efabaa
    Signed-off-by: Mohan Srinivasan <srmohan@google.com>

--- include/linux/mmc/core.h
+++ include/linux/mmc/core.h
@@ -109,9 +109,11 @@ struct mmc_request {
 	void			(*done)(struct mmc_request *);/* completion function */
 	struct mmc_host		*host;
 	struct mmc_cmdq_req	*cmdq_req;
-	struct request *req;
-	ktime_t io_start;
-	int lat_hist_enabled;
+	struct request          *req;
+	ktime_t			io_start;
+#ifdef CONFIG_BLOCK
+	int			lat_hist_enabled;
+#endif
 };
 
 struct mmc_bus_ops {
