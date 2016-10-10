01cfd4d06de4b9a1eee40f10e016fccfbebd557a
Sachin Bhayare <sachin.bhayare@codeaurora.org>
2016-10-10 19:04:22 +0530

    msm: mdss: fix panel resume issue after wakeup delay optimization
    As part of wakeup delay optimization post panel on commands
    were not sent because of incorrect check for avoiding sleep.
    
    Fix issue by let the post panel on commands get executed and sleep is
    skipped.
    
    Change-Id: I71c9fb8330c8df0751bab1a3e7233413c784981b
    Signed-off-by: Sachin Bhayare <sachin.bhayare@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -777,8 +777,9 @@ static int mdss_dsi_post_panel_on(struct mdss_panel_data *pdata)
 			goto end;
 
 	cmds = &ctrl->post_panel_on_cmds;
-	if ((cmds->cmd_cnt) &&(pinfo->mipi.init_delay)) {
-		msleep(VSYNC_DELAY);	/* wait for a vsync passed */
+	if (cmds->cmd_cnt) {
+		if (pinfo->mipi.init_delay)
+			msleep(VSYNC_DELAY);	/* wait for a vsync passed */
 		mdss_dsi_panel_cmds_send(ctrl, cmds, CMD_REQ_COMMIT);
 	}
 
