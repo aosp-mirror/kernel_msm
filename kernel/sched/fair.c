02211981cfaf6cc4b15cd3f360444b2673ce67f1
Vincent Guittot <vincent.guittot@linaro.org>
2016-10-20 16:11:46 -0700

    sched: propagate asynchrous detach
    Backported by Andres Oportus <andresoportus@google.com> from:
    Subject	[tip:sched/core] sched/fair: Propagate asynchrous detach
    Commit-ID:  4e5160766fcc9f41bbd38bac11f92dce993644aa
    Gitweb:     http://git.kernel.org/tip/4e5160766fcc9f41bbd38bac11f92dce993644aa
    Author:     Vincent Guittot <vincent.guittot@linaro.org>
    AuthorDate: Tue, 8 Nov 2016 10:53:46 +0100
    Committer:  Ingo Molnar <mingo@kernel.org>
    CommitDate: Wed, 16 Nov 2016 10:29:10 +0100
    
    sched/fair: Propagate asynchrous detach
    
    A task can be asynchronously detached from cfs_rq when migrating
    between CPUs. The load of the migrated task is then removed from
    source cfs_rq during its next update. We use this event to set
    propagation flag.
    
    During the load balance, we take advantage of the update of blocked
    load to propagate any pending changes.
    
    The propagation relies on patch:
    
      "sched: Fix hierarchical order in rq->leaf_cfs_rq_list"
    
    ... which orders children and parents, to ensure that it's done in one pass.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Morten.Rasmussen@arm.com
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: bsegall@google.com
    Cc: kernellwp@gmail.com
    Cc: pjt@google.com
    Cc: yuyang.du@intel.com
    Link: http://lkml.kernel.org/r/1478598827-32372-6-git-send-email-vincent.guittot@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    [ backport to android-3.18 ]
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    Change-Id: I5577d6f703d3f38bedda4410f5759eb54d7385b1

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2868,13 +2868,14 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
 		sa->load_avg = max_t(long, sa->load_avg - r, 0);
 		sa->load_sum = max_t(s64, sa->load_sum - r * LOAD_AVG_MAX, 0);
 		removed = 1;
+		set_tg_cfs_propagate(cfs_rq);
 	}
 
 	if (atomic_long_read(&cfs_rq->removed_util_avg)) {
 		long r = atomic_long_xchg(&cfs_rq->removed_util_avg, 0);
-		sa->util_avg = max_t(long, sa->util_avg - r, 0);
-		sa->util_sum = max_t(s32, sa->util_sum - r * LOAD_AVG_MAX, 0);
-		removed_util = 1;
+		sub_positive(&sa->util_avg, r);
+		sub_positive(&sa->util_sum, r * LOAD_AVG_MAX);
+		set_tg_cfs_propagate(cfs_rq);
 	}
 
 	decayed = __update_load_avg(now, cpu_of(rq_of(cfs_rq)), sa,
@@ -7090,6 +7091,10 @@ static void update_blocked_averages(int cpu)
 
 		if (update_cfs_rq_load_avg(cfs_rq_clock_task(cfs_rq), cfs_rq, true))
 			update_tg_load_avg(cfs_rq, 0);
+
+		/* Propagate pending load changes to the parent */
+		if (cfs_rq->tg->se[cpu])
+			update_load_avg(cfs_rq->tg->se[cpu], 0);
 	}
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 }
