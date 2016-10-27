90f3c69fccc5239a5a874955468403bcbb2f252a
Wei Wang <wvw@google.com>
2016-10-27 15:53:03 -0700

    power: Add detailed suspend abort reason
    Test: dumpsys batterystats
    Bug: 30874086
    
    Change-Id: I7126755c6b3ee4b2a7d00194dead2c653dc1d260

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -36,9 +36,6 @@ static int try_to_freeze_tasks(bool user_only)
 	unsigned int elapsed_msecs;
 	bool wakeup = false;
 	int sleep_usecs = USEC_PER_MSEC;
-#ifdef CONFIG_PM_SLEEP
-	char suspend_abort[MAX_SUSPEND_ABORT_LEN];
-#endif
 
 	do_gettimeofday(&start);
 
@@ -68,11 +65,6 @@ static int try_to_freeze_tasks(bool user_only)
 			break;
 
 		if (pm_wakeup_pending()) {
-#ifdef CONFIG_PM_SLEEP
-			pm_get_active_wakeup_sources(suspend_abort,
-				MAX_SUSPEND_ABORT_LEN);
-			log_suspend_abort_reason(suspend_abort);
-#endif
 			wakeup = true;
 			break;
 		}
