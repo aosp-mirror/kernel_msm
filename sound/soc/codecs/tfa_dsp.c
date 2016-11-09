8bd2330755ebdb8da3dbbb652583f5f4f3b29bbc
Nick Desaulniers <ndesaulniers@google.com>
2016-11-09 15:37:50 -0800

    sound: soc: fix self assignment bug
    sound/soc/codecs/tfa_dsp.c:2598:10: error: explicitly assigning value of
    variable of type 'int' to itself
          [-Werror,-Wself-assign]
            profile = profile;
            ~~~~~~~ ^ ~~~~~~~
    
    Bug: 32778703
    Change-Id: Idc9d8787bfee02429b88c361a45ccb3c495b99d4

--- sound/soc/codecs/tfa_dsp.c
+++ sound/soc/codecs/tfa_dsp.c
@@ -2594,8 +2594,6 @@ enum Tfa98xx_Error tfaRunSpeakerCalibration(Tfa98xx_handle_t handle, int profile
 {
 	enum Tfa98xx_Error err = Tfa98xx_Error_Ok;
 	int calibrateDone, spkr_count = 0;
-	/* Avoid warning in user-space */
-	profile = profile;
 
 #ifdef __KERNEL__ /* Necessary otherwise we are thrown out of operating mode in kernel (because of internal clock) */
 	if ((strnstr(tfaContProfileName(handle, profile), ".cal", strlen(tfaContProfileName(handle, profile))) == NULL) && (tfa98xx_dev_family(handle) == 2)) {
