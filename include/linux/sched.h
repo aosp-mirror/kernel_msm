960e362136daea135572f71cecca9b75686abb4c
Steve Muckle <smuckle@linaro.org>
2016-11-11 14:04:43 -0800

    sched: backport cpufreq hooks from 4.9-rc4
    The scheduler cpufreq hooks are required by the schedutil cpufreq
    governor.
    
    Signed-off-by: Steve Muckle <smuckle@linaro.org>
    [trivial cherry-picking fixes]
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Change-Id: I1f3879e5c1c8356b14e8b5a5fc973c37bdb3a0e6

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3163,4 +3163,21 @@ struct cpu_cycle_counter_cb {
 };
 int register_cpu_cycle_counter_cb(struct cpu_cycle_counter_cb *cb);
 
+#define SCHED_CPUFREQ_RT        (1U << 0)
+#define SCHED_CPUFREQ_DL        (1U << 1)
+#define SCHED_CPUFREQ_IOWAIT    (1U << 2)
+
+#define SCHED_CPUFREQ_RT_DL     (SCHED_CPUFREQ_RT | SCHED_CPUFREQ_DL)
+
+#ifdef CONFIG_CPU_FREQ
+struct update_util_data {
+	void (*func)(struct update_util_data *data, u64 time, unsigned int flags);
+};
+
+void cpufreq_add_update_util_hook(int cpu, struct update_util_data *data,
+                       void (*func)(struct update_util_data *data, u64 time,
+                                    unsigned int flags));
+void cpufreq_remove_update_util_hook(int cpu);
+#endif /* CONFIG_CPU_FREQ */
+
 #endif
