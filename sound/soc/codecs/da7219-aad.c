1923c3dd372c5065514cfd89fc5c3153a95c4152
John Muir <muirj@google.com>
2016-11-14 16:10:07 -0800

    ASoC: da7219: Revert use of device_get_named_child_node.
    Commit 13409e40de09e225c21bd433c30e1044a79a02c3 introduces the
    use of device_get_named_child_node which doesn't exist in this
    version of Linux.
    
    Bug: 32029151
    Test: Kernel compiles.
    Change-Id: I72149e723eb62aca935b30a74e07db46ab61ad5b

--- sound/soc/codecs/da7219-aad.c
+++ sound/soc/codecs/da7219-aad.c
@@ -547,7 +547,7 @@ static struct da7219_aad_pdata *da7219_aad_fw_to_pdata(struct snd_soc_codec *cod
 	const char *fw_str;
 	u32 fw_val32;
 
-	aad_np = device_get_named_child_node(dev, "da7219_aad");
+	aad_np = of_find_node_by_name(dev->of_node, "da7219_aad");
 	if (!aad_np)
 		return NULL;
 
