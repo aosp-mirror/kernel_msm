97c191ffd67d7d7952f7f09e1b5e842f7816e976
Michelle.Lu <Michelle.Lu@quantatw.com>
2016-11-18 06:40:02 +0000

    Modify bootreason to meet requirements (2/3)
    [Description]
    	Modify bootreason to meet requiremets
    	can get bootreason via "getprop ro.boot.bootreason"
    
    [Customer Impact]
    	- None
    [Affected Area]
    	- cmdline, ro.boot.bootreason
    [Side effect of the change]
    	- None
    
    Change-Id: I9923b9d481a5a53ac45f80ee7b4cfd317fb91d6e

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -295,6 +295,10 @@ static void msm_restart_prepare(const char *cmd)
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 	}
 
+	if(in_panic) {
+		 qpnp_pon_set_restart_reason(PON_RESTART_REASON_PANIC);
+	}
+
 	if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
 			qpnp_pon_set_restart_reason(
