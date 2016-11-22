09b679d2ba35c87fcffe3d79cbea69e59102fe22
Srinivas Girigowda <sgirigow@codeaurora.org>
2016-11-22 14:15:37 -0800

    qcacld-2.0: wlan host driver upgrade to 4.4.25.027s_1
    wlan host driver upgrade to 4.4.25.027s_1.
    's' indicates security branch.
    
    Change-Id: Iacc139ba3ce1ecb0fb8ef8b01424f5899889088e
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
+++ drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
@@ -42,9 +42,9 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_MINOR            4
 #define QWLAN_VERSION_PATCH            25
 #define QWLAN_VERSION_EXTRA            ""
-#define QWLAN_VERSION_BUILD            26
+#define QWLAN_VERSION_BUILD            27
 
-#define QWLAN_VERSIONSTR               "4.4.25.027"
+#define QWLAN_VERSIONSTR               "4.4.25.027s_1"
 
 
 #define AR6320_REV1_VERSION             0x5000000
