1415108f121dd23efae00ed8fbf21a1bba966e2c
y00365468 <y00365468@notesmail.huawei.com>
2016-11-30 20:06:24 +0800

    USB: fix a crash caused by div0.
    Change-Id: Ica2c2449ca6aff853da944a114b776736e7e0880

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -1587,8 +1587,12 @@ static int debug_mtp_read_stats(struct seq_file *s, void *unused)
 		}
 	}
 
-	seq_printf(s, "vfs_write(time in usec) min:%d\t max:%d\t avg:%d\n",
-						min, max, sum / iteration);
+	if (0 != iteration)
+	{
+		seq_printf(s, "vfs_write(time in usec) min:%d\t max:%d\t avg:%d\n",
+							min, max, sum / iteration);
+	}
+
 	min = max = sum = iteration = 0;
 	seq_puts(s, "\n=======================\n");
 	seq_puts(s, "MTP Read Stats:\n");
@@ -1609,8 +1613,12 @@ static int debug_mtp_read_stats(struct seq_file *s, void *unused)
 		}
 	}
 
-	seq_printf(s, "vfs_read(time in usec) min:%d\t max:%d\t avg:%d\n",
-						min, max, sum / iteration);
+	if (0 != iteration)
+	{
+		seq_printf(s, "vfs_read(time in usec) min:%d\t max:%d\t avg:%d\n",
+							min, max, sum / iteration);
+	}
+
 	spin_unlock_irqrestore(&dev->lock, flags);
 	return 0;
 }
