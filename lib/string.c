475faf125234c999d5f251eeb28231e4c2bab868
Yueyao Zhu <yueyao@google.com>
2016-12-07 15:36:48 -0800

    lib/string: fix logic in sysfs_match_string
    sysfs_streq() returns true when the strings are equal,
    and sysfs_match_string should return the index when
    sysfs_streq() returns true instead of false.
    
    Bug: 33417679
    Change-Id: I5686d019e3e60025eea2419445378e478a7cab37

--- lib/string.c
+++ lib/string.c
@@ -674,7 +674,7 @@ int __sysfs_match_string(const char * const *array, size_t n, const char *str)
 		item = array[index];
 		if (!item)
 			break;
-		if (!sysfs_streq(item, str))
+		if (sysfs_streq(item, str))
 			return index;
 	}
 
