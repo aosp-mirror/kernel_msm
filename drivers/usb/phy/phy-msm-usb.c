515f6621d0475eb55dea3b028364fb83e382cccb
chester_sun <Chester_Sun@compal.com>
2016-12-14 15:15:50 +0800

    usb: reset charger state and type as absence of vbus signal
    In order to ensure the correct initial status of charger state and type at the beginning of charger detection.
    
    Change-Id: I8deae7c0ddab7e6aaf52b241420ac7c5376528f3

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -2933,6 +2933,10 @@ static void msm_otg_set_vbus_state(int online)
 		pr_debug("PMIC: BSV clear\n");
 		msm_otg_dbg_log_event(&motg->phy, "PMIC: BSV CLEAR",
 				init, motg->inputs);
+
+		motg->chg_state = USB_CHG_STATE_UNDEFINED;
+		motg->chg_type = USB_INVALID_CHARGER;
+
 		if (!test_and_clear_bit(B_SESS_VLD, &motg->inputs) && init)
 			return;
 	}
