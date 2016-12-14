324181821c790ca02a2180ff01576d3c4d15fa6d
Philip Cuadra <philipcuadra@google.com>
2016-12-14 14:42:17 -0800

    Move sync logic to be run always
    Lift sync logic out of energy_aware_wake_cpu and run that logic before
    select_idle_sibling() or energy_aware_wake_cpu().
    
    At high CPU utilization, the scheduler falls back to
    select_idle_sibling().  On Marlin, select_idle_sibling() wasn't
    respecting the sync flag to force the task to be scheduled on the same
    CPU.  This caused the sync flag to be ignored and thrash on binder with
    sync enabled.  This fixes the thrash we saw.
    
    Test:  manually ran hwbinder_benchmark
    Change-Id: I47d21bd3ef936d8e0980b618145de29d3ef6d480

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5532,7 +5532,7 @@ static inline int find_best_target(struct task_struct *p, bool boosted, bool pre
 	return target_cpu;
 }
 
-static int energy_aware_wake_cpu(struct task_struct *p, int target, int sync)
+static int energy_aware_wake_cpu(struct task_struct *p, int target)
 {
 	struct sched_domain *sd;
 	struct sched_group *sg, *sg_target;
@@ -5541,14 +5541,6 @@ static int energy_aware_wake_cpu(struct task_struct *p, int target, int sync)
 	unsigned long min_util;
 	unsigned long new_util;
 	int i;
-
-	if (sysctl_sched_sync_hint_enable && sync) {
-		int cpu = smp_processor_id();
-		cpumask_t search_cpus;
-		cpumask_and(&search_cpus, tsk_cpus_allowed(p), cpu_online_mask);
-		if (cpumask_test_cpu(cpu, &search_cpus))
-			return cpu;
-	}
 
 	sd = rcu_dereference(per_cpu(sd_ea, task_cpu(p)));
 
@@ -5702,10 +5694,24 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	}
 
 	if (!sd) {
-		if (energy_aware() && !cpu_rq(cpu)->rd->overutilized)
-			new_cpu = energy_aware_wake_cpu(p, prev_cpu, sync);
-		else if (sd_flag & SD_BALANCE_WAKE) /* XXX always ? */
-			new_cpu = select_idle_sibling(p, new_cpu);
+		int sync_used = 0;
+
+		if (sysctl_sched_sync_hint_enable && sync) {
+			cpumask_t search_cpus;
+			cpumask_and(&search_cpus, tsk_cpus_allowed(p),
+				    cpu_online_mask);
+			if (cpumask_test_cpu(cpu, &search_cpus)) {
+				sync_used = 1;
+				new_cpu = cpu;
+			}
+		}
+
+		if (!sync_used) {
+			if (energy_aware() && !cpu_rq(cpu)->rd->overutilized)
+				new_cpu = energy_aware_wake_cpu(p, prev_cpu);
+			else if (sd_flag & SD_BALANCE_WAKE) /* XXX always ? */
+				new_cpu = select_idle_sibling(p, new_cpu);
+		}
 
 	} else while (sd) {
 		struct sched_group *group;
