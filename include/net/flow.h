08426ef41736604f8cfd3b84a8073bcba2a675b8
Prashant Malani <pmalani@google.com>
2016-12-22 13:24:08 -0800

    Revert "net: core: add UID to flows, rules, and routes"
    This reverts commit dcab085571a1f7d6f30a827646166017fb63bcb9.
    
    Change-Id: Ia642e6172da7cf92fb9ab25ca62910631d07240d
    Signed-off-by: Prashant Malani <pmalani@google.com>

--- include/net/flow.h
+++ include/net/flow.h
@@ -10,7 +10,6 @@
 #include <linux/socket.h>
 #include <linux/in6.h>
 #include <linux/atomic.h>
-#include <linux/uidgid.h>
 
 /*
  * ifindex generation is per-net namespace, and loopback is
@@ -31,7 +30,6 @@ struct flowi_common {
 #define FLOWI_FLAG_ANYSRC		0x01
 #define FLOWI_FLAG_KNOWN_NH		0x02
 	__u32	flowic_secid;
-	kuid_t  flowic_uid;
 };
 
 union flowi_uli {
@@ -68,7 +66,6 @@ struct flowi4 {
 #define flowi4_proto		__fl_common.flowic_proto
 #define flowi4_flags		__fl_common.flowic_flags
 #define flowi4_secid		__fl_common.flowic_secid
-#define flowi4_uid		__fl_common.flowic_uid
 
 	/* (saddr,daddr) must be grouped, same order as in IP header */
 	__be32			saddr;
@@ -125,7 +122,6 @@ struct flowi6 {
 #define flowi6_proto		__fl_common.flowic_proto
 #define flowi6_flags		__fl_common.flowic_flags
 #define flowi6_secid		__fl_common.flowic_secid
-#define flowi6_uid		__fl_common.flowic_uid
 	struct in6_addr		daddr;
 	struct in6_addr		saddr;
 	__be32			flowlabel;
@@ -169,7 +165,6 @@ struct flowi {
 #define flowi_proto	u.__fl_common.flowic_proto
 #define flowi_flags	u.__fl_common.flowic_flags
 #define flowi_secid	u.__fl_common.flowic_secid
-#define flowi_uid	u.__fl_common.flowic_uid
 } __attribute__((__aligned__(BITS_PER_LONG/8)));
 
 static inline struct flowi *flowi4_to_flowi(struct flowi4 *fl4)
