99e1d9cad38dd5af269fd463aa70553a59352045
Karthikeyan Mani <kmani@codeaurora.org>
2017-01-03 14:46:23 -0800

    ASoC: msm: qdsp6v2: Fix out-of-bounds access in put functions
    Add out of bounds check in routing put functions
    for the mux value before accessing the texts
    pointer of soc_enum struct with mux as index.
    
    CRs-fixed: 1097569
    Change-Id: Ib9ef8d398f0765754b0f79666963fac043b66078
    Signed-off-by: Karthikeyan Mani <kmani@codeaurora.org>
    Signed-off-by: Bikshapathi Kothapeta <bkotha@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -2000,6 +2000,10 @@ static int msm_routing_ext_ec_put(struct snd_kcontrol *kcontrol,
 	pr_debug("%s: msm_route_ec_ref_rx = %d value = %ld\n",
 		 __func__, msm_route_ext_ec_ref,
 		 ucontrol->value.integer.value[0]);
+	if (mux >= e->items) {
+		pr_err("%s: Invalid mux value %d\n", __func__, mux);
+		return -EINVAL;
+	}
 
 	if (mux >= e->items) {
 		pr_err("%s: Invalid mux value %d\n", __func__, mux);
