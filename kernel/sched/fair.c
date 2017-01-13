968e188922f5f3383456f5d59e974d6f9bd6c0ea
Chris Redpath <chris.redpath@arm.com>
2017-01-13 15:32:35 +0000

    Revert "WIP: UTIL_EST: use estimated utilization on load balancing paths"
    This reverts commit d9a26d3828456f196d76fab7d578c77ce5a76ca7.
    
    Change-Id: I7fe1b72ac84469cb3f682222fd6da59017c95c13
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5122,7 +5122,7 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 
 static inline bool task_fits_spare(struct task_struct *p, int cpu)
 {
-	return __task_fits(p, cpu, cpu_util(cpu, UTIL_EST));
+	return __task_fits(p, cpu, cpu_util(cpu, UTIL_AVG));
 }
 
 static bool cpu_overutilized(int cpu)
@@ -5293,7 +5293,7 @@ find_idlest_group(struct sched_domain *sd, struct task_struct *p,
 			 * Look for group which has most spare capacity on a
 			 * single cpu.
 			 */
-			spare_capacity = capacity_of(i) - cpu_util(i, UTIL_EST);
+			spare_capacity = capacity_of(i) - cpu_util(i, UTIL_AVG);
 			if (spare_capacity > max_spare_capacity) {
 				max_spare_capacity = spare_capacity;
 				spare_group = group;
