974b525b46667ab64c3305e48e3578da60b1477c
Jerry Zhang <zhangjerry@google.com>
2017-01-13 14:25:58 -0800

    usb: gadget: f_fs: Free buffer on epfile cleanup
    Change-Id: I355600d902559dbfc8cb87d139a98244d9bab4a9
    Signed-off-by: Jerry Zhang <zhangjerry@google.com>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -1745,6 +1745,7 @@ static void ffs_epfiles_destroy(struct ffs_epfile *epfiles, unsigned count)
 			dput(epfile->dentry);
 			epfile->dentry = NULL;
 		}
+        kfree(epfile->buffer);
 	}
 
 	kfree(epfiles);
