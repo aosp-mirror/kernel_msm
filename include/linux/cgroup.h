ff8d1d402c2ca85e41cd9ab2bf9bd148fddde83a
Amit Pundir <amit.pundir@linaro.org>
2017-01-26 15:02:42 +0000

    Revert "cgroup: Fix issues in allow_attach callback"
    This reverts commit 43eac67037f50df420dfe7a7571f27dfac52a90b.
    
    Part of Chromium commit series:
    
    CHROMIUM: remove Android's cgroup generic permissions checks
    
    The implementation is utterly broken, resulting in all processes being
    allows to move tasks between sets (as long as they have access to the
    "tasks" attribute), and upstream is heading towards checking only
    capability anyway, so let's get rid of this code.
    
    BUG=b:31790445,chromium:647994
    TEST=Boot android container, examine logcat
    
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/394967
    Reviewed-by: Ricky Zhou <rickyz@chromium.org>
    Reviewed-by: John Stultz <john.stultz@linaro.org>
    
    Change-Id: Ie4b516571c06456927f4bb19e02b148b385b13fb
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -949,7 +949,7 @@ static inline int cgroup_attach_task_all(struct task_struct *from,
 static inline int subsys_cgroup_allow_attach(struct cgroup_subsys_state *css,
 					     void *tset)
 {
-	return -EINVAL;
+	return 0;
 }
 
 static inline int cgroup_attach_task_to_root(struct task_struct *tsk, int wait)
