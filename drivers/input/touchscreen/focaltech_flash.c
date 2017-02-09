1dcd18cf38402650dc160017320b81066ab95c82
frank_chien <Frank_Chien@compal.com>
2017-02-09 12:28:47 +0800

    touch: Update TP firmware
    Update TP 0x30 firmware version 0X2
    
    Change-Id: I3a977470262cc85f4f812491b653d58ab4fa60ea

--- drivers/input/touchscreen/focaltech_flash.c
+++ drivers/input/touchscreen/focaltech_flash.c
@@ -99,7 +99,7 @@
 * Static variables
 *******************************************************************************/
 static unsigned char CTPM_FW_I_FILE[] = {
-	#include "FTS_FW/Default_V0x0A_20170123_app.i"
+	#include "FTS_FW/TP3207_0x30_V0x02_20170125_app.i"
 };
 
  struct fts_Upgrade_Info fts_updateinfo[] =
