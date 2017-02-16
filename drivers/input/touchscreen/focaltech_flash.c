3001b8e024a751fad4d2f966a119a3cb901cbb65
nevill_jiang <nevill_jiang@compal.com>
2017-02-16 17:27:02 +0800

    lionfish: Update TP firmware
    1. Upgrade version v0A
    2. Change threshold from 2500 to 3000
    
    Change-Id: I8ed4b3a08c05924d5bbeea1ddadb2b78bea2327c

--- drivers/input/touchscreen/focaltech_flash.c
+++ drivers/input/touchscreen/focaltech_flash.c
@@ -99,7 +99,7 @@
 * Static variables
 *******************************************************************************/
 static unsigned char CTPM_FW_FNW1_BLACK[] = {
-	#include "FTS_FW/CEI_DBW1_3207_V0x09_20160920_app.i"
+	#include "FTS_FW/CEI_DBW1_3207_0x71_V0x0A_20170214_app.i"
 };
 
  struct fts_Upgrade_Info fts_updateinfo[] =
