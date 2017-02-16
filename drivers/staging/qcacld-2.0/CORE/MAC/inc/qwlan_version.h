e0c238dc5edce192f56a41bb0444a359bcfc3b12
Subhani Shaik <subhanis@codeaurora.org>
2017-02-15 17:32:05 -0800

    qcacld-2.0: wlan host driver upgrade to 4.4.23.015
    wlan host driver upgrade to 4.4.23.015
    
    Signed-off-by: Subhani Shaik <subhanis@codeaurora.org>
    Change-Id: I9c1c3669a2139726d39cfc027eacbac4f049ebe1

--- drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
+++ drivers/staging/qcacld-2.0/CORE/MAC/inc/qwlan_version.h
@@ -44,7 +44,7 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_EXTRA            ""
 #define QWLAN_VERSION_BUILD            14
 
-#define QWLAN_VERSIONSTR               "4.4.23.014"
+#define QWLAN_VERSIONSTR               "4.4.23.015"
 
 
 #define AR6320_REV1_VERSION             0x5000000
