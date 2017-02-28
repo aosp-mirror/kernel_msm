9e687d25ee49a05264a21d675446c6d71374254e
Oleg Matcovschi <omatcovschi@google.com>
2017-02-28 14:10:37 -0800

    qpnp-power-on: Enable power, vol_down buttons
    Fix incorrect code resolution during merge.
    
    Bug: 35759579
    Change-Id: I6479a26cfaf482deab343972ce317c9f844c3525
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- drivers/input/misc/qpnp-power-on.c
+++ drivers/input/misc/qpnp-power-on.c
@@ -823,6 +823,9 @@ qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 			input_report_key(pon->pon_input, cfg->key_code, 1);
 			input_sync(pon->pon_input);
 		}
+
+		input_report_key(pon->pon_input, cfg->key_code, key_status);
+		input_sync(pon->pon_input);
 	}
 
 	cfg->old_state = !!key_status;
