60a4fcb4fa558feb313a7128f3f951bc22f0bb29
nevill_jiang <nevill_jiang@compal.com>
2017-03-02 18:45:02 +0800

    lionfish: Update TP Firmware
    Tuning sensitivity
    
    Change-Id: Ie10f3096f74ac5d5e2d5d5bd9e43b3dd6904c4c9

--- drivers/input/touchscreen/focaltech_flash.c
+++ drivers/input/touchscreen/focaltech_flash.c
@@ -99,7 +99,7 @@
 * Static variables
 *******************************************************************************/
 static unsigned char CTPM_FW_FNW1_BLACK[] = {
-	#include "FTS_FW/CEI_DBW1_3207_0x71_V0x0B_20170217_app.i"
+	#include "FTS_FW/CEI_DBW1_3207_V0x0C_20170221_app.i"
 };
 
  struct fts_Upgrade_Info fts_updateinfo[] =
