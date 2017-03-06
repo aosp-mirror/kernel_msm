2efcb3ccfe4506571daf4aef6b1e78c4ec963fcd
Insun Song <insun.song@broadcom.com>
2017-03-06 11:16:50 +0900

    net: wireless: bcmdhd: remove unsed WEXT file.
    WEXT API was already obsoleted and should be removed.
    
    Bug: 32124445
    Change-Id: I470be1dab3d65b692fe03cb6531728db0b677180
    Signed-off-by: Insun Song <insun.song@broadcom.com>

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_custom_gpio.c
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_custom_gpio.c
@@ -32,7 +32,6 @@
 #include <dhd_linux.h>
 
 #include <wlioctl.h>
-#include <wl_iw.h>
 
 #define WL_ERROR(x) printf x
 #define WL_TRACE(x)
@@ -131,6 +130,12 @@ dhd_custom_get_mac_address(void *adapter, unsigned char *buf)
 	return ret;
 }
 #endif /* GET_CUSTOM_MAC_ENABLE */
+
+struct cntry_locales_custom {
+	char iso_abbrev[WLC_CNTRY_BUF_SZ];
+	char custom_locale[WLC_CNTRY_BUF_SZ];
+	int32 custom_locale_rev;
+};
 
 #if !defined(CUSTOMER_HW4)
 /* Customized Locale table : OPTIONAL feature */
