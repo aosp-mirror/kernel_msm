2f25fad137612a7a303f374ddc3a82678eb29109
Daniel Rosenberg <drosen@google.com>
2017-03-06 10:34:31 +0900

    ANDROID: ion: check for kref overflow
    Userspace can cause the kref to handles to increment
    arbitrarily high. Ensure it does not overflow.
    
    Bug: 31992382
    Test: See bug for poc
    Change-Id: I5abb3a804ffab9ff1aafc1c849fd653a1e6ed626
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -16,6 +16,8 @@
  *
  */
 
+#include <linux/atomic.h>
+#include <linux/err.h>
 #include <linux/file.h>
 #include <linux/freezer.h>
 #include <linux/fs.h>
@@ -392,6 +394,15 @@ struct ion_buffer *ion_handle_buffer(struct ion_handle *handle)
 static void ion_handle_get(struct ion_handle *handle)
 {
 	kref_get(&handle->ref);
+}
+
+/* Must hold the client lock */
+static struct ion_handle* ion_handle_get_check_overflow(struct ion_handle *handle)
+{
+	if (atomic_read(&handle->ref.refcount) + 1 == 0)
+		return ERR_PTR(-EOVERFLOW);
+	ion_handle_get(handle);
+	return handle;
 }
 
 int ion_handle_put(struct ion_handle *handle)
@@ -1307,7 +1318,7 @@ struct ion_handle *ion_import_dma_buf(struct ion_client *client, int fd)
 	/* if a handle exists for this buffer just take a reference to it */
 	handle = ion_handle_lookup(client, buffer);
 	if (!IS_ERR(handle)) {
-		ion_handle_get(handle);
+		handle = ion_handle_get_check_overflow(handle);
 		mutex_unlock(&client->lock);
 		goto end;
 	}
