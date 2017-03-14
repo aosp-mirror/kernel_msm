ff7244e9c9beb1145292074e9ff2b013d67212f3
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-03-13 17:29:35 -0700

    qcacld-2.0: Fix memory leak issue
    Memory should be allocated to radar_event only when radar phy
    event needs to be sent to WMA layer.
    
    Add changes to fix memory leak.
    
    CRs-Fixed: 1065466
    Bug: 34514463
    Change-Id: Ia3e93ddd47913956c27487472b6a70eb68d63fd9
    Signed-off-by: Ecco Park <eccopark@google.com>

--- drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
@@ -35558,13 +35558,6 @@ int wma_dfs_indicate_radar(struct ieee80211com *ic,
 		WMA_LOGE("%s:DFS- Invalid WMA handle", __func__);
 		return -ENOENT;
 	}
-	radar_event = (struct wma_dfs_radar_indication *)
-		vos_mem_malloc(sizeof(struct wma_dfs_radar_indication));
-	if (radar_event == NULL)
-	{
-		WMA_LOGE("%s:DFS- Invalid radar_event", __func__);
-		return -ENOENT;
-	}
 
 	/*
 	 * Do not post multiple Radar events on the same channel.
@@ -35579,6 +35572,14 @@ int wma_dfs_indicate_radar(struct ieee80211com *ic,
 	if ((ichan->ic_ieee  != (wma->dfs_ic->last_radar_found_chan)) ||
 	    ( pmac->sap.SapDfsInfo.disable_dfs_ch_switch == VOS_TRUE) )
 	{
+		radar_event = (struct wma_dfs_radar_indication *)
+			vos_mem_malloc(sizeof(struct wma_dfs_radar_indication));
+		if (radar_event == NULL) {
+			adf_os_spin_unlock_bh(&ic->chan_lock);
+			WMA_LOGE("%s: DFS- Invalid radar_event", __func__);
+			return -ENOMEM;
+		}
+
 		/* Indicate the radar event to HDD to stop the netif Tx queues*/
 		hdd_radar_event.ieee_chan_number = ichan->ic_ieee;
 		hdd_radar_event.chan_freq = ichan->ic_freq;
