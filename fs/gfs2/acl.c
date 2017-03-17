9de991ea8a814ce62f83fd8a8127c468fae91046
Lianchao Song <songlianchao@huawei.com>
2017-03-17 10:57:37 +0800

    posix_acl: Clear SGID bit when setting file permissions
    CVE-2016-7097
    
    When file permissions are modified via chmod(2) and the user is not in
    the owning group or capable of CAP_FSETID, the setgid bit is cleared in
    inode_change_ok().  Setting a POSIX ACL via setxattr(2) sets the file
    permissions as well as the new ACL, but doesn't clear the setgid bit in
    a similar way; this allows to bypass the check in chmod(2).  Fix that.
    
    Change-Id: I661966ded60c16e5460b380738974efc3212c3fa
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Signed-off-by: Lianchao Song <songlianchao@huawei.com>

--- fs/gfs2/acl.c
+++ fs/gfs2/acl.c
@@ -268,14 +268,13 @@ static int gfs2_xattr_system_set(struct dentry *dentry, const char *name,
 
 	if (type == ACL_TYPE_ACCESS) {
 		umode_t mode = inode->i_mode;
-		error = posix_acl_equiv_mode(acl, &mode);
 
-		if (error <= 0) {
+		error = posix_acl_update_mode(inode, &inode->i_mode, &acl);
+		if (error){
 			posix_acl_release(acl);
 			acl = NULL;
 
-			if (error < 0)
-				return error;
+			return error;
 		}
 
 		error = gfs2_set_mode(inode, mode);
