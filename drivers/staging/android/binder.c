21c64694c1b6631c6af6e04d84b094201c313037
Todd Kjos <tkjos@google.com>
2017-03-22 15:33:56 -0700

    binder: turn on same debug messages as in common kernel
    The marlin kernel has no debug messages enabled. The default
    settings from the common kernel should be used which enable:
    BINDER_DEBUG_USER_ERROR, BINDER_DEBUG_FAILED_TRANSACTION,
    and BINDER_DEBUG_DEAD_TRANSACTION.
    
    Bug: 36406078
    Change-Id: Ieca841c008eb8887f1a1d2dcdb4083f1dd80ea22
    Test: tested manually
    Signed-off-by: Todd Kjos <tkjos@google.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -110,7 +110,8 @@ enum {
 	BINDER_DEBUG_SPINLOCKS              = 1U << 16,
 	BINDER_DEBUG_TODO_LISTS             = 1U << 17,
 };
-static uint32_t binder_debug_mask;
+static uint32_t binder_debug_mask = BINDER_DEBUG_USER_ERROR |
+	BINDER_DEBUG_FAILED_TRANSACTION | BINDER_DEBUG_DEAD_TRANSACTION;
 module_param_named(debug_mask, binder_debug_mask, uint, S_IWUSR | S_IRUGO);
 
 static char *binder_devices_param = CONFIG_ANDROID_BINDER_DEVICES;
