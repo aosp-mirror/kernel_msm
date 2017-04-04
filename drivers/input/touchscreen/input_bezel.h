ad773a5d72fe07413f9af8b13922028dc31b3cb3
Arpit Gupta <arpit.gupta@borqs.com>
2017-04-04 16:05:42 +0530

    M0058431 : Adding support for Bezel with coordinate displacement
    Change-Id: I0c67ab2799a0843fb7ca0c3c5e1764988cc90229
    
    Conflicts:
    	drivers/input/touchscreen/Kconfig

---
+++ drivers/input/touchscreen/input_bezel.h
@@ -0,0 +1,43 @@
+/* drivers/input/touchscreen/input_bezel.h
+ *
+ * Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _INPUT_BEZEL_H
+#define _INPUT_BEZEL_H
+
+#include <linux/list.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/timer.h>
+#include <linux/mod_devicetable.h>
+
+#define BEZEL_INSET_POS 10
+/* Approximated to distance for angular movement of 3 degree :
+   2 * pi * 227 * (3/360): rounded up; square of 11.88... */
+#define ANGULAR_DISTANCE_THRESHOLD  141
+
+struct bezel_data {
+	u8 inset;
+	u8 angular_dist_thresh;
+	u8 thickness;
+	u32 sq_rad_position;
+	bool bezel_touch_status;
+};
+
+int bezel_register_device(struct device *dev, unsigned int panel_maxx);
+int bezel_report_wheel(int x, int y, struct bezel_data *bdata);
+void bezel_reset(void);
+int ABS(int a, int b) ;
+
+#endif /* _INPUT_BEZEL_H */
