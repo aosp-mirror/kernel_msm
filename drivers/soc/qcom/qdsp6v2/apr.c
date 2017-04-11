ecb46273d1ba357ad48ab4456a80438b1a2454aa
Banajit Goswami <bgoswami@codeaurora.org>
2017-04-10 19:56:25 -0700

    soc: q6dspv2: apr: fix client registration refcount
    Audio Packet Router (APR) is used by multiple audio services
    to communicate between APSS and ADSP. These audio services
    registers for service level APR communication (port 0xFFFFFFFF),
    or for session level APR communication (using port 0x101 etc.).
    The services might choose to call apr_register for any port at
    random. The expectation is that the refcounting for the number
    of ports registered with APR for any specific service, is handled
    irrespective of the order in which registrations are done. The
    current logic fails to handle the refcounting when apr_register
    is called for 0xFFFFFFFF before other session based ports. Fix
    this correctly using the service count (svc_cnt) variable in apr_svc.
    
    CRs-fixed: 2022490
    Bug: 34088848
    Change-Id: I2fcd1269facf24d509db0d90314e0d2545a2ad67
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>

--- drivers/soc/qcom/qdsp6v2/apr.c
+++ drivers/soc/qcom/qdsp6v2/apr.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2015, 2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -404,19 +404,19 @@ struct apr_svc *apr_register(char *dest, char *svc_name, apr_fn svc_fn,
 			mutex_unlock(&svc->m_lock);
 			return NULL;
 		}
-		if (!svc->port_cnt && !svc->svc_cnt)
+		if (!svc->svc_cnt)
 			clnt->svc_cnt++;
 		svc->port_cnt++;
 		svc->port_fn[temp_port] = svc_fn;
 		svc->port_priv[temp_port] = priv;
+		svc->svc_cnt++;
 	} else {
 		if (!svc->fn) {
-			if (!svc->port_cnt && !svc->svc_cnt)
+			if (!svc->svc_cnt)
 				clnt->svc_cnt++;
 			svc->fn = svc_fn;
-			if (svc->port_cnt)
-				svc->svc_cnt++;
 			svc->priv = priv;
+			svc->svc_cnt++;
 		}
 	}
 
@@ -622,24 +622,17 @@ int apr_deregister(void *handle)
 	client_id = svc->client_id;
 	clnt = &client[dest_id][client_id];
 
-	if (svc->port_cnt > 0 || svc->svc_cnt > 0) {
+	if (svc->svc_cnt > 0) {
 		if (svc->port_cnt)
 			svc->port_cnt--;
-		else if (svc->svc_cnt)
-			svc->svc_cnt--;
-		if (!svc->port_cnt && !svc->svc_cnt) {
+		svc->svc_cnt--;
+		if (!svc->svc_cnt) {
 			client[dest_id][client_id].svc_cnt--;
-			svc->need_reset = 0x0;
-		}
-	} else if (client[dest_id][client_id].svc_cnt > 0) {
-		client[dest_id][client_id].svc_cnt--;
-		if (!client[dest_id][client_id].svc_cnt) {
-			svc->need_reset = 0x0;
 			pr_debug("%s: service is reset %pK\n", __func__, svc);
 		}
 	}
 
-	if (!svc->port_cnt && !svc->svc_cnt) {
+	if (!svc->svc_cnt) {
 		svc->priv = NULL;
 		svc->id = 0;
 		svc->fn = NULL;
