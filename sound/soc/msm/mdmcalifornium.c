94846c1b4b647c812cb77d17e956b2e8063ef773
Aditya Bavanari <abavanar@codeaurora.org>
2017-04-17 19:57:59 +0530

    ASoC: msm: Fix kernel crash in "Speaker Function" mixer control
    Use snd_soc_kcontrol_codec instead of snd_kcontrol_chip
    to obtain the codec information from the kcontrol.
    
    CRs-Fixed: 2034533
    
    Change-Id: Id95dc40d5cfd2b204fa96213d78aef921c5a2006
    Signed-off-by: Aditya Bavanari <abavanar@codeaurora.org>
    Signed-off-by: Bikshapathi Kothapeta <bkotha@codeaurora.org>

--- sound/soc/msm/mdmcalifornium.c
+++ sound/soc/msm/mdmcalifornium.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -414,7 +414,7 @@ static void mdm_ext_control(struct snd_soc_codec *codec)
 static int mdm_mi2s_set_spk(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
-	struct snd_soc_codec *codec = snd_kcontrol_chip(kcontrol);
+	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
 
 	pr_debug("%s()\n", __func__);
 
