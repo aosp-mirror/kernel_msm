92f22a210987fa9a9e8689acf51f23b1a3cf7995
Gopikrishna Mogasati <gmogas@codeaurora.org>
2017-04-20 16:25:20 +0530

    diag: Fix to check for command length size
    Fix to validate payload length size while forwarding
    commands to remote processor.
    
    CRs-Fixed: 2004417
    Change-Id: Ia047f7895002409e176eeb9e1ddfef69849636ea
    Signed-off-by: Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
    Signed-off-by: Gopikrishna Mogasati <gmogas@codeaurora.org>

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -904,6 +904,11 @@ static int diag_send_raw_data_remote(int proc, void *buf, int len,
 			return -EBADMSG;
 		}
 		payload = *(uint16_t *)(buf + 2);
+		if (payload > DIAG_MAX_HDLC_BUF_SIZE) {
+			pr_err("diag: Dropping packet, payload size is %d\n",
+				payload);
+			return -EBADMSG;
+		}
 		driver->hdlc_encode_buf_len = payload;
 		/*
 		 * Adding 5 bytes for start (1 byte), version (1 byte),
