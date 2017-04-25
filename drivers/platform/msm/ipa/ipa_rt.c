4901a3a79d35e4e820e85acdd15689c89fed29e8
Utkarsh Saxena <usaxena@codeaurora.org>
2017-04-25 17:39:41 +0530

    msm: ipa: Fix for missing int overflow check in the refcount library
    Overflow of reference counter can lead to memory leak.
    
    Before incrementing the reference count, check with
    U32_MAX and return for error check.
    
    Bug: 35467471
    Change-Id: Ib96d36574ee086ec73c9836110cb2c98e8ae3d66
    Acked-by: Mohammed Javid <mjavid@qti.qualcomm.com>
    Signed-off-by: Utkarsh Saxena <usaxena@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_rt.c
+++ drivers/platform/msm/ipa/ipa_rt.c
@@ -1145,6 +1145,10 @@ int ipa_get_rt_tbl(struct ipa_ioc_get_rt_tbl *lookup)
 	mutex_lock(&ipa_ctx->lock);
 	entry = __ipa_add_rt_tbl(lookup->ip, lookup->name);
 	if (entry && entry->cookie == IPA_COOKIE) {
+		if (entry->ref_cnt == ((u32)~0U)) {
+			IPAERR("fail: ref count crossed limit\n");
+			goto ret;
+		}
 		entry->ref_cnt++;
 		lookup->hdl = entry->id;
 
@@ -1154,6 +1158,8 @@ int ipa_get_rt_tbl(struct ipa_ioc_get_rt_tbl *lookup)
 
 		result = 0;
 	}
+
+ret:
 	mutex_unlock(&ipa_ctx->lock);
 
 	return result;
