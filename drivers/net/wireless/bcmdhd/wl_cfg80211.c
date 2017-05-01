38eb8c4c3f16c9ae88ea198fbd7bac49af62cd02
joe_tsai <joe_tsai@asus.com>
2017-05-01 12:38:33 +0800

    2017-04-partner: CVE-2017-0568: A-34197514_1: net: wireless: bcmdhd: fix overrun in wl_run_escan
    prevent buffer overrun case where WLC_GET_VALID_CHANNELS IOCTL
     overriden by attacker and its return manipulated.
    
    Signed-off-by: Insun Song <insun.song@broadcom.com>
    Bug: 34197514
    
    Change-Id: Iaa09fa6e7df4dbd2a4d0f752cfbdf3175beeb5fd
    Reviewed-on: http://mcrd1-22-pc.corpnet.asus/code-review/master/295093
    Reviewed-by: joe_tsai <joe_tsai@asus.com>
    Tested-by: joe_tsai <joe_tsai@asus.com>

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -2383,6 +2383,15 @@ wl_run_escan(struct bcm_cfg80211 *cfg, struct net_device *ndev,
 			if (!wl_get_valid_channels(ndev, chan_buf, sizeof(chan_buf))) {
 				list = (wl_uint32_list_t *) chan_buf;
 				n_valid_chan = dtoh32(list->count);
+
+				if (n_valid_chan > WL_NUMCHANNELS) {
+					WL_ERR(("wrong n_valid_chan:%d\n",
+						n_valid_chan));
+					kfree(default_chan_list);
+					err = -EINVAL;
+					goto exit;
+				}
+
 				for (i = 0; i < num_chans; i++)
 				{
 #ifdef WL_HOST_BAND_MGMT
