eae8fa97c100695c2de0ec6e30e4cae7717fc709
Chris Fries <cfries@google.com>
2017-05-23 22:21:30 -0500

    ASoC: sdm845: Fixup include path
    Local paths should be included with double quotes.
    
    Signed-off-by: Chris Fries <cfries@google.com>

--- sound/soc/msm/sdm845.c
+++ sound/soc/msm/sdm845.c
@@ -31,7 +31,7 @@
 #include <sound/q6core.h>
 #include <sound/pcm_params.h>
 #include <sound/info.h>
-#include <device_event.h>
+#include "device_event.h"
 #include <linux/qdsp6v2/audio_notifier.h>
 #include "qdsp6v2/msm-pcm-routing-v2.h"
 #include "../codecs/wcd934x/wcd934x.h"
