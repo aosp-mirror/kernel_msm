f3289d2c8bd3551acd3f9fcf7ad8092e9a5774cf
Chris Fries <cfries@google.com>
2017-05-23 22:17:38 -0500

    msm: camera: Fixup include paths for compilation
    Use double quotes for local includes.
    
    Signed-off-by: Chris Fries <cfries@google.com>

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_sensor_utils/cam_sensor_util.h
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_sensor_utils/cam_sensor_util.h
@@ -18,8 +18,8 @@
 #include <linux/gpio.h>
 #include <linux/of_gpio.h>
 #include <linux/of.h>
-#include <cam_sensor_cmn_header.h>
-#include <cam_req_mgr_util.h>
+#include "cam_sensor_cmn_header.h"
+#include "cam_req_mgr_util.h"
 #include <cam_req_mgr_interface.h>
 #include <cam_mem_mgr.h>
 #include "cam_soc_util.h"
