960e96d8abe7590fd5c13f48c50b6c6c9fc9cfc3
Vijay kumar Tumati <vtumati@codeaurora.org>
2017-05-26 10:39:16 +0530

    msm: camera: sensor: Fix a bug in CCI with I2C write
    when the writes in array has continuous addresses
    
    Signed-off-by: Vijay kumar Tumati <vtumati@codeaurora.org>
    
    Bug: 37750412
    Test: Use i2c configuration containing consecutive registers
    
    Change-Id: I9a0b5f4e9c6d86aadb6ee9987a81b09c1b9afc1a

--- drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
+++ drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
@@ -403,7 +403,13 @@ static int32_t msm_cci_calc_cmd_len(struct cci_device *cci_dev,
 			if (cmd->reg_addr + 1 ==
 				(cmd+1)->reg_addr) {
 				len += data_len;
-				*pack += data_len;
+				if(len > cci_dev->payload_size){
+					len = len - data_len;
+					break;
+				}
+				else{
+					*pack += data_len;
+				}
 			} else
 				break;
 			i += data_len;
