41a82991e3fde4983c5998d16775554644a63b78
Jason Lee <leejason@codeaurora.org>
2017-05-31 11:42:18 -0700

    msm: camera: Always report real time timestamp
    Kernel will always report timestamp value in real time, also adding in
    AV timer value if necessary.
    HAL will do any necessary conversion to monotonic time.
    
    Bug: 37924594
    Change-Id: I5cd9441ced2e45888f0331db38e0d2b744f63e91
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -3943,9 +3943,9 @@ void msm_isp_process_axi_irq_stream(struct vfe_device *vfe_dev,
 
 	if (vfe_dev->vt_enable) {
 		msm_isp_get_avtimer_ts(ts);
-		time_stamp = &ts->vt_time;
-	} else {
 		time_stamp = &ts->buf_time;
+	} else {
+		time_stamp = &ts->event_time;
 	}
 
 	frame_id = vfe_dev->axi_data.
