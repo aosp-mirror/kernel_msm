01e18ed4a0014bc8022e51bd9f7f04a3868f5d40
Naveen Ramaraj <nramaraj@codeaurora.org>
2017-06-01 16:07:15 -0700

    soc: qcom: sleepstate: Enable/Disable IPCRTR wakelocks
    The acquiring of wakelock prevents the system from entering
    suspend for high frequency non-wakeup sensor requests. With
    the change IPCRTR now does not acquire a wakelock while cpu
    is out of suspend. This allows the cpu to enter suspend.
    The wakelocks are enabled in the suspend path so that future
    wakeup capable sensor events gets handled without issues.
    The wakelocks are disabled post resume from suspend.
    This change affects only the data on the SLPI edge.
    
    Bug: 38040392
    Change-Id: Iae3dafb735bfb0d3000487f2fa2216d68e9a026f
    Signed-off-by: Ramakrishnan Ganesh <ramakris@codeaurora.org>
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>

--- drivers/soc/qcom/smp2p_sleepstate.c
+++ drivers/soc/qcom/smp2p_sleepstate.c
@@ -14,6 +14,8 @@
 #include <linux/of_gpio.h>
 #include <linux/platform_device.h>
 #include <linux/suspend.h>
+#include <linux/delay.h>
+#include <linux/ipc_router.h>
 #include "smp2p_private.h"
 
 #define SET_DELAY (2 * HZ)
@@ -35,10 +37,18 @@ static int sleepstate_pm_notifier(struct notifier_block *nb,
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
 		gpio_set_value(slst_gpio_base_id + PROC_AWAKE_ID, 0);
+		/*This would be tuned. Adding sleep to allow handling
+		of any pending data */
+		msleep(25);
+		msm_ipc_router_set_ws_allowed(true);
 		break;
 
 	case PM_POST_SUSPEND:
 		gpio_set_value(slst_gpio_base_id + PROC_AWAKE_ID, 1);
+		/*This would be tuned. Adding to allow handling
+		of any pending data */
+		msleep(25);
+		msm_ipc_router_set_ws_allowed(false);
 		break;
 	}
 	return NOTIFY_DONE;
