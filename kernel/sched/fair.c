fd4bfd5132d52a75a4ae35d8fcaaa89721583d72
Joel Fernandes <joelaf@google.com>
2017-06-13 17:48:29 -0700

    Revert "ANDROID: sched/rt: rt cpu selection integration with EAS."
    These patches were seen to be problematic on marlin and we reverted it
    there. Doing the same on wahoo.
    
    Test: Boot
    Bug: 62595294
    
    This reverts commit 5830ad897e9757d8925b62921000eb8499549de9.
    
    Change-Id: Ic5600b89d7798af576118b7021003834cbfce94a
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5750,6 +5750,17 @@ static int wake_affine(struct sched_domain *sd, struct task_struct *p,
 	return 1;
 }
 
+static inline unsigned long task_util(struct task_struct *p)
+{
+#ifdef CONFIG_SCHED_WALT
+	if (!walt_disabled && sysctl_sched_use_walt_task_util) {
+		unsigned long demand = p->ravg.demand;
+		return (demand << 10) / walt_ravg_window;
+	}
+#endif
+	return p->se.avg.util_avg;
+}
+
 static inline unsigned long boosted_task_util(struct task_struct *task);
 
 static inline bool __task_fits(struct task_struct *p, int cpu, int util)
