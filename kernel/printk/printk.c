0d6871912b60295b28c35d99eab904f73923d642
Siqi Lin <siqilin@google.com>
2017-06-15 16:35:20 -0700

    ANDROID: kernel: printk: Add cpu and pid to printk log prefix
    Add CPU number and process ID to printk log messages. This information
    is useful to get a timeline of what was running on a CPU before a
    watchdog.
    
    Based on commit bf1f48d03d41 ("kernel: printk: Add cpu and pid into log prefix")
    from android-msm-marlin-3.18.
    
    Bug: 62670012
    Change-Id: I7c0119f22db08638f4da102927215bbe03120b3f
    Signed-off-by: Siqi Lin <siqilin@google.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -236,11 +236,15 @@ struct printk_log {
 	u8 facility;		/* syslog facility */
 	u8 flags:5;		/* internal record flags */
 	u8 level:3;		/* syslog level */
+	unsigned int cpu;
+	pid_t pid;
 }
 #ifdef CONFIG_HAVE_EFFICIENT_UNALIGNED_ACCESS
 __packed __aligned(4)
 #endif
 ;
+
+static struct printk_log *last_msg;
 
 /*
  * The logbuf_lock protects kmsg buffer, indices, counters.  This can be taken
@@ -478,6 +482,9 @@ static int log_store(int facility, int level,
 	/* insert message */
 	log_next_idx += msg->len;
 	log_next_seq++;
+
+	/* record last_msg for extension */
+	last_msg = msg;
 
 	return msg->text_len;
 }
@@ -1057,6 +1064,43 @@ static size_t print_time(u64 ts, char *buf)
 		       (unsigned long)ts, rem_nsec / 1000);
 }
 
+static bool printk_cpu = IS_ENABLED(CONFIG_PRINTK_CPU_ID);
+module_param_named(cpu, printk_cpu, bool, S_IRUGO | S_IWUSR);
+
+static bool printk_pid = IS_ENABLED(CONFIG_PRINTK_PID);
+module_param_named(pid, printk_pid, bool, S_IRUGO | S_IWUSR);
+
+static size_t print_cpu(unsigned int cpu, char *buf)
+{
+	if (!printk_cpu)
+		return 0;
+
+	if (!buf)
+		return snprintf(NULL, 0, "c%u ", cpu);
+
+	return sprintf(buf, "c%u ", cpu);
+}
+
+static size_t print_pid(pid_t pid, char *buf)
+{
+	if (!printk_pid)
+		return 0;
+
+	if (!buf)
+		return snprintf(NULL, 0, "%6u ", pid);
+
+	return sprintf(buf, "%6u ", pid);
+}
+
+static void update_msg_ext(unsigned int cpu, pid_t pid)
+{
+	if (!last_msg)
+		return;
+
+	last_msg->cpu = cpu;
+	last_msg->pid = pid;
+}
+
 static size_t print_prefix(const struct printk_log *msg, bool syslog, char *buf)
 {
 	size_t len = 0;
@@ -1077,6 +1121,8 @@ static size_t print_prefix(const struct printk_log *msg, bool syslog, char *buf)
 	}
 
 	len += print_time(msg->ts_nsec, buf ? buf + len : NULL);
+	len += print_cpu(msg->cpu, buf ? buf + len : NULL);
+	len += print_pid(msg->pid, buf ? buf + len : NULL);
 	return len;
 }
 
@@ -1568,6 +1614,8 @@ static struct cont {
 	u8 facility;			/* log facility of first message */
 	enum log_flags flags;		/* prefix, newline flags */
 	bool flushed:1;			/* buffer sealed and committed */
+	unsigned int cpu;
+	pid_t pid;
 } cont;
 
 static void cont_flush(enum log_flags flags)
@@ -1587,6 +1635,7 @@ static void cont_flush(enum log_flags flags)
 			  cont.ts_nsec, NULL, 0, cont.buf, cont.len);
 		cont.flags = flags;
 		cont.flushed = true;
+		update_msg_ext(cont.cpu, cont.pid);
 	} else {
 		/*
 		 * If no fragment of this line ever reached the console,
@@ -1594,6 +1643,7 @@ static void cont_flush(enum log_flags flags)
 		 */
 		log_store(cont.facility, cont.level, flags, 0,
 			  NULL, 0, cont.buf, cont.len);
+		update_msg_ext(cont.cpu, cont.pid);
 		cont.len = 0;
 	}
 }
@@ -1621,6 +1671,8 @@ static bool cont_add(int facility, int level, const char *text, size_t len)
 		cont.flags = 0;
 		cont.cons = 0;
 		cont.flushed = false;
+		cont.cpu = smp_processor_id();
+		cont.pid = current->pid;
 	}
 
 	memcpy(cont.buf + cont.len, text, len);
@@ -1720,6 +1772,7 @@ asmlinkage int vprintk_emit(int facility, int level,
 		printed_len += log_store(0, 2, LOG_PREFIX|LOG_NEWLINE, 0,
 					 NULL, 0, recursion_msg,
 					 strlen(recursion_msg));
+		update_msg_ext(logbuf_cpu, current->pid);
 	}
 
 	/*
@@ -1779,10 +1832,12 @@ asmlinkage int vprintk_emit(int facility, int level,
 		/* buffer line if possible, otherwise store it right away */
 		if (cont_add(facility, level, text, text_len))
 			printed_len += text_len;
-		else
+		else {
 			printed_len += log_store(facility, level,
 						 lflags | LOG_CONT, 0,
 						 dict, dictlen, text, text_len);
+			update_msg_ext(logbuf_cpu, current->pid);
+		}
 	} else {
 		bool stored = false;
 
@@ -1803,9 +1858,11 @@ asmlinkage int vprintk_emit(int facility, int level,
 
 		if (stored)
 			printed_len += text_len;
-		else
+		else {
 			printed_len += log_store(facility, level, lflags, 0,
 						 dict, dictlen, text, text_len);
+			update_msg_ext(logbuf_cpu, current->pid);
+		}
 	}
 
 	logbuf_cpu = UINT_MAX;
