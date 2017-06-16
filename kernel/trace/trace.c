6a0c1d496aeb8c81c5fcf4a46634f9bcb554a33d
Joel Fernandes <joelaf@google.com>
2017-06-16 15:11:29 -0700

    trace: Make sure saved_tgids array is resized
    Kernel bug causes saved_tgids to be overwritten. Resize saved_tgids when
    resize is requested to avoid memory corruption.
    
    Bug: 62669736
    
    Change-Id: I9157506c9d835b9dae854595a1d7ef5b2fb3967d
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -1288,7 +1288,6 @@ void tracing_reset_all_online_cpus(void)
 
 #define SAVED_CMDLINES_DEFAULT 128
 #define NO_CMDLINE_MAP UINT_MAX
-static unsigned saved_tgids[SAVED_CMDLINES_DEFAULT];
 static arch_spinlock_t trace_cmdline_lock = __ARCH_SPIN_LOCK_UNLOCKED;
 struct saved_cmdlines_buffer {
 	unsigned map_pid_to_cmdline[PID_MAX_DEFAULT+1];
@@ -1296,6 +1295,7 @@ struct saved_cmdlines_buffer {
 	unsigned cmdline_num;
 	int cmdline_idx;
 	char *saved_cmdlines;
+	int *saved_tgids;
 };
 static struct saved_cmdlines_buffer *savedcmd;
 
@@ -1322,6 +1322,13 @@ static int allocate_cmdlines_buffer(unsigned int val,
 
 	s->saved_cmdlines = kmalloc(val * TASK_COMM_LEN, GFP_KERNEL);
 	if (!s->saved_cmdlines) {
+		kfree(s->map_cmdline_to_pid);
+		return -ENOMEM;
+	}
+
+	s->saved_tgids = kmalloc(val * sizeof(*s->saved_tgids), GFP_KERNEL);
+	if (!s->saved_tgids) {
+		kfree(s->saved_cmdlines);
 		kfree(s->map_cmdline_to_pid);
 		return -ENOMEM;
 	}
@@ -1527,7 +1534,7 @@ static int trace_save_cmdline(struct task_struct *tsk)
 	}
 
 	set_cmdline(idx, tsk->comm);
-	saved_tgids[idx] = tsk->tgid;
+	savedcmd->saved_tgids[idx] = tsk->tgid;
 	arch_spin_unlock(&trace_cmdline_lock);
 
 	return 1;
@@ -1579,7 +1586,7 @@ int trace_find_tgid(int pid)
 	arch_spin_lock(&trace_cmdline_lock);
 	map = savedcmd->map_pid_to_cmdline[pid];
 	if (map != NO_CMDLINE_MAP)
-		tgid = saved_tgids[map];
+		tgid = savedcmd->saved_tgids[map];
 	else
 		tgid = -1;
 
@@ -3895,6 +3902,7 @@ tracing_saved_cmdlines_size_read(struct file *filp, char __user *ubuf,
 static void free_saved_cmdlines_buffer(struct saved_cmdlines_buffer *s)
 {
 	kfree(s->saved_cmdlines);
+	kfree(s->saved_tgids);
 	kfree(s->map_cmdline_to_pid);
 	kfree(s);
 }
