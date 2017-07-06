8c8f57a62b1f0c4191a5f8fc7d4b685ab4d6e3f0
Joel Fernandes <joelaf@google.com>
2017-07-06 10:57:53 -0700

    ANDROID: tracing/ring_buffer: Try harder to allocate
    Ftrace fails to allocate large buffers on walleye/taimen due to large
    number of CPUs coupled with effects of large amounts of caching in the
    page cache.  Currently the ring buffer allocation doesn't retry after
    direct-reclaim made some progress but still wasn't able to find a free
    page. On retrying I see that the allocations almost always succeed.  The
    retry doesn't happen because GFP_NORETRY is used in the tracer to
    prevent the case where we might OOM, however this is incorrect in the
    sense a RETRY doesn't necessarily mean it will OOM (as seen in my
    testing, most of the time the retry just needs to loop back and find a
    free page). In order to retry but still prevent the OOM, I re-use the
    PF_DUMPCORE flag which bails out incase we are about to OOM.
    
    Test:
    * systrace -b 64000 and -b 100000 is working after dropping GFP_NORETRY.
    * Used memeater to use 3GB memory and -b 64000 and saw that OOM didn't
    trigger with PF_DUMPCORE (and triggered without PF_DUMPCORE).
    
    Bug: 62918608
    Change-Id: I85de23e843d2a4fdf2276905b1ae8af4486b3bd7
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -1142,15 +1142,12 @@ static int __rb_allocate_pages(long nr_pages, struct list_head *pages, int cpu)
 	struct buffer_page *bpage, *tmp;
 	long i;
 
+	/* Bail out before going to OOM */
+	current->flags |= PF_DUMPCORE;
 	for (i = 0; i < nr_pages; i++) {
 		struct page *page;
-		/*
-		 * __GFP_NORETRY flag makes sure that the allocation fails
-		 * gracefully without invoking oom-killer and the system is
-		 * not destabilized.
-		 */
 		bpage = kzalloc_node(ALIGN(sizeof(*bpage), cache_line_size()),
-				    GFP_KERNEL | __GFP_NORETRY,
+				    GFP_KERNEL,
 				    cpu_to_node(cpu));
 		if (!bpage)
 			goto free_pages;
@@ -1158,16 +1155,18 @@ static int __rb_allocate_pages(long nr_pages, struct list_head *pages, int cpu)
 		list_add(&bpage->list, pages);
 
 		page = alloc_pages_node(cpu_to_node(cpu),
-					GFP_KERNEL | __GFP_NORETRY, 0);
+					GFP_KERNEL, 0);
 		if (!page)
 			goto free_pages;
 		bpage->page = page_address(page);
 		rb_init_page(bpage->page);
 	}
 
+	current->flags &= ~PF_DUMPCORE;
 	return 0;
 
 free_pages:
+	current->flags &= ~PF_DUMPCORE;
 	list_for_each_entry_safe(bpage, tmp, pages, list) {
 		list_del_init(&bpage->list);
 		free_buffer_page(bpage);
