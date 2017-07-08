8ee38cf0dfeb89d928e0fc9b7f49612de7e896bb
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2017-07-07 18:30:54 -0700

    smb-lib: fix usbin_icl_change interrupt storm
    We observed an interrupt storm on that interrupt
    even after the usb path is suspended.
    
    So keep this interrupt disabled as long as the usb
    path is suspended.
    
    Note that since the current limit votes can come in
    prior to interrupt registration, rerun the election
    on the votable after interrupts are registered.
    
    Change-Id: Ib43b071b7b0decf8e50d27d585bea04b727f9475
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/power/supply/qcom/smb-lib.h
+++ drivers/power/supply/qcom/smb-lib.h
@@ -353,6 +353,7 @@ struct smb_charger {
 	int			fake_input_current_limited;
 	bool			pr_swap_in_progress;
 	int			typec_mode;
+	int			usb_icl_change_irq_enabled;
 
 	int			fake_port_temp;
 	bool			port_overheat;
