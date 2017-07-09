fc4ac15a006ba23eda0b898739a466ac968eaff6
Andres Oportus <andresoportus@google.com>
2017-07-09 13:09:48 -0700

    ANDROID: cpufreq: stats: add per task/uid/freq/cluster stats
    Adds:
    -Per task nodes in /proc/PID/time_in_state showing per
    frequency(state)/cluster(policy) times
    -Global /proc/uid_time_in_state showing per uid/frequency/cluster
    times including uid removal from /proc/uid_cputime/remove_uid_range
    -Global all_time_in_state showing per frequency/cluster/cpu times
    
    Bug: 62303881
    Bug: 62340637
    
    Tests: Reading /proc/$$/time_in_state, /proc/uid_time_in_state, and
    /sys/devices/system/cpu/cpufreq/all_time_in_state. Writing
    /proc/uid_cputime/remove_uid_range
    
    Change-Id: I222843b6f714717da9377817c19ac0d5400cb41a
    Signed-off-by: Andres Oportus <andresoportus@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -75,6 +75,7 @@
 #include <linux/binfmts.h>
 #include <linux/context_tracking.h>
 #include <linux/compiler.h>
+#include <linux/cpufreq.h>
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
@@ -2176,6 +2177,10 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 
 #ifdef CONFIG_SCHEDSTATS
 	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
+#endif
+
+#ifdef CONFIG_CPU_FREQ_STAT
+	cpufreq_task_stats_init(p);
 #endif
 
 	RB_CLEAR_NODE(&p->dl.rb_node);
