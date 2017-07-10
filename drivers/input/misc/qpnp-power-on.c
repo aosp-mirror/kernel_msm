0962206e7b4c6c52e525364f005c79ef0838dbbf
Patrick Tjin <pattjin@google.com>
2017-07-10 09:52:29 -0700

    qpnp-power-on: add old state to debug message
    Add old state to detect if a simulated keypress would be
    generated to aid in debugging.
    
    Bug: 38461731
    Change-Id: I471dd7b72dabaffe51b57bf87813440b350a6654
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- drivers/input/misc/qpnp-power-on.c
+++ drivers/input/misc/qpnp-power-on.c
@@ -811,8 +811,9 @@ qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 		return -EINVAL;
 	}
 
-	pr_info("PMIC input: code=%d, sts=0x%hhx, bit=0x%hhx, type=%d\n",
-		cfg->key_code, pon_rt_sts, pon_rt_bit, cfg->pon_type);
+	pr_info("PMIC input: code=%d, sts=0x%hhx, bit=0x%hhx, type=%d, os=%d\n",
+		cfg->key_code, pon_rt_sts, pon_rt_bit, cfg->pon_type,
+		cfg->old_state);
 	key_status = pon_rt_sts & pon_rt_bit;
 
 	if (pon->report_key) {
