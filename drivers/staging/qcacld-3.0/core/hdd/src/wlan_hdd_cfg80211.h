6eb5169ca62617709a93add0ba21b41c1229cc3b
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-07-11 11:32:13 -0700

    qcacld-3.0: Advertize set tx power limit capability
    Advertize set tx power limit capability.
    
    CRs-Fixed: 2074653
    Bug: 62437848

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.h
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.h
@@ -2477,6 +2477,7 @@ enum qca_wlan_vendor_attr_sap_conditional_chan_switch {
 #define WIFI_FEATURE_CONTROL_ROAMING    0x800000  /* Enable/Disable roaming */
 #define WIFI_FEATURE_IE_WHITELIST       0x1000000 /* Support Probe IE white listing */
 #define WIFI_FEATURE_SCAN_RAND          0x2000000 /* Support MAC & Probe Sequence Number randomization */
+#define WIFI_FEATURE_SET_TX_POWER_LIMIT 0x4000000 /* Support Tx Power Limit setting */
 
 /**
  * enum wifi_logger_supported_features - values for supported logger features
