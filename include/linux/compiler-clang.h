e6170f1460f0c565947c6344a0da9b294ace04c3
David Rientjes <rientjes@google.com>
2017-07-12 14:42:23 -0700

    BACKPORT: compiler, clang: suppress warning for unused static inline functions
    GCC explicitly does not warn for unused static inline functions for
    -Wunused-function.  The manual states:
    
    Warn whenever a static function is declared but not defined or a
    non-inline static function is unused.
    
    Clang does warn for static inline functions that are unused.
    
    It turns out that suppressing the warnings avoids potentially
    complex #ifdef directives, which also reduces LOC.
    
    Suppress the warning for clang.
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Bug: 32455818
    BUg: 33244601
    Change-Id: I31a7ff4b828538a66d439203be8965c3d53e6763
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -10,3 +10,10 @@
 #undef uninitialized_var
 #define uninitialized_var(x) x = *(&(x))
 #endif
+
+/*
+* GCC does not warn about unused static inline functions for
+* -Wunused-function.  This turns out to avoid the need for complex #ifdef
+* directives.  Suppress the warning in clang as well.
+*/
+#define inline inline __attribute__((unused))
