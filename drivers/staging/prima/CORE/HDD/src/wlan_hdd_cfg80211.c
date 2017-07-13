522ffcd46579c48c8453fa232319ac79c17b4bc5
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2017-07-13 18:58:59 +0530

    wlan: Fix Uninitialized memory issue
    Propagation from qcacld-2.0 to prima.
    
    There is a possibility to read uninitialized memory within api
    __wlan_hdd_cfg80211_testmode.
    To resolve this issue, initilaize buffer hb_params with zero.
    
    Change-Id: Ia8061610a8c35aa7290177c0dcd2c5c36d9fcb35
    CRs-Fixed: 2075967

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -16734,6 +16734,7 @@ static int __wlan_hdd_cfg80211_testmode(struct wiphy *wiphy, void *data, int len
                 return -EINVAL;
             }
 
+            vos_mem_zero(hb_params, sizeof(tSirLPHBReq));
             vos_mem_copy(hb_params, buf, buf_len);
             smeStatus = sme_LPHBConfigReq((tHalHandle)(pHddCtx->hHal),
                                hb_params,
