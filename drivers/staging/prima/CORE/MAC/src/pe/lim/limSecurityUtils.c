89dae2992f3a641c1a955e26ab2236932180e45b
Sridhar Selvaraj <sselvara@codeaurora.org>
2017-07-18 11:51:34 +0530

    wlan: Update limComputeCrc32 to pass uint16_t
    qcacld-3.0 to prima propagation
    
    Update limComputeCrc32() to pass uint16_t as a length type.
    Currently uint8_t is being passed as length and there will be type
    mismatch when authentication frame to be encrypted will be larger
    than 255 bytes.
    
    Change-Id: Ic009197c13a2d70c9015a184acff2e82bf80eaba
    CRs-Fixed: 2076603

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limSecurityUtils.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limSecurityUtils.c
@@ -666,7 +666,7 @@ limEncryptAuthFrame(tpAniSirGlobal pMac, tANI_U8 keyId, tANI_U8 *pKey, tANI_U8 *
  */
 
 void
-limComputeCrc32(tANI_U8 *pDest, tANI_U8 * pSrc, tANI_U8 len)
+limComputeCrc32(tANI_U8 *pDest, tANI_U8 * pSrc, tANI_U16 len)
 {
     tANI_U32 crc;
     int i;
