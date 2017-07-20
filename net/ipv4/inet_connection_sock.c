48fefe535283b2cacc8ff77553de1e52136e240d
y00230200 <y00230200@notesmail.huawei.com>
2017-07-20 11:05:28 +0800

    dccp/tcp: do not inherit mc_list from parent
    CVE-2017-8890
    
    syzkaller found a way to trigger double frees from ip_mc_drop_socket()
    
    It turns out that leave a copy of parent mc_list at accept() time,
    which is very bad.
    (cherry picked from commit 265825f83654bee15b2655d57f8fdd8068cb7c05)
    
    Change-Id: If53e8622bb94c9896f8897591e9757df33588c56

--- net/ipv4/inet_connection_sock.c
+++ net/ipv4/inet_connection_sock.c
@@ -689,6 +689,7 @@ struct sock *inet_csk_clone_lock(const struct sock *sk,
 		inet_sk(newsk)->inet_num = ntohs(inet_rsk(req)->loc_port);
 		inet_sk(newsk)->inet_sport = inet_rsk(req)->loc_port;
 		newsk->sk_write_space = sk_stream_write_space;
+		inet_sk(newsk)->mc_list = NULL;
 
 		newsk->sk_mark = inet_rsk(req)->ir_mark;
 
