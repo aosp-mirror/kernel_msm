067b10c28c22284f741c1584fe832c21e5aafec8
Sunil Khatri <sunilkh@codeaurora.org>
2017-07-25 16:54:27 +0530

    msm: kgsl: Protect the event->handle with spinlock
    event->handle pointer can be used after free due to
    the race condition between kgsl_sync_callback and
    kgsl_sync_fence_async_cancel.
    
    Protect the event->handle with a spinlock to
    avoid concurrent access issues.
    
    Change-Id: I3719e401af9ece82ac68b72f2aef784c7fdc1104
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>

--- drivers/gpu/msm/kgsl_cmdbatch.h
+++ drivers/gpu/msm/kgsl_cmdbatch.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2015, 2017 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -86,6 +86,7 @@ struct kgsl_cmdbatch {
  * @context: Pointer to the KGSL context that owns the cmdbatch
  * @timestamp: Pending timestamp for the event
  * @handle: Pointer to a sync fence handle
+ * @handle_lock: Spin lock to protect handle
  * @device: Pointer to the KGSL device
  */
 struct kgsl_cmdbatch_sync_event {
@@ -95,6 +96,7 @@ struct kgsl_cmdbatch_sync_event {
 	struct kgsl_context *context;
 	unsigned int timestamp;
 	struct kgsl_sync_fence_waiter *handle;
+	spinlock_t handle_lock;
 	struct kgsl_device *device;
 };
 
