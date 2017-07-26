a88c301adbf64874d09e786e1f1544df18bb8996
Nicholas Troast <ntroast@codeaurora.org>
2017-07-26 14:17:34 -0700

    power: qpnp-smb2: expose CHARGE_COUNTER prop via batt psy
    CHARGE_COUNTER is a required battery power supply property. Add it.
    
    Change-Id: Ieb9c64447f88213479ce14b738f71fa703ffc194
    Signed-off-by: Nicholas Troast <ntroast@codeaurora.org>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -942,6 +942,7 @@ static enum power_supply_property smb2_batt_props[] = {
 	POWER_SUPPLY_PROP_DIE_HEALTH,
 	POWER_SUPPLY_PROP_RERUN_AICL,
 	POWER_SUPPLY_PROP_DP_DM,
+	POWER_SUPPLY_PROP_CHARGE_COUNTER,
 	POWER_SUPPLY_PROP_IPD,
 };
 
@@ -1047,6 +1048,9 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_RERUN_AICL:
 		val->intval = 0;
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_COUNTER:
+		rc = smblib_get_prop_batt_charge_counter(chg, val);
 		break;
 	case POWER_SUPPLY_PROP_IPD:
 		rc = smblib_get_ipd(chg, val);
