e10b6cd81d1aecf520226001520b47fcb143e6d1
Mark Salyzyn <salyzyn@google.com>
2017-08-04 09:58:40 -0700

    FROMLIST: [PATCH v5 10/12] arm64: vdso: replace gettimeofday.S with global vgettimeofday.C
    (cherry picked from url https://patchwork.kernel.org/patch/10044501/)
    
    Take an effort from the previous 9 patches to recode the arm64 vdso
    code from assembler to C previously submitted by
    Andrew Pinski <apinski@cavium.com>, rework it for use in both arm and
    arm64, overlapping any optimizations for each architecture. But
    instead of landing it in arm64, land the result into lib/vdso and
    unify both implementations to simplify future maintenance.
    
    apinski@cavium.com makes the following claims in the original patch:
    
    This allows the compiler to optimize the divide by 1000 and remove
    the other divides.
    
    On ThunderX, gettimeofday improves by 32%.  On ThunderX 2,
    gettimeofday improves by 18%.
    
    Note I noticed a bug in the old (arm64) implementation of
    __kernel_clock_getres; it was checking only the lower 32bits of the
    pointer; this would work for most cases but could fail in a few.
    
    <end of claim>
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Dmitry Safonov <dsafonov@virtuozzo.com>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Laura Abbott <labbott@redhat.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Andy Gross <andy.gross@linaro.org>
    Cc: Kevin Brodsky <kevin.brodsky@arm.com>
    Cc: Andrew Pinski <apinski@cavium.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-arm-kernel@lists.infradead.org
    Bug: 63737556
    Bug: 20045882
    Change-Id: I71ff27ff5bfa323354fda6867b01ec908d8d6cbd

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -265,7 +265,7 @@ void update_vsyscall(struct timekeeper *tk)
 						   tk->tkr_raw.shift) +
 						  tk->tkr_raw.xtime_nsec;
 		vdso_data->xtime_clock_sec	= tk->xtime_sec;
-		vdso_data->xtime_clock_nsec	= tk->tkr_mono.xtime_nsec;
+		vdso_data->xtime_clock_snsec	= tk->tkr_mono.xtime_nsec;
 		vdso_data->cs_mono_mult		= tk->tkr_mono.mult;
 		vdso_data->cs_raw_mult		= tk->tkr_raw.mult;
 		/* tkr_mono.shift == tkr_raw.shift */
