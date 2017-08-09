bb234bffdc01d81d338104c2349b08faba19554b
Madhuri Athota <mathota@codeaurora.org>
2017-08-09 14:56:53 +0530

    msm: kgsl: Add disable-wake-on-touch devicetree property
    Add a devicetree "disable-wake-on-touch" property support
    to disable GPU wake up on touch input events. This will
    help save power in case of unintended taps and swipes,
    for example, when the screen is wet.
    
    Bug : 63865835
    
    Change-Id: I35768dc78c473272472aaf9c0e09e66d75817b2c
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>
    Signed-off-by: Madhuri Athota <mathota@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.h
+++ drivers/gpu/msm/kgsl_pwrctrl.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -133,6 +133,7 @@ struct kgsl_regulator {
  * @pm_qos_req_dma - the power management quality of service structure
  * @pm_qos_active_latency - allowed CPU latency in microseconds when active
  * @pm_qos_wakeup_latency - allowed CPU latency in microseconds during wakeup
+ * @input_disable - To disable GPU wakeup on touch input event
  * @bus_control - true if the bus calculation is independent
  * @bus_mod - modifier from the current power level for the bus vote
  * @bus_percent_ab - current percent of total possible bus usage
@@ -184,6 +185,7 @@ struct kgsl_pwrctrl {
 	struct pm_qos_request pm_qos_req_dma;
 	unsigned int pm_qos_active_latency;
 	unsigned int pm_qos_wakeup_latency;
+	bool input_disable;
 	bool bus_control;
 	int bus_mod;
 	unsigned int bus_percent_ab;
