b4378a71132909014bcd79a431313ed3eccbf08b
John Muir <muirj@google.com>
2017-08-23 10:01:04 -0700

    arm-smccc: Move smccc invocation definitions to resolve errors.
    Move definition of __invoke_psci_fn_hvc and __invoke_psci_fn_smc
    into include/linux/arm-scmcc.h in order to resolve compile errors
    introduced in a previous change.
    
    Change-Id: I2c49a7ccb6e5739e04c88433ae3b94693033ff57
    Signed-off-by: John Muir <muirj@google.com>

--- drivers/firmware/psci.c
+++ drivers/firmware/psci.c
@@ -109,26 +109,6 @@ bool psci_power_state_is_valid(u32 state)
 	return !(state & ~valid_mask);
 }
 
-static unsigned long __invoke_psci_fn_hvc(unsigned long function_id,
-			unsigned long arg0, unsigned long arg1,
-			unsigned long arg2)
-{
-	struct arm_smccc_res res;
-
-	arm_smccc_hvc(function_id, arg0, arg1, arg2, 0, 0, 0, 0, &res);
-	return res.a0;
-}
-
-static unsigned long __invoke_psci_fn_smc(unsigned long function_id,
-			unsigned long arg0, unsigned long arg1,
-			unsigned long arg2)
-{
-	struct arm_smccc_res res;
-
-	arm_smccc_smc(function_id, arg0, arg1, arg2, 0, 0, 0, 0, &res);
-	return res.a0;
-}
-
 static int psci_to_linux_errno(int errno)
 {
 	switch (errno) {
--- include/linux/arm-smccc.h
+++ include/linux/arm-smccc.h
@@ -101,4 +101,25 @@ asmlinkage void arm_smccc_hvc(unsigned long a0, unsigned long a1,
 			unsigned long a5, unsigned long a6, unsigned long a7,
 			struct arm_smccc_res *res);
 
+
+static inline unsigned long __invoke_psci_fn_hvc(unsigned long function_id,
+			unsigned long arg0, unsigned long arg1,
+			unsigned long arg2)
+{
+	struct arm_smccc_res res;
+
+	arm_smccc_hvc(function_id, arg0, arg1, arg2, 0, 0, 0, 0, &res);
+	return res.a0;
+}
+
+static inline unsigned long __invoke_psci_fn_smc(unsigned long function_id,
+			unsigned long arg0, unsigned long arg1,
+			unsigned long arg2)
+{
+	struct arm_smccc_res res;
+
+	arm_smccc_smc(function_id, arg0, arg1, arg2, 0, 0, 0, 0, &res);
+	return res.a0;
+}
+
 #endif /*__LINUX_ARM_SMCCC_H*/
