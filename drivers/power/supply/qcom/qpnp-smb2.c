e5e6690adeb074d05c8c76414db9276d6ad6c043
John Muir <muirj@google.com>
2017-09-01 11:42:49 -0700

    power: qpnp-smb2: Move Vega temperature range change.
    Old code was running before the driver's mutexes were
    initialized.
    
    Bug: 65291242
    Test: Kernel boots. Verified that register settings match
          expectations after boot has completed.
    
    Change-Id: Iddb2322bdf99f509109a8d8cd706d91277d296ab
    Signed-off-by: John Muir <muirj@google.com>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -321,13 +321,6 @@ static int smb2_parse_dt(struct smb2 *chip)
 					&chg->otg_delay_ms);
 	if (rc < 0)
 		chg->otg_delay_ms = OTG_DEFAULT_DEGLITCH_TIME_MS;
-
-	/* config vega model: the JEITA_CCCOMP_CFG_REG, FVCOMP regs ( compensate
-	current -1600mA,voltage -200mV ) in jeita Tcold,Thot temperature range. */
-	if (of_property_read_bool(node, "lenovo,vega-model")) {
-		smblib_write(chg, JEITA_FVCOMP_CFG_REG, 0x1A);
-		smblib_write(chg, JEITA_CCCOMP_CFG_REG, 0x3F);
-	}
 	return 0;
 }
 
@@ -2345,6 +2338,13 @@ static int smb2_probe(struct platform_device *pdev)
 		goto cleanup;
 	}
 	batt_charge_type = val.intval;
+
+	/* config vega model: the JEITA_CCCOMP_CFG_REG, FVCOMP regs ( compensate
+	current -1600mA,voltage -200mV ) in jeita Tcold,Thot temperature range. */
+	if (of_property_read_bool(chg->dev->of_node, "lenovo,vega-model")) {
+		smblib_write(chg, JEITA_FVCOMP_CFG_REG, 0x1A);
+		smblib_write(chg, JEITA_CCCOMP_CFG_REG, 0x3F);
+	}
 
 	device_init_wakeup(chg->dev, true);
 
