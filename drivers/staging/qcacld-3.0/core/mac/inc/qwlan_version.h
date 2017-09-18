78e0d695dee58b217d9766712f55d176a466137c
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-09-18 12:05:56 -0700

    qcacld-3.0: Release 5.1.3.01H
    Release 5.1.3.01H.
    
    Change-Id: Idcb14ea90451491154f3afc800b8fe4e490c2da1
    CRs-Fixed: 774533
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
+++ drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
@@ -41,9 +41,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            3
-#define QWLAN_VERSION_EXTRA            "G"
+#define QWLAN_VERSION_EXTRA            "H"
 #define QWLAN_VERSION_BUILD            01
 
-#define QWLAN_VERSIONSTR               "5.1.3.01G"
+#define QWLAN_VERSIONSTR               "5.1.3.01H"
 
 #endif /* QWLAN_VERSION_H */
