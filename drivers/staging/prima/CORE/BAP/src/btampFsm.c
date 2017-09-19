6dc228bbd3fb2cc3343cf0ea304a1886fc12de6c
Jeff Johnson <jjohnson@codeaurora.org>
2017-09-19 08:38:56 -0700

    wlan: bap: Replace instances of unadorned %p
    Replace instances of unadorned %p in CORE/BAP.
    
    Change-Id: Ia7a4b72b425f0f453b9d2b47917153e975667f96
    CRs-Fixed: 2111275

--- drivers/staging/prima/CORE/BAP/src/btampFsm.c
+++ drivers/staging/prima/CORE/BAP/src/btampFsm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2014, 2017 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -191,7 +191,7 @@ bapSetKey( v_PVOID_t pvosGCtx, tCsrRoamSetKey *pSetKeyInfo )
         return VOS_STATUS_E_FAULT;
     }
 
-    VOS_TRACE( VOS_MODULE_ID_BAP, VOS_TRACE_LEVEL_INFO_HIGH, "%s: btampContext value: %p", __func__,  btampContext);
+    VOS_TRACE( VOS_MODULE_ID_BAP, VOS_TRACE_LEVEL_INFO_HIGH, "%s: btampContext value: %pK", __func__,  btampContext);
 
     /* Fill in the event structure */ 
     bapEvent.event = eWLAN_BAP_RSN_SUCCESS;
@@ -1398,7 +1398,7 @@ signalHCIPhysLinkDiscEvent
 #ifdef BAP_DEBUG
   /* Trace the tBtampCtx being passed in. */
   VOS_TRACE( VOS_MODULE_ID_BAP, VOS_TRACE_LEVEL_INFO_HIGH,
-            "WLAN BAP Context Monitor: btampContext value = %p in %s:%d", btampContext, __func__, __LINE__ );
+            "WLAN BAP Context Monitor: btampContext value = %pK in %s:%d", btampContext, __func__, __LINE__ );
 #endif //BAP_DEBUG
 
     /* Loop disconnecting all Logical Links on this Physical Link */
