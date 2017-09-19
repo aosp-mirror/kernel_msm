350ab55d5513dfc2f62f97a60f0fe4cbb085b3c5
Jeff Johnson <jjohnson@codeaurora.org>
2017-09-19 08:35:23 -0700

    wlan: sme: Replace instances of unadorned %p
    Replace instances of unadorned %p in CORE/SME.
    
    Change-Id: I8176263cf21ec7281dd5ebbaecbe1c8568465a37
    CRs-Fixed: 2111275

--- drivers/staging/prima/CORE/SME/src/QoS/sme_Qos.c
+++ drivers/staging/prima/CORE/SME/src/QoS/sme_Qos.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2014, 2017 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -1418,7 +1418,7 @@ sme_QosStatusType sme_QosInternalSetupReq(tpAniSirGlobal pMac,
          pentry->tspec_mask = pACInfo->tspec_mask_status;
          pentry->QoSInfo = Tspec_Info;
          VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
-                   "%s: %d: Creating entry on session %d at %p with flowID %d",
+                   "%s: %d: Creating entry on session %d at %pK with flowID %d",
                    __func__, __LINE__,
                    sessionId, pentry, QosFlowID);
          csrLLInsertTail(&sme_QosCb.flow_list, &pentry->link, VOS_TRUE);
@@ -1748,7 +1748,7 @@ sme_QosStatusType sme_QosInternalSetupReq(tpAniSirGlobal pMac,
          pentry->tspec_mask = tmask;
          pentry->QoSInfo = Tspec_Info;
          VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
-                   "%s: %d: On session %d creating entry at %p with flowID %d",
+                   "%s: %d: On session %d creating entry at %pK with flowID %d",
                    __func__, __LINE__,
                    sessionId, pentry, QosFlowID);
          csrLLInsertTail(&sme_QosCb.flow_list, &pentry->link, VOS_TRUE);
@@ -2009,7 +2009,7 @@ sme_QosStatusType sme_QosInternalModifyReq(tpAniSirGlobal pMac,
       flow_info->reason = SME_QOS_REASON_MODIFY;
       VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
                 "%s: %d: On session %d creating modified "
-                "entry at %p with flowID %d",
+                "entry at %pK with flowID %d",
                 __func__, __LINE__,
                 sessionId, pNewEntry, pNewEntry->QosFlowID);
       //add the new entry under construction to the Flow List
@@ -2449,7 +2449,7 @@ sme_QosStatusType sme_QosInternalReleaseReq(tpAniSirGlobal pMac,
                   pACInfo->requested_QoSInfo[flow_info->tspec_mask - 1];
                //delete the entry from Flow List
                VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
-                         "%s: %d: Deleting entry at %p with flowID %d",
+                         "%s: %d: Deleting entry at %pK with flowID %d",
                          __func__, __LINE__,
                          flow_info, QosFlowID);
                csrLLRemoveEntry(&sme_QosCb.flow_list, pEntry, VOS_TRUE );
@@ -2494,7 +2494,7 @@ sme_QosStatusType sme_QosInternalReleaseReq(tpAniSirGlobal pMac,
                //delete the entry from Flow List
                VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
                          "%s: %d: On session %d deleting entry at "
-                         "%p with flowID %d",
+                         "%pK with flowID %d",
                          __func__, __LINE__,
                          sessionId, flow_info, QosFlowID);
                csrLLRemoveEntry(&sme_QosCb.flow_list, pEntry, VOS_TRUE );
@@ -2652,7 +2652,7 @@ sme_QosStatusType sme_QosInternalReleaseReq(tpAniSirGlobal pMac,
                           flow_info->QosFlowID);
 
             VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH,
-                      "%s: %d: Deleting entry at %p with flowID %d",
+                      "%s: %d: Deleting entry at %pK with flowID %d",
                       __func__, __LINE__,
                       flow_info, flow_info->QosFlowID);
          }
@@ -2702,7 +2702,7 @@ sme_QosStatusType sme_QosInternalReleaseReq(tpAniSirGlobal pMac,
          pACInfo->num_flows[flow_info->tspec_mask - 1]--;
          //delete the entry from Flow List
          VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
-                   "%s: %d: On session %d deleting entry at %p with flowID %d",
+                   "%s: %d: On session %d deleting entry at %pK with flowID %d",
                    __func__, __LINE__,
                    sessionId, flow_info, QosFlowID);
          csrLLRemoveEntry(&sme_QosCb.flow_list, pEntry, VOS_TRUE );
@@ -3153,7 +3153,7 @@ eHalStatus sme_QosESEProcessReassocTspecRsp(tpAniSirGlobal pMac, v_U8_t sessionI
     }
 
     VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_WARN,
-             "TspecLen = %d, pbFrames = %p, pTspecIE = %p",
+             "TspecLen = %d, pbFrames = %pK, pTspecIE = %pK",
              tspecIeLen, pCsrConnectedInfo->pbFrames, pTspecIE);
 
     numTspec = (tspecIeLen)/sizeof(tDot11fIEWMMTSPEC);
@@ -6242,7 +6242,7 @@ static eHalStatus sme_QosBufferExistingFlows(tpAniSirGlobal pMac,
          }
          //delete the entry from Flow List
          VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
-                   "%s: %d: Deleting original entry at %p with flowID %d",
+                   "%s: %d: Deleting original entry at %pK with flowID %d",
                    __func__, __LINE__,
                    flow_info, flow_info->QosFlowID);
          csrLLRemoveEntry(&sme_QosCb.flow_list, pEntry, VOS_TRUE );
@@ -6293,7 +6293,7 @@ static eHalStatus sme_QosDeleteExistingFlows(tpAniSirGlobal pMac,
                                    flow_info->QosFlowID);
          }
          VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH,
-                   "%s: %d: Deleting entry at %p with flowID %d",
+                   "%s: %d: Deleting entry at %pK with flowID %d",
                    __func__, __LINE__,
                    flow_info, flow_info->QosFlowID);
          //delete the entry from Flow List
@@ -6686,7 +6686,7 @@ eHalStatus sme_QosModifyFnp(tpAniSirGlobal pMac, tListElem *pEntry)
    case SME_QOS_REASON_MODIFY:
       //delete the original entry from Flow List
       VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
-                "%s: %d: Deleting original entry at %p with flowID %d",
+                "%s: %d: Deleting original entry at %pK with flowID %d",
                 __func__, __LINE__,
                 flow_info, flow_info->QosFlowID);
       csrLLRemoveEntry(&sme_QosCb.flow_list, pEntry, VOS_TRUE );
@@ -6885,7 +6885,7 @@ eHalStatus sme_QosReassocSuccessEvFnp(tpAniSirGlobal pMac, tListElem *pEntry)
    {
       //delete the entry from Flow List
       VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
-                "%s: %d: Deleting entry at %p with flowID %d",
+                "%s: %d: Deleting entry at %pK with flowID %d",
                 __func__, __LINE__,
                 flow_info, flow_info->QosFlowID);
       csrLLRemoveEntry(&sme_QosCb.flow_list, pEntry, VOS_TRUE );
@@ -6988,7 +6988,7 @@ eHalStatus sme_QosAddTsFailureFnp(tpAniSirGlobal pMac, tListElem *pEntry)
       }
       //delete the entry from Flow List
       VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
-                "%s: %d: Deleting entry at %p with flowID %d",
+                "%s: %d: Deleting entry at %pK with flowID %d",
                 __func__, __LINE__,
                 flow_info, flow_info->QosFlowID);
       csrLLRemoveEntry(&sme_QosCb.flow_list, pEntry, VOS_TRUE );
@@ -7206,7 +7206,7 @@ eHalStatus sme_QosAddTsSuccessFnp(tpAniSirGlobal pMac, tListElem *pEntry)
    if(delete_entry)
    {
       VOS_TRACE(VOS_MODULE_ID_SME, VOS_TRACE_LEVEL_INFO_HIGH, 
-                "%s: %d: Deleting entry at %p with flowID %d",
+                "%s: %d: Deleting entry at %pK with flowID %d",
                 __func__, __LINE__,
                 flow_info, flow_info->QosFlowID);
       //delete the entry from Flow List
