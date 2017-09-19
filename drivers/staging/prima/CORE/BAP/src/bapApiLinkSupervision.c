0815d83d25c53a91572b18f0491bfea7036837f7
Jeff Johnson <jjohnson@codeaurora.org>
2017-09-19 08:38:56 -0700

    wlan: bap: Replace instances of unadorned %p
    Replace instances of unadorned %p in CORE/BAP.
    
    Change-Id: Ia7a4b72b425f0f453b9d2b47917153e975667f96
    CRs-Fixed: 2111275

--- drivers/staging/prima/CORE/BAP/src/bapApiLinkSupervision.c
+++ drivers/staging/prima/CORE/BAP/src/bapApiLinkSupervision.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2013 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2013, 2017 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -520,7 +520,7 @@ static VOS_STATUS WLANBAP_TxLinkSupervisionCB
         }
     }
     VOS_TRACE( VOS_MODULE_ID_BAP, VOS_TRACE_LEVEL_INFO,
-               "%s:Returned Vos Packet:%p\n", __func__, pPacket );
+               "%s:Returned Vos Packet:%pK\n", __func__, pPacket );
 
     vos_pkt_return_packet( pPacket );
 
