c1f1b1cc25149282eceb51ca5c8c61ac645c61b1
y00230200 <yanghongliang.yang@huawei.com>
2017-09-19 18:06:55 +0800

    msm: ipa: Fix for missing int overflow check in the refcount
    CVE-2017-0746
    
    Overflow of reference counter can lead to memory leak. Before incrementing the reference count, check with
    U32_MAX and return for error check.
    
    Signed-off-by: Utkarsh Saxena <usaxena@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_rt.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_rt.c
@@ -1607,6 +1607,10 @@ int ipa3_get_rt_tbl(struct ipa_ioc_get_rt_tbl *lookup)
 	mutex_lock(&ipa3_ctx->lock);
 	entry = __ipa3_find_rt_tbl(lookup->ip, lookup->name);
 	if (entry && entry->cookie == IPA_COOKIE) {
+		if (entry->ref_cnt == U32_MAX) {
+			IPAERR("fail: ref count crossed limit\n");
+			goto ret;
+		}
 		entry->ref_cnt++;
 		lookup->hdl = entry->id;
 
@@ -1616,6 +1620,8 @@ int ipa3_get_rt_tbl(struct ipa_ioc_get_rt_tbl *lookup)
 
 		result = 0;
 	}
+
+ret:
 	mutex_unlock(&ipa3_ctx->lock);
 
 	return result;
