64beac2e2b95d4edd05dae92a94f287623eef53f
Badhri Jagan Sridharan <Badhri@google.com>
2017-09-21 15:00:43 -0700

    usb: dwc3-msm: Switch to freezable workqueue for sm_work
    Use freezable workqueue for running the sm_work. The sm_work
    add/removes the platform_dev to switch between host and the
    gadget mode. However, the work_item does not have protection
    against the system suspend/resume. Using freezable workqueue
    ensures that the device tree is not changed while the
    system is suspending or resuming. Using the system freezable
    workqueue instead of creating a new one.
    
    Bug: 65638337
    Change-Id: I9242e9386043cea4f8f765010e32efaa3f10059d
    Signed-off-by: Badhri Jagan Sridharan <Badhri@google.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -1584,7 +1584,7 @@ static int msm_dwc3_usbdev_notify(struct notifier_block *self,
 	}
 
 	mdwc->hc_died = true;
-	schedule_delayed_work(&mdwc->sm_work, 0);
+	queue_delayed_work(system_freezable_wq, &mdwc->sm_work, 0);
 	return 0;
 }
 
@@ -2329,7 +2329,7 @@ static void dwc3_ext_event_notify(struct dwc3_msm *mdwc)
 		clear_bit(B_SUSPEND, &mdwc->inputs);
 	}
 
-	schedule_delayed_work(&mdwc->sm_work, 0);
+	queue_delayed_work(system_freezable_wq, &mdwc->sm_work, 0);
 }
 
 static void dwc3_resume_work(struct work_struct *w)
@@ -3126,7 +3126,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 		dwc3_msm_id_notifier(&mdwc->id_nb, true, mdwc->extcon_id);
 	else if (!pval.intval) {
 		/* USB cable is not connected */
-		schedule_delayed_work(&mdwc->sm_work, 0);
+		queue_delayed_work(system_freezable_wq, &mdwc->sm_work, 0);
 	} else {
 		if (pval.intval > 0)
 			dev_info(mdwc->dev, "charger detection in progress\n");
@@ -3839,7 +3839,7 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 	}
 
 	if (work)
-		schedule_delayed_work(&mdwc->sm_work, delay);
+		queue_delayed_work(system_freezable_wq, &mdwc->sm_work, delay);
 
 ret:
 	return;
