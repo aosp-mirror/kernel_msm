47ba538a694b6e63add6b591cde8d941a882511c
gaurank kathpalia <gkathpal@codeaurora.org>
2017-09-28 16:41:57 +0530

    qcacld-2.0: Check for upper bound in P2P NOA event
    Check for the upper bounds for number of NOA descriptors
    received in the P2P NOA event.
    
    Bug: 68992432
    Change-Id: Id7ecf064f2c25f378f76d795902713da8520507f
    CRs-Fixed: 2132226
    Signed-off-by: Ecco Park <eccopark@google.com>

--- drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
@@ -5974,6 +5974,12 @@ static int wma_p2p_noa_event_handler(void *handle, u_int8_t *event, u_int32_t le
 		descriptors = WMI_UNIFIED_NOA_ATTR_NUM_DESC_GET(p2p_noa_info);
 		noa_ie.num_descriptors = (u_int8_t)descriptors;
 
+		if (noa_ie.num_descriptors > WMA_MAX_NOA_DESCRIPTORS) {
+			WMA_LOGD("Sizing down the no of desc %d to max",
+					noa_ie.num_descriptors);
+			noa_ie.num_descriptors = WMA_MAX_NOA_DESCRIPTORS;
+		}
+
 		WMA_LOGI("%s: index %u, oppPs %u, ctwindow %u, "
 				"num_descriptors = %u", __func__, noa_ie.index,
 				noa_ie.oppPS, noa_ie.ctwindow, noa_ie.num_descriptors);
