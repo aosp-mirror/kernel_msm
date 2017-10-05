a48b95fae62f78289fb88760e158a19e69054bdb
Devin Kim <dojip.kim@lge.com>
2017-10-05 15:24:06 -0700

    wireless: bcmdhd: Fix mis-used function get_country_code()
    Bug: 65681129
    Change-Id: I1c184dc0c1e78c1ddf22db236fe8d5772eb28e69
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux_platdev.c
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_linux_platdev.c
@@ -232,7 +232,11 @@ void *wifi_platform_get_country_code(wifi_adapter_info_t *adapter, char *ccode)
 
 	DHD_TRACE(("%s\n", __FUNCTION__));
 	if (plat_data->get_country_code) {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 58))
+		return plat_data->get_country_code(ccode, 0);
+#else
 		return plat_data->get_country_code(ccode);
+#endif
 	}
 #endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39)) */
 
