a58b14a63226248620ef1cdb79b645f8557dbd2f
John Muir <muirj@google.com>
2017-10-13 18:02:45 -0700

    qcacld: Fix compile when WLAN_FEATURE_FILS_SK is not defined
    When this feature is not enabled, but WLAN_FEATURE_ROAM_OFFLOAD
    is enabled, then the compile fails.
    
    Bug: 66812792
    Test: Compile passes.
    Change-Id: I818da634f5e692e04dab102fefcb79582871d4a5

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_assoc.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_assoc.c
@@ -1939,6 +1939,7 @@ void hdd_save_gtk_params(hdd_adapter_t *adapter,
 	uint8_t *kek;
 	uint32_t kek_len;
 
+#if defined(WLAN_FEATURE_FILS_SK)
 	if (is_reassoc) {
 		kek = csr_roam_info->kek;
 		kek_len = csr_roam_info->kek_len;
@@ -1951,6 +1952,10 @@ void hdd_save_gtk_params(hdd_adapter_t *adapter,
 		kek = csr_roam_info->fils_join_rsp->kek;
 		kek_len = csr_roam_info->fils_join_rsp->kek_len;
 	}
+#else
+	kek = csr_roam_info->kek;
+	kek_len = csr_roam_info->kek_len;
+#endif
 
 	wlan_hdd_save_gtk_offload_params(adapter, NULL, kek, kek_len,
 					 csr_roam_info->replay_ctr,
