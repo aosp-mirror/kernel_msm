1f1527ce748e0d6f27941acc218b0f4f0d003c27
Ahmed ElArabawy <arabawy@google.com>
2017-10-17 14:49:50 +0000

    Revert "qcacld-2.0: Set length of challenge text sent by SAP to 128"
    This reverts commit 08dd28aa0a4316bad608239d34d2dac9c265a623.
    The reverted commit causes a kernel panic
    
    Bug: 67775060
    Test: Manual Test
    
    Change-Id: I1a24eb27c97f79c3073026bfac43273af5790588
    Signed-off-by: Ahmed ElArabawy <arabawy@google.com>

--- drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limSendManagementFrames.c
+++ drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -3483,11 +3483,10 @@ limSendAuthMgmtFrame(tpAniSirGlobal pMac,
                      * transaction number, status code and 128 bytes
                      * for challenge text.
                      */
-                    bodyLen = SIR_MAC_AUTH_FRAME_INFO_LEN +
-                              SIR_MAC_SAP_AUTH_CHALLENGE_LENGTH +
-                              SIR_MAC_CHALLENGE_ID_LEN;
+
                     frameLen = sizeof(tSirMacMgmtHdr) +
-                               bodyLen;
+                               sizeof(tSirMacAuthFrame);
+                    bodyLen  = sizeof(tSirMacAuthFrameBody);
                 }
 
                 break;
