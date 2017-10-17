d1f94b2ac8f2104f097591715b8cc57a55930acb
Brian Anderson <brianderson@google.com>
2017-10-16 17:07:15 -0700

    staging android: Keep timestamp info of merged fences.
    Before this patch, sync_fence_merge discarded sync points that
    already signaled. This is adequate for the purposes of meeting
    ordering dependencies and event notification. However, it does
    not allow for proper retrieval of the final event timestamp
    via sync_fence_info.
    
    It is particularly troublesome when merging fences where all
    the sync points have already signaled and all timestamp info
    gets discarded.
    
    This patch adds sync points even if they are retired and
    updates the logic that initializes the fence->status to the
    number of unsignaled sync points.
    
    This behavior matches the behavior before the fence api
    conversion in:
    commit 0f0d8406fb9c ("android: convert sync to fence api, v6")
    
    Devices based on kernels prior to 3.17 do not have this bug.
    
    I verified his fixes eglGetFrameTimestampsANDROID on devices
    that rely on newer versions of the kernel.
    
    Bug: 66916041
    Signed-off-by: Brian Anderson <brianderson@google.com>
    Change-Id: Ia4c48029225979413701b9c97d6a76041715a304

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -242,24 +242,22 @@ void sync_fence_install(struct sync_fence *fence, int fd)
 }
 EXPORT_SYMBOL(sync_fence_install);
 
-static void sync_fence_add_pt(struct sync_fence *fence,
-			      int *i, struct fence *pt)
+static int sync_fence_add_pt(struct sync_fence *fence, int i, struct fence *pt)
 {
-	fence->cbs[*i].sync_pt = pt;
-	fence->cbs[*i].fence = fence;
-
-	if (!fence_add_callback(pt, &fence->cbs[*i].cb, fence_check_cb_func)) {
-		fence_get(pt);
-		(*i)++;
-	}
+	fence->cbs[i].sync_pt = pt;
+	fence->cbs[i].fence = fence;
+	fence_get(pt);
+	return fence_add_callback(
+		pt, &fence->cbs[i].cb, fence_check_cb_func) ? 0 : 1;
 }
 
 struct sync_fence *sync_fence_merge(const char *name,
 				    struct sync_fence *a, struct sync_fence *b)
 {
 	int num_fences = a->num_fences + b->num_fences;
+	int status = 0;
 	struct sync_fence *fence;
-	int i, i_a, i_b;
+	int i = 0, i_a = 0, i_b = 0;
 	unsigned long size = offsetof(struct sync_fence, cbs[num_fences]);
 
 	fence = sync_fence_alloc(size, name);
@@ -275,37 +273,26 @@ struct sync_fence *sync_fence_merge(const char *name,
 	 * If a sync_fence can only be created with sync_fence_merge
 	 * and sync_fence_create, this is a reasonable assumption.
 	 */
-	for (i = i_a = i_b = 0; i_a < a->num_fences && i_b < b->num_fences; ) {
+	while (i_a < a->num_fences && i_b < b->num_fences) {
 		struct fence *pt_a = a->cbs[i_a].sync_pt;
 		struct fence *pt_b = b->cbs[i_b].sync_pt;
-
-		if (pt_a->context < pt_b->context) {
-			sync_fence_add_pt(fence, &i, pt_a);
-
-			i_a++;
-		} else if (pt_a->context > pt_b->context) {
-			sync_fence_add_pt(fence, &i, pt_b);
-
-			i_b++;
-		} else {
-			if (pt_a->seqno - pt_b->seqno <= INT_MAX)
-				sync_fence_add_pt(fence, &i, pt_a);
-			else
-				sync_fence_add_pt(fence, &i, pt_b);
-
-			i_a++;
-			i_b++;
-		}
+		struct fence *pt =
+			(pt_a->context < pt_b->context) ? pt_a :
+			(pt_a->context > pt_b->context) ? pt_b :
+			fence_is_later(pt_a, pt_b) ? pt_a : pt_b;
+		status += sync_fence_add_pt(fence, i++, pt);
+		i_a += pt->context == pt_a->context ? 1 : 0;
+		i_b += pt->context == pt_b->context ? 1 : 0;
 	}
 
 	for (; i_a < a->num_fences; i_a++)
-		sync_fence_add_pt(fence, &i, a->cbs[i_a].sync_pt);
+		status += sync_fence_add_pt(fence, i++, a->cbs[i_a].sync_pt);
 
 	for (; i_b < b->num_fences; i_b++)
-		sync_fence_add_pt(fence, &i, b->cbs[i_b].sync_pt);
+		status += sync_fence_add_pt(fence, i++, b->cbs[i_b].sync_pt);
 
-	if (num_fences > i)
-		atomic_sub(num_fences - i, &fence->status);
+	if (num_fences > status)
+		atomic_sub(num_fences - status, &fence->status);
 	fence->num_fences = i;
 
 	sync_fence_debug_add(fence);
