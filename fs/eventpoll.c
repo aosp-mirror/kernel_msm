fc91e8ba554fed88a3257c7ca0dafde9ee64bc27
Kelly Rossmoyer <krossmo@google.com>
2017-10-19 13:24:10 -0700

    fs: Improve eventpoll logging to stop indicting timerfd
    timerfd doesn't create any wakelocks; eventpoll can, and is creating the
    wakelocks we see called "[timerfd]".  eventpoll creates two kinds of
    wakelocks: a single top-level lock associated with the eventpoll fd
    itself, and one additional lock for each fd it is polling that needs such
    a lock (e.g. those using EPOLLWAKEUP).  Current code names the per-fd
    locks using the undecorated names of the fds' associated files (hence
    "[timerfd]"), and is naming the top-level lock after the PID of the caller
    and the name of the file behind the first fd for which a per-fd lock is
    created.  To make things clearer, the top-level lock is now named using
    the caller PID and an "epollfd" designation, while the per-fd locks are
    also named with the caller's PID (to associate them with the top-level
    lock) and their respective fds' file names.
    
    Bug: 63622255
    Bug: 38042165
    Test: Ran on device and observed new wakelock naming in bugreport, dumpsys
    batterystats, /d/tracing/trace, and d/wakeup_reasons.
    Change-Id: I32bfe5fe6766bb569329a450cc7163d52d59ed34
    Signed-off-by: Kelly Rossmoyer <krossmo@google.com>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1237,17 +1237,23 @@ static int ep_create_wakeup_source(struct epitem *epi)
 {
 	const char *name;
 	struct wakeup_source *ws;
+	char task_comm_buf[TASK_COMM_LEN];
+	char buf[64];
 
-	name = epi->ffd.file->f_path.dentry->d_name.name;
+	get_task_comm(task_comm_buf, current);
+
 	if (!epi->ep->ws) {
-		char buf[64];
-		snprintf(buf, sizeof(buf), "eventpoll pid:%d file:%s", current->pid, name);
+		snprintf(buf, sizeof(buf), "epoll_%.*s_epollfd",
+			 (int)sizeof(task_comm_buf), task_comm_buf);
 		epi->ep->ws = wakeup_source_register(buf);
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
 
-	ws = wakeup_source_register(name);
+	name = epi->ffd.file->f_path.dentry->d_name.name;
+	snprintf(buf, sizeof(buf), "epoll_%.*s_file:%s",
+		 (int)sizeof(task_comm_buf), task_comm_buf, name);
+	ws = wakeup_source_register(buf);
 
 	if (!ws)
 		return -ENOMEM;
