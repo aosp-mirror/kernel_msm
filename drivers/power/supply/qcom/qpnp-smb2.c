35c3ae3a18d20b397fa23dfff945e9d3dfff29b4
Weifeng Zhuang <zhuangwf@lenovo.com>
2017-11-09 09:20:29 +0800

    battery voltage set only in JEITA highest battery temperature
    This device can be charged only in highest temperature with full voltage 4200mV
    and others temperature with full voltage 4400mV.
    
    Bug: 68351541
    Test: put the device into highest or lowest temperature chamber,the device can  be charged
          along the preset temperature.the full battery voltage can be expected.
    
    Change-Id: I74fbca7c01bf0629d694a63f91b4e8dc32ed5aed

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -2399,6 +2399,7 @@ static int smb2_probe(struct platform_device *pdev)
 	/* config vega model: the JEITA_CCCOMP_CFG_REG, FVCOMP regs ( compensate
 	current -1600mA,voltage -200mV ) in jeita Tcold,Thot temperature range. */
 	if (of_property_read_bool(chg->dev->of_node, "lenovo,vega-model")) {
+		smblib_write(chg, JEITA_EN_CFG_REG, JEITA_EN_HARDLIMIT_BIT | JEITA_EN_HOT_SL_FCV_BIT | JEITA_EN_HOT_SL_CCC_BIT | JEITA_EN_COLD_SL_CCC_BIT);
 		smblib_write(chg, JEITA_FVCOMP_CFG_REG, 0x1A);
 		smblib_write(chg, JEITA_CCCOMP_CFG_REG, 0x3F);
 	}
