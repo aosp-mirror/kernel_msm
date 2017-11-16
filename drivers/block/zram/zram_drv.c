ff164e8e1aba6a94ad130bc71e7306e2a6668f98
Minchan Kim <minchan@kernel.org>
2017-11-15 17:32:56 -0800

    BACKPORT: zram: set BDI_CAP_STABLE_WRITES once
    With fast swap storage, the platform wants to use swap more aggressively
    and swap-in is crucial to application latency.
    
    The rw_page() based synchronous devices like zram, pmem and btt are such
    fast storage.  When I profile swapin performance with zram lz4
    decompress test, S/W overhead is more than 70%.  Maybe, it would be
    bigger in nvdimm.
    
    This patchset reduces swap-in latency by skipping swapcache if the swap
    device is a synchronous device like a rw_page() based device.
    
    It enhances by 45% my swapin test (5G sequential swapin, no readahead)
    from 2.41sec to 1.64sec.
    
    This patch (of 4):
    
    Commit 19b7ccf8651d ("block: get rid of blk_integrity_revalidate()")
    fixed a weird thing (i.e., reset BDI_CAP_STABLE_WRITES flag
    unconditionally whenever revalidat_disk is called) so zram doesn't need
    to reset the flag any more when revalidating the bdev.  Instead, set the
    flag just once when the zram device is created.
    
    It shouldn't change any behavior.
    
    Link: http://lkml.kernel.org/r/1505886205-9671-2-git-send-email-minchan@kernel.org
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Ilya Dryomov <idryomov@gmail.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Ross Zwisler <ross.zwisler@linux.intel.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Huang Ying <ying.huang@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Bug: 74579412
    (cherry picked from commit e447a0151f7ce8dd884fea48279274bd64434c29)
    Signed-off-by: Peter Kalauskas <peskal@google.com>
    Change-Id: I59c9006094cec4e6628b5d8f1ae32c6d81f9e365

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -127,14 +127,6 @@ static inline bool is_partial_io(struct bio_vec *bvec)
 }
 #endif
 
-static void zram_revalidate_disk(struct zram *zram)
-{
-	revalidate_disk(zram->disk);
-	/* revalidate_disk reset the BDI_CAP_STABLE_WRITES so set again */
-	zram->disk->queue->backing_dev_info->capabilities |=
-		BDI_CAP_STABLE_WRITES;
-}
-
 /*
  * Check if request is within bounds and aligned on zram logical blocks.
  */
@@ -1387,7 +1379,8 @@ static ssize_t disksize_store(struct device *dev,
 	zram->comp = comp;
 	zram->disksize = disksize;
 	set_capacity(zram->disk, zram->disksize >> SECTOR_SHIFT);
-	zram_revalidate_disk(zram);
+
+	revalidate_disk(zram->disk);
 	up_write(&zram->init_lock);
 
 	return len;
@@ -1434,7 +1427,7 @@ static ssize_t reset_store(struct device *dev,
 	/* Make sure all the pending I/O are finished */
 	fsync_bdev(bdev);
 	zram_reset_device(zram);
-	zram_revalidate_disk(zram);
+	revalidate_disk(zram->disk);
 	bdput(bdev);
 
 	mutex_lock(&bdev->bd_mutex);
@@ -1554,6 +1547,7 @@ static int zram_add(void)
 	/* zram devices sort of resembles non-rotational disks */
 	queue_flag_set_unlocked(QUEUE_FLAG_NONROT, zram->disk->queue);
 	queue_flag_clear_unlocked(QUEUE_FLAG_ADD_RANDOM, zram->disk->queue);
+
 	/*
 	 * To ensure that we always get PAGE_SIZE aligned
 	 * and n*PAGE_SIZED sized I/O requests.
@@ -1579,6 +1573,8 @@ static int zram_add(void)
 		zram->disk->queue->limits.discard_zeroes_data = 0;
 	queue_flag_set_unlocked(QUEUE_FLAG_DISCARD, zram->disk->queue);
 
+	zram->disk->queue->backing_dev_info->capabilities |=
+					BDI_CAP_STABLE_WRITES;
 	add_disk(zram->disk);
 
 	ret = sysfs_create_group(&disk_to_dev(zram->disk)->kobj,
