44abb5d1f584e0f2a72c651f373bc29e3e1e4c42
Fernando Lugo <flugo@google.com>
2017-11-28 13:19:57 -0800

    spi: spi-geni-qcom: use FRAGMENTATION for cs toggle
    According to Qcom *CS_TOGGLE flag is used for toggling CS line between
    spi words. However, cs_change is used to indicate CS line should be
    toggled between messages, as mentioned in the documentation:
    
    (i) If the transfer isn’t the last one in the message, this flag is used
    to make the chipselect briefly go inactive in the middle of the message.
    Toggling chipselect in this way may be needed to terminate a chip
    command, letting a single spi_message perform all of group of chip
    transactions together.
    
    So, in order to have that behavior use FRAGMENTATION instead of
    *CS_TOGGLE
    
    Change-Id: I541bd5cfbd4d0ba0b7fe4c66ab3ed2add07ffbce
    Signed-off-by: Fernando Lugo <flugo@google.com>

--- drivers/spi/spi-geni-qcom.c
+++ drivers/spi/spi-geni-qcom.c
@@ -355,10 +355,6 @@ static struct msm_gpi_tre *setup_config0_tre(struct spi_transfer *xfer,
 	if (mode & SPI_CPHA)
 		flags |= GSI_CPHA;
 
-	if (!list_is_last(&xfer->transfer_list, &spi->cur_msg->transfers))
-		if (xfer->cs_change)
-			flags |= GSI_CS_TOGGLE;
-
 	word_len = xfer->bits_per_word - MIN_WORD_LEN;
 	pack |= (GSI_TX_PACK_EN | GSI_RX_PACK_EN);
 	ret = get_spi_clk_cfg(mas->cur_speed_hz, mas, &idx, &div);
@@ -586,9 +582,9 @@ static int setup_gsi_xfer(struct spi_transfer *xfer,
 	}
 
 	cs |= spi_slv->chip_select;
-	if (list_is_last(&xfer->transfer_list, &spi->cur_msg->transfers))
-		if (xfer->cs_change)
-			go_flags |= FRAGMENTATION;
+	if (!list_is_last(&xfer->transfer_list, &spi->cur_msg->transfers) ==
+	    !xfer->cs_change)
+		go_flags |= FRAGMENTATION;
 
 	go_tre = setup_go_tre(cmd, cs, rx_len, go_flags, mas);
 
@@ -916,13 +912,9 @@ static void setup_fifo_xfer(struct spi_transfer *xfer,
 		m_cmd = SPI_RX_ONLY;
 
 	spi_tx_cfg &= ~CS_TOGGLE;
-	if (list_is_last(&xfer->transfer_list, &spi->cur_msg->transfers)) {
-		if (xfer->cs_change)
-			m_param |= FRAGMENTATION;
-	} else {
-		if (xfer->cs_change)
-			spi_tx_cfg |= CS_TOGGLE;
-	}
+	if (!list_is_last(&xfer->transfer_list, &spi->cur_msg->transfers) ==
+	    !xfer->cs_change)
+		m_param |= FRAGMENTATION;
 	if (!(mas->cur_word_len % MIN_WORD_LEN)) {
 		trans_len =
 			((xfer->len << 3) / mas->cur_word_len) & TRANS_LEN_MSK;
