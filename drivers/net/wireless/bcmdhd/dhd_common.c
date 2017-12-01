7e09cd8ebc007fa153991440aabb486be707c7d6
y00230200 <yanghongliang.yang@huawei.com>
2017-12-01 14:53:57 +0800

    net: wireless: bcmdhd: fix security issue in wl_iw_parse_ssid_list_tlv
    CVE-2017-13161
    
    1. parse TLV length as unsigned int not to make fall through case.
    2. fix for when index is max -1 which valid range.
       previously the case was treated as error at the last check of
       while loop.

--- drivers/net/wireless/bcmdhd/dhd_common.c
+++ drivers/net/wireless/bcmdhd/dhd_common.c
@@ -3250,25 +3250,37 @@ wl_iw_parse_ssid_list_tlv(char** list_str, wlc_ssid_ext_t* ssid, int max, int *b
 {
 	char* str;
 	int idx = 0;
+	uint8 len;
 
-	if ((list_str == NULL) || (*list_str == NULL) || (*bytes_left < 0)) {
+	if ((list_str == NULL) || (*list_str == NULL) || (*bytes_left <= 0)) {
 		DHD_ERROR(("%s error paramters\n", __FUNCTION__));
-		return -1;
+		return BCME_BADARG;
 	}
 	str = *list_str;
 	while (*bytes_left > 0) {
+		if (idx >= max) {
+			DHD_ERROR(("%s number of SSIDs more than %d\n",
+					__FUNCTION__, idx));
+			return BCME_BADARG;
+		}
 
 		if (str[0] != CSCAN_TLV_TYPE_SSID_IE) {
 			*list_str = str;
-			DHD_TRACE(("nssid=%d left_parse=%d %d\n", idx, *bytes_left, str[0]));
+			DHD_TRACE(("nssid=%d left_parse=%d %d\n", idx,
+					*bytes_left, str[0]));
 			return idx;
 		}
 
 		/* Get proper CSCAN_TLV_TYPE_SSID_IE */
 		*bytes_left -= 1;
+		if (*bytes_left == 0) {
+			DHD_ERROR(("%s no length field.\n", __FUNCTION__));
+			return BCME_BADARG;
+		}
 		str += 1;
 
-		if (str[0] == 0) {
+		len = str[0];
+		if (len == 0) {
 			/* Broadcast SSID */
 			ssid[idx].SSID_len = 0;
 			memset((char*)ssid[idx].SSID, 0x0, DOT11_MAX_SSID_LEN);
@@ -3276,20 +3288,17 @@ wl_iw_parse_ssid_list_tlv(char** list_str, wlc_ssid_ext_t* ssid, int max, int *b
 			str += 1;
 
 			DHD_TRACE(("BROADCAST SCAN  left=%d\n", *bytes_left));
-		}
-		else if (str[0] <= DOT11_MAX_SSID_LEN) {
+		} else if (len <= DOT11_MAX_SSID_LEN) {
 			/* Get proper SSID size */
-			ssid[idx].SSID_len = str[0];
+			ssid[idx].SSID_len = len;
 			*bytes_left -= 1;
-			str += 1;
-
 			/* Get SSID */
 			if (ssid[idx].SSID_len > *bytes_left) {
-				DHD_ERROR(("%s out of memory range len=%d but left=%d\n",
+				DHD_ERROR(("%s out of memory len=%d, left=%d\n",
 				__FUNCTION__, ssid[idx].SSID_len, *bytes_left));
-				return -1;
+				return BCME_BADARG;
 			}
-
+			str += 1;
 			memcpy((char*)ssid[idx].SSID, str, ssid[idx].SSID_len);
 
 			*bytes_left -= ssid[idx].SSID_len;
@@ -3297,17 +3306,13 @@ wl_iw_parse_ssid_list_tlv(char** list_str, wlc_ssid_ext_t* ssid, int max, int *b
 			ssid[idx].hidden = TRUE;
 
 			DHD_TRACE(("%s :size=%d left=%d\n",
-				(char*)ssid[idx].SSID, ssid[idx].SSID_len, *bytes_left));
+				(char *)ssid[idx].SSID, ssid[idx].SSID_len,
+				*bytes_left));
+		} else {
+			DHD_ERROR(("### SSID size more than %d\n", str[0]));
+			return BCME_BADARG;
 		}
-		else {
-			DHD_ERROR(("### SSID size more that %d\n", str[0]));
-			return -1;
-		}
-
-		if (idx++ >  max) {
-			DHD_ERROR(("%s number of SSIDs more that %d\n", __FUNCTION__, idx));
-			return -1;
-		}
+		idx++;
 	}
 
 	*list_str = str;
