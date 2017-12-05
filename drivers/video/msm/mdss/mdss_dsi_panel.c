d09061f59795209a0115b19798624e964efe32e3
chester_sun <Chester_Sun@compal.com>
2017-12-05 16:56:16 +0800

    display: initialize the display driver for in-cell panel
    Change-Id: I0c8bfe2fdd79776b05a29016f61dc719ebddd5af

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -362,6 +362,13 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 			pr_err("gpio request failed\n");
 			return rc;
 		}
+
+		msleep(1);
+		if (gpio_is_valid(ctrl_pdata->disp_avdden_gpio)) {
+			gpio_direction_output(ctrl_pdata->disp_avdden_gpio, 0);
+			gpio_set_value((ctrl_pdata->disp_avdden_gpio), 1);
+		}
+
 		if (!pinfo->cont_splash_enabled) {
 			if (gpio_is_valid(ctrl_pdata->disp_en_gpio))
 				gpio_set_value((ctrl_pdata->disp_en_gpio), 1);
@@ -371,6 +378,15 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 					pdata->panel_info.rst_seq[i]);
 				if (pdata->panel_info.rst_seq[++i])
 					usleep_range(pinfo->rst_seq[i] * 1000, pinfo->rst_seq[i] * 1000);
+			}
+
+			if (gpio_is_valid(ctrl_pdata->tp_reset_gpio)){
+				gpio_direction_output(ctrl_pdata->tp_reset_gpio, 0);
+				gpio_set_value((ctrl_pdata->tp_reset_gpio), 0);
+				msleep(10);
+				gpio_set_value((ctrl_pdata->tp_reset_gpio), 1);
+				msleep(20);
+				gpio_free(ctrl_pdata->tp_reset_gpio);
 			}
 
 			if (gpio_is_valid(ctrl_pdata->bklt_en_gpio))
@@ -398,7 +414,7 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 			gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
 			gpio_free(ctrl_pdata->disp_en_gpio);
 		}
-		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		gpio_set_value((ctrl_pdata->rst_gpio), 1);
 		gpio_free(ctrl_pdata->rst_gpio);
 		if (gpio_is_valid(ctrl_pdata->mode_gpio))
 			gpio_free(ctrl_pdata->mode_gpio);
