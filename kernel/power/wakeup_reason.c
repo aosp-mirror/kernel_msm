2b4317f62c9cf1fa26829f7179606336acd729de
Patrick Tjin <pattjin@google.com>
2017-12-05 14:55:14 -0800

    PM: wakeup_reasons: Fix formatting for printk
    current output looks like this:
    
    [  133.772467] Abort: Wakeup IRQ 730 wdog-bark pending[  133.777343] PM: suspend exit 2017-11-30 19:05:34.974168313 UTC
    
    Change it to this:
    
    [  133.772467] Abort: Wakeup IRQ 730 wdog-bark pending
    [  133.777343] PM: suspend exit 2017-11-30 19:05:34.974168313 UTC
    
    Change-Id: Ic38338a3274b83256788077db6f1040605b79603
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -161,7 +161,7 @@ static void print_wakeup_sources(void)
 	const struct list_head *wakeups;
 
 	if (suspend_abort) {
-		pr_info("Abort: %s", abort_reason);
+		pr_info("Abort: %s\n", abort_reason);
 		return;
 	}
 
