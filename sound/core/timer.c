ea3f3f5b59cfd5cfbb4f46ab1e904293cdff92a1
y00230200 <yanghongliang.yang@huawei.com>
2017-12-12 10:54:27 +0800

    ALSA: timer: Fix race between read and ioctl
    CVE-2017-1000380
    
    The read from ALSA timer device, the function snd_timer_user_tread(),
    may access to an uninitialized struct snd_timer_user fields when the
    read is concurrently performed while the ioctl like
    snd_timer_user_tselect() is invoked.  We have already fixed the races
    among ioctls via a mutex, but we seem to have forgotten the race
    between read vs ioctl.

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -417,7 +417,7 @@ static void snd_timer_notify1(struct snd_timer_instance *ti, int event)
 /* start/continue a master timer */
 static int snd_timer_start1(struct snd_timer_instance *timeri,
 			    bool start, unsigned long ticks)
- {
+{
 	struct snd_timer *timer;
 	int result;
 	unsigned long flags;
@@ -1881,6 +1881,7 @@ static ssize_t snd_timer_user_read(struct file *file, char __user *buffer,
 
 	tu = file->private_data;
 	unit = tu->tread ? sizeof(struct snd_timer_tread) : sizeof(struct snd_timer_read);
+	mutex_lock(&tu->ioctl_lock);
 	spin_lock_irq(&tu->qlock);
 	while ((long)count - result >= unit) {
 		while (!tu->qused) {
@@ -1896,7 +1897,9 @@ static ssize_t snd_timer_user_read(struct file *file, char __user *buffer,
 			add_wait_queue(&tu->qchange_sleep, &wait);
 
 			spin_unlock_irq(&tu->qlock);
+			mutex_unlock(&tu->ioctl_lock);
 			schedule();
+			mutex_lock(&tu->ioctl_lock);
 			spin_lock_irq(&tu->qlock);
 
 			remove_wait_queue(&tu->qchange_sleep, &wait);
@@ -1935,6 +1938,7 @@ static ssize_t snd_timer_user_read(struct file *file, char __user *buffer,
 	}
 	spin_unlock_irq(&tu->qlock);
  _error:
+	mutex_unlock(&tu->ioctl_lock);
 	return result > 0 ? result : err;
 }
 
