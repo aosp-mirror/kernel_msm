bccfd8ca1f7442a2c49d0c915ae59de3d0dd0062
Devin Kim <dojip.kim@lge.com>
2018-01-18 13:48:51 -0800

    f2fs: Fix compilation error
    Change-Id: Id6c72101258d6c0c3a6949df6d6b59972099d646
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- fs/f2fs/inline.c
+++ fs/f2fs/inline.c
@@ -87,15 +87,10 @@ int f2fs_read_inline_data(struct inode *inode, struct page *page)
 	struct page *ipage;
 
 	if (trace_android_fs_dataread_start_enabled()) {
-		char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-		path = android_fstrace_get_pathname(pathbuf,
-						    MAX_TRACE_PATHBUF_LEN,
-						    page->mapping->host);
 		trace_android_fs_dataread_start(page->mapping->host,
 						page_offset(page),
 						PAGE_SIZE, current->pid,
-						path, current->comm);
+						current->comm);
 	}
 
 	ipage = get_node_page(F2FS_I_SB(inode), inode->i_ino);
