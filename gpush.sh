5cd779b2e66f71c83b4eeee7c1c6d7124d335827
julius_yen <julius_yen@compal.com>
2018-01-25 09:26:16 +0800

    ling: remove unnecessary build scripts
    Change-Id: I2b2276a318cbe53183d6950a7b186a6be2ecc910

--- gpush.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-git push origin HEAD:refs/for/$1%r=nevill_jiang@compal.com,r=neil_kao@compal.com,r=astor_hung@compal.com,r=abel_fang@compal.com
-
--- signed_wlan.sh
+++
@@ -1,6 +0,0 @@
-export PATH=$PATH:~/sardine_workspace/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/
-
-cp drivers/staging/prima/wlan.ko .
-perl scripts/sign-file sha512 signing_key.priv signing_key.x509 wlan.ko
-arm-linux-androideabi-strip --strip-debug wlan.ko
-cp wlan.ko ~/sardine_workspace/device/compal/sardine-kernel/
