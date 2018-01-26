84766ff2d94460f7409fe8720bb9032c9eed0fe8
Devin Kim <dojip.kim@lge.com>
2018-01-26 12:54:05 -0800

    uid_cputime: Fix compilation errors
    Bug: 72530627
    Change-Id: Ic7b762ab2d7151418557dcc9e801d12bc09b02ef
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- drivers/misc/uid_cputime.c
+++ drivers/misc/uid_cputime.c
@@ -101,14 +101,11 @@ static int uid_stat_show(struct seq_file *m, void *v)
 			return -ENOMEM;
 		}
 		/* if this task is exiting, we have already accounted for the
-		 * time and power.
+		 * time.
 		 */
-		if (task->cpu_power == ULLONG_MAX)
-			continue;
 		task_cputime_adjusted(task, &utime, &stime);
 		uid_entry->active_utime += utime;
 		uid_entry->active_stime += stime;
-		uid_entry->active_power += task->cpu_power;
 	} while_each_thread(temp, task);
 	read_unlock(&tasklist_lock);
 
@@ -218,8 +215,6 @@ static int process_notifier(struct notifier_block *self,
 	task_cputime_adjusted(task, &utime, &stime);
 	uid_entry->utime += utime;
 	uid_entry->stime += stime;
-	uid_entry->power += task->cpu_power;
-	task->cpu_power = ULLONG_MAX;
 
 exit:
 	mutex_unlock(&uid_lock);
