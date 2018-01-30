59084af0f704d6ecd58cd255f32bf8a21970a09c
Ahmed ElArabawy <arabawy@google.com>
2018-01-30 11:20:52 -0800

    WiFi: Support 2 Provisioned MAC Addresses
    Currently the Google OEM driver reads only one MAC Address from the
    flash, and pass it to CNSS. If it can not find the mac address,
    it will create a random address instead.
    It also uses the old API for CNSS where both provisioned and derived MAC
    addresses are passed in a single API
    
    This Commit:
    1. Reads 2 MAC Addresses from the Flash FS
    2. If only one (or no) MAC addresses are found, return with error (don't
    generate a random mac address)
    3. Make sure provisioned MAC Addresses are globally administered (so
    derived addresses will not replicate a provisioned address)
    4. Use a separate API for Provisioned MAC addresses and for derived MAC
    addresses
    
    Bug: 72698233
    Test: Manual Test
    
    Change-Id: I517f41ab7ae9f6f802561a589a06cd8a5674d737
    Signed-off-by: Ahmed ElArabawy <arabawy@google.com>

--- drivers/staging/wlan_mac/google_wlan_mac.c
+++ drivers/staging/wlan_mac/google_wlan_mac.c
@@ -31,53 +31,87 @@
 static void set_wifi_mac(void)
 {
 	u8 mac[12] = {0};
-	unsigned int i, size;
-	unsigned char *mac_addr = NULL;
+	unsigned int size;
+	unsigned char *mac_addr1 = NULL;
+	unsigned char *mac_addr2 = NULL;
 	struct device_node *node;
-	unsigned int mac_found = 0;
+	unsigned int mac1_found = 0;
+	unsigned int mac2_found = 0;
 
 	node = of_find_node_by_path(CDB_PATH);
 	if (!node)
 		pr_err("[WLAN] CDB Node not created under %s", CDB_PATH);
-	else
-		mac_addr = (unsigned char *)
+	else {
+		mac_addr1 = (unsigned char *)
 				of_get_property(node, WIFI_MAC_1, &size);
 
-	if (mac_addr) {
-		if (sscanf(mac_addr,
-			   "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
-			   &mac[0], &mac[1], &mac[2], &mac[3], &mac[4],
-			   &mac[5]) == 6) {
-			mac_found = 1;
-		} else if (sscanf(mac_addr,
-				  "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx",
-				  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4],
-				  &mac[5]) == 6) {
-			mac_found = 1;
-		}
+		mac_addr2 = (unsigned char *)
+				of_get_property(node, WIFI_MAC_2, &size);
 	}
 
-	if (!mac_found) {
-		pr_err("[WLAN] No Default MAC !!!!! Use Random Mac\n");
-
-		get_random_bytes(mac, 6);
-
-		/*To avoid invalid mac, set mac[0] and mac[1]*/
-		mac[0] = 0x00;
-		mac[1] = 0x88;
+	/* In case Missing Provisioned MAC Addresses, exit with error */
+	if (!mac_addr1 || !mac_addr2) {
+		pr_err("[WLAN] Missing Provisioned MAC addresses\n");
+		return;
 	}
 
-	/* Now derive the second mac address by flipping the locally administred bit */
-	/* TODO(arabawy) We should use the second MAC Address once driver is ready */
-
-	mac[6] = mac[0] | 2;
-	for (i = 1;  i < 6; i++) {
-		mac[i + 6] = mac[i];
+	/* Start decoding MAC Addresses
+	 * Note that 2 formats are supported for now
+	 * AA:BB:CC:DD:EE:FF (with separating colons) and
+	 * AABBCCDDEEFF (without separating colons) */
+	if (sscanf(mac_addr1,
+		   "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
+		   &mac[0], &mac[1], &mac[2], &mac[3], &mac[4],
+		   &mac[5]) == 6) {
+		mac1_found = 1;
+	} else if (sscanf(mac_addr1,
+			  "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx",
+			  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4],
+			  &mac[5]) == 6) {
+		mac1_found = 1;
 	}
 
+	if (sscanf(mac_addr2,
+		   "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx",
+		   &mac[6], &mac[7], &mac[8], &mac[9], &mac[10],
+		   &mac[11]) == 6) {
+		mac2_found = 1;
+	} else if (sscanf(mac_addr2,
+			  "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx",
+			  &mac[6], &mac[7], &mac[8], &mac[9], &mac[10],
+			  &mac[11]) == 6) {
+		mac2_found = 1;
+	}
+
+	/* Make sure Addresses decoding succeeds */
+	if (!mac1_found || !mac2_found) {
+		pr_err("[WLAN] Invalid format for Provisioned MAC Addresses\n");
+		return;
+	}
+
+	/* Make sure Provisioned MAC Addresses are globally Administered */
+	if ((mac[0] & 2) || (mac[6] & 2)) {
+		pr_err("[WLAN] Invalid Provisioned MAC Addresses\n");
+		return;
+	}
+
+	/* Send provisioned MAC Addresses to the platform driver */
 	if (cnss_utils_set_wlan_mac_address(mac, sizeof(mac)) != 0) {
-		pr_err("[WLAN] set wlan mac address failed\n");
+		pr_err("[WLAN] set wlan mac addresses failed\n");
+		return;
 	}
+
+	/* Now derive the derived mac addresses
+	 * by flipping the locally administred bit */
+	mac[0] = mac[0] | 2;
+	mac[6] = mac[6] | 2;
+
+	if (cnss_utils_set_wlan_derived_mac_address(mac, sizeof(mac)) != 0) {
+		pr_err("[WLAN] set wlan derived  mac addresses failed\n");
+		return;
+	}
+
+	return;
 }
 
 static int __init wifi_mac_init(void)
