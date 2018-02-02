5ef5adf8f517f39d082a53923e360852f3faf097
chester_sun <Chester_Sun@compal.com>
2018-02-02 17:32:30 +0800

    display: remove no needed control flow and init. code for          in-cell display and touch power on/off sequence in kernel
    Change-Id: Ifaea885f5d2ccc65604eeca5951b18f981f6af1d

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1611,11 +1611,6 @@ static int mdss_dsi_unblank(struct mdss_panel_data *pdata)
 		ctrl_pdata->ctrl_state |= CTRL_STATE_PANEL_INIT;
 	}
 
-	if ((pdata->panel_info.type == MIPI_CMD_PANEL) &&
-		mipi->vsync_enable && mipi->hw_vsync_mode) {
-		mdss_dsi_set_tear_on(ctrl_pdata);
-	}
-
 error:
 	mdss_dsi_clk_ctrl(ctrl_pdata, ctrl_pdata->dsi_clk_handle,
 			  MDSS_DSI_ALL_CLKS, MDSS_DSI_CLK_OFF);
@@ -1673,7 +1668,6 @@ static int mdss_dsi_blank(struct mdss_panel_data *pdata, int power_state)
 			ctrl_pdata->switch_mode(pdata, SWITCH_TO_VIDEO_MODE);
 		} else if (pdata->panel_info.type == MIPI_VIDEO_PANEL) {
 			ctrl_pdata->switch_mode(pdata, SWITCH_TO_CMD_MODE);
-			mdss_dsi_set_tear_off(ctrl_pdata);
 		}
 	}
 
