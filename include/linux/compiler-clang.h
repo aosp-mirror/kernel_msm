03a0ab560211b54ddcabdbd81bbfc384810e4f7a
Paul Lawrence <paullawrence@google.com>
2018-02-06 15:36:03 -0800

    BACKPORT: kasan: add compiler support for clang
    Patch series "kasan: support alloca, LLVM", v4.
    
    This patch (of 5):
    
    For now we can hard-code ASAN ABI level 5, since historical clang builds
    can't build the kernel anyway.  We also need to emulate gcc's
    __SANITIZE_ADDRESS__ flag, or memset() calls won't be instrumented.
    
    Link: http://lkml.kernel.org/r/20171204191735.132544-2-paullawrence@google.com
    Signed-off-by: Greg Hackmann <ghackmann@google.com>
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Acked-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Masahiro Yamada <yamada.masahiro@socionext.com>
    Cc: Matthias Kaehlcke <mka@chromium.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry-picked from 53a98ed73b848432a51631346b02049bb7fa039d)
    Change-Id: I4f694d1c06ad286e165d68938ff0f4348dc5fedf
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    (cherry picked from commit 08da675c9421b1a7df9aadee84640908390aeb3c)

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -34,3 +34,12 @@
 #endif
 
 #define __nosafestack	__attribute__((no_sanitize("safe-stack")))
+
+/* all clang versions usable with the kernel support KASAN ABI version 5 */
+#define KASAN_ABI_VERSION 5
+
+/* emulate gcc's __SANITIZE_ADDRESS__ flag */
+#if __has_feature(address_sanitizer)
+#define __SANITIZE_ADDRESS__
+#endif
+
