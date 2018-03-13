1f31818d00f0916c4786c53e01e5b93930ec698a
Kyle Tso <kyletso@android.com>
2018-03-13 21:18:53 +0800

    dt-bindings: usb: Add typec bindings
    Bug: 74618869
    
    Change-Id: Icaab9b0caac7942baf08be7d3b924fb3fb5520d5
    Signed-off-by: Kyle Tso <kyletso@google.com>

---
+++ include/dt-bindings/usb/typec.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2018 Google, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ */
+
+#ifndef _DT_BINDINGS_USB_TYPEC
+#define _DT_BINDINGS_USB_TYPEC
+
+#define TYPEC_PORT_DFP	0x0
+#define TYPEC_PORT_UFP	0x1
+#define TYPEC_PORT_DRP	0x2
+
+#define PDO_TYPE_FIXED	0x0
+#define PDO_TYPE_BATT	0x1
+#define PDO_TYPE_VAR	0x2
+
+#define TYPEC_SINK	0x0
+#define TYPEC_SOURCE	0x1
+
+#endif /* _DT_BINDINGS_USB_TYPEC */
