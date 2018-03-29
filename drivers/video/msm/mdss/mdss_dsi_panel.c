d6cc09838a4de0219c95d7ea5c27ae058e3b9cae
Sachin Bhayare <sachin.bhayare@codeaurora.org>
2018-03-29 17:27:51 +0530

    msm: mdss: add header file to fix compilation
    Add mdss_debug.h to fix MDSS_XLOG compilation.
    
    Change-Id: I07101d2f1bb620c26d3d0e1c4449d8fcb83f7fda
    Signed-off-by: Sachin Bhayare <sachin.bhayare@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -24,6 +24,7 @@
 #include <linux/string.h>
 
 #include "mdss_dsi.h"
+#include "mdss_debug.h"
 #ifdef TARGET_HW_MDSS_HDMI
 #include "mdss_dba_utils.h"
 #endif
