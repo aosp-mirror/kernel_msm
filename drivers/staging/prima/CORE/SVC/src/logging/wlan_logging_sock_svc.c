5174e484025cb3a4cfd567975a504ae16f864b4f
Hanumanth Reddy Pothula <c_hpothu@codeaurora.org>
2018-04-11 15:35:36 +0530

    wlan: Use request manager while requesting fw mem dump
    We are transitioning to the new request manager framework. Change
    wlan_hdd_fw_mem_dump_req() and wlan_hdd_fw_mem_dump_cb to this
    framework.
    
    Change-Id: Ia9c1c43a837d391716cc4ac82a47986223854c15
    CRs-Fixed: 2215453

--- drivers/staging/prima/CORE/SVC/src/logging/wlan_logging_sock_svc.c
+++ drivers/staging/prima/CORE/SVC/src/logging/wlan_logging_sock_svc.c
@@ -1242,17 +1242,14 @@ static int wlan_logging_thread(void *Arg)
 				 * memdump complete. If it's null,then something is
 				 * not right.
 				 */
-				if (gwlan_logging.fw_mem_dump_ctx.svc_fw_mem_dump_req_cb &&
-				    gwlan_logging.fw_mem_dump_ctx.svc_fw_mem_dump_req_cb_arg) {
+				if (gwlan_logging.fw_mem_dump_ctx.svc_fw_mem_dump_req_cb) {
 					((hdd_fw_mem_dump_req_cb)
 					gwlan_logging.fw_mem_dump_ctx.svc_fw_mem_dump_req_cb)(
-					(struct hdd_fw_mem_dump_req_ctx*)
 					gwlan_logging.fw_mem_dump_ctx.svc_fw_mem_dump_req_cb_arg);
 
 					/*invalidate the callback pointers*/
 					spin_lock_irqsave(&gwlan_logging.fw_mem_dump_ctx.fw_mem_dump_lock,flags);
 					gwlan_logging.fw_mem_dump_ctx.svc_fw_mem_dump_req_cb = NULL;
-					gwlan_logging.fw_mem_dump_ctx.svc_fw_mem_dump_req_cb_arg = NULL;
 					spin_unlock_irqrestore(&gwlan_logging.fw_mem_dump_ctx.fw_mem_dump_lock,flags);
 				}
 		}
