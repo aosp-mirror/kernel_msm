67ecb4e9b6b33af9c7b3d7c860d722c8fd605366
Padma, Santhosh Kumar <skpadma@codeaurora.org>
2018-04-11 12:46:39 +0530

    qcacld-3.0: Enable SAE and GCMP by default
    Add changes to enaible SAE/GCMP by default.
    
    Change-Id: I4d7597e019379429916e9684fa1126fda3c97e41
    CRs-Fixed: 2217451
    Bug: 119512579
    Signed-off-by: Srinivas Girigowda <quic_sgirigow@quicinc.com>
    Signed-off-by: Sunil Ravi <sunilravi@google.com>

--- drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_cfg.h
+++ drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_cfg.h
@@ -14053,7 +14053,7 @@ enum hw_filter_mode {
 #define CFG_ENABLE_GCMP_NAME    "gcmp_enabled"
 #define CFG_ENABLE_GCMP_MIN     (0)
 #define CFG_ENABLE_GCMP_MAX     (1)
-#define CFG_ENABLE_GCMP_DEFAULT (0)
+#define CFG_ENABLE_GCMP_DEFAULT (1)
 
 /*
  * <ini>
@@ -14318,7 +14318,7 @@ enum hw_filter_mode {
  */
 
 #define CFG_IS_SAE_ENABLED_NAME    "sae_enabled"
-#define CFG_IS_SAE_ENABLED_DEFAULT (0)
+#define CFG_IS_SAE_ENABLED_DEFAULT (1)
 #define CFG_IS_SAE_ENABLED_MIN     (0)
 #define CFG_IS_SAE_ENABLED_MAX     (1)
 
