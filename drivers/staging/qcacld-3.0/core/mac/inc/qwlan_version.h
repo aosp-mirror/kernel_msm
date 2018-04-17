1c82f17b26c55570ae6b4bc2918e74844860de38
Srinivas Girigowda <sgirigow@codeaurora.org>
2018-04-17 10:20:48 -0700

    Release 5.2.1.1G
    Release 5.2.1.1G
    
    Change-Id: I62ff8fabab3a92259addd07b5bd273ac544d8836
    CRs-Fixed: 774533
    Bug: 77999591

--- drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
+++ drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
@@ -41,9 +41,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "E"
+#define QWLAN_VERSION_EXTRA            "G"
 #define QWLAN_VERSION_BUILD            1
 
-#define QWLAN_VERSIONSTR               "5.2.1.1E"
+#define QWLAN_VERSIONSTR               "5.2.1.1G"
 
 #endif /* QWLAN_VERSION_H */
