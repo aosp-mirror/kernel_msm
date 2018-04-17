e6c8c9215efcee3adbba69b9e40b376e021af54f
Alok Kumar <alokkuma@codeaurora.org>
2018-04-17 12:11:38 +0530

    wlan: Fix NULL pointer dereference issue in WLANTL_SampleRxRSSI
    Add null check for pTLCb->atlSTAClients before dereferencing it.
    
    Change-Id: Id3be987092a1d5129d9707fa3145de264a2eb4dd
    CRs-Fixed: 2225758

--- drivers/staging/prima/CORE/TL/src/wlan_qct_tl.c
+++ drivers/staging/prima/CORE/TL/src/wlan_qct_tl.c
@@ -6142,10 +6142,18 @@ static void WLANTL_CacheEapol(WLANTL_CbType* pTLCb, vos_pkt_t* vosTempBuff)
 static void WLANTL_SampleRxRSSI(WLANTL_CbType* pTLCb, void * pBDHeader,
                                 uint8_t sta_id)
 {
-   WLANTL_STAClientType *pClientSTA = pTLCb->atlSTAClients[sta_id];
-   uint8_t count = pClientSTA->rssi_sample_cnt;
-   uint8_t old_idx = pClientSTA->rssi_stale_idx;
+   uint8_t count;
+   uint8_t old_idx;
    s8 curr_RSSI, curr_RSSI0, curr_RSSI1;
+   WLANTL_STAClientType *pClientSTA = pTLCb->atlSTAClients[sta_id];
+
+   if(pClientSTA == NULL) {
+      TLLOGE(VOS_TRACE( VOS_MODULE_ID_TL, VOS_TRACE_LEVEL_ERROR,
+      " %s: pClientSTA is NULL", __func__));
+      return;
+   }
+   count = pClientSTA->rssi_sample_cnt;
+   old_idx = pClientSTA->rssi_stale_idx;
 
    curr_RSSI0 = WLANTL_GETRSSI0(pBDHeader);
    curr_RSSI1 = WLANTL_GETRSSI1(pBDHeader);
