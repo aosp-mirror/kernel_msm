f143ef76d91bb9439784a5bc46add36b1122fa32
David Lin <dtwlin@google.com>
2018-05-01 11:04:21 -0700

    misc: cs40l2x: Add the driver support
    This patch adds the driver support for the cs40l2x family driver IC.
    
    Bug: 78128429
    Test: manual haptics test
    Change-Id: I6a8f7660c30fd4d2e5074cd5198cc789f4a2b5ce
    Signed-off-by: David Lin <dtwlin@google.com>

---
+++ include/linux/platform_data/cs40l2x.h
@@ -0,0 +1,22 @@
+/*
+ * linux/platform_data/cs40l2x.h -- Platform data for
+ * CS40L20/CS40L25/CS40L25A/CS40L25B
+ *
+ * Copyright 2018 Cirrus Logic, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef __CS40L2X_H
+#define __CS40L2X_H
+
+struct cs40l2x_platform_data {
+	unsigned int boost_ind;
+	unsigned int boost_cap;
+	unsigned int boost_ipk;
+	bool refclk_gpio2;
+};
+
+#endif /* __CS40L2X_H */
