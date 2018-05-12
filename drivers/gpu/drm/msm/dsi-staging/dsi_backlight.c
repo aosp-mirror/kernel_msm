9dadf7d07abf51f1edf12746c5ca66387a84f38c
Adrian Salido <salidoa@google.com>
2018-05-11 17:54:19 -0700

    drm/msm/dsi-staging: remove s6e3ha8 hlpm specific support
    Same functionality can be handled in binned brightness mode.
    
    Bug: 79213318
    Change-Id: I3c90cc7b4076b41dcb028d2df1d7a637c7651eb0
    Signed-off-by: Adrian Salido <salidoa@google.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_backlight.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_backlight.c
@@ -470,76 +470,6 @@ int dsi_backlight_get_dpms(struct dsi_backlight_config *bl)
 		return SDE_MODE_DPMS_ON;
 }
 
-#define DSI_PANEL_S6E3HA8_HLPM_OFF    0x20
-#define DSI_PANEL_S6E3HA8_HLPM_LOW    0x23
-#define DSI_PANEL_S6E3HA8_HLPM_HIGH   0x22
-
-static u8 dsi_panel_s6e3ha8_bl_to_mode(const struct dsi_backlight_config *bl,
-				       u32 bl_lvl, unsigned int state)
-{
-	u8 mode;
-
-	if ((state & (BL_CORE_FBBLANK | BL_CORE_SUSPENDED)) ||
-	    !(state & BL_STATE_LP) || bl_lvl == 0) {
-		mode = DSI_PANEL_S6E3HA8_HLPM_OFF;
-	} else {
-		const int bl_threshold = bl->bl_min_level +
-				(bl->bl_max_level - bl->bl_min_level) / 2;
-
-		mode = bl_lvl > bl_threshold ? DSI_PANEL_S6E3HA8_HLPM_HIGH :
-				DSI_PANEL_S6E3HA8_HLPM_LOW;
-	}
-
-	return mode;
-}
-
-static int dsi_panel_s6e3ha8_bl_update(struct dsi_backlight_config *bl,
-				       u32 bl_lvl)
-{
-	struct dsi_panel *panel = container_of(bl, struct dsi_panel, bl_config);
-	struct mipi_dsi_device *dsi = &panel->mipi_device;
-	struct backlight_properties *props = &bl->bl_device->props;
-	u8 current_mode, target_mode;
-
-	/* if display off, there's nothing to do */
-	if (props->state & (BL_CORE_FBBLANK | BL_CORE_SUSPENDED))
-		return 0;
-
-	current_mode = dsi_panel_s6e3ha8_bl_to_mode(bl, bl->bl_actual,
-						    bl->last_state);
-	target_mode = dsi_panel_s6e3ha8_bl_to_mode(bl, bl_lvl, props->state);
-
-	if (target_mode != current_mode) {
-		u8 payload[2] = { MIPI_DCS_WRITE_CONTROL_DISPLAY, target_mode };
-		int rc;
-
-		pr_debug("changing mode 0x%x -> 0x%x\n",
-			 current_mode, target_mode);
-		mutex_lock(&panel->panel_lock);
-		rc = mipi_dsi_dcs_write_buffer(dsi, payload, sizeof(payload));
-		mutex_unlock(&panel->panel_lock);
-		if (rc < 0)
-			return rc;
-
-		/* ensure backlight update after lpm exit */
-		if (target_mode == DSI_PANEL_S6E3HA8_HLPM_OFF)
-			bl->bl_actual = -1;
-	}
-
-	/* no need to send backlight command if HLPM active */
-	if (target_mode != DSI_PANEL_S6E3HA8_HLPM_OFF)
-		return 0;
-
-	return dsi_backlight_update_dcs(bl, bl_lvl);
-}
-
-static int dsi_panel_s6e3ha8_bl_register(struct dsi_backlight_config *bl)
-{
-	bl->update_bl = dsi_panel_s6e3ha8_bl_update;
-
-	return 0;
-}
-
 #define MAX_BINNED_BL_MODES 10
 
 struct binned_lp_node {
@@ -694,10 +624,6 @@ static const struct of_device_id dsi_backlight_dt_match[] = {
 	{
 		.compatible = "google,dsi_binned_lp",
 		.data = dsi_panel_binned_lp_register,
-	},
-	{
-		.compatible = "google,s6e3ha8_panel",
-		.data = dsi_panel_s6e3ha8_bl_register,
 	},
 	{}
 };
