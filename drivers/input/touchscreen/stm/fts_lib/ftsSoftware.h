2b98f6c7fef7787727066faad871dc3fcb5c3d78
Siarhei Vishniakou <svv@google.com>
2018-05-15 15:15:53 -0700

    input: touchscreen: stm: cleanup driver
    Remove redundant definitions
    
    Bug: 62057517
    Test: compile only
    Signed-Off-By: Siarhei Vishniakou <svv@google.com>
    Change-Id: I6c5a210056631d6483b14a3f71ffd131a29b2abc

--- drivers/input/touchscreen/stm/fts_lib/ftsSoftware.h
+++ drivers/input/touchscreen/stm/fts_lib/ftsSoftware.h
@@ -25,14 +25,6 @@
 #include <linux/types.h>
 #include "ftsHardware.h"
 
-
-
-/* unsigned types */
-typedef uint8_t u8;	/* /< basic type that represent one byte (or 8 bits) */
-typedef uint16_t u16;	/* /< basic type that represent 2 bytes (or 16 bits) */
-typedef uint32_t u32;	/* /< basic type that represent 4 bytes (or 32 bits) */
-typedef uint64_t u64;	/* /< basic type that represent 8 bytes (or 64 bits) */
-
 /* signed type */
 typedef signed char i8;	/* /< basic type that represent one signed byte (or 8
 			 * bits) */
