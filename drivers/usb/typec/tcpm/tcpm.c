16cfa90c6f8b4a1390aef3e9453a0fd4e4c32688
Badhri Jagan Sridharan <badhri@google.com>
2018-05-16 19:33:25 -0700

    tcpm: Add callback to print rtc time if required
    Change file location:
            driver/usb/typec/tcpm.c => driver/usb/typec/tcpm/tcpm.c
    
    Bug: 116661237
    Bug: 79889897
    Bug: 140367237
    Change-Id: Iaa1ea3b95ef3e538b4c79f77b3fd876fd46e8a3a
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>
    Signed-off-by: rickyniu <rickyniu@google.com>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -477,6 +477,9 @@ static void tcpm_log(struct tcpm_port *port, const char *fmt, ...)
 	     port->state == TOGGLING))
 		return;
 
+	if (port->tcpc->log_rtc)
+		port->tcpc->log_rtc(port->tcpc);
+
 	va_start(args, fmt);
 	_tcpm_log(port, fmt, args);
 	va_end(args);
