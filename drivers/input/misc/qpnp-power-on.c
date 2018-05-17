86574ced5d3fbf8a9d7b1aff658e8475f9093f56
Wei Wang <wvw@google.com>
2018-05-17 14:39:27 -0700

    qpnp-power-on: print pmic keypress debug message
    Bug: 79926599
    Test: Boot and see message
    Change-Id: I95f35b6fc77e9078b475c465b782b59fc631ef53
    Signed-off-by: Wei Wang <wvw@google.com>

--- drivers/input/misc/qpnp-power-on.c
+++ drivers/input/misc/qpnp-power-on.c
@@ -945,8 +945,9 @@ qpnp_pon_input_dispatch(struct qpnp_pon *pon, u32 pon_type)
 		return -EINVAL;
 	}
 
-	pr_debug("PMIC input: code=%d, sts=0x%hhx\n",
-					cfg->key_code, pon_rt_sts);
+	pr_info("PMIC input: code=%d, sts=0x%hhx, bit=0x%hhx, type=%d, os=%d\n",
+		cfg->key_code, pon_rt_sts, pon_rt_bit, cfg->pon_type,
+		cfg->old_state);
 	key_status = pon_rt_sts & pon_rt_bit;
 
 	if (pon->kpdpwr_dbc_enable && cfg->pon_type == PON_KPDPWR) {
