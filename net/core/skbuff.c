d1ca759618ecb921545939a55975253b60ffea29
Benedict Wong <benedictwong@google.com>
2018-05-24 21:00:58 +0000

    Revert "BACKPORT: skbuff: Reset secpath only on xnet when scrub a packet"
    This reverts commit 84dde8e9a368cd8a329c801cc171283c56b101b7.
    
    Reason for revert: Appears unlikely that XFRM-I will land in upstream kernels in the immediate future. UAPI incompatibilities are a cause for concern. Reverting to ensure we do not end up with divergent UAPIs.
    
    Signed-off-by: Benedict Wong <benedictwong@google.com>
    Change-Id: I69fe306b7e8e1d3fd523307a4cc069e18f92d5d5

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -4399,6 +4399,7 @@ void skb_scrub_packet(struct sk_buff *skb, bool xnet)
 	skb->skb_iif = 0;
 	skb->ignore_df = 0;
 	skb_dst_drop(skb);
+	secpath_reset(skb);
 	nf_reset(skb);
 	nf_reset_trace(skb);
 
@@ -4406,7 +4407,6 @@ void skb_scrub_packet(struct sk_buff *skb, bool xnet)
 		return;
 
 	ipvs_reset(skb);
-	secpath_reset(skb);
 	skb_orphan(skb);
 	skb->mark = 0;
 }
