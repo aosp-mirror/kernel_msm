fd66b4c6bd794c7c9edb0e09c6ec8c3aa427f926
Thierry Strudel <tstrudel@google.com>
2018-05-24 13:59:28 -0700

    max1720x: fix default nCGain
    CGain = (nCGain & 0xFFC0 >> 4)
    
    Bug: 80257141
    Change-Id: Id35b8b7271589a1f8522f38a5697de7a7db7c2ff
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- drivers/power/supply/max1720x_battery.c
+++ drivers/power/supply/max1720x_battery.c
@@ -1110,7 +1110,7 @@ static int max1720x_handle_dt_shadow_config(struct max1720x_chip *chip)
 	if ((nRAM_updated[NVRAM_U16_INDEX(MAX1720X_NVRAM_NVCFG0)] &
 	     MAX1720X_NNVCFG0_ENCG) &&
 	    !nRAM_updated[NVRAM_U16_INDEX(MAX1720X_NVRAM_CGAIN)])
-		nRAM_updated[NVRAM_U16_INDEX(MAX1720X_NVRAM_CGAIN)] = 0x0400;
+		nRAM_updated[NVRAM_U16_INDEX(MAX1720X_NVRAM_CGAIN)] = 0x4000;
 
 	if (memcmp(nRAM_updated, nRAM_current, MAX1720X_NVRAM_SIZE)) {
 		ret = regmap_raw_write(chip->regmap_nvram, MAX1720X_NVRAM_START,
