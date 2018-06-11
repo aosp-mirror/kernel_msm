026f8540bf6f99f673e87d50a14246f9b9ab0f84
Miguel de Dios <migueldedios@google.com>
2018-06-11 12:09:26 -0700

    hrtimer: Change cpu_relax() in hrtimer_cancel() to udelay(1) instead.
    There is a lock contention in the case when we execute run_hrtimer and
    the hrtimer's callback wakes up a thread which tries to cancel the
    timer.  The thread trying to cancel the timer fails and retries because
    the hrtimer handler is active but the thread also casues lock contention
    which inhibits the hrtimer handler from making forward progress.
    
    Bug: 79694843
    Test: manual - flash device and check using a trace.
    
    Change-Id: I366f0ec178fa23d38788ca5b93ded88056b6c552
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Suggested by: Martin Liu <liumartin@google.com>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -32,6 +32,7 @@
  */
 
 #include <linux/cpu.h>
+#include <linux/delay.h>
 #include <linux/export.h>
 #include <linux/percpu.h>
 #include <linux/hrtimer.h>
@@ -1042,7 +1043,7 @@ int hrtimer_cancel(struct hrtimer *timer)
 
 		if (ret >= 0)
 			return ret;
-		cpu_relax();
+		udelay(1);
 	}
 }
 EXPORT_SYMBOL_GPL(hrtimer_cancel);
