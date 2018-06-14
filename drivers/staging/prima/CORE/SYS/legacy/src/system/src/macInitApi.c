80e7806633723b24d9ea73e7085f4c2922cd36ae
Abhinav Kumar <abhikuma@codeaurora.org>
2018-06-14 16:52:56 +0530

    wlan: Fix memory leak in mac_open
    Make sure to free cfg resource in allocated during
    cfg_init when case of pe_open failure happen.
    
    Change-Id: I651585dabec344253fb3015d974f9f4685d24304
    CRs-Fixed: 2027588

--- drivers/staging/prima/CORE/SYS/legacy/src/system/src/macInitApi.c
+++ drivers/staging/prima/CORE/SYS/legacy/src/system/src/macInitApi.c
@@ -189,6 +189,7 @@ tSirRetStatus macStop(tHalHandle hHal, tHalStopType stopType)
 tSirRetStatus macOpen(tHalHandle *pHalHandle, tHddHandle hHdd, tMacOpenParameters *pMacOpenParms)
 {
     tpAniSirGlobal pMac = NULL;
+    tSirRetStatus status = eSIR_SUCCESS;
 
     if(pHalHandle == NULL)
         return eSIR_FAILURE;
@@ -239,7 +240,12 @@ tSirRetStatus macOpen(tHalHandle *pHalHandle, tHddHandle hHdd, tMacOpenParameter
     }
 
 
-    return peOpen(pMac, pMacOpenParms);
+    status = peOpen(pMac, pMacOpenParms);
+    if (eSIR_SUCCESS != status) {
+           sysLog(pMac, LOGE, FL("peOpen() failure"));
+           vos_mem_vfree(pMac);
+    }
+    return status;
 }
 
 /** -------------------------------------------------------------
