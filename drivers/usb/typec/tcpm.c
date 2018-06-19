1fa999beaef7f4a508b64ef5292a1cfd5a76a258
Kyle Tso <kyletso@google.com>
2018-06-19 21:10:25 +0800

    tcpm: Notify TCPC when usb_comm_capable is set
    If HVDCP_TIMEOUT is skipped in smb-lib, PD state machine will start
    before APSD finishes. If device is UFP and PD port partner supports USB
    data capability, device will not start USB controller because the
    charger type is unknown. To fix this, notify TCPC of the USB capability
    of the port partner. TCPC will start USB controller regardless of the
    charger type if usb_comm_capable is set.
    
    Bug: 116661237
    Bug: 109747735
    Change-Id: Ieb0c31ad1e5a012cdac17e221dceaa50b6e7de74
    Signed-off-by: Kyle Tso <kyletso@google.com>

--- drivers/usb/typec/tcpm.c
+++ drivers/usb/typec/tcpm.c
@@ -3009,6 +3009,8 @@ static void run_state_machine(struct tcpm_port *port)
 			tcpm_pd_send_control(port, PD_CTRL_ACCEPT);
 			port->usb_comm_capable = port->sink_request &
 						 RDO_USB_COMM;
+			/* Notify TCPC of usb_comm_capable. */
+			tcpm_set_attached_state(port, true);
 			tcpm_set_state(port, SRC_TRANSITION_SUPPLY,
 				       PD_T_SRC_TRANSITION);
 		}
@@ -3211,6 +3213,8 @@ static void run_state_machine(struct tcpm_port *port)
 		tcpm_set_pd_capable(port, true);
 		port->usb_comm_capable = port->source_caps[0] &
 					 PDO_FIXED_USB_COMM;
+		/* Notify TCPC of usb_comm_capable. */
+		tcpm_set_attached_state(port, true);
 		port->hard_reset_count = 0;
 		ret = tcpm_pd_send_request(port);
 		if (ret < 0) {
