e80d03c8f3c2465d9a87e7b3c73766e9f3f273e6
Connor O'Brien <connoro@google.com>
2018-06-26 17:45:55 -0700

    sched: cpufreq: stop ignoring util updates
    Eliminate the check for SCHED_CPUFREQ_WALT flag in
    cpufreq_update_util(), update calling code to stop using that flag,
    and replace its definition with a placeholder.
    
    Test: Trace shows more frequent util updates
    Bug: 110604715
    Change-Id: I3b74e950b984194f08ecbbf91872467a200c9d1d
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -3131,7 +3131,6 @@ void walt_irq_work(struct irq_work *irq_work)
 	struct rq *rq;
 	int cpu;
 	u64 wc;
-	int flag = SCHED_CPUFREQ_WALT;
 	bool is_migration = false;
 	int level = 0;
 
@@ -3171,16 +3170,16 @@ void walt_irq_work(struct irq_work *irq_work)
 
 	for_each_sched_cluster(cluster) {
 		for_each_cpu(cpu, &cluster->cpus) {
-			int nflag = flag;
+			int nflag = 0;
 
 			rq = cpu_rq(cpu);
 
 			if (is_migration) {
 				if (rq->notif_pending) {
-					nflag |= SCHED_CPUFREQ_INTERCLUSTER_MIG;
+					nflag = SCHED_CPUFREQ_INTERCLUSTER_MIG;
 					rq->notif_pending = false;
 				} else {
-					nflag |= SCHED_CPUFREQ_FORCE_UPDATE;
+					nflag = SCHED_CPUFREQ_FORCE_UPDATE;
 				}
 			}
 
