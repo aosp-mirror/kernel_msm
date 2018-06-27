dcfdbc78cfe85bac4abca015206d1be26f50f6f0
Minchan Kim <minchan@google.com>
2018-06-27 22:04:07 +0900

    mm: mm_event: add reclaim stat
    This patch adds page reclaim mm_event stat so that we could
    keep tracking [avg|max]_latency for the handling the event
    as well as count of the event.
    
    Direct reclaim latency is usually a most popular latency source
    caused by memory pressure so we need to track it down to hunt
    down application's jank problem.
    
    Bug: 116825025
    Change-Id: I215c3972f76389404da7c4806a776bf753daac01
    Signed-off-by: Minchan Kim <minchan@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2985,6 +2985,7 @@ out:
 unsigned long try_to_free_pages(struct zonelist *zonelist, int order,
 				gfp_t gfp_mask, nodemask_t *nodemask)
 {
+	ktime_t event_ts;
 	unsigned long nr_reclaimed;
 	struct scan_control sc = {
 		.nr_to_reclaim = SWAP_CLUSTER_MAX,
@@ -3005,6 +3006,7 @@ unsigned long try_to_free_pages(struct zonelist *zonelist, int order,
 	if (throttle_direct_reclaim(gfp_mask, zonelist, nodemask))
 		return 1;
 
+	mm_event_start(&event_ts);
 	trace_mm_vmscan_direct_reclaim_begin(order,
 				sc.may_writepage,
 				gfp_mask);
@@ -3012,6 +3014,7 @@ unsigned long try_to_free_pages(struct zonelist *zonelist, int order,
 	nr_reclaimed = do_try_to_free_pages(zonelist, &sc);
 
 	trace_mm_vmscan_direct_reclaim_end(nr_reclaimed);
+	mm_event_end(MM_RECLAIM, event_ts);
 
 	return nr_reclaimed;
 }
@@ -3614,9 +3617,13 @@ static int kswapd(void *p)
 		 * after returning from the refrigerator
 		 */
 		if (!ret) {
+			ktime_t event_ts;
+
 			trace_mm_vmscan_kswapd_wake(pgdat->node_id, order);
+			mm_event_start(&event_ts);
 			balanced_classzone_idx = balance_pgdat(pgdat, order,
 								classzone_idx);
+			mm_event_end(MM_RECLAIM, event_ts);
 		}
 	}
 
