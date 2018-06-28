3bf736212909c60c861baadfd84968f44493097c
John Dias <joaodias@google.com>
2018-06-27 17:44:59 -0700

    sched: fair: avoid little cpus due to sync, prev_bias
    Important threads can get forced to little cpu's
    when the sync or prev_bias hints are followed
    blindly. This patch adds a check to see whether
    those paths are forcing the task to a cpu that
    has less capacity than other cpu's available for
    the task. If so, we ignore the sync and prev_bias
    and allow the scheduler to make a free decision.
    
    Bug: 110714207
    Change-Id: Ie38061cd78a4e19c8783ffd460cc81caebfe9c75
    Signed-off-by: John Dias <joaodias@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7305,6 +7305,29 @@ bias_to_waker_cpu(struct task_struct *p, int cpu, struct cpumask *rtg_target)
 	       task_fits_max(p, cpu);
 }
 
+static inline bool
+task_is_boosted(struct task_struct *p) {
+#ifdef CONFIG_CGROUP_SCHEDTUNE
+	return schedtune_task_boost(p) > 0;
+#else
+	return get_sysctl_sched_cfs_boost() > 0;
+#endif
+}
+
+/*
+ * Check whether cpu is in the fastest set of cpu's that p should run on.
+ * If p is boosted, prefer that p runs on a faster cpu; otherwise, allow p
+ * to run on any cpu.
+ */
+static inline bool
+cpu_is_in_target_set(struct task_struct *p, int cpu)
+{
+	int first_cpu = start_cpu(task_is_boosted(p));
+	int next_usable_cpu = cpumask_next(first_cpu - 1, tsk_cpus_allowed(p));
+	return cpu >= next_usable_cpu || next_usable_cpu >= nr_cpu_ids;
+}
+
+
 #define SCHED_SELECT_PREV_CPU_NSEC	2000000
 #define SCHED_FORCE_CPU_SELECTION_NSEC	20000000
 
@@ -7317,6 +7340,10 @@ bias_to_prev_cpu(struct task_struct *p, struct cpumask *rtg_target)
 #else
 	u64 ms = sched_clock();
 #endif
+
+	if (!cpu_is_in_target_set(p, prev_cpu)) {
+		return false;
+	}
 
 	if (cpu_isolated(prev_cpu) || !idle_cpu(prev_cpu))
 		return false;
@@ -7391,11 +7418,10 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu,
 	schedstat_inc(this_rq()->eas_stats.secb_attempts);
 
 	rcu_read_lock();
+	boosted = task_is_boosted(p);
 #ifdef CONFIG_CGROUP_SCHEDTUNE
-	boosted = schedtune_task_boost(p) > 0;
 	prefer_idle = schedtune_prefer_idle(p) > 0;
 #else
-	boosted = get_sysctl_sched_cfs_boost() > 0;
 	prefer_idle = 0;
 #endif
 
@@ -7536,7 +7562,8 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 			bool about_to_idle = (cpu_rq(cpu)->nr_running < 2);
 
 			if (sysctl_sched_sync_hint_enable && sync &&
-			    !_wake_cap && about_to_idle)
+			    !_wake_cap && about_to_idle &&
+			    cpu_is_in_target_set(p, cpu))
 				return cpu;
 		}
 
