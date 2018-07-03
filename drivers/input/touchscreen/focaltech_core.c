08fb731368f91bba0105773490bdbf59ec015b76
billh_chen <BillH_Chen@compal.com>
2018-07-03 20:58:06 +0800

    touch: add turn on/off function to support touch lock feature
    bug: 64523299
    Change-Id: I82aa1b447962adbbe41c44762bf5bbf64b791926
    Signed-off-by: billh_chen <BillH_Chen@compal.com>

--- drivers/input/touchscreen/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_core.c
@@ -136,6 +136,7 @@ unsigned int suspend_resume_recovery_count = 0;
 unsigned int plam_recovery_count = 0;
 
 bool ts_pwr_disabled = false;
+static int g_ts_boot = 1;
 
 struct irq_desc *fts_irq_desc = NULL;
 
@@ -880,13 +881,19 @@ static int fts_power_on(struct fts_ts_data *data, bool on)
 			"Regulator vdd enable failed rc=%d\n", rc);
 		return rc;
 	}
-	regulator_set_optimum_mode(data->vcc_i2c, 100000);
-	rc = regulator_enable(data->vcc_i2c);
-	if (rc) {
-		dev_err(&data->client->dev,
-			"Regulator vcc_i2c enable failed rc=%d\n", rc);
-		regulator_set_optimum_mode(data->vdd, 100);
-		regulator_disable(data->vdd);
+
+	if (g_ts_boot) {
+		FTS_DBG("Enable regulator output for I2C only once\n");
+
+		regulator_set_optimum_mode(data->vcc_i2c, 100000);
+		rc = regulator_enable(data->vcc_i2c);
+		if (rc) {
+			dev_err(&data->client->dev,
+				"Regulator vcc_i2c enable failed rc=%d\n", rc);
+			regulator_set_optimum_mode(data->vdd, 100);
+			regulator_disable(data->vdd);
+		} else
+			g_ts_boot = 0;
 	}
 
 	return rc;
@@ -898,18 +905,6 @@ power_off:
 		dev_err(&data->client->dev,
 			"Regulator vdd disable failed rc=%d\n", rc);
 		return rc;
-	}
-	regulator_set_optimum_mode(data->vcc_i2c, 100);
-	rc = regulator_disable(data->vcc_i2c);
-	if (rc) {
-		dev_err(&data->client->dev,
-			"Regulator vcc_i2c disable failed rc=%d\n", rc);
-		regulator_set_optimum_mode(data->vdd, 100000);
-		rc = regulator_enable(data->vdd);
-		if (rc) {
-			dev_err(&data->client->dev,
-				"Regulator vdd enable failed rc=%d\n", rc);
-		}
 	}
 
 	return rc;
@@ -1334,6 +1329,67 @@ int fts_ts_disable(struct device *dev)
 }
 
 /*******************************************************************************
+*  Name: fts_ts_open
+*  Brief:
+*  Input:
+*  Output:
+*  Return:
+*******************************************************************************/
+static int fts_ts_open(struct input_dev *input_dev)
+{
+	struct fts_ts_data *data;
+	int err = 0;
+
+	pr_err("[fts]%s, start\n", __func__);
+
+	if (input_dev == NULL)
+		pr_err("[fts]%s, input_dev is NULL\n", __func__);
+	else
+		data = input_get_drvdata(input_dev);
+
+	pr_err("[fts]%s, after input\n", __func__);
+
+	err = fts_ts_resume(&(data->client->dev));
+
+	pr_err("[fts]%s, finish, err : %d\n", __func__, err);
+	if (err < 0) {
+		pr_err("[fts]%s, TP power on fail, err : %d\n", __func__, err);
+		return err;
+	}
+	ts_pwr_disabled = false;
+
+	return 0;
+}
+
+/*******************************************************************************
+*  Name: fts_ts_close
+*  Brief:
+*  Input:
+*  Output:
+*  Return:
+*******************************************************************************/
+static void fts_ts_close(struct input_dev *input_dev)
+{
+	struct fts_ts_data *data;
+	int err = 0;
+
+	pr_err("[fts]%s, start\n", __func__);
+
+	data = input_get_drvdata(input_dev);
+	pr_err("[fts]%s, after input\n", __func__);
+
+	err = fts_ts_disable(&data->client->dev);
+
+	pr_err("[fts]%s, finish, err : %d\n", __func__, err);
+	if (err < 0) {
+		pr_err("[fts]%s, TP power disable fail, err : %d\n",
+			__func__, err);
+		return;
+	}
+	ts_pwr_disabled = true;
+}
+
+/*******************************************************************************
 *  Name: fts_ts_suspend
 *  Brief:
 *  Input:
@@ -1351,7 +1407,7 @@ int fts_ts_suspend(struct device *dev)
 		return 0;
 	}
 
-	printk(KERN_ERR "[fts]%s, start\n", __func__);
+	pr_err("[fts]%s, start\n", __func__);
 
 	if (data->suspended) {
 		dev_info(dev, "Already in suspend state\n");
@@ -1404,6 +1460,9 @@ int fts_ts_resume(struct device *dev)
 		dev_dbg(dev, "Already in awake state\n");
 		return 0;
 	}
+
+	pr_err("[fts]%s, start\n", __func__);
+
 #ifdef CONFIG_TOUCHSCREEN_FTS_PSENSOR
 	if (fts_psensor_support_enabled() && data->pdata->psensor_support &&
 	    device_may_wakeup(dev) && data->psensor_pdata->tp_psensor_opened) {
@@ -2040,6 +2099,9 @@ static int fts_ts_probe(struct i2c_client *client,
 	input_dev->name = "fts_ts";
 	input_dev->id.bustype = BUS_I2C;
 	input_dev->dev.parent = &client->dev;
+
+	input_dev->open = fts_ts_open;
+	input_dev->close = fts_ts_close;
 
 	input_set_drvdata(input_dev, data);
 	i2c_set_clientdata(client, data);
