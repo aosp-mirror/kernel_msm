761f350153108ee01b96a180fd8b6b2714ce8e1b
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2018-07-18 15:55:47 +0530

    qcacld-2.0: Possible buffer overflow in wma_stats_ext_event_handler
    Check for stats ext info data len does not take TLV header
    size into account which could lead to buffer overflow
    when copying data where TLV header size is taken into
    account.
    
    Fix is to subtract TLV header size and stats_ext_info
    size from max allowed size when validating stats ext
    info data length.
    
    Bug: 111128838
    Change-Id: I34e35a0aab396af3d93a0f61e0ab6a2da09f22ab
    CRs-Fixed: 2280404
    Signed-off-by: Roger Wang <wangroger@google.com>

--- drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
@@ -7251,7 +7251,7 @@ static int wma_stats_ext_event_handler(void *handle, u_int8_t *event_buf,
 	alloc_len += stats_ext_info->data_len;
 
 	if (stats_ext_info->data_len > (WMA_SVC_MSG_MAX_SIZE -
-	    sizeof(*stats_ext_info))) {
+	    WMI_TLV_HDR_SIZE - sizeof(*stats_ext_info))) {
 		WMA_LOGE("Excess data_len:%d", stats_ext_info->data_len);
 		VOS_ASSERT(0);
 		return -EINVAL;
