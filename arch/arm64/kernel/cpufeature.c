bf2074dc3b53a82156950c4d6754e28a788e41e9
David Lin <dtwlin@google.com>
2018-07-26 16:20:45 -0700

    Revert "arm64: errata: Calling enable functions for CPU errata too"
    This reverts commit 22c95b83811224763af40fd571f6e8b23fc9636d.
    
    Bug: 111785512
    Signed-off-by: David Lin <dtwlin@google.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -860,7 +860,8 @@ void update_cpu_capabilities(const struct arm64_cpu_capabilities *caps,
  * Run through the enabled capabilities and enable() it on all active
  * CPUs
  */
-void __init enable_cpu_capabilities(const struct arm64_cpu_capabilities *caps)
+static void __init
+enable_cpu_capabilities(const struct arm64_cpu_capabilities *caps)
 {
 	int i;
 
@@ -1019,7 +1020,6 @@ void __init setup_cpu_features(void)
 
 	/* Set the CPU feature capabilies */
 	setup_feature_capabilities();
-	enable_errata_workarounds();
 	setup_cpu_hwcaps();
 
 	/* Advertise that we have computed the system capabilities */
