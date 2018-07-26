d92688ba35c498b7ab0e8ba43fb7df5e4437dc52
David Lin <dtwlin@google.com>
2018-07-26 16:20:32 -0700

    Revert "arm64: cpufeature: Pass capability structure to ->enable callback"
    This reverts commit f6f50a1a6e76505d941f5272dfd91f20089cd532.
    
    Bug: 111785512
    Signed-off-by: David Lin <dtwlin@google.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -872,8 +872,7 @@ void __init enable_cpu_capabilities(const struct arm64_cpu_capabilities *caps)
 			 * uses an IPI, giving us a PSTATE that disappears when
 			 * we return.
 			 */
-			stop_machine(caps[i].enable, (void *)&caps[i],
-							cpu_online_mask);
+			stop_machine(caps[i].enable, NULL, cpu_online_mask);
 }
 
 #ifdef CONFIG_HOTPLUG_CPU
@@ -988,7 +987,7 @@ void verify_local_cpu_capabilities(void)
 		if (!feature_matches(__raw_read_system_reg(caps[i].sys_reg), &caps[i]))
 			fail_incapable_cpu("arm64_features", &caps[i]);
 		if (caps[i].enable)
-			caps[i].enable((void *)&caps[i]);
+			caps[i].enable(NULL);
 	}
 
 	for (i = 0, caps = arm64_hwcaps; caps[i].matches; i++) {
