a1690b0f4dc890ace358e1622ff1abd83a53c7b4
y00230200 <yanghongliang.yang@huawei.com>
2018-07-30 10:22:09 +0800

    udp: consistently apply ufo or fragmentation
    CVE-2017-1000
    
    Change-Id: I7d474b4c0398a327e0998b2fa2017633a05753b1
    Signed-off-by: y00230200 <yanghongliang.yang@huawei.com>

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1296,11 +1296,11 @@ emsgsize:
 
 	skb = skb_peek_tail(&sk->sk_write_queue);
 	cork->length += length;
-	if (((length > mtu) ||
-	     (skb && skb_is_gso(skb))) &&
+	if ((skb && skb_is_gso(skb)) ||
+	    ((length > mtu) &&
 	    (sk->sk_protocol == IPPROTO_UDP) &&
 	    (rt->dst.dev->features & NETIF_F_UFO) &&
-	    (sk->sk_type == SOCK_DGRAM)) {
+	    (sk->sk_type == SOCK_DGRAM))) {
 		err = ip6_ufo_append_data(sk, getfrag, from, length,
 					  hh_len, fragheaderlen,
 					  transhdrlen, mtu, flags, rt);
