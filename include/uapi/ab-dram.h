348fe5b96fd5ea6c8997c3fe834efa015d237599
Alex Tang <pztang@google.com>
2018-08-01 15:27:27 -0700

    misc: add airbrush dram manager
    ION like DRAM manager for Airbrush remote memory.
    
    Change-Id: Ia0ad38ef79776c2e27b531c347604323423ee250
    Signed-off-by: Alex Tang <pztang@google.com>
    Signed-off-by: Adam Hampson <ahampson@google.com>

---
+++ include/uapi/ab-dram.h
@@ -0,0 +1,24 @@
+/*
+ * Driver interface for the Paintbox Image Processing Unit
+ *
+ * Copyright (C) 2018 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __AB_DRAM_UAPI_H__
+#define __AB_DRAM_UAPI_H__
+
+#include <linux/ioctl.h>
+#include <linux/types.h>
+
+#define AB_DRAM_ALLOCATE_MEMORY		_IOW('a', 1, size_t)
+
+#endif /* __AB_DRAM_UAPI_H__ */
