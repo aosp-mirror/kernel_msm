035c86d7e1e74c2aa7aaf90198d64f4546fc1cd7
Minchan Kim <minchan@google.com>
2018-08-06 15:12:44 +0900

    mm: mm_event: add special kernel allocation stat
    Record the count of special page allocation on the process context.
    
    This patch aims for accounting of special page allocation which
    consumed a lot by android system.
    At this moment, ION system heap is good candidate(it could cover
    other kernel allocation in future).
    With that, we could keep tracking burst kernel allocation owner
    so that it would be useful to find places caused by lmk, reclaim,
    compaction latency.
    
    Bug: 116825025
    Change-Id: I5942fd940d98baa2eb814f66b076cb37ecd3b4aa
    Signed-off-by: Minchan Kim <minchan@google.com>
    (cherry picked from commit 0fc7b97b79c73902f1fe0a60efc8b551cb6a3251)
    Signed-off-by: Minchan Kim <minchan@google.com>

--- mm/mm_event.c
+++ mm/mm_event.c
@@ -49,6 +49,12 @@ void mm_event_end(enum mm_event_type event, ktime_t start)
 	record_stat();
 }
 
+void mm_event_count(enum mm_event_type event, int count)
+{
+	current->mm_event[event].count += count;
+	record_stat();
+}
+
 static struct dentry *mm_event_root;
 
 static int period_ms_set(void *data, u64 val)
