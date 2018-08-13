4e4b17405617e8659be598a3115ea887048dca47
mark_pu <mark_pu@compal.com>
2018-08-13 10:11:08 +0800

    gauge: modify TWM default soc value to 16%
    Issue tracker #111769252, need set 55 mAh = 1%
    Battery capacity is 340 mAh.
    So, SOC value is 55/340 = 16%
    
    bug: 111769252
    Change-Id: Ief027213b9eb8762a5168bcd45d06b543e33f794

--- drivers/power/qpnp-fg-gen3.c
+++ drivers/power/qpnp-fg-gen3.c
@@ -150,7 +150,7 @@
 #define DEFAULT_COLD_CC 100
 #define DEFAULT_HOT_FV 4305
 #define DEFAULT_HOT_CC 100
-#define DEFAULT_TWM_SOC_VALUE 15
+#define DEFAULT_TWM_SOC_VALUE 16
 
 static int fg_decode_voltage_15b(struct fg_sram_param *sp,
 	enum fg_sram_param_id id, int val);
