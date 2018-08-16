2c9e83356b406e26b1c55ef36928bea8ef33706b
Randall Huang <huangrandall@google.com>
2018-08-16 14:54:54 +0800

    Do not enable clock scaling and sanitize
    Test: boot to home, check /sys/kernel/debug/mmc0/scale
    Change-Id: Ib9db84d930213f4f5969a9f5768736cb06b124ef
    Signed-off-by: Randall Huang <huangrandall@google.com>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -5051,8 +5051,6 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 	msm_host->mmc->caps2 |= msm_host->pdata->caps2;
 	msm_host->mmc->caps2 |= MMC_CAP2_BOOTPART_NOACC;
 	msm_host->mmc->caps2 |= MMC_CAP2_HS400_POST_TUNING;
-	msm_host->mmc->caps2 |= MMC_CAP2_CLK_SCALE;
-	msm_host->mmc->caps2 |= MMC_CAP2_SANITIZE;
 	msm_host->mmc->caps2 |= MMC_CAP2_MAX_DISCARD_SIZE;
 	msm_host->mmc->caps2 |= MMC_CAP2_SLEEP_AWAKE;
 	msm_host->mmc->pm_caps |= MMC_PM_KEEP_POWER | MMC_PM_WAKE_SDIO_IRQ;
