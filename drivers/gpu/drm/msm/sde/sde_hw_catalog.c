709af5bf80439f560e9341f9fc24da5ab4bfd19b
linpeter <linpeter@google.com>
2018-08-31 17:34:45 +0800

    drm/msm/sde: enable concurrent writeback support
    Set default to true for capturing frame on primary through WB
    using concurrent writeback HW feature
    
    Bug: 113632926
    
    cmd: adb shell vndservice call display.qservice 21 i32 1 i32 1 i32 2
    
    Change-Id: I971b1378c830c882696c26a14751604fc40ab083
    Signed-off-by: linpeter <linpeter@google.com>

--- drivers/gpu/drm/msm/sde/sde_hw_catalog.c
+++ drivers/gpu/drm/msm/sde/sde_hw_catalog.c
@@ -3260,6 +3260,7 @@ static int _sde_hardware_pre_caps(struct sde_mdss_cfg *sde_cfg, uint32_t hw_rev)
 	} else if (IS_SDM670_TARGET(hw_rev)) {
 		/* update sdm670 target here */
 		sde_cfg->has_wb_ubwc = true;
+		sde_cfg->has_cwb_support = true;
 		sde_cfg->perf.min_prefill_lines = 24;
 		sde_cfg->vbif_qos_nlvl = 8;
 		sde_cfg->ts_prefill_rev = 2;
