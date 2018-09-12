17b8eb8b8cca80d42f6962236a26e9d714d2a98b
Randall Huang <huangrandall@google.com>
2018-09-12 15:35:15 +0800

    security: pfe: Set DUN size accroding to file system and storage type
    EXT4 FS and F2FS has different way of setting Data Unit Number (DUN)
    size value for UFS and eMMC storage devices. EXT4 FS uses sector number
    while F2FS uses inode|pgidx. Check Storage and file system type
    before setting the DUN value in Inline Crypto Engine (ICE).
    
    Bug: 113643905
    Change-Id: If822863893fc0725a5ff0410e7418c352ad70fc1
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

--- security/pfe/pfk_ecryptfs.h
+++ security/pfe/pfk_ecryptfs.h
@@ -26,7 +26,9 @@ int pfk_ecryptfs_parse_inode(const struct bio *bio,
 	const struct inode *inode,
 	struct pfk_key_info *key_info,
 	enum ice_cryto_algo_mode *algo,
-	bool *is_pfe);
+	bool *is_pfe,
+	unsigned int *data_unit,
+	const char *storage_type);
 
 bool pfk_ecryptfs_allow_merge_bio(const struct bio *bio1,
 	const struct bio *bio2, const struct inode *inode1,
