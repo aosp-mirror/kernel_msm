1010b7ce1446513b1a937a7dd02e95990d15d75c
Suzuki K Poulose <suzuki.poulose@arm.com>
2018-09-20 13:17:39 -0600

    BACKPORT: coresight: platform: Fix leaking device reference
    Backport: context code is different from upstream.
    
    We don't drop the reference on the remote device while parsing the
    connection, held by bus_find_device(). Fix this by duplicating the
    device name and dropping the reference.
    
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Kim Phillips <kim.phillips@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (Upstream commit 5111e749c775ebae6f7d39c6f836cb3f06c7b938).
    
    Bug: 145687602
    Change-Id: I0f32ca365e8b393fef31c4396b7b72ff7a17103c
    Signed-off-by: Yabin Cui <yabinc@google.com>

--- drivers/hwtracing/coresight/of_coresight.c
+++ drivers/hwtracing/coresight/of_coresight.c
@@ -168,7 +168,9 @@ static int of_coresight_parse_endpoint(struct device *dev,
 		ret = of_property_read_string(rparent, "coresight-name",
 					      &pdata->child_names[i]);
 		if (ret)
-			pdata->child_names[i] = dev_name(rdev)
+			pdata->child_names[i] = devm_kstrdup(dev,
+							     dev_name(rdev),
+							     GFP_KERNEL);
 		pdata->child_ports[i] = rendpoint.id;
 		/* Connection record updated */
 		ret = 1;
@@ -178,6 +180,8 @@ static int of_coresight_parse_endpoint(struct device *dev,
 		of_node_put(rparent);
 	if (rport)
 		of_node_put(rport);
+	if (rdev)
+		put_device(rdev);
 
 	return ret;
 }
