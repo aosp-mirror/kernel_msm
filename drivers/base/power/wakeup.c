6d5295c90d49320b08da46b1c623aab95fb9c036
Miguel de Dios <migueldedios@google.com>
2018-09-24 19:04:39 -0700

    drivers: wakeup: Add wakeup reason logging
    Log kernel wakeup reasons the same as how we did in previous devices.
    
    Change-Id: Ib3ce99342496ad59d348720722848d9597dafae3
    Signed-off-by: Miguel de Dios <migueldedios@google.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -935,6 +935,7 @@ void pm_system_irq_wakeup(unsigned int irq_number)
 			else if (desc->action && desc->action->name)
 				name = desc->action->name;
 
+			log_base_wakeup_reason(irq_number);
 			pr_warn("%s: %d triggered %s\n", __func__,
 					irq_number, name);
 
