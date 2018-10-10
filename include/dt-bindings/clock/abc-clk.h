e4d5718c79c92e251e6135a3a37fbbc52588faa2
Adam Hampson <ahampson@google.com>
2018-10-09 17:44:43 -0700

    dt-bindings: clock: update airbrush bindings
    Bug: 117494005
    Change-Id: I5849b22e832e7ca6314349261c93884540475a00
    Signed-off-by: Adam Hampson <ahampson@google.com>

--- include/dt-bindings/clock/abc-clk.h
+++ include/dt-bindings/clock/abc-clk.h
@@ -27,7 +27,8 @@
 #define MOUT_IPU_PLL_IPU		2
 #define DOUT_IPU_IPU_PLL_CLK_DIV_1	3
 #define MOUT_IPU_AONCLK_PLLCLK1		4
-#define IPU_NR_CLK			5
+#define CLK_BLK_IPU_UID_IPU_IPCLKPORT_CLK_IPU 5
+#define IPU_NR_CLK			6
 
 /*MIF*/
 #define MIF_NR_CLK			1
@@ -37,6 +38,7 @@
 #define MOUT_TPU_PLL_TPU		2
 #define DOUT_TPU_TPU_PLL_DIV_CLK_1	3
 #define MOUT_TPU_AONCLK_PLLCLK		4
-#define TPU_NR_CLK			5
+#define CLK_BLK_TPU_UID_TPU_IPCLKPORT_CLK_TPU 5
+#define TPU_NR_CLK			6
 
 #endif
