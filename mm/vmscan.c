3b612f4f745bd50f9f473bdf452776ab7c12100a
Johannes Weiner <hannes@cmpxchg.org>
2018-10-26 15:06:04 -0700

    BACKPORT: mm: workingset: tell cache transitions from workingset thrashing
    Refaults happen during transitions between workingsets as well as in-place
    thrashing.  Knowing the difference between the two has a range of
    applications, including measuring the impact of memory shortage on the
    system performance, as well as the ability to smarter balance pressure
    between the filesystem cache and the swap-backed workingset.
    
    During workingset transitions, inactive cache refaults and pushes out
    established active cache.  When that active cache isn't stale, however,
    and also ends up refaulting, that's bonafide thrashing.
    
    Introduce a new page flag that tells on eviction whether the page has been
    active or not in its lifetime.  This bit is then stored in the shadow
    entry, to classify refaults as transitioning or thrashing.
    
    How many page->flags does this leave us with on 32-bit?
    
    	20 bits are always page flags
    
    	21 if you have an MMU
    
    	23 with the zone bits for DMA, Normal, HighMem, Movable
    
    	29 with the sparsemem section bits
    
    	30 if PAE is enabled
    
    	31 with this patch.
    
    So on 32-bit PAE, that leaves 1 bit for distinguishing two NUMA nodes.  If
    that's not enough, the system can switch to discontigmem and re-gain the 6
    or 7 sparsemem section bits.
    
    Link: http://lkml.kernel.org/r/20180828172258.3185-3-hannes@cmpxchg.org
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Daniel Drake <drake@endlessm.com>
    Tested-by: Suren Baghdasaryan <surenb@google.com>
    Cc: Christopher Lameter <cl@linux.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Johannes Weiner <jweiner@fb.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Enderborg <peter.enderborg@sony.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit 1899ad18c6072d689896badafb81267b0a1092a4)
    
    Conflicts:
            mm/workingset.c
    
    (manual merge in workingset_refault, used inc_node_state instead of
    inc_lruvec_state)
    
    Bug: 111308141
    Test: modified lmkd to use PSI and tested using lmkd_unit_test
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ifad7cfee8bb98b62b223fe82241639af19b7dddb

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2094,6 +2094,7 @@ static void shrink_active_list(unsigned long nr_to_scan,
 		}
 
 		ClearPageActive(page);	/* we are de-activating */
+		SetPageWorkingset(page);
 		list_add(&page->lru, &l_inactive);
 	}
 
