1106208cc22d75781b2f954d0fab4f0a1357f4ef
Nick Desaulniers <ndesaulniers@google.com>
2018-11-05 16:11:16 -0800

    ANDROID: sched/fair: fix -Wdeclaration-after-statement
    Fixes the warning: kernel/sched/fair.c:5935:17: warning: ISO
          C90 forbids mixing declarations and code [-Wdeclaration-after-statement]
            struct cfs_rq *cfs_rq;
                           ^
    
    Fixes commit 1f31b828343b ("ANDROID: sched/fair: Cleanup cpu_util{_wake}()")
    
    Change-Id: Idc1a9c73e3379ff96d2e9aa7c3070708a40d9b01
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5911,6 +5911,7 @@ struct energy_env {
  */
 static unsigned long cpu_util_wake(int cpu, struct task_struct *p)
 {
+	struct cfs_rq *cfs_rq;
 	unsigned int util;
 
 #ifdef CONFIG_SCHED_WALT
@@ -5932,7 +5933,6 @@ static unsigned long cpu_util_wake(int cpu, struct task_struct *p)
 #ifdef CONFIG_SCHED_WALT
 	util = max_t(long, cpu_util(cpu) - task_util(p), 0);
 #else
-	struct cfs_rq *cfs_rq;
 
 	cfs_rq = &cpu_rq(cpu)->cfs;
 	util = READ_ONCE(cfs_rq->avg.util_avg);
