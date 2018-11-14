90dc2b0558cbd56db991baf954e62971cc47caa2
Thierry Strudel <tstrudel@google.com>
2018-11-13 17:22:11 -0800

    smb-lib: force "wireless" psy offline on last thermal mitigation index
    in case DT defines wlc mitigation current values, for the disconnection
    of the "wireless" power supply. Ensure it is online when setting lower
    indexes.
    
    Test: echo 4 > /sys/class/thermal/cooling_device2/cur_state
    Bug: 119501863
    Change-Id: Idffee9212dfa89ab5c218c98cfa5f9953a3ac512
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -2233,6 +2233,8 @@ int smblib_set_prop_batt_status(struct smb_charger *chg,
 int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 				const union power_supply_propval *val)
 {
+	union power_supply_propval pval;
+
 	if (val->intval < 0)
 		return -EINVAL;
 
@@ -2244,13 +2246,22 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 
 	chg->system_temp_level = val->intval;
 
+	if (chg->wlc_thermal_mitigation && !chg->wlc_psy)
+		chg->wlc_psy = power_supply_get_by_name("wireless");
+
 	if (chg->system_temp_level == chg->thermal_levels) {
 		if (chg->thermal_mitigation)
 			vote(chg->chg_disable_votable,
 			     THERMAL_DAEMON_VOTER, true, 0);
-		if (chg->wlc_thermal_mitigation)
+		if (chg->wlc_thermal_mitigation) {
 			vote(chg->dc_icl_votable,
 			     THERMAL_DAEMON_VOTER, true, 0);
+			if (chg->wlc_psy) {
+				pval.intval = 0;
+				power_supply_set_property(chg->wlc_psy,
+					POWER_SUPPLY_PROP_ONLINE, &pval);
+			}
+		}
 		return 0;
 	}
 
@@ -2259,9 +2270,15 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 		if (chg->thermal_mitigation)
 			vote(chg->fcc_votable,
 			     THERMAL_DAEMON_VOTER, false, 0);
-		if (chg->wlc_thermal_mitigation)
+		if (chg->wlc_thermal_mitigation) {
+			if (chg->wlc_psy) {
+				pval.intval = 1;
+				power_supply_set_property(chg->wlc_psy,
+					POWER_SUPPLY_PROP_ONLINE, &pval);
+			}
 			vote(chg->dc_icl_votable,
 			     THERMAL_DAEMON_VOTER, false, 0);
+		}
 		return 0;
 	}
 
@@ -2269,10 +2286,15 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 		vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, true,
 		     chg->thermal_mitigation[chg->system_temp_level]);
 
-	if (chg->wlc_thermal_mitigation)
+	if (chg->wlc_thermal_mitigation) {
+		if (chg->wlc_psy) {
+			pval.intval = 1;
+			power_supply_set_property(chg->wlc_psy,
+					POWER_SUPPLY_PROP_ONLINE, &pval);
+		}
 		vote(chg->dc_icl_votable, THERMAL_DAEMON_VOTER, true,
 		     chg->wlc_thermal_mitigation[chg->system_temp_level]);
-
+	}
 	return 0;
 }
 
