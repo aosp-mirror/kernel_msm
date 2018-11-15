a152f7c59edf5a7b4cff84c5f9d9d37df02041c4
chened <chened@google.com>
2018-11-15 14:36:16 +0800

    msm: camera: update OIS firmware Ver0E.
    Update new firmware header, API, and add the rule to check module maker.
    
    Test: update fw version on dev board passed
    Bug: 118901571
    Change-Id: I99e832cb5bf9e3157285708aa3006c6da9102ae8
    Signed-off-by: chened <chened@google.com>

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_fw_update/fw_update.c
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_fw_update/fw_update.c
@@ -4,14 +4,16 @@
 
 #include "fw_update.h"
 
-#define OIS_CUR_FW_VERSION           0x08
+#define OIS_CUR_FW_VERSION           0x0E
 #define OIS_COMPONENT_I2C_ADDR_WRITE 0x7C
-#define MK_SHARP                     0x04170000
-#define MK_LGIT                      0x09170000
+#define OIS_REARWIDE_I2C_ADDR_WRITE  0x76
+#define OIS_REARTELE_I2C_ADDR_WRITE  0x78
+#define MV_SHARP                     0x04170000
+#define MV_LGIT                      0x09170000
+#define MA_WIDE                      0x00000200
+#define MA_TELE                      0x00000300
 
 static struct camera_io_master *g_io_master_info;
-bool g_first = true;
-
 
 void RamWrite32A(UINT_16 RamAddr, UINT_32 RamData)
 {
@@ -31,7 +33,8 @@ void RamWrite32A(UINT_16 RamAddr, UINT_32 RamData)
 
 	rc = camera_io_dev_write(io_master_info, &i2c_reg_settings);
 	if (rc < 0)
-		CAM_ERR(CAM_SENSOR, "[OISFW] %s : write failed\n", __func__);
+		CAM_ERR(CAM_SENSOR, "[OISFW] %s : write i2c failed, sid:0x%x\n",
+			__func__, io_master_info->cci_client->sid);
 }
 
 void RamRead32A(UINT_16 RamAddr, UINT_32 *ReadData)
@@ -42,7 +45,8 @@ void RamRead32A(UINT_16 RamAddr, UINT_32 *ReadData)
 		ReadData, CAMERA_SENSOR_I2C_TYPE_WORD,
 		CAMERA_SENSOR_I2C_TYPE_DWORD);
 	if (rc < 0)
-		CAM_ERR(CAM_SENSOR, "[OISFW]:%s read i2c failed\n", __func__);
+		CAM_ERR(CAM_SENSOR, "[OISFW]:%s read i2c failed, sid:0x%x\n",
+			__func__, g_io_master_info->cci_client->sid);
 }
 
 void WitTim(UINT_16 UsWitTim)
@@ -128,44 +132,50 @@ int checkHighLevelCommand(int cnt)
 int doFWupdate(UINT_16 CAL_ID, UINT_32 MODULE_MAKER)
 {
 	int rc = 0;
+	UINT_8 code_vendor = 0;
+	UINT_8 code_header = 0;
+	UINT_32 module_vendor = MODULE_MAKER & 0xFFFF0000;
+	UINT_32 module_angle = MODULE_MAKER & 0x0000FF00;
 
-	if (MODULE_MAKER == MK_SHARP) {
-		if (CAL_ID == 0x0) {
-			CAM_INFO(CAM_SENSOR,
-				"[OISFW]:%s not to update FW because of unknown CAL_ID.",
-				__func__);
-			rc = -EINVAL;
-		} else if (CAL_ID == 0x1) {
-			CAM_INFO(CAM_SENSOR,
-				"[OISFW]:%s F40_FlashDownload(0, 4, 0 )",
-				__func__);
-			rc = F40_FlashDownload(0, 4, 0);
+	if (module_vendor == MV_SHARP)
+		code_vendor = 4;
+	else if (module_vendor == MV_LGIT)
+		code_vendor = 9;
+
+	if (module_angle == MA_WIDE && CAL_ID == 0x1)
+		code_header = 0x01;
+	else if (module_angle == MA_WIDE && (CAL_ID == 0x2 || CAL_ID != 0x0))
+		code_header = 0x02;
+	else if (module_angle == MA_TELE && CAL_ID == 0x1)
+		code_header = 0x81;
+	else if (module_angle == MA_TELE && (CAL_ID == 0x2 || CAL_ID != 0x0))
+		code_header = 0x82;
+
+	if (code_vendor != 0 && code_header != 0) {
+		F40_BootMode();
+		/* Target slave address to 0x7C/0x7D */
+		g_io_master_info->cci_client->sid =
+				OIS_COMPONENT_I2C_ADDR_WRITE >> 1;
+		CAM_INFO(CAM_SENSOR,
+			"[OISFW]: BootMode(sid:0x%x), " \
+			"start flash download(0x%x, 0x%x)\n",
+			g_io_master_info->cci_client->sid,
+			code_vendor, code_header);
+		rc = F40_FlashDownload(0, code_vendor, code_header);
+		/* Replace the I2C slave address with OIS component */
+		if (module_angle == MA_WIDE) {
+			g_io_master_info->cci_client->sid =
+				OIS_REARWIDE_I2C_ADDR_WRITE >> 1;
 		} else {
-			CAM_INFO(CAM_SENSOR,
-				"[OISFW]:%s F40_FlashDownload(0, 4, 1 )",
-				__func__);
-			rc = F40_FlashDownload(0, 4, 1);
+			g_io_master_info->cci_client->sid =
+				OIS_REARTELE_I2C_ADDR_WRITE >> 1;
 		}
-	} else if (MODULE_MAKER == MK_LGIT) {
-		if (CAL_ID == 0x0) {
-			CAM_INFO(CAM_SENSOR,
-				"[OISFW]:%s not to update FW because of unknown CAL_ID.",
-				__func__);
-			rc = -EINVAL;
-		} else if (CAL_ID == 0x1) {
-			CAM_INFO(CAM_SENSOR,
-				"[OISFW]:%s F40_FlashDownload(0, 9, 0 )",
-				__func__);
-			rc = F40_FlashDownload(0, 9, 0);
-		} else {
-			CAM_INFO(CAM_SENSOR,
-				"[OISFW]:%s F40_FlashDownload(0, 9, 1 )",
-				__func__);
-			rc = F40_FlashDownload(0, 9, 1);
-		}
+		WitTim(50);
 	} else {
 		CAM_ERR(CAM_SENSOR,
-			"[OISFW]:%s unknown module maker.", __func__);
+			"[OISFW]:%s unknown module_maker(0x%x) " \
+			"or code_header(0x%x).",
+			__func__, module_vendor, CAL_ID);
 		rc = -EINVAL;
 	}
 
@@ -176,7 +186,6 @@ int doFWupdate(UINT_16 CAL_ID, UINT_32 MODULE_MAKER)
 		CAM_ERR(CAM_SENSOR,
 			"[OISFW]%s: OIS FW update failed rc = %d.\n",
 			__func__, rc);
-		rc = -EINVAL;
 	}
 
 	return rc;
@@ -194,10 +203,10 @@ bool checkOISFWversion(UINT_16 *cal_id, UINT_32 *module_maker)
 
 	RamAddr = 0x8000;
 	RamRead32A(RamAddr, &UlReadVal);
+	*module_maker = UlReadVal;
 	FW_version = UlReadVal & 0xFF;
-	*module_maker = UlReadVal & 0xFFFF0000;
 	CAM_INFO(CAM_SENSOR, "[OISFW]:%s module_version =  0x%02x.\n",
-		__func__, UlReadVal);
+		__func__, FW_version);
 
 	RamAddr = 0x8004;
 	RamRead32A(RamAddr, &UlReadVal);
@@ -232,21 +241,25 @@ int checkOISFWUpdate(struct cam_sensor_ctrl_t *s_ctrl)
 	UINT_16 cal_id;
 	UINT_32 module_maker;
 
-	if (g_first != true) {
-		CAM_INFO(CAM_SENSOR, "[OISFW]%s: No need.\n", __func__);
+	if (s_ctrl->sensordata->slave_info.sensor_id != 0x363 &&
+		s_ctrl->sensordata->slave_info.sensor_id != 0x481) {
+		CAM_INFO(CAM_SENSOR,
+			"[OISFW]%s: SensorId:0x%x no need update.\n",
+			__func__, s_ctrl->sensordata->slave_info.sensor_id);
 		return 0;
 	}
-	g_first = false;
-
-	CAM_INFO(CAM_SENSOR, "[OISFW]:%s 1. sid = %d\n", __func__,
-		s_ctrl->io_master_info.cci_client->sid);
 
 	/* Bcakup the I2C slave address */
 	cci_client_sid_backup = s_ctrl->io_master_info.cci_client->sid;
 
 	/* Replace the I2C slave address with OIS component */
-	s_ctrl->io_master_info.cci_client->sid =
-		OIS_COMPONENT_I2C_ADDR_WRITE >> 1;
+	if (s_ctrl->sensordata->slave_info.sensor_id == 0x363) {
+		s_ctrl->io_master_info.cci_client->sid =
+			OIS_REARWIDE_I2C_ADDR_WRITE >> 1;
+	} else {
+		s_ctrl->io_master_info.cci_client->sid =
+			OIS_REARTELE_I2C_ADDR_WRITE >> 1;
+	}
 
 	g_io_master_info = &(s_ctrl->io_master_info);
 	WitTim(100);
@@ -275,8 +288,6 @@ int checkOISFWUpdate(struct cam_sensor_ctrl_t *s_ctrl)
 	/* Restore the I2C slave address */
 	s_ctrl->io_master_info.cci_client->sid =
 		cci_client_sid_backup;
-	CAM_INFO(CAM_SENSOR, "[OISFW]:%s 2. sid = %d\n", __func__,
-		s_ctrl->io_master_info.cci_client->sid);
 
 	CAM_INFO(CAM_SENSOR, "[OISFW]:%s rc = %d\n", __func__, rc);
 
