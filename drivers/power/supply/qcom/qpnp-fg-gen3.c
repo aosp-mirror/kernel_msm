e6220b7b12f0a61f74cf585dff3cc80b96c45722
Wasb Liu <wasbliu@google.com>
2018-11-16 17:01:42 +0800

    qpnp-fg-gen3: don't report soc until soc is ready
    It's to fix that FAKE_SOC was showed in charger mode
    
    Bug: 119444759
    Change-Id: I00f8de8ca84a65ef0a24749a9f4382cc04bd4dce
    Signed-off-by: Wasb Liu <wasbliu@google.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -897,9 +897,14 @@ static int fg_get_prop_capacity(struct fg_chip *chip, int *val)
 		return 0;
 	}
 
-	if (chip->battery_missing || !chip->soc_reporting_ready) {
+	if (chip->battery_missing) {
 		*val = BATT_MISS_SOC;
 		return 0;
+	}
+
+	if (!chip->soc_reporting_ready) {
+		*val = BATT_MISS_SOC;
+		return -EBUSY;
 	}
 
 	if (is_batt_empty(chip)) {
