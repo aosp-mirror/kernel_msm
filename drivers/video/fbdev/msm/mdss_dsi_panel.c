44d140463bf7006448084ef95c46adba46cfcb75
chester_sun <Chester_Sun@compal.com>
2018-12-05 13:25:01 +0800

    display: parse the panel DT for HBM commands
    In order to read commands of high brightness mode from
    the panel device tree.
    
    Change-Id: Ie48c0e1bf20f783852f72a34d91d4cfec485cbc1
    Signed-off-by: chester_sun <Chester_Sun@compal.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -2932,6 +2932,13 @@ static int mdss_panel_parse_dt(struct device_node *np,
 		"qcom,mdss-dsi-idle-off-command",
 		"qcom,mdss-dsi-idle-off-command-state");
 
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->hbm0_on_cmds,
+		"qcom,mdss-dsi-hbm0-on-command", NULL);
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->hbm1_on_cmds,
+		"qcom,mdss-dsi-hbm1-on-command", NULL);
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->hbm_off_cmds,
+		"qcom,mdss-dsi-hbm-off-command", NULL);
+
 	rc = of_property_read_u32(np, "qcom,mdss-dsi-idle-fps", &tmp);
 	pinfo->mipi.frame_rate_idle = (!rc ? tmp : 60);
 
