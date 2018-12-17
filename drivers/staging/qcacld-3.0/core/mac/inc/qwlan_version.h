3609ecae81ab69e44356ba55ee2364f01e168932
Srinivas Girigowda <sgirigow@codeaurora.org>
2018-12-17 15:36:02 -0800

    Release 5.2.1.1L
    Release 5.2.1.1L
    
    Bug: 119512579
    CRs-Fixed: 774533
    Change-Id: Iea571590c19a9c3ec2a4f645a457b1816681bec5
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>
    Signed-off-by: Sunil Ravi <sunilravi@google.com>

--- drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
+++ drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
@@ -44,6 +44,6 @@
 #define QWLAN_VERSION_EXTRA            "K"
 #define QWLAN_VERSION_BUILD            1
 
-#define QWLAN_VERSIONSTR               "5.2.1.1K"
+#define QWLAN_VERSIONSTR               "5.2.1.1L"
 
 #endif /* QWLAN_VERSION_H */
