eb882ae2e64f73cfd3b54c82d84f85b9d4f62c9d
chened <chened@google.com>
2018-12-28 12:22:29 +0800

    msm: camera: Increase eeprom block size
    Increase max number of eeprom memory block.
    
    Bug:122025893
    Test: Build pass, test camera on F2 Protp
    Change-Id: Id74c51b5b493b78e7ce884f240f4d3c386c1bf5b
    Signed-off-by: chened <chened@google.com>

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_eeprom/cam_eeprom_dev.h
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_eeprom/cam_eeprom_dev.h
@@ -34,7 +34,7 @@
 #define PROPERTY_MAXSIZE 32
 
 #define MSM_EEPROM_MEMORY_MAP_MAX_SIZE         80
-#define MSM_EEPROM_MAX_MEM_MAP_CNT             43
+#define MSM_EEPROM_MAX_MEM_MAP_CNT             47
 #define MSM_EEPROM_MEM_MAP_PROPERTIES_CNT      8
 
 enum cam_eeprom_state {
