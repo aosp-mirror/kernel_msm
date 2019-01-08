ce72b3d821631265c8c54a9f4ad1831f05222612
Martin Liu <liumartin@google.com>
2019-01-08 13:10:58 +0800

    Revert "mm: refer the backing device for readahead"
    This reverts commit c2f121124108ffb83b9ff1833c80b64a6c6393a5.
    
    Bug: 122216621
    Test: boot
    Change-Id: I9c26524dd2c2f90597e933abf945e66f80452a4c
    Signed-off-by: Martin Liu <liumartin@google.com>

--- mm/readahead.c
+++ mm/readahead.c
@@ -371,8 +371,7 @@ ondemand_readahead(struct address_space *mapping,
 		   bool hit_readahead_marker, pgoff_t offset,
 		   unsigned long req_size)
 {
-	unsigned long max = min((unsigned long)ra->ra_pages,
-		inode_to_bdi(mapping->host)->ra_pages);
+	unsigned long max = ra->ra_pages;
 	pgoff_t prev_offset;
 
 	/*
