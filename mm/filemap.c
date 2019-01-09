920d10af571cc40cac869ea995a044dff419f566
Kirill A. Shutemov <kirill@shutemov.name>
2019-01-09 22:01:17 +0000

    BACKPORT: FROMGIT: filemap-drop-the-mmap_sem-for-all-blocking-operations-fix
    Here's a fixup for "filemap: drop the mmap_sem for all blocking operations".
    
    do_sync_mmap_readahead() drops mmap_sem now, so by the time of
    dereferencing vmf->vma for count_memcg_event_mm() the VMA can be gone.
    
    (cherry pick from mmotm: it's not a git tree)
    Bug: 122231176
    Link: http://lkml.kernel.org/r/20181228235106.okk3oastsnpxusxs@kshutemo-mobl1
    Cc: Josef Bacik <josef@toxicpanda.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Tejun Heo <tj@kernel.org>
    Change-Id: Ib1f4a6cc9e0f3e19b427a3de1ee9b4f568635f34
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: Miguel de Dios <migueldedios@google.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -2473,11 +2473,11 @@ int filemap_fault(struct vm_fault *vmf)
 			vmf->vma, vmf->flags, ra, file, page, offset);
 	} else if (!page) {
 		/* No page in the page cache at all */
-		fpin = do_sync_mmap_readahead(
-			vmf->vma, vmf->flags, ra, file, offset);
 		count_vm_event(PGMAJFAULT);
 		count_memcg_event_mm(vmf->vma->vm_mm, PGMAJFAULT);
 		ret = VM_FAULT_MAJOR;
+		fpin = do_sync_mmap_readahead(
+			vmf->vma, vmf->flags, ra, file, offset);
 retry_find:
 		page = pagecache_get_page(mapping, offset,
 					  FGP_CREAT|FGP_FOR_MMAP,
