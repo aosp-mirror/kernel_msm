1261b9a97f43700781de1241f3edd600bb3054fd
Connor O'Brien <connoro@google.com>
2019-02-11 18:07:54 -0800

    cpufreq: schedutil: fix check for stale utilization values
    Part of the fix from commit d86ab9cff8b9 ("cpufreq: schedutil: use now
    as reference when aggregating shared policy requests") is reversed in
    commit 05d2ca242067 ("cpufreq: schedutil: Ignore CPU load older than
    WALT window size") due to a porting mistake. Restore it while keeping
    the relevant change from the latter patch.
    
    Bug: 117438867
    Test: build & boot
    Change-Id: I21399be760d7c8e2fff6c158368a285dc6261647
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -486,7 +486,6 @@ static unsigned int sugov_next_freq_shared(struct sugov_cpu *sg_cpu, u64 time)
 {
 	struct sugov_policy *sg_policy = sg_cpu->sg_policy;
 	struct cpufreq_policy *policy = sg_policy->policy;
-	u64 last_freq_update_time = sg_policy->last_freq_update_time;
 	unsigned long util = 0, max = 1;
 	unsigned int j;
 
@@ -502,7 +501,7 @@ static unsigned int sugov_next_freq_shared(struct sugov_cpu *sg_cpu, u64 time)
 		 * enough, don't take the CPU into account as it probably is
 		 * idle now (and clear iowait_boost for it).
 		 */
-		delta_ns = last_freq_update_time - j_sg_cpu->last_update;
+		delta_ns = time - j_sg_cpu->last_update;
 		if (delta_ns > stale_ns) {
 			j_sg_cpu->iowait_boost = 0;
 			j_sg_cpu->iowait_boost_pending = false;
