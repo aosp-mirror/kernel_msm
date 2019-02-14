0ec4b221b0591936f0a2b5d5ce619caa80a5658a
Badhri Jagan Sridharan <badhri@google.com>
2019-02-13 19:33:13 -0800

    power_supply: Add DC_RESET power-supply property
    Add property POWER_SUPPLY_PROP_DC_RESET to notify the removal
    of wireless charger
    
    Bug: 77536221
    Change-Id: Id51bce7b6b37021a6fd18008aee6078c3fb8f260
    Signed-off-by: Sahil Chandna <chandna@codeaurora.org>
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -441,6 +441,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(voltage_vph),
 	POWER_SUPPLY_ATTR(adapter_details),
 	POWER_SUPPLY_ATTR(otg_fastroleswap),
+	POWER_SUPPLY_ATTR(dc_reset),
 	/* Charge pump properties */
 	POWER_SUPPLY_ATTR(cp_status1),
 	POWER_SUPPLY_ATTR(cp_status2),
