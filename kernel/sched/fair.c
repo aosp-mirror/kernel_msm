c59e694da449d10bd3444dc50dc016047198380d
Rick Yiu <rickyiu@google.com>
2019-02-27 14:52:37 +0800

    sched/fair: avoid little cpus due to sync blindly
    Check whether cpu is in the fastest set of cpu's
    that p should run on to avoid bad sync behavior.
    
    Bug: 117438867
    Test: boot to home, operation normal
    Change-Id: Ie5d175b97fd2084199d63e157a8229b6ca829ea2
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8265,7 +8265,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 		int _wake_cap = wake_cap(p, cpu, prev_cpu);
 		int _cpus_allowed = cpumask_test_cpu(cpu, &p->cpus_allowed);
 
-		if (_cpus_allowed) {
+		if (_cpus_allowed && cpu_is_in_target_set(p, cpu)) {
 			bool about_to_idle = sysctl_sched_sync_hint_enable &&
 				wake_affine_idle(sd, p, cpu, prev_cpu, sync);
 
