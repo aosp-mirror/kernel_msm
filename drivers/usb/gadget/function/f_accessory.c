0db8c3747883e6ce311766948eae7597c0325b54
Howard Yen <howardyen@google.com>
2019-03-20 16:11:57 +0800

    usb: gadget: f_accessory: Avoid double-free
    Pick up from pa/1035269 and pa/1036710 to avoid potential crash issue.
    
    Bug: 128961681
    Test: boot, gadget accessory work
    Change-Id: I3c99b1d1a55701ef06695aa23d38f8272a0aaed7
    Signed-off-by: Howard Yen <howardyen@google.com>

--- drivers/usb/gadget/function/f_accessory.c
+++ drivers/usb/gadget/function/f_accessory.c
@@ -607,8 +607,10 @@ fail:
 	pr_err("acc_bind() could not allocate requests\n");
 	while ((req = req_get(dev, &dev->tx_idle)))
 		acc_request_free(req, dev->ep_in);
-	for (i = 0; i < RX_REQ_MAX; i++)
+	for (i = 0; i < RX_REQ_MAX; i++) {
 		acc_request_free(dev->rx_req[i], dev->ep_out);
+		dev->rx_req[i] = NULL;
+	}
 	return -1;
 }
 
@@ -1105,8 +1107,10 @@ acc_function_unbind(struct usb_configuration *c, struct usb_function *f)
 
 	while ((req = req_get(dev, &dev->tx_idle)))
 		acc_request_free(req, dev->ep_in);
-	for (i = 0; i < RX_REQ_MAX; i++)
+	for (i = 0; i < RX_REQ_MAX; i++) {
 		acc_request_free(dev->rx_req[i], dev->ep_out);
+		dev->rx_req[i] = NULL;
+	}
 
 	acc_hid_unbind(dev);
 }
