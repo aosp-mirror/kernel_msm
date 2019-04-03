e10d6c2e7258c1ede3289c39ffcd579f02fb0af5
josephjang <josephjang@google.com>
2019-04-03 11:52:43 +0800

    reboot: debug: Add reboot timeout handler to debug reboot hang issue.
    The reboot timeout timer default is 15 seconds. Timeout handler will dump
    blocked stack to lastKmsg and trigger kernel panic to reboot
    device if kernel stuck at restart/shutdown process.
    Note:
      1. User can use following command to simulate reboot hang for verification.
         Command: adb reboot debug-reboot-hang
      2. If CONFIG_DEBUG_REBOOT_DEFAULT_ON is not defined, user will need to issue
         following command to enable reboot debug feature first.
         Command: #echo 1 > /sys/module/debug_reboot/parameters/enable
    
    bug: 128561401
    Change-Id: If2e8a9af391ac44a874e78a50811d24125607a6f
    Signed-off-by: josephjang <josephjang@google.com>

--- drivers/power/reset/debug-reboot.c
+++ drivers/power/reset/debug-reboot.c
@@ -17,9 +17,14 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/reboot.h>
+#include <linux/time.h>
+#include <linux/sched/debug.h>
 
 #define PANIC_REBOOT_CMD "debug-reboot-panic"
 #define WDOG_REBOOT_CMD "debug-reboot-watchdog"
+#define HANG_REBOOT_CMD "debug-reboot-hang"
+#define REBOOT_TIMER_TIMEOUT_MS 15000
+static struct timer_list reboot_timer;
 
 #ifdef CONFIG_DEBUG_REBOOT_DEFAULT_ON
 static bool enable = 1;
@@ -46,6 +51,17 @@ static void debug_reboot_panic(void)
 	pr_err("debug-reboot: Trigger panic failed!\n");
 }
 
+static void debug_reboot_hang(void)
+{
+	pr_info("debug-reboot: Hogging a CPU now\n");
+
+	for(;;)
+		;
+
+	/* Should not reach here */
+	pr_err("debug-reboot: Simulate reboot hang fail !\n");
+}
+
 static void debug_reboot_watchdog(void)
 {
 	pr_info("debug-reboot: Trigger a watchdog\n");
@@ -57,16 +73,38 @@ static void debug_reboot_watchdog(void)
 
 	/* Disable interrupts and loop forever */
 	local_irq_disable();
-	for(;;)
-		;
+	debug_reboot_hang();
 
 	/* Should not reach here */
 	pr_err("debug-reboot: Trigger watchdog failed!\n");
 }
 
+static void reboot_timeout(unsigned long unused)
+{
+	pr_info("debug-reboot: Reboot timer (%d seconds) timeout - Show All Blocked State\n"
+		, (REBOOT_TIMER_TIMEOUT_MS/1000));
+
+	show_state_filter(TASK_UNINTERRUPTIBLE);
+
+	debug_reboot_panic();
+}
+
+static void add_reboot_timer(void)
+{
+	pr_info("debug-reboot: Create reboot monitor timer now\n");
+
+	reboot_timer.function = reboot_timeout;
+	reboot_timer.expires = jiffies +
+		msecs_to_jiffies(REBOOT_TIMER_TIMEOUT_MS);
+	add_timer(&reboot_timer);
+}
+
 static int debug_reboot_notify(struct notifier_block *nb,
 				unsigned long code, void *cmd)
 {
+	/*add a timer to monitor reboot or shutdown process.*/
+	add_reboot_timer();
+
 	if (enable && code == SYS_RESTART) {
 		if (cmd != NULL) {
 			if (!strcmp((char *)cmd, PANIC_REBOOT_CMD)) {
@@ -75,7 +113,11 @@ static int debug_reboot_notify(struct notifier_block *nb,
 			} else if (!strcmp((char *)cmd, WDOG_REBOOT_CMD)) {
 				debug_reboot_watchdog();
 				/* Should not return... */
+			} else if (!strcmp((char *)cmd, HANG_REBOOT_CMD)) {
+				debug_reboot_hang();
+				/* Should not return... */
 			}
+
 		}
 	}
 	return NOTIFY_DONE;
@@ -83,16 +125,21 @@ static int debug_reboot_notify(struct notifier_block *nb,
 
 static struct notifier_block debug_reboot_nb = {
 	.notifier_call = debug_reboot_notify,
+	.priority = INT_MAX,
 };
 
 static int __init debug_reboot_init(void)
 {
+	init_timer(&reboot_timer);
+
 	return register_reboot_notifier(&debug_reboot_nb);
 }
 
 static void __exit debug_reboot_exit(void)
 {
 	unregister_reboot_notifier(&debug_reboot_nb);
+
+	del_timer_sync(&reboot_timer);
 }
 
 module_init(debug_reboot_init);
