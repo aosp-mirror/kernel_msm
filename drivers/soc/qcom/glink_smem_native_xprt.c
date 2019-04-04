18e3e92caf9fba74085fbc3d3e3fb586bcfcdcea
Hardik Arya <harya@codeaurora.org>
2019-04-04 15:24:25 +0530

    soc: qcom: Return error in case of invalid input
    Currently we are returning success case zero in
    case of invalid input. The patch returns error
    properly in case of invalid input.
    
    Bug: 127513124
    Change-Id: I6e3903381ef7ec8d0dd536623d10213460d0ae8e
    Signed-off-by: Hardik Arya <harya@codeaurora.org>

--- drivers/soc/qcom/glink_smem_native_xprt.c
+++ drivers/soc/qcom/glink_smem_native_xprt.c
@@ -441,8 +441,10 @@ static int fifo_read(struct edge_info *einfo, void *_data, int len)
 	uint32_t fifo_size = einfo->rx_fifo_size;
 	uint32_t n;
 
-	if (read_index >= fifo_size || write_index >= fifo_size)
-		return 0;
+	if (read_index >= fifo_size || write_index >= fifo_size) {
+		WARN_ON_ONCE(1);
+		return -EINVAL;
+	}
 	while (len) {
 		ptr = einfo->rx_fifo + read_index;
 		if (read_index <= write_index)
@@ -486,8 +488,10 @@ static uint32_t fifo_write_body(struct edge_info *einfo, const void *_data,
 	uint32_t fifo_size = einfo->tx_fifo_size;
 	uint32_t n;
 
-	if (read_index >= fifo_size || *write_index >= fifo_size)
-		return 0;
+	if (read_index >= fifo_size || *write_index >= fifo_size) {
+		WARN_ON_ONCE(1);
+		return -EINVAL;
+	}
 	while (len) {
 		ptr = einfo->tx_fifo + *write_index;
 		if (*write_index < read_index) {
