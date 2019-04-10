df099413fa9834ea174685cd41d1c0511dcb397c
Minchan Kim <minchan@google.com>
2019-04-10 19:45:31 +0900

    Revert "bdi: introduce BDI_CAP_SYNCHRONOUS_IO"
    This reverts commit 88dc54b93467cb3b776c3c06be21071263b88812.
    
    Conflicts:
    	drivers/block/zram/zram_drv.c
    
    Bug: 130276584
    Change-Id: I2412186a5b0a9e9e1ee77ff0e2047d7c4bb16849
    Signed-off-by: Minchan Kim <minchan@google.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1937,7 +1937,7 @@ static int zram_add(void)
 		blk_queue_max_write_zeroes_sectors(zram->disk->queue, UINT_MAX);
 
 	zram->disk->queue->backing_dev_info->capabilities |=
-			(BDI_CAP_STABLE_WRITES | BDI_CAP_SYNCHRONOUS_IO);
+					BDI_CAP_STABLE_WRITES;
 	disk_to_dev(zram->disk)->groups = zram_disk_attr_groups;
 	add_disk(zram->disk);
 
