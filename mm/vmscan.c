98eb0f4f6987932c9fb08e695b13c4acdaf03635
Minchan Kim <minchan@google.com>
2019-04-10 17:35:22 +0900

    Revert "mm: make shrink_page_list with pages work from multiple zones"
    This reverts commit 54d3ac19a0375e92a1dc8c02bc93aac074c56613.
    
    Bug: 130273563
    Change-Id: Idf0a4c7e5ea8da0e506acfbd212f0f5b28f858d4
    Signed-off-by: Minchan Kim <minchan@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -985,8 +985,6 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 			goto keep;
 
 		VM_BUG_ON_PAGE(PageActive(page), page);
-		if (pgdat)
-			VM_BUG_ON_PAGE(page_pgdat(page) != pgdat, page);
 
 		sc->nr_scanned++;
 
@@ -1075,8 +1073,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 			/* Case 1 above */
 			if (current_is_kswapd() &&
 			    PageReclaim(page) &&
-			    (pgdat &&
-				test_bit(PGDAT_WRITEBACK, &pgdat->flags))) {
+			    test_bit(PGDAT_WRITEBACK, &pgdat->flags)) {
 				nr_immediate++;
 				goto activate_locked;
 
@@ -1198,8 +1195,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 			 */
 			if (page_is_file_cache(page) &&
 			    (!current_is_kswapd() || !PageReclaim(page) ||
-			     (pgdat &&
-				!test_bit(PGDAT_DIRTY, &pgdat->flags)))) {
+			     !test_bit(PGDAT_DIRTY, &pgdat->flags))) {
 				/*
 				 * Immediately reclaim when written back.
 				 * Similar in principal to deactivate_page()
