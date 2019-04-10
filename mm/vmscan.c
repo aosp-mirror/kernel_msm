bc3b9b55872bba342608a7e6bec083692bd04f9e
Minchan Kim <minchan@google.com>
2019-04-10 17:36:45 +0900

    Revert "mm: prevent to write out dirty page in CMA by may_writepage"
    This reverts commit 16ef91359c6141170a1d98ef2405012dce08d00a.
    
    Bug: 130273563
    Change-Id: I8ad3a0f8a68c61b357967e1d4366f063c2e1bdfc
    Signed-off-by: Minchan Kim <minchan@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -973,7 +973,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 		struct address_space *mapping;
 		struct page *page;
 		int may_enter_fs;
-		enum page_references references = PAGEREF_RECLAIM;
+		enum page_references references = PAGEREF_RECLAIM_CLEAN;
 		bool dirty, writeback;
 
 		cond_resched();
@@ -1368,8 +1368,6 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 		.gfp_mask = GFP_KERNEL,
 		.priority = DEF_PRIORITY,
 		.may_unmap = 1,
-		/* Doesn't allow to write out dirty page */
-		.may_writepage = 0,
 	};
 	unsigned long ret;
 	struct page *page, *next;
