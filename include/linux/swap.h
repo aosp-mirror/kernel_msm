05b4c09dd68d04bb3aea7afc12fea8f64a0babf0
Minchan Kim <minchan@google.com>
2019-04-10 17:57:48 +0900

    Revert "mm: swap: swap ratio support"
    This reverts commit e518593c49e56c9baa7f22808cac13a2ba5d8026.
    
    Signed-off-by: Minchan Kim <minchan@google.com>
    
    Conflicts:
    	include/linux/swap.h
    	include/linux/swapfile.h
    	mm/swap_ratio.c
    
    Bug: 130276584
    Change-Id: I93d9cef5ad26e849338e440ee2e1e18f13313229

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -178,7 +178,6 @@ enum {
 
 #define SWAP_CLUSTER_MAX 32UL
 #define COMPACT_CLUSTER_MAX SWAP_CLUSTER_MAX
-#define SWAPFILE_CLUSTER	256
 
 #define SWAP_MAP_MAX	0x3e	/* Max duplication count, in first swap_map */
 #define SWAP_MAP_BAD	0x3f	/* Note pageblock is bad, in first swap_map */
@@ -273,8 +272,6 @@ struct swap_info_struct {
 					 */
 	struct work_struct discard_work; /* discard worker */
 	struct swap_cluster_list discard_clusters; /* discard clusters list */
-	unsigned int write_pending;
-	unsigned int max_writes;
 	struct plist_node avail_lists[0]; /*
 					   * entries in swap_avail_heads, one
 					   * entry per node.
@@ -360,8 +357,6 @@ extern unsigned long mem_cgroup_shrink_node(struct mem_cgroup *mem,
 						unsigned long *nr_scanned);
 extern unsigned long shrink_all_memory(unsigned long nr_pages);
 extern int vm_swappiness;
-extern int sysctl_swap_ratio;
-extern int sysctl_swap_ratio_enable;
 extern int remove_mapping(struct address_space *mapping, struct page *page);
 extern unsigned long vm_total_pages;
 
--- include/linux/swapfile.h
+++ include/linux/swapfile.h
@@ -8,13 +8,8 @@
  */
 extern spinlock_t swap_lock;
 extern struct plist_head swap_active_head;
-extern spinlock_t swap_avail_lock;
-extern struct plist_head *swap_avail_heads;
 extern struct swap_info_struct *swap_info[];
 extern int try_to_unuse(unsigned int, bool, unsigned long);
-extern int swap_ratio(struct swap_info_struct **si, int node);
-extern void setup_swap_ratio(struct swap_info_struct *p, int prio);
-extern bool is_swap_ratio_group(int prio);
 extern unsigned long generic_max_swapfile_size(void);
 extern unsigned long max_swapfile_size(void);
 
