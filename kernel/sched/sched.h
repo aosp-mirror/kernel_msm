75942e9e6ea3b9c494eb82f3666754db68346556
Connor O'Brien <connoro@google.com>
2019-04-15 14:12:42 -0700

    sched: delete unused & buggy function definitions
    None of these functions does what its name implies when
    CONFIG_SCHED_WALT=n. While all are currently unused, future patches
    could introduce subtle bugs by calling any of them from non WALT
    specific code. Delete the functions so it's obvious if new callers are
    added.
    
    Test: build kernel
    Change-Id: I2e8d6198021147cee4cd2bdbd850724b456ee3e1
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2998,7 +2998,6 @@ task_in_cum_window_demand(struct rq *rq, struct task_struct *p)
 }
 
 static inline bool hmp_capable(void) { return false; }
-static inline bool is_max_capacity_cpu(int cpu) { return true; }
 static inline bool is_min_capacity_cpu(int cpu)
 {
 #ifdef CONFIG_SMP
@@ -3007,22 +3006,6 @@ static inline bool is_min_capacity_cpu(int cpu)
 #else
 	return true;
 #endif
-}
-
-static inline int
-preferred_cluster(struct sched_cluster *cluster, struct task_struct *p)
-{
-	return 1;
-}
-
-static inline struct sched_cluster *rq_cluster(struct rq *rq)
-{
-	return NULL;
-}
-
-static inline u64 scale_load_to_cpu(u64 load, int cpu)
-{
-	return load;
 }
 
 #ifdef CONFIG_SMP
@@ -3055,11 +3038,6 @@ static inline int update_preferred_cluster(struct related_thread_group *grp,
 }
 
 static inline void add_new_task_to_grp(struct task_struct *new) {}
-
-static inline int same_freq_domain(int src_cpu, int dst_cpu)
-{
-	return 1;
-}
 
 static inline void clear_reserved(int cpu) { }
 static inline int alloc_related_thread_groups(void) { return 0; }
@@ -3101,13 +3079,6 @@ static inline bool early_detection_notify(struct rq *rq, u64 wallclock)
 {
 	return 0;
 }
-
-#ifdef CONFIG_SMP
-static inline unsigned int power_cost(int cpu, u64 demand)
-{
-	return SCHED_CAPACITY_SCALE;
-}
-#endif
 
 static inline void note_task_waking(struct task_struct *p, u64 wallclock) { }
 static inline void walt_map_freq_to_load(void) { }
