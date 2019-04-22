6df920a8faba5ba41ee48340aabedaaee6c4a6ea
Minchan Kim <minchan@google.com>
2019-04-23 01:07:18 +0800

    mm: mm_event: add swapin stat
    Many embedded devices use zram as swap. Compared to storage swap
    (e.g. UFS), swapin from zram(ie., decompression) is extremly fast
    so it might be not major fault but minor. So this patch provides
    swapin latency tracking to distinguish them from storage major
    fault.
    
    Bug: 80168800
    Bug: 116825053
    Bug: 153442668
    Test: boot
    Change-Id: I1c32430e32a051916ede5219bd5f40a9002652bc
    Signed-off-by: Minchan Kim <minchan@google.com>
    (cherry picked from commit 9de60685b99e819e57fb6c2bf636d5ac973e4f01)
    Signed-off-by: Martin Liu <liumartin@google.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3113,7 +3113,7 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 unlock:
 	pte_unmap_unlock(vmf->pte, vmf->ptl);
 out:
-	return ret;
+	return ret | VM_FAULT_SWAP;
 out_nomap:
 	mem_cgroup_cancel_charge(page, memcg, false);
 	pte_unmap_unlock(vmf->pte, vmf->ptl);
@@ -3125,7 +3125,7 @@ out_release:
 		unlock_page(swapcache);
 		put_page(swapcache);
 	}
-	return ret;
+	return ret | VM_FAULT_SWAP;
 }
 
 /*
