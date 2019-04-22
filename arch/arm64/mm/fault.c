a0e12fc1d30776aa70b6350c2f0f6b399ecccf2e
Minchan Kim <minchan@google.com>
2019-04-23 00:38:18 +0800

    mm: mm_event: add page fault stat
    This patch add major and minor fault mm_event stat so that we could
    keep tracking [avg|max]_latency for the handling the event
    as well as count of the event.
    
    With major fault, we could see how long the IO is delayed. It's very
    tightly coupled with application's latency.
    
    With major+minor fault, we could see how many of pages are allocated
    for the process in the period. It would help to see memory spike.
    
    Bug: 80168800
    Bug: 116825053
    Test: boot
    Change-Id: I8a4434493e3ec291227961939a24c3d57a18fd5b
    Signed-off-by: Minchan Kim <minchan@google.com>
    (cherry picked from commit b202d56eda972909ca4dc969693f2ca45b8ef6e3)
    Signed-off-by: Martin Liu <liumartin@google.com>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -408,6 +408,10 @@ static int __kprobes do_page_fault(unsigned long addr, unsigned int esr,
 	int fault, sig, code, major = 0;
 	unsigned long vm_flags = VM_READ | VM_WRITE;
 	unsigned int mm_flags = FAULT_FLAG_ALLOW_RETRY | FAULT_FLAG_KILLABLE;
+	bool was_major = false;
+	ktime_t event_ts;
+
+	mm_event_start(&event_ts);
 
 	if (notify_page_fault(regs, esr))
 		return 0;
@@ -508,6 +512,7 @@ retry:
 		 * that point.
 		 */
 		if (major) {
+			was_major = true;
 			tsk->maj_flt++;
 			perf_sw_event(PERF_COUNT_SW_PAGE_FAULTS_MAJ, 1, regs,
 				      addr);
@@ -516,6 +521,11 @@ retry:
 			perf_sw_event(PERF_COUNT_SW_PAGE_FAULTS_MIN, 1, regs,
 				      addr);
 		}
+
+		if (was_major)
+			mm_event_end(MM_MAJ_FAULT, event_ts);
+		else
+			mm_event_end(MM_MIN_FAULT, event_ts);
 
 		return 0;
 	}
