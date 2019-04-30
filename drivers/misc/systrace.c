aa704fd37c014c36d1c7193f6aa1bfdf33a910d8
Ian <ianchang@google.com>
2019-04-30 10:49:45 -0700

    misc: add kernel systrace capability
    All ATRACE_XXX trace messages are identified and parsed
    at chrome tracing UI to correlate kernel activities and events
    to other system and userspace events.
    
    Enabling the capability depends on CONFIG_TRACING.
    
    Bug: 111098596
    Test: Build, flash and boot successfully.
          Specific trace event is generated at the path of
          /d/tracing/events/systrace/...
    Change-Id: I0a7ed2f76b04124a81b5eed5d9dc97bac31f9861
    Signed-off-by: Ian <ianchang@google.com>
    Signed-off-by: Wei Wang <wvw@google.com>

---
+++ drivers/misc/systrace.c
@@ -0,0 +1,17 @@
+/*
+ * Systrace Kernel Tracing Integration
+ *
+ * Copyright (C) 2019 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#define CREATE_TRACE_POINTS
+#include <trace/events/systrace.h>
