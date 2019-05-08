9a52bb88dc3736dca0c813151a4dd3475deab740
Naman Padhiar <npadhiar@codeaurora.org>
2019-05-08 16:34:37 +0530

    icnss: Add check on msa region
    When icnss receive server arrive it send wlfw_msa_mem_info_send_sync_msg
    QMI request to firmware and in response expect range of addresses and size
    to be mapped. Add condition to check whether addresses in response falls
    under valid range otherwise it asserts.
    
    Change-Id: I9a8542cb6c3b3cefe112d1f08a76dd2eadf68d2f
    Signed-off-by: Naman Padhiar <npadhiar@codeaurora.org>

--- drivers/soc/qcom/icnss.c
+++ drivers/soc/qcom/icnss.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -298,6 +298,7 @@ static int wlfw_msa_mem_info_send_sync_msg(void)
 	struct wlfw_msa_info_req_msg_v01 req;
 	struct wlfw_msa_info_resp_msg_v01 resp;
 	struct msg_desc req_desc, resp_desc;
+	uint64_t max_mapped_addr;
 
 	if (!penv || !penv->wlfw_clnt) {
 		ret = -ENODEV;
@@ -342,7 +343,19 @@ static int wlfw_msa_mem_info_send_sync_msg(void)
 		goto out;
 	}
 
+	max_mapped_addr = penv->msa_pa + penv->msa_mem_size;
 	for (i = 0; i < resp.mem_region_info_len; i++) {
+		if (resp.mem_region_info[i].size > penv->msa_mem_size ||
+		    resp.mem_region_info[i].region_addr > max_mapped_addr ||
+		    resp.mem_region_info[i].region_addr < penv->msa_pa ||
+		    resp.mem_region_info[i].size +
+		    resp.mem_region_info[i].region_addr > max_mapped_addr) {
+			pr_debug("Received out of range Addr: 0x%llx Size: 0x%x\n",
+					resp.mem_region_info[i].region_addr,
+					resp.mem_region_info[i].size);
+			ret = -EINVAL;
+			goto fail_unwind;
+		}
 		penv->icnss_mem_region[i].reg_addr =
 			resp.mem_region_info[i].region_addr;
 		penv->icnss_mem_region[i].size =
@@ -357,6 +370,9 @@ static int wlfw_msa_mem_info_send_sync_msg(void)
 			 penv->icnss_mem_region[i].secure_flag);
 	}
 
+fail_unwind:
+	memset(&penv->icnss_mem_region[0], 0,
+	    sizeof(penv->icnss_mem_region[0]) * i);
 out:
 	return ret;
 }
