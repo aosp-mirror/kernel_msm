46f987861d5ee2513d31329e970f35966721189c
Chris Lew <clew@codeaurora.org>
2019-06-03 11:47:57 -0700

    rpmsg: glink: Remove IRQF_NO_SUSPEND
    Currently interrupts with hardware wake capabilities still cause the
    CPUs to handle the interrupt at the GIC layer when IRQF_NO_SUSPEND is
    used. This is reproduced in both s2idle and mem suspend.
    
    Remove IRQF_NO_SUSPEND and add enable_irq_wake to properly account for
    the wakeup interrupt.
    
    Change-Id: Ieb8de40102fb402ca2f040020f56b8e7c42b3611
    Signed-off-by: Chris Lew <clew@codeaurora.org>
    Signed-off-by: Ananth Raghavan Subramanian <sananth@codeaurora.org>
    Signed-off-by: Kelly Rossmoyer <krossmo@google.com>
    Bug: 123377615
    Bug: 131260677
    Bug: 143857220
    (cherry picked from commit 3beed12790990d3e35b02f38c739866973e23a8b)
    Signed-off-by: Kyle Lin <kylelin@google.com>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -2002,15 +2002,17 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 	irq = of_irq_get(dev->of_node, 0);
 	ret = devm_request_irq(dev, irq,
 			       qcom_glink_native_intr,
-			       IRQF_NO_SUSPEND | IRQF_SHARED,
-			       glink->irq_name, glink);
-
+			       IRQF_SHARED,
+			       "glink-native", glink);
 	if (ret) {
 		dev_err(dev, "failed to request IRQ\n");
 		goto unregister;
 	}
 
 	glink->irq = irq;
+	ret = enable_irq_wake(glink->irq);
+	if (ret)
+		dev_err(dev, "failed to set irq wake\n");
 
 	size = of_property_count_u32_elems(dev->of_node, "cpu-affinity");
 	if (size > 0) {
