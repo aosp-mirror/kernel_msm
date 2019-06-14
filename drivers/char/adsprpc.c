5aaf40b4a9ba283344e39368e5326a1b79d1a687
Robert Lee <lerobert@google.com>
2019-06-14 11:19:05 +0800

    msm: adsprpc: fix block in wait_for_completion
    Adsp is busy in high priority threads (CCC5/ADM) and cannot schedule
    low priority thread frpck_0_0 to complete the adsprpc's request.
    
    Replaced it by wait_for_completion_timeout. When timeout happen, trigger
    ADSP SSR to recover adsp from the busy state.
    
    Bug: 132430192
    Change-Id: I0f80f252408cf8269186374e8fd4f0e76597d426
    Signed-off-by: Robert Lee <lerobert@google.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -103,6 +103,7 @@
 #define FASTRPC_STATIC_HANDLE_LISTENER (3)
 #define FASTRPC_STATIC_HANDLE_MAX (20)
 #define FASTRPC_LATENCY_CTRL_ENB  (1)
+#define FASTRPC_TIMEOUT (3000) /* 3s */
 
 #define INIT_FILELEN_MAX (2*1024*1024)
 #define INIT_MEMLEN_MAX  (8*1024*1024)
@@ -1930,9 +1931,16 @@ static int fastrpc_internal_invoke(struct fastrpc_file *fl, uint32_t mode,
 	if (err)
 		goto bail;
  wait:
-	if (kernel)
-		wait_for_completion(&ctx->work);
-	else {
+	if (kernel) {
+		int rc = wait_for_completion_timeout(&ctx->work,
+				msecs_to_jiffies(FASTRPC_TIMEOUT));
+		if (!rc) {
+			pr_err("wait for completion timeout and trigger ADSP SSR b/132430192\n");
+			/* b/132430192 WA to trigger adsp SSR */
+			subsystem_restart("adsp");
+			goto bail;
+		}
+	} else {
 		interrupted = wait_for_completion_interruptible(&ctx->work);
 		VERIFY(err, 0 == (err = interrupted));
 		if (err)
