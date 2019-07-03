37041babdf19350e6135f5be35de79f519ddd1ad
Jim Blackler <jimblackler@google.com>
2019-07-03 17:33:51 +0100

    ANDROID: Fixes to locking around handle_lmk_event
    get_task_struct used to reserve 'selected' outside rcu_read_lock block.
    Remove the need for get_task_mm, removing the possibility of lock issues there.
    
    Bug: 133479338
    Signed-off-by: Jim Blackler <jimblackler@google.com>
    Change-Id: I1399e2f669242c04e0e397bc09c987358aa97a0a
    (cherry picked from commit 203e8aa1374802aa4d6eb7097087dc2b4240516d)
    Signed-off-by: Ajit Kumar <kajit@codeaurora.org>
    
    Change-Id: I2cdbf9201c0afc10b7bf619383abd8e6d360587f
    Signed-off-by: Ajit Kumar <kajit@codeaurora.org>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -118,21 +118,15 @@ struct lmk_event {
 	struct list_head list;
 };
 
-void handle_lmk_event(struct task_struct *selected, short min_score_adj)
+void handle_lmk_event(struct task_struct *selected, int selected_tasksize,
+		      short min_score_adj)
 {
 	int head;
 	int tail;
 	struct lmk_event *events;
 	struct lmk_event *event;
 	int res;
-	long rss_in_pages = -1;
 	char taskname[MAX_TASKNAME];
-	struct mm_struct *mm = get_task_mm(selected);
-
-	if (mm) {
-		rss_in_pages = get_mm_rss(mm);
-		mmput(mm);
-	}
 
 	res = get_cmdline(selected, taskname, MAX_TASKNAME - 1);
 
@@ -171,7 +165,7 @@ void handle_lmk_event(struct task_struct *selected, short min_score_adj)
 	event->maj_flt = selected->maj_flt;
 	event->oom_score_adj = selected->signal->oom_score_adj;
 	event->start_time = nsec_to_clock_t(selected->real_start_time);
-	event->rss_in_pages = rss_in_pages;
+	event->rss_in_pages = selected_tasksize;
 	event->min_score_adj = min_score_adj;
 
 	event_buffer.head = (head + 1) & (MAX_BUFFERED_EVENTS - 1);
@@ -787,6 +781,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		lowmem_deathpending_timeout = jiffies + HZ;
 		rem += selected_tasksize;
 		rcu_read_unlock();
+		get_task_struct(selected);
 		/* give the system time to free up the memory */
 		msleep_interruptible(20);
 		trace_almk_shrink(selected_tasksize, ret,
@@ -795,16 +790,15 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	} else {
 		trace_almk_shrink(1, ret, other_free, other_file, 0);
 		rcu_read_unlock();
-		handle_lmk_event(selected, min_score_adj);
 	}
 
 	lowmem_print(4, "lowmem_scan %lu, %x, return %lu\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	mutex_unlock(&scan_mutex);
-	rcu_read_unlock();
-
-	if (selected)
-		handle_lmk_event(selected, min_score_adj);
+	if (selected) {
+		handle_lmk_event(selected, selected_tasksize, min_score_adj);
+		put_task_struct(selected);
+	}
 	return rem;
 }
 
