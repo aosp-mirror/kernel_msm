5e91d115daa35b73d137ce47aff1544b9d7bb943
Santhosh Behara <quic_sbehara@quicinc.com>
2019-07-05 15:53:15 +0530

    msm: vidc: print extradata buffer information
    Add debug log to print extradata buffer information
    like size and address for encoder.
    
    Bug: 134841228
    Change-Id: I63ae39aa6b15f7daa1fc61ea091fbcc92d2811ad
    Signed-off-by: Santhosh Behara <santhoshbehara@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -4255,6 +4255,11 @@ static void populate_frame_data(struct vidc_frame_data *data,
 	if (extra_idx && extra_idx < VIDEO_MAX_PLANES) {
 		data->extradata_addr = mbuf->smem[extra_idx].device_addr;
 		data->extradata_size = vb->planes[extra_idx].length;
+		if (inst->session_type == MSM_VIDC_ENCODER) {
+			dprintk(VIDC_DBG, "%s: Extradata addr: %x, size: %d\n",
+				__func__, data->extradata_addr,
+				data->extradata_size);
+		}
 		data->flags |= HAL_BUFFERFLAG_EXTRADATA;
 	}
 }
