bee85afb037d64c47115801699c1d0a4e3eba747
David Rhodes <david.rhodes@cirrus.com>
2019-07-09 18:01:43 -0500

    ASoC: cs35l41: Select FW name based on Rev ID
    Assign part name in DSP struct based on rev ID
    if DT property 'fwname-use-revid' is added
    
    Bug: 141352774
    Change-Id: I15db9ab8fa01eb826a4970da82b86b78f34c3598
    Signed-off-by: David Rhodes <david.rhodes@cirrus.com>
    Signed-off-by: HW Lee <hwlee@google.com>

--- sound/soc/codecs/cs35l41.c
+++ sound/soc/codecs/cs35l41.c
@@ -2714,6 +2714,9 @@ static int cs35l41_handle_of_data(struct device *dev,
 	pdata->invert_pcm = of_property_read_bool(np,
 					"cirrus,invert-pcm");
 
+	pdata->fwname_use_revid = of_property_read_bool(np,
+					"cirrus,fwname-use-revid");
+
 	if (of_property_read_u32(np, "cirrus,temp-warn_threshold", &val) >= 0)
 		pdata->temp_warn_thld = val | CS35L41_VALID_PDATA;
 
@@ -2895,7 +2898,6 @@ static int cs35l41_dsp_init(struct cs35l41_private *cs35l41)
 	int ret, i;
 
 	dsp = &cs35l41->dsp;
-	dsp->part = "cs35l41";
 	dsp->num = 1;
 	dsp->type = WMFW_HALO;
 	dsp->rev = 0;
@@ -2903,6 +2905,9 @@ static int cs35l41_dsp_init(struct cs35l41_private *cs35l41)
 	dsp->dev = cs35l41->dev;
 	dsp->regmap = cs35l41->regmap;
 	dsp->tuning_has_prefix = cs35l41->pdata.tuning_has_prefix;
+
+	if (!cs35l41->pdata.fwname_use_revid)
+		dsp->part = "cs35l41";
 
 	dsp->base = CS35L41_DSP1_CTRL_BASE;
 	dsp->base_sysinfo = CS35L41_DSP1_SYS_ID;
@@ -3405,6 +3410,8 @@ int cs35l41_probe(struct cs35l41_private *cs35l41,
 	switch (reg_revid) {
 	case CS35L41_REVID_A0:
 		cs35l41->amp_hibernate = CS35L41_HIBERNATE_INCOMPATIBLE;
+		if (cs35l41->pdata.fwname_use_revid)
+			cs35l41->dsp.part = "cs35l41-revA";
 		ret = regmap_multi_reg_write(cs35l41->regmap,
 				cs35l41_reva0_errata_patch,
 				ARRAY_SIZE(cs35l41_reva0_errata_patch));
@@ -3416,6 +3423,8 @@ int cs35l41_probe(struct cs35l41_private *cs35l41,
 		break;
 	case CS35L41_REVID_B0:
 		cs35l41->amp_hibernate = CS35L41_HIBERNATE_INCOMPATIBLE;
+		if (cs35l41->pdata.fwname_use_revid)
+			cs35l41->dsp.part = "cs35l41-revB0";
 		ret = regmap_multi_reg_write(cs35l41->regmap,
 				cs35l41_revb0_errata_patch,
 				ARRAY_SIZE(cs35l41_revb0_errata_patch));
@@ -3426,6 +3435,8 @@ int cs35l41_probe(struct cs35l41_private *cs35l41,
 		}
 		break;
 	case CS35L41_REVID_B2:
+		if (cs35l41->pdata.fwname_use_revid)
+			cs35l41->dsp.part = "cs35l41-revB2";
 		ret = regmap_multi_reg_write(cs35l41->regmap,
 				cs35l41_revb2_errata_patch,
 				ARRAY_SIZE(cs35l41_revb2_errata_patch));
