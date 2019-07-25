5f86f640016c263c29abee13dfab53b4809dc4c1
yixuanjiang <yixuanjiang@google.com>
2019-07-25 11:30:14 +0800

    mfd: adnc: export iaxxx header
    move header file to uapi folder
    
    Bug: 138320651
    Test: audio/soundtrigger/sensor/SSR/get_pwr_status works well
    Change-Id: I01b769b41e22ea64cd0bdb2898ce074fab323752
    Signed-off-by: yixuanjiang <yixuanjiang@google.com>

--- include/uapi/linux/mfd/adnc/iaxxx-odsp.h
+++ include/uapi/linux/mfd/adnc/iaxxx-odsp.h
@@ -15,12 +15,24 @@
 #ifndef __IAXXX_ODSP_H__
 #define __IAXXX_ODSP_H__
 
+#define IAXXX_PACKAGE_VER_STR_SIZE_MAX  (100)
+#define IAXXX_PLUGIN_VER_STR_SIZE_MAX   (100)
+#define IAXXX_MAX_PLUGIN_ENDPOINTS       (16)
+#define IAXXX_MAX_VER_STR_SIZE           (20)
+
+enum {
+	IAXXX_FW_CRASH,
+	IAXXX_FW_IDLE,
+	IAXXX_FW_ACTIVE
+};
+
 struct iaxxx_plugin_info {
 	uint32_t plg_idx;
 	uint32_t pkg_id;
 	uint32_t block_id;
 	uint32_t inst_id;
 	uint32_t priority;
+	uint32_t config_id;
 };
 
 struct iaxxx_plugin_param {
@@ -54,6 +66,26 @@ struct iaxxx_set_event {
 	uint32_t block_id;
 };
 
+struct iaxxx_evt_trigger {
+	uint16_t src_id;
+	uint16_t evt_id;
+	uint32_t src_opaque;
+};
+
+struct iaxxx_evt_read_subscription {
+	uint16_t src_id;
+	uint16_t evt_id;
+	uint16_t dst_id;
+	uint32_t dst_opaque;
+};
+
+struct iaxxx_evt_retrieve_notification {
+	uint16_t src_id;
+	uint16_t evt_id;
+	uint32_t src_opaque;
+	uint32_t dst_opaque;
+};
+
 struct iaxxx_evt_info {
 	uint16_t src_id;
 	uint16_t event_id;
@@ -80,6 +112,84 @@ struct iaxxx_plugin_custom_cfg {
 	uint32_t custom_config_id;
 };
 
+struct iaxxx_plugin_error_info {
+	uint8_t  block_id;
+	uint32_t error_code;
+	uint8_t  error_instance;
+};
+
+struct iaxxx_plugin_set_param_blk_with_ack_info {
+	uint32_t inst_id;
+	uint8_t  block_id;
+	uint32_t param_blk_id;
+	uint32_t set_param_blk_size;
+	uint64_t set_param_blk_buffer;
+	uint64_t response_buffer;
+	uint32_t response_buf_size;
+	uint32_t max_retries;
+};
+
+struct iaxxx_plugin_status_info {
+	uint32_t inst_id;
+	uint32_t block_id;
+	uint8_t create_status;
+	uint8_t enable_status;
+	uint16_t process_count;
+	uint16_t process_err_count;
+	uint32_t in_frames_consumed;
+	uint32_t out_frames_produced;
+	uint32_t private_memsize;
+	uint8_t frame_notification_mode;
+	uint8_t state_management_mode;
+};
+
+struct iaxxx_plugin_endpoint_status_info {
+	uint32_t inst_id;
+	uint8_t ep_index;
+	uint8_t direction;
+	uint8_t status;
+	uint8_t frame_status;
+	uint8_t endpoint_status;
+	uint8_t usage;
+	uint8_t mandatory;
+	uint16_t counter;
+	uint8_t op_encoding;
+	uint8_t op_sample_rate;
+	uint16_t op_frame_length;
+};
+
+struct iaxxx_plugin_get_package_version {
+	uint8_t inst_id;
+	char version[IAXXX_PACKAGE_VER_STR_SIZE_MAX];
+	uint32_t len;
+};
+
+struct iaxxx_plugin_get_plugin_version {
+	uint8_t inst_id;
+	char version[IAXXX_PLUGIN_VER_STR_SIZE_MAX];
+	uint32_t len;
+};
+
+struct iaxxx_plugin_endpoint_timestamps {
+	uint8_t proc_id;
+	uint64_t timestamps[IAXXX_MAX_PLUGIN_ENDPOINTS];
+};
+
+struct iaxxx_proc_execution_status {
+	uint8_t proc_id;
+	uint32_t status;
+};
+
+struct iaxxx_sys_versions {
+	uint32_t app_ver_num; /* output */
+	char app_ver_str[IAXXX_MAX_VER_STR_SIZE]; /* output */
+	uint32_t app_ver_str_len; /* input */
+
+	uint32_t rom_ver_num; /* output */
+	char rom_ver_str[IAXXX_MAX_VER_STR_SIZE]; /* output */
+	uint32_t rom_ver_str_len; /* input */
+};
+
 /* IOCTL Magic character */
 #define IAXXX_IOCTL_MAGIC 'I'
 
@@ -102,4 +212,21 @@ struct iaxxx_plugin_custom_cfg {
 #define ODSP_PLG_SET_CUSTOM_CFG _IO(IAXXX_IOCTL_MAGIC, 0x20)
 #define ODSP_PLG_GET_PARAM_BLK _IO(IAXXX_IOCTL_MAGIC, 0x21)
 #define ODSP_PLG_CREATE_STATIC_PACKAGE _IO(IAXXX_IOCTL_MAGIC, 0x22)
+#define ODSP_PLG_READ_PLUGIN_ERROR _IO(IAXXX_IOCTL_MAGIC, 0x23)
+#define ODSP_PLG_SET_PARAM_BLK_WITH_ACK _IO(IAXXX_IOCTL_MAGIC, 0x24)
+#define ODSP_PLG_GET_STATUS_INFO _IO(IAXXX_IOCTL_MAGIC, 0x25)
+#define ODSP_PLG_GET_ENDPOINT_STATUS _IO(IAXXX_IOCTL_MAGIC, 0x26)
+#define ODSP_EVENT_TRIGGER _IO(IAXXX_IOCTL_MAGIC, 0x27)
+#define ODSP_EVENT_READ_SUBSCRIPTION _IO(IAXXX_IOCTL_MAGIC, 0x28)
+#define ODSP_EVENT_RETRIEVE_NOTIFICATION _IO(IAXXX_IOCTL_MAGIC, 0x29)
+#define ODSP_PLG_GET_PACKAGE_VERSION _IO(IAXXX_IOCTL_MAGIC, 0x2A)
+#define ODSP_PLG_GET_PLUGIN_VERSION _IO(IAXXX_IOCTL_MAGIC, 0x2B)
+#define ODSP_EVENT_RESET_READ_INDEX _IO(IAXXX_IOCTL_MAGIC, 0x2C)
+#define ODSP_PLG_GET_ENDPOINT_TIMESTAMPS _IO(IAXXX_IOCTL_MAGIC, 0x2D)
+#define ODSP_GET_PROC_EXECUTION_STATUS _IO(IAXXX_IOCTL_MAGIC, 0x2E)
+#define ODSP_GET_SYS_VERSIONS _IO(IAXXX_IOCTL_MAGIC, 0x2F)
+#define ODSP_GET_SYS_DEVICE_ID _IO(IAXXX_IOCTL_MAGIC, 0x30)
+#define ODSP_GET_SYS_MODE _IO(IAXXX_IOCTL_MAGIC, 0x31)
+#define ODSP_GET_FW_STATUS _IO(IAXXX_IOCTL_MAGIC, 0x32)
+#define ODSP_RESET_FW _IO(IAXXX_IOCTL_MAGIC, 0x33)
 #endif
