cd8a5ab68f0b39166f5e00ea7c85ea237cfa7a53
Adrian Salido <salidoa@google.com>
2019-10-08 16:28:12 +0800

    drm/msm/dsi-staging: move init delay before power on if lp11 init
    If lp11 init is set it means that display power on will happen after
    LP11 is initialized. In such case allow the existing init delay to
    happen between LP11 and RESET. RESET already allows a delay to be added
    after asserting RESET if required.
    
    Bug: 142159002
    Change-Id: Ieafec3e2332cf3cd0e9c09e77deccc81891550d9
    Signed-off-by: Adrian Salido <salidoa@google.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -3821,6 +3821,9 @@ int dsi_panel_prepare(struct dsi_panel *panel)
 
 	mutex_lock(&panel->panel_lock);
 
+	if (panel->init_delay_us)
+		usleep_range(panel->init_delay_us, panel->init_delay_us);
+
 	if (panel->lp11_init) {
 		rc = dsi_panel_power_on(panel);
 		if (rc) {
@@ -3829,9 +3832,6 @@ int dsi_panel_prepare(struct dsi_panel *panel)
 			goto error;
 		}
 	}
-
-	if (panel->init_delay_us)
-		usleep_range(panel->init_delay_us, panel->init_delay_us);
 
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_PRE_ON);
 	if (rc) {
