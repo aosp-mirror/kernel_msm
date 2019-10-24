a7ece38efd184feab1438581919a4071cd514e35
Satya Tangirala <satyat@google.com>
2019-10-24 14:44:27 -0700

    BACKPORT: FROMLIST: scsi: ufs: UFS crypto API
    Introduce functions to manipulate UFS inline encryption hardware
    in line with the JEDEC UFSHCI v2.1 specification and to work with the
    block keyslot manager.
    
    Bug: 137270441
    Test: tested as series; see I26aac0ac7845a9064f28bb1421eb2522828a6dec
    Change-Id: I4c6ba30f30eaea83da6822381ae2aa85b40e7b90
    Signed-off-by: Satya Tangirala <satyat@google.com>
    Link: https://patchwork.kernel.org/patch/11214745/
    (cherry picked from commit eedb625131ca2f07bec492ff92c382494a2d74c8)

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -841,6 +841,11 @@ enum ufshcd_slowio_systype {
  * @is_urgent_bkops_lvl_checked: keeps track if the urgent bkops level for
  *  device is known or not.
  * @scsi_block_reqs_cnt: reference counting for scsi block requests
+ * @crypto_capabilities: Content of crypto capabilities register (0x100)
+ * @crypto_cap_array: Array of crypto capabilities
+ * @crypto_cfg_register: Start of the crypto cfg array
+ * @crypto_cfgs: Array of crypto configurations (i.e. config for each slot)
+ * @ksm: the keyslot manager tied to this hba
  */
 struct ufs_hba {
 	void __iomem *mmio_base;
@@ -1146,6 +1151,15 @@ struct ufs_hba {
 	/* To monitor slow UFS I/O requests. */
 	u64 slowio_min_us;
 	u64 slowio[UFSHCD_SLOWIO_OP_MAX][UFSHCD_SLOWIO_SYS_MAX];
+
+#ifdef CONFIG_SCSI_UFS_CRYPTO
+	/* crypto */
+	union ufs_crypto_capabilities crypto_capabilities;
+	union ufs_crypto_cap_entry *crypto_cap_array;
+	u32 crypto_cfg_register;
+	union ufs_crypto_cfg_entry *crypto_cfgs;
+	struct keyslot_manager *ksm;
+#endif /* CONFIG_SCSI_UFS_CRYPTO */
 };
 
 static inline void ufshcd_mark_shutdown_ongoing(struct ufs_hba *hba)
