3667823850bb892dcaece1aa162ebf8358a99cad
Steve Muckle <smuckle@google.com>
2019-11-01 15:59:35 -0700

    GKI: drivers: soc: qcom: socinfo: export symbols
    These symbols are required for CONFIG_MODEMSMEM=y.
    
    Bug: 143786304
    Test: load modemsmem as module
    Change-Id: I5d03b53ac1a5614699b740053fe1df87c8713b14
    Signed-off-by: Steve Muckle <smuckle@google.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -590,6 +590,7 @@ uint32_t socinfo_get_g_platform_type(void)
 			socinfo->v0_15.g_hw_platform : 0)
 		: 0;
 }
+EXPORT_SYMBOL_GPL(socinfo_get_g_platform_type);
 
 uint32_t socinfo_get_g_platform_version(void)
 {
@@ -598,6 +599,7 @@ uint32_t socinfo_get_g_platform_version(void)
 			socinfo->v0_15.g_platform_version : 0)
 		: 0;
 }
+EXPORT_SYMBOL_GPL(socinfo_get_g_platform_version);
 
 uint32_t socinfo_get_g_platform_subtype(void)
 {
@@ -606,6 +608,7 @@ uint32_t socinfo_get_g_platform_subtype(void)
 			socinfo->v0_15.g_hw_platform_subtype : 0)
 		: 0;
 }
+EXPORT_SYMBOL_GPL(socinfo_get_g_platform_subtype);
 
 enum pmic_model socinfo_get_pmic_model(void)
 {
