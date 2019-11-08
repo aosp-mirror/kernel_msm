90bbeb8af324eb599bd8aab074d2ebac5f4d6535
Wei Wang <wvw@google.com>
2019-11-08 00:44:14 -0800

    kernel: sched: fix cpu cpu_capacity_orig being capped incorrectly
    update_cpu_capacity will update cpu_capacity_orig capped with
    thermal_cap, in non-WALT case, thermal_cap is previous
    cpu_capacity_orig. This caused cpu_capacity_orig being capped
    incorrectly.
    
    Test: Build
    Bug: 144143594
    Bug: 145190425
    Change-Id: I1ff9d9c87554c2d2395d46b215276b7ab50585c0
    Signed-off-by: Wei Wang <wvw@google.com>
    (cherry picked from commit dac65a5a494f8d0c80101acc5d482d94cda6f158)
    (cherry picked from commit 3de2e701178f59e6fcc361e2e59130759fcf8a55)

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3079,7 +3079,7 @@ static inline void update_cpu_cluster_capacity(const cpumask_t *cpus) { }
 #ifdef CONFIG_SMP
 static inline unsigned long thermal_cap(int cpu)
 {
-	return cpu_rq(cpu)->cpu_capacity_orig;
+	return SCHED_CAPACITY_SCALE;
 }
 #endif
 
