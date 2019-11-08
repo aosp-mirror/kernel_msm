638ce9d96db92def46355d613905d74d49390301
Wei Wang <wvw@google.com>
2019-11-08 00:15:38 -0800

    drivers: arch_topology: wire up thermal limit for arch_scale_max_freq_capacity
    before patch and "echo 50000 > /sys/class/thermal/tz-by-name/sdm-therm/emul_temp"
    com.android.uibench.janktests.UiBenchJankTests#testInvalidateTree: PASSED (02m6.247s)
            gfx-avg-slow-ui-thread: 0.07110321338664297
            gfx-avg-missed-vsync: 0.0
            gfx-avg-high-input-latency: 74.25140826299423
            gfx-max-frame-time-50: 12
            gfx-min-total-frames: 2250
            gfx-avg-frame-time-99: 11.8
            gfx-avg-num-frame-deadline-missed: 1.6
            gfx-avg-frame-time-50: 9.6
            gfx-max-high-input-latency: 99.86666666666667
            gfx-avg-frame-time-90: 11.0
            gfx-avg-frame-time-95: 11.0
            gfx-max-frame-time-95: 13
            gfx-max-frame-time-90: 13
            gfx-max-slow-draw: 0.0
            gfx-max-frame-time-99: 13
            gfx-avg-slow-draw: 0.0
            gfx-max-total-frames: 2251
            gfx-avg-jank: 43.678000000000004
            gfx-max-slow-bitmap-uploads: 0.0
            gfx-max-missed-vsync: 0.0
            gfx-avg-total-frames: 2250
            gfx-max-jank: 96.67
            gfx-max-slow-ui-thread: 0.13333333333333333
            gfx-max-num-frame-deadline-missed: 3
            gfx-avg-slow-bitmap-uploads: 0.0
    
    aefore patch and "echo 50000 > /sys/class/thermal/tz-by-name/sdm-therm/emul_temp"
    google/perf/jank/UIBench/UIBench (1 Test)
    ----------------------------------------
    [1/1] com.android.uibench.janktests.UiBenchJankTests#testInvalidateTree: PASSED (02m7.027s)
            gfx-avg-slow-ui-thread: 0.0
            gfx-avg-missed-vsync: 0.0
            gfx-avg-high-input-latency: 11.53777777777778
            gfx-max-frame-time-50: 7
            gfx-min-total-frames: 2250
            gfx-avg-frame-time-99: 8.0
            gfx-avg-num-frame-deadline-missed: 0.0
            gfx-avg-frame-time-50: 7.0
            gfx-max-high-input-latency: 41.15555555555556
            gfx-avg-frame-time-90: 7.2
            gfx-avg-frame-time-95: 7.8
            gfx-max-frame-time-95: 8
            gfx-max-frame-time-90: 8
            gfx-max-slow-draw: 0.0
            gfx-max-frame-time-99: 8
            gfx-avg-slow-draw: 0.0
            gfx-max-total-frames: 2250
            gfx-avg-jank: 0.0
            gfx-max-slow-bitmap-uploads: 0.0
            gfx-max-missed-vsync: 0.0
            gfx-avg-total-frames: 2250
            gfx-max-jank: 0.0
            gfx-max-slow-ui-thread: 0.0
            gfx-max-num-frame-deadline-missed: 0
            gfx-avg-slow-bitmap-uploads: 0.0
    
    Bug: 143162654
    Test: use emul_temp to change thermal condition and see capacity changed
    Change-Id: Idbf943f9c831c288db40d820682583ade3bbf05e
    Signed-off-by: Wei Wang <wvw@google.com>
    (cherry picked from commit 1a1b85b536651daa3dc7dab4fcd155c8cfe1b9a0)

--- include/linux/arch_topology.h
+++ include/linux/arch_topology.h
@@ -45,11 +45,13 @@ unsigned long topology_get_freq_scale(struct sched_domain *sd, int cpu)
 }
 
 DECLARE_PER_CPU(unsigned long, max_freq_scale);
+DECLARE_PER_CPU(unsigned long, max_thermal_scale);
 
 static inline
 unsigned long topology_get_max_freq_scale(struct sched_domain *sd, int cpu)
 {
-	return per_cpu(max_freq_scale, cpu);
+	return min(per_cpu(max_freq_scale, cpu),
+		   per_cpu(max_thermal_scale, cpu));
 }
 
 #endif /* _LINUX_ARCH_TOPOLOGY_H_ */
