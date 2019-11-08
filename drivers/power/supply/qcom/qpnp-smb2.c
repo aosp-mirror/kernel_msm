d955624b8901d4c8e0a27f8d217ce939285c47af
liucheng <liucheng@longcheer.com>
2019-11-08 11:35:04 +0800

    [charging]enable AICL
    Bug:144109593
    
    Change-Id: Ib3a00623c0580d47b1ef3d78f5b08ec995db95fe
    Signed-off-by: sunzhiquan <sunzhiquan@longcheer.com>

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -1636,8 +1636,6 @@ static int smb2_init_hw(struct smb2 *chip)
 	 * start from min and AICL ADC disable
 	 */
 	rc = smblib_masked_write(chg, USBIN_AICL_OPTIONS_CFG_REG,
-				 USBIN_AICL_EN_BIT, 0);/*disable aicl */
-	rc = smblib_masked_write(chg, USBIN_AICL_OPTIONS_CFG_REG,
 			USBIN_AICL_START_AT_MAX_BIT
 				| USBIN_AICL_ADC_EN_BIT, 0);
 	if (rc < 0) {
