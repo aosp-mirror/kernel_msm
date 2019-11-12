fbabe265d9f347c25639825dbd9ef7c7f84234ac
Wei Wang <wvw@google.com>
2019-11-12 10:33:30 -0800

    trace: sched: add capacity change tracing
    Add a new tracepoint sched_capacity_update when capacity value
    updated.
    
    Bug: 144177658
    Bug: 144961676
    Test: Boot and grab trace to check
    Change-Id: I30ee55bfcc2fb5a92dd448ad364768ee428f3cc4
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Jimmy Shiu <jimmyshiu@google.com>

--- include/trace/events/sched.h
+++ include/trace/events/sched.h
@@ -1590,6 +1590,28 @@ TRACE_EVENT_CONDITION(sched_overutilized,
 		__entry->overutilized ? 1 : 0, __entry->cpulist)
 );
 
+TRACE_EVENT(sched_capacity_update,
+
+	TP_PROTO(int cpu),
+
+	TP_ARGS(cpu),
+
+	TP_STRUCT__entry(
+		__field(unsigned int, cpu)
+		__field(unsigned int, capacity)
+		__field(unsigned int, capacity_orig)
+	),
+
+	TP_fast_assign(
+		__entry->cpu			= cpu;
+		__entry->capacity		= capacity_of(cpu);
+		__entry->capacity_orig		= capacity_orig_of(cpu);
+	),
+
+	TP_printk("cpu=%d capacity=%u capacity_orig=%u",
+		__entry->cpu, __entry->capacity, __entry->capacity_orig)
+);
+
 /*
  * Tracepoint for sched_get_nr_running_avg
  */
