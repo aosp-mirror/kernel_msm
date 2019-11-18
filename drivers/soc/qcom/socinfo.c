614139ac062866b18e06a9152413e8d3ca0cdb77
Quentin Perret <qperret@google.com>
2019-11-18 17:20:09 +0000

    GKI: drivers: soc: qcom: merge socinfo and boot_stats
    boot_stats implements a single function, used explicitely from socinfo.
    There is no need to modularize them independently, so let's merge them
    to reduce the pain.
    
    Bug: 144523239
    Test: modules load, same devices probe on kona
    Change-Id: I56f5c2bc6c652e5fa52d0fa7974c26bd2d59c003
    Signed-off-by: Quentin Perret <qperret@google.com>
    Tested-by: Connor O'Brien <connoro@google.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -20,7 +20,7 @@
 
 #include <soc/qcom/socinfo.h>
 #include <linux/soc/qcom/smem.h>
-#include <soc/qcom/boot_stats.h>
+#include "boot_stats.h"
 
 #define BUILD_ID_LENGTH 32
 #define CHIP_ID_LENGTH 32
