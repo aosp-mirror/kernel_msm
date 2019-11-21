7f432a4f5fcac5b25f867defdcc29d19142a7968
davidycchen <davidycchen@google.com>
2019-11-21 16:34:48 +0800

    i2c: change geni_i2c_clk_map for 1M i2c
    The i2c clk frequency is only around 700k when we set it as 1M.
    Qualcomm recommands us to reduce the t_cycle, t_high, t_low values.
    
    Bug: 142835286
    Test: i2c clk frequency is over 900k after this commit
    
    Change-Id: I1f495e06670cd282111d0477ddd74dff2b9f58db
    Signed-off-by: davidycchen <davidycchen@google.com>

--- drivers/i2c/busses/i2c-qcom-geni.c
+++ drivers/i2c/busses/i2c-qcom-geni.c
@@ -158,7 +158,7 @@ struct geni_i2c_clk_fld {
 static struct geni_i2c_clk_fld geni_i2c_clk_map[] = {
 	{KHz(100), 7, 10, 11, 26},
 	{KHz(400), 2,  5, 12, 24},
-	{KHz(1000), 1, 3,  9, 18},
+	{KHz(1000), 1, 1,  5, 13},
 };
 
 static int geni_i2c_clk_map_idx(struct geni_i2c_dev *gi2c)
