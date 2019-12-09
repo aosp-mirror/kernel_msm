e8b3449000a58501734368aca7a6a4333adf7a98
Mark Salyzyn <salyzyn@google.com>
2019-12-09 09:11:00 -0800

    GKI: arm64: smmu: add missing exports for ARM_SMMU=m for qcom,qsmmu-v500 for upstream
    Upstreamable components.
    
    Automatically generated by add_EXPORT_SYMBOL_GPL
    
    The following symbols are exported:
     - generic_device_group
     - iommu_alloc_resv_region
     - iommu_device_register
     - iommu_group_get_for_dev
     - iommu_group_ref_get
     - of_phandle_iterator_args
     - pci_device_group
     - pci_for_each_dma_alias
     - pci_request_acs
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 145919068
    Test: compile
    Change-Id: I9d6b27770cc496c96e2c67bc158520b10434838b

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -103,6 +103,7 @@ int iommu_device_register(struct iommu_device *iommu)
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(iommu_device_register);
 
 void iommu_device_unregister(struct iommu_device *iommu)
 {
@@ -789,6 +790,7 @@ struct iommu_group *iommu_group_ref_get(struct iommu_group *group)
 	kobject_get(group->devices_kobj);
 	return group;
 }
+EXPORT_SYMBOL_GPL(iommu_group_ref_get);
 
 /**
  * iommu_group_put - Decrement group reference
@@ -962,6 +964,7 @@ struct iommu_group *generic_device_group(struct device *dev)
 {
 	return iommu_group_alloc();
 }
+EXPORT_SYMBOL_GPL(generic_device_group);
 
 /*
  * Use standard PCI bus topology, isolation features, and DMA alias quirks
@@ -1029,6 +1032,7 @@ struct iommu_group *pci_device_group(struct device *dev)
 	/* No shared group found, allocate new */
 	return iommu_group_alloc();
 }
+EXPORT_SYMBOL_GPL(pci_device_group);
 
 /**
  * iommu_group_get_for_dev - Find or create the IOMMU group for a device
@@ -1088,6 +1092,7 @@ struct iommu_group *iommu_group_get_for_dev(struct device *dev)
 
 	return group;
 }
+EXPORT_SYMBOL_GPL(iommu_group_get_for_dev);
 
 struct iommu_domain *iommu_group_default_domain(struct iommu_group *group)
 {
@@ -1950,6 +1955,7 @@ struct iommu_resv_region *iommu_alloc_resv_region(phys_addr_t start,
 	region->type = type;
 	return region;
 }
+EXPORT_SYMBOL_GPL(iommu_alloc_resv_region);
 
 /* Request that a device is direct mapped by the IOMMU */
 int iommu_request_dm_for_dev(struct device *dev)
