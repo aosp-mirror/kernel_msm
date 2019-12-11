679a71d913c72ab2fc3d5ec98b55982c2e4a0cb9
Puma Hsu <pumahsu@google.com>
2019-12-11 14:40:49 +0800

    Revert "usb: gadget: f_accessory: Add protection on acc_ctrlrequest"
    This reverts commit 628d240e005dd46bca29e71d462c60472d3a824e.
    
    This commit would conflict with QCOM's(54c56913dabbfd6560867db4dd6eef707d680e90)
    because it removes dev's assignment from the claim. It would cause the
    API always return -ENODEV while checking dev
    
    Bug: 146763266
    Bug: 145783525
    Test: Device can handle request
    Change-Id: I8a72e951c6d4042131a29119928cd49288ffe966
    Signed-off-by: Puma Hsu <pumahsu@google.com>
    Signed-off-by: Wilson Sung <wilsonsung@google.com>

--- drivers/usb/gadget/function/f_accessory.c
+++ drivers/usb/gadget/function/f_accessory.c
@@ -870,7 +870,7 @@ static void acc_complete_setup_noop(struct usb_ep *ep, struct usb_request *req)
 int acc_ctrlrequest(struct usb_composite_dev *cdev,
 				const struct usb_ctrlrequest *ctrl)
 {
-	struct acc_dev	*dev;
+	struct acc_dev	*dev = _acc_dev;
 	int	value = -EOPNOTSUPP;
 	struct acc_hid_dev *hid;
 	int offset;
@@ -889,6 +889,7 @@ int acc_ctrlrequest(struct usb_composite_dev *cdev,
 	 */
 	if (!dev)
 		return -ENODEV;
+
 /*
 	printk(KERN_INFO "acc_ctrlrequest "
 			"%02x.%02x v%04x i%04x l%u\n",
