6fd3a6aeea7c9deaf81942e07e9494aea061cc87
Wilson Sung <wilsonsung@google.com>
2019-12-26 18:42:01 +0800

    Revert "mm: fix the race between swapin_readahead and SWP_SYNCHRONOUS_IO path"
    This reverts commit 16a127c723319d13a68db7168e9c30de5a776d47.
    Bug: 146762954
    
    Change-Id: I7ec6bffd552e99608b3d0fd1cbe8213ad3fe3879
    Signed-off-by: Wilson Sung <wilsonsung@google.com>

--- mm/memory.c
+++ mm/memory.c
@@ -2898,8 +2898,6 @@ int do_swap_page(struct vm_fault *vmf)
 	struct mem_cgroup *memcg;
 	struct vma_swap_readahead swap_ra;
 	swp_entry_t entry;
-	struct swap_info_struct *si;
-	bool skip_swapcache = false;
 	pte_t pte;
 	int locked;
 	int exclusive = 0;
