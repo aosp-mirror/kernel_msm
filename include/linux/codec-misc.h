86fe958f0c61c7a646257351512bbadd9015469d
rogerfang <rogerfang@google.com>
2019-12-30 11:43:09 +0800

    asoc: codec: add speaker impedance get functions
    Add one of codec detection APIs.
    This will show and get the impedance values of speaker
    by floating format.
    
    Bug:144901001
    Test: manually test
    ACTION_SPEAKER_IMPEDANCE
    $ adb shell cat /sys/devices/platform/codec_detect/resistance_left_right
    0,0
    
    Change-Id: I6ffa471b07c5e7f435def82f407234d7d006806e
    Signed-off-by: rogerfang <rogerfang@google.com>

--- include/linux/codec-misc.h
+++ include/linux/codec-misc.h
@@ -20,6 +20,7 @@ void codec_detect_state_callback(state_cb cb);
 void codec_detect_hs_state_callback(state_cb cb);
 void codec_detect_number_callback(number_cb cb);
 int codec_detect_status_notifier(unsigned long val);
+int codec_misc_amp_put(int ch, long val);
 
 enum {
 	CODEC_STATE_UNKNOWN = -99,
--- sound/soc/codecs/codec-misc.c
+++ sound/soc/codecs/codec-misc.c
@@ -24,6 +24,21 @@
 static struct platform_device *codec_pdev;
 
 /*
+ * Amplifier status
+ */
+struct amp_stat_priv_type {
+	uint32_t action;
+	struct mutex lock;
+	struct device *ampdev_l;
+	struct device *ampdev_r;
+	long imp_l;
+	long imp_r;
+	long thermal_l;
+	long thermal_r;
+	int active;
+};
+
+/*
  * DSP Speech Up/Down time and counts
  */
 struct wdsp_stat_priv_type {
@@ -48,6 +63,7 @@ struct codec_misc_priv_type {
 	state_cb st_cb;
 	state_cb hs_cb;
 	number_cb nb_cb;
+	struct amp_stat_priv_type amp_stat_priv;
 };
 
 /*
@@ -256,6 +272,76 @@ int codec_detect_status_notifier(unsigned long val)
 }
 EXPORT_SYMBOL(codec_detect_status_notifier);
 
+/*
+ * update amplify impedance function
+ */
+int codec_misc_amp_put(int ch, long val)
+{
+	int ret = 0;
+	struct codec_misc_priv_type *misc_priv = NULL;
+	struct amp_stat_priv_type *amp_priv = NULL;
+
+	if (IS_ERR(codec_pdev) || IS_ERR(&codec_pdev->dev))
+		return -EINVAL;
+
+	misc_priv = dev_get_drvdata(&codec_pdev->dev);
+
+	if (!misc_priv)
+		return -EINVAL;
+
+	amp_priv = &misc_priv->amp_stat_priv;
+
+	if (!amp_priv)
+		return -EINVAL;
+
+	mutex_lock(&amp_priv->lock);
+
+	if (ch == 0)
+		amp_priv->imp_l = val;
+	else
+		amp_priv->imp_r = val;
+
+	dev_dbg(&codec_pdev->dev, "%s: (%d, %ld) %ld, %ld\n", __func__,
+		ch, val, amp_priv->imp_l, amp_priv->imp_r);
+
+	mutex_unlock(&amp_priv->lock);
+
+	return ret;
+}
+EXPORT_SYMBOL(codec_misc_amp_put);
+
+/*
+ * amplify impedance show function
+ */
+static ssize_t resistance_left_right_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	struct codec_misc_priv_type *misc_priv = dev_get_drvdata(dev);
+	struct amp_stat_priv_type *amp_priv = &misc_priv->amp_stat_priv;
+	int ret = 0;
+
+	char resistance[128];
+	int scale = 100000;
+
+	mutex_lock(&amp_priv->lock);
+
+	scnprintf(resistance,
+		sizeof(resistance), "%d.%d,%d.%d",
+		amp_priv->imp_l/scale,
+		amp_priv->imp_l%scale,
+		amp_priv->imp_r/scale,
+		amp_priv->imp_r%scale);
+
+	dev_info(dev, "%s: %s (%ld, %ld, %ld)\n",
+		__func__, resistance, amp_priv->imp_l, amp_priv->imp_r, scale);
+	ret = scnprintf(buf, PAGE_SIZE, "%s", resistance);
+
+	mutex_unlock(&amp_priv->lock);
+
+	return ret;
+}
+static DEVICE_ATTR_RO(resistance_left_right);
+
 static int misc_probe(struct platform_device *pdev)
 {
 	int ret;
@@ -298,6 +384,14 @@ static int misc_probe(struct platform_device *pdev)
 		goto err_wdsp_stat;
 	}
 
+	ret = device_create_file(&codec_pdev->dev,
+		&dev_attr_resistance_left_right);
+	if (ret) {
+		dev_err(&codec_pdev->dev,
+			"failed to create resistance_left_right\n");
+		goto err_resistance_left_right;
+	}
+
 	srcu_init_notifier_head(&misc_priv->core_notifier_list);
 	misc_priv->nb_core.notifier_call = codec_misc_notify;
 
@@ -315,12 +409,15 @@ static int misc_probe(struct platform_device *pdev)
 
 	misc_priv->wdsp_stat_priv.ktime_zero = ktime_set(0, 0);
 	mutex_init(&misc_priv->wdsp_stat_priv.lock);
+	mutex_init(&misc_priv->amp_stat_priv.lock);
 
 	dev_set_drvdata(&codec_pdev->dev, misc_priv);
 	dev_info(&codec_pdev->dev, "%s: create codec_success\n");
 	return ret;
 
 err_notifier_reg:
+	device_remove_file(&pdev->dev, &dev_attr_resistance_left_right);
+err_resistance_left_right:
 	device_remove_file(&codec_pdev->dev, &dev_attr_wdsp_stat);
 err_wdsp_stat:
 	device_remove_file(&codec_pdev->dev, &dev_attr_hwinfo_part_number);
@@ -330,6 +427,7 @@ err_headset_codec_state:
 	device_remove_file(&codec_pdev->dev, &dev_attr_codec_state);
 err_codec_state:
 	mutex_destroy(&misc_priv->wdsp_stat_priv.lock);
+	mutex_destroy(&misc_priv->amp_stat_priv.lock);
 	devm_kfree(&pdev->dev, misc_priv);
 	return ret;
 }
@@ -343,7 +441,9 @@ static int misc_remove(struct platform_device *pdev)
 	device_remove_file(&pdev->dev, &dev_attr_hwinfo_part_number);
 	device_remove_file(&pdev->dev, &dev_attr_headset_codec_state);
 	device_remove_file(&pdev->dev, &dev_attr_codec_state);
+	device_remove_file(&pdev->dev, &dev_attr_resistance_left_right);
 	mutex_destroy(&misc_priv->wdsp_stat_priv.lock);
+	mutex_destroy(&misc_priv->amp_stat_priv.lock);
 	devm_kfree(&pdev->dev, misc_priv);
 
 	return 0;
