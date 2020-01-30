67637788b00a855fd71e67c59cef73219c57fe84
Mark Salyzyn <salyzyn@google.com>
2020-01-30 12:28:46 -0800

    GKI: enable tristate QCOM_LAZY_MAPPING for qcom,qsmmu-v500
    New driver module:
    
    drivers/iommu/msm_dma_iommu_mapping.ko
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Test: compile
    Bug: 141888626
    Change-Id: I516105fa8866cdab5de81f8b47fb6a2255027837

--- include/linux/msm_dma_iommu_mapping.h
+++ include/linux/msm_dma_iommu_mapping.h
@@ -11,7 +11,7 @@
 #include <linux/scatterlist.h>
 #include <linux/dma-mapping.h>
 
-#ifdef CONFIG_QCOM_LAZY_MAPPING
+#if IS_ENABLED(CONFIG_QCOM_LAZY_MAPPING)
 /*
  * This function is not taking a reference to the dma_buf here. It is expected
  * that clients hold reference to the dma_buf until they are done with mapping
