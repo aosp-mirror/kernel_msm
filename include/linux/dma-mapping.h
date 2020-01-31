400ea1d9e773612cd851301e696607e947a57160
Mark Salyzyn <salyzyn@google.com>
2020-01-31 08:23:42 -0800

    GKI: DMA_ATTR_* movement to be compatible with upstream
    Upstream 4.19, 5.4 and kernel.org (today) require that the
    DMA_ATTR_* flags be moved up one notch in order to be ABI
    compatible.
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Test: build
    Bug: 141888626
    Change-Id: I2634d65b3f7b5a187d9ad2dac9b66b79929d6d3c

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -63,54 +63,54 @@
  */
 #define DMA_ATTR_NO_WARN	(1UL << 8)
 /*
+ * DMA_ATTR_PRIVILEGED: used to indicate that the buffer is fully
+ * accessible at an elevated privilege level (and ideally inaccessible or
+ * at least read-only at lesser-privileged levels).
+ */
+#define DMA_ATTR_PRIVILEGED		(1UL << 9)
+
+/*
  * DMA_ATTR_STRONGLY_ORDERED: Specifies that accesses to the mapping must
  * not be buffered, reordered, merged with other accesses, or unaligned.
  * No speculative access may occur in this mapping.
  */
-#define DMA_ATTR_STRONGLY_ORDERED	(1UL << 9)
+#define DMA_ATTR_STRONGLY_ORDERED	(1UL << 10)
 /*
  * DMA_ATTR_SKIP_ZEROING: Do not zero mapping.
  */
-#define DMA_ATTR_SKIP_ZEROING		(1UL << 10)
+#define DMA_ATTR_SKIP_ZEROING		(1UL << 11)
 /*
  * DMA_ATTR_NO_DELAYED_UNMAP: Used by msm specific lazy mapping to indicate
  * that the mapping can be freed on unmap, rather than when the ion_buffer
  * is freed.
  */
-#define DMA_ATTR_NO_DELAYED_UNMAP	(1UL << 11)
+#define DMA_ATTR_NO_DELAYED_UNMAP	(1UL << 12)
 /*
  * DMA_ATTR_EXEC_MAPPING: The mapping has executable permissions.
  */
-#define DMA_ATTR_EXEC_MAPPING		(1UL << 12)
+#define DMA_ATTR_EXEC_MAPPING		(1UL << 13)
 /*
  * DMA_ATTR_IOMMU_USE_UPSTREAM_HINT: Normally an smmu will override any bus
  * attributes (i.e cacheablilty) provided by the client device. Some hardware
  * may be designed to use the original attributes instead.
  */
-#define DMA_ATTR_IOMMU_USE_UPSTREAM_HINT	(1UL << 13)
+#define DMA_ATTR_IOMMU_USE_UPSTREAM_HINT	(1UL << 14)
 /*
  * When passed to a DMA map call the DMA_ATTR_FORCE_COHERENT DMA
  * attribute can be used to force a buffer to be mapped as IO coherent.
  */
-#define DMA_ATTR_FORCE_COHERENT			(1UL << 14)
+#define DMA_ATTR_FORCE_COHERENT			(1UL << 15)
 /*
  * When passed to a DMA map call the DMA_ATTR_FORCE_NON_COHERENT DMA
  * attribute can be used to force a buffer to not be mapped as IO
  * coherent.
  */
-#define DMA_ATTR_FORCE_NON_COHERENT		(1UL << 15)
+#define DMA_ATTR_FORCE_NON_COHERENT		(1UL << 16)
 /*
  * DMA_ATTR_DELAYED_UNMAP: Used by ION, it will ensure that mappings are not
  * removed on unmap but instead are removed when the ion_buffer is freed.
  */
-#define DMA_ATTR_DELAYED_UNMAP		(1UL << 16)
-
-/*
- * DMA_ATTR_PRIVILEGED: used to indicate that the buffer is fully
- * accessible at an elevated privilege level (and ideally inaccessible or
- * at least read-only at lesser-privileged levels).
- */
-#define DMA_ATTR_PRIVILEGED		(1UL << 17)
+#define DMA_ATTR_DELAYED_UNMAP		(1UL << 17)
 
 /*
  * DMA_ATTR_IOMMU_USE_LLC_NWA: Overrides the bus attributes to use the System
