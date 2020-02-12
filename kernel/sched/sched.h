8bc7fc013391af48aea7d0556bacb144a7328c30
Kyle Lin <kylelin@google.com>
2020-02-12 17:48:00 +0800

    kernel: sched: account for real time utilization
    PELT doesn't account for real time task utilization in cpu_util().
    As the result a CPU busy running RT task is considered as low
    utilization by the scheduler. Fix this by adding real time loading
    in to account.
    
    Bug: 147385228
    Test: boot to home and run audio test
    
    Change-Id: Ie4412b186608b9a618f0d35cee9a7310db481f7c
    Signed-off-by: Kyle Lin <kylelin@google.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2040,7 +2040,12 @@ static inline unsigned long task_util(struct task_struct *p)
  *
  * Return: the (estimated) utilization for the specified CPU
  */
+
+#ifdef CONFIG_SCHED_WALT
 static inline unsigned long cpu_util(int cpu)
+#else
+static inline unsigned long __cpu_util(int cpu)
+#endif
 {
 	struct cfs_rq *cfs_rq;
 	unsigned int util;
@@ -2147,11 +2152,17 @@ static inline unsigned long cpu_util_rt(int cpu)
 	return rt_rq->avg.util_avg;
 }
 
+static inline unsigned long cpu_util(int cpu)
+{
+	return min(__cpu_util(cpu) + cpu_util_rt(cpu), capacity_orig_of(cpu));
+}
+
 static inline unsigned long
 cpu_util_freq(int cpu, struct sched_walt_cpu_load *walt_load)
 {
-	return min(cpu_util(cpu) + cpu_util_rt(cpu), capacity_orig_of(cpu));
+	return min(cpu_util(cpu), capacity_orig_of(cpu));
 }
+
 
 #define sched_ravg_window TICK_NSEC
 #define sysctl_sched_use_walt_cpu_util 0
