0ad244ac1e1ab3d42104bfd1e237a1d65071fc53
Wei Wang <wvw@google.com>
2020-02-13 11:43:29 -0800

    sched: adopt capacity margin 20% from EAS PELT's capacity_margin
    We have seen cases tasks stuck on little without migration.
    This also aligns with is_packing_eligible capacity margin calculation.
    
    Bug: 143472450
    Bug: 147785606
    Change-Id: I8650e6acc541172eb6a981c6b4b4c875d44dbb28
    Signed-off-by: Wei Wang <wvw@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -194,13 +194,13 @@ unsigned int capacity_margin				= 1280;
 
 /* Migration margins */
 unsigned int sysctl_sched_capacity_margin_up[MAX_MARGIN_LEVELS] = {
-			[0 ... MAX_MARGIN_LEVELS-1] = 1078}; /* ~5% margin */
+			[0 ... MAX_MARGIN_LEVELS-1] = 1280}; /* ~20% margin */
 unsigned int sysctl_sched_capacity_margin_down[MAX_MARGIN_LEVELS] = {
-			[0 ... MAX_MARGIN_LEVELS-1] = 1205}; /* ~15% margin */
+			[0 ... MAX_MARGIN_LEVELS-1] = 1280}; /* ~20% margin */
 unsigned int sched_capacity_margin_up[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1078}; /* ~5% margin */
+			[0 ... NR_CPUS-1] = 1280}; /* ~20% margin */
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1205}; /* ~15% margin */
+			[0 ... NR_CPUS-1] = 1280}; /* ~20% margin */
 
 #ifdef CONFIG_SCHED_WALT
 /* 1ms default for 20ms window size scaled to 1024 */
