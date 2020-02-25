ea7f375580f6141c4788454c20424e8a071af314
Woody Lin <woodylin@google.com>
2020-02-25 23:02:40 +0800

    led-class: Fix a led-class de-init bug
    Disable the `led_cdev' via `led_sysfs_disable()' when unregistering it.
    This fixes a race condition bug that since device references might still
    held by any leds_class attribute file opener after this, so `led_cdev'
    can be accessed even after the unregistering. Disable the led sysfs of
    it to avoid later accesses from sub-modules of led-class, e.g., the
    `led_trigger_store()'.
    
    Bug: 146829723
    Change-Id: Ic492552405d0fe294d0833a685eb68243907aabb
    Signed-off-by: Woody Lin <woodylin@google.com>

--- drivers/leds/led-class.c
+++ drivers/leds/led-class.c
@@ -323,6 +323,7 @@ EXPORT_SYMBOL_GPL(of_led_classdev_register);
  */
 void led_classdev_unregister(struct led_classdev *led_cdev)
 {
+	mutex_lock(&led_cdev->led_access);
 #ifdef CONFIG_LEDS_TRIGGERS
 	down_write(&led_cdev->trigger_lock);
 	if (led_cdev->trigger)
@@ -331,6 +332,8 @@ void led_classdev_unregister(struct led_classdev *led_cdev)
 #endif
 
 	led_cdev->flags |= LED_UNREGISTERING;
+	led_sysfs_disable(led_cdev);
+	mutex_unlock(&led_cdev->led_access);
 
 	/* Stop blinking */
 	led_stop_software_blink(led_cdev);
@@ -347,8 +350,6 @@ void led_classdev_unregister(struct led_classdev *led_cdev)
 	down_write(&leds_list_lock);
 	list_del(&led_cdev->node);
 	up_write(&leds_list_lock);
-
-	mutex_destroy(&led_cdev->led_access);
 }
 EXPORT_SYMBOL_GPL(led_classdev_unregister);
 
