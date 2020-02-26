95c6752172c44ec96d913b2f2a7af7aceb6f9534
Wasb Liu <wasbliu@google.com>
2020-02-27 00:17:39 +0800

    google_battery: store cycle count to eeprom
    Bug: 153918349
    Change-Id: I950bc9897c7a5098a7b9cdd6a5b6cbd055f2fb77
    Signed-off-by: Wasb Liu <wasbliu@google.com>
    Signed-off-by: Ken Tsou <kentsou@google.com>
    (cherry picked from commit 0cd45b619ba4f831cd29b23bcbbdc19cd84e57fb)

--- drivers/power/supply/google/google_bms.h
+++ drivers/power/supply/google/google_bms.h
@@ -357,6 +357,8 @@ ssize_t ttf_dump_details(char *buf, int max_size,
 #define GBMS_BGPN_LEN	10
 /* Battery device info length */
 #define GBMS_DINF_LEN	32
+/* Battery device info length */
+#define GBMS_CNTB_LEN	16
 
 /**
  * Tags are u32 constants: hardcoding as hex since characters constants of more
@@ -375,6 +377,7 @@ enum gbms_tags {
 	GBMS_TAG_MINF = 0x4d494e46,
 	GBMS_TAG_DINF = 0x44494e46,
 	GBMS_TAG_BGPN = 0x4247504e,
+	GBMS_TAG_CNTB = 0x434e5442,
 	GBMS_TAG_CELL = 0x43454c4c,
 };
 
