5ef711393db757d38183919ccf26e24b6cdd0da0
Mark Salyzyn <salyzyn@google.com>
2020-02-27 09:24:44 -0800

    GKI: cma: redirect page allocation to CMA (part deux)
    In commit 46f8fca539686ce8493ff82206f9de2d07c9d72c
    ("cma: redirect page allocation to CMA") the ___GFP_CMA bit was
    introduced, but was snuck below an established upstream
    ___GFP_NOLOCKDEP bit which was shifted up.  This introduces an ABI
    incompatibility to the GKI kernel because of macro tricks
    surrounding CONFIG_LOCKDEP.
    
    Define ___GFP_CMA just beyond the kernel.org latest definitions
    instead, and ensure that the resulting __GFP_BITS_MASK is correct
    for the CONFIG_LOCKDEP=n case.  This will help preserve the ABI.
    
    There will be an ABI issue should upstream introduce a new ___GFP_*
    flag that conflicts with ___GFP_CMA, which has not happened between
    4.18 and 5.6+, by touching both __GFP_BITS_SHIFT and __GFP_BITS_MASK
    in this adjustment, hopefully future selves will take an appropriate
    action after seeing the spirit of this adjustment.
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Test: compile
    Bug: 150378964
    Change-Id: I336dfcf9a1ea0f63773a03ad6e8414a53aa800ab

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -39,12 +39,12 @@ struct vm_area_struct;
 #define ___GFP_ACCOUNT		0x100000u
 #define ___GFP_DIRECT_RECLAIM	0x200000u
 #define ___GFP_KSWAPD_RECLAIM	0x400000u
-#define ___GFP_CMA		0x800000u
 #ifdef CONFIG_LOCKDEP
-#define ___GFP_NOLOCKDEP	0x1000000u
+#define ___GFP_NOLOCKDEP	0x800000u
 #else
 #define ___GFP_NOLOCKDEP	0
 #endif
+#define ___GFP_CMA		0x1000000u
 /* If the above are modified, __GFP_BITS_SHIFT may need updating */
 
 /*
@@ -219,8 +219,13 @@ struct vm_area_struct;
 #define __GFP_NOLOCKDEP ((__force gfp_t)___GFP_NOLOCKDEP)
 
 /* Room for N __GFP_FOO bits */
-#define __GFP_BITS_SHIFT (24 + IS_ENABLED(CONFIG_LOCKDEP))
+#define __GFP_BITS_SHIFT (25)
+#ifdef CONFIG_LOCKDEP
 #define __GFP_BITS_MASK ((__force gfp_t)((1 << __GFP_BITS_SHIFT) - 1))
+#else
+#define __GFP_BITS_MASK (((__force gfp_t)((1 << __GFP_BITS_SHIFT) - 1)) & \
+				~0x800000u) /* ghost of ___GFP_NOLOCKDEP */
+#endif
 
 /**
  * DOC: Useful GFP flag combinations
