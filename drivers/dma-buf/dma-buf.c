05d95b4fba5646916ea5eea36416c64a807a7b55
Suren Baghdasaryan <surenb@google.com>
2020-03-02 14:07:32 -0800

    GKI: remote dma_buf.ktime when debugfs is disabled
    dma_buf.ktime is not used when debugfs is disabled. Remove it when debugfs
    is disabled.
    
    Bug: 150611569
    Test: build
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I68a05f5e85b2ffe7228debe5e939b065a8cbfa59

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -608,7 +608,9 @@ struct dma_buf *dma_buf_export(const struct dma_buf_export_info *exp_info)
 	init_waitqueue_head(&dmabuf->poll);
 	dmabuf->cb_excl.poll = dmabuf->cb_shared.poll = &dmabuf->poll;
 	dmabuf->cb_excl.active = dmabuf->cb_shared.active = 0;
+#if defined(CONFIG_DEBUG_FS)
 	dmabuf->ktime = ktime_get();
+#endif
 
 	if (!resv) {
 		resv = (struct reservation_object *)&dmabuf[1];
