26ca615b1af2d798c6353e429aafa6c27b829eb2
Suren Baghdasaryan <surenb@google.com>
2020-03-04 15:11:28 -0800

    ANDROID: replace NR_INDIRECTLY_RECLAIMABLE_BYTES with NR_KERNEL_MISC_RECLAIMABLE
    Use NR_KERNEL_MISC_RECLAIMABLE instead of
    NR_INDIRECTLY_RECLAIMABLE_BYTES for kgsl allocations and in sysstats.
    
    Bug: 150808082
    Test: build
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ice5167bd9b380bb4c4b4d810aa685d211bcf2f80

--- drivers/gpu/msm/kgsl_pool.c
+++ drivers/gpu/msm/kgsl_pool.c
@@ -83,8 +83,8 @@ _kgsl_pool_add_page(struct kgsl_page_pool *pool, struct page *p)
 	list_add_tail(&p->lru, &pool->page_list);
 	pool->page_count++;
 	spin_unlock(&pool->list_lock);
-	mod_node_page_state(page_pgdat(p), NR_INDIRECTLY_RECLAIMABLE_BYTES,
-				(PAGE_SIZE << pool->pool_order));
+	mod_node_page_state(page_pgdat(p), NR_KERNEL_MISC_RECLAIMABLE,
+				(1 << pool->pool_order));
 }
 
 /* Returns a page from specified pool */
@@ -102,9 +102,9 @@ _kgsl_pool_get_page(struct kgsl_page_pool *pool)
 	spin_unlock(&pool->list_lock);
 
 	if (p != NULL)
-		mod_node_page_state(page_pgdat(p),
-				NR_INDIRECTLY_RECLAIMABLE_BYTES,
-				-(PAGE_SIZE << pool->pool_order));
+		mod_node_page_state(page_pgdat(p), NR_KERNEL_MISC_RECLAIMABLE,
+				-(1 << pool->pool_order));
+
 	return p;
 }
 
