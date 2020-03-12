ee6a37aa8107db2ea7e09c1e059db09dd6fb9e25
achigoliu <achigoliu@google.com>
2020-03-12 16:20:46 +0800

    netfilter: conntrack: fix kmemleak false positive
    backtrace:
      [<0000000002de3c18>] __nf_conntrack_alloc+0x68/0x154
      [<00000000eaf1ebb8>] init_conntrack+0xec/0x834
      [<0000000089f748cf>] resolve_normal_ct+0x1b8/0x2b0
      [<00000000f30b9c8f>] nf_conntrack_in+0x18c/0x9e0
      [<0000000040778445>] ipv6_conntrack_local+0x58/0x64
      [<0000000095e0f887>] nf_hook_slow+0x84/0x124
      [<00000000920c2dd3>] ip6_xmit+0x59c/0xbf8
      [<00000000aa24675b>] inet6_csk_xmit+0xc0/0x12c
      [<000000006dc62e9d>] __tcp_transmit_skb+0x8b8/0xd20
      [<000000007e2dd67a>] tcp_connect+0x3f0/0x90c
      [<000000002df7630f>] tcp_v6_connect+0x748/0x7cc
      [<000000007317387e>] __inet_stream_connect+0xb8/0x3e4
      [<00000000dab8020c>] inet_stream_connect+0x48/0x70
      [<00000000b5e27e12>] SyS_connect+0x138/0x1ec
      [<000000002f42a13d>] __sys_trace_return+0x0/0x4
      [<0000000090fa2892>] 0xffffffffffffffff
    
    Bug: 150836591
    Bug: 141972862
    Change-Id: Ib621059c844aea39db30d10e0a85624df25ea032
    Signed-off-by: achigoliu <achigoliu@google.com>

--- net/netfilter/nf_conntrack_core.c
+++ net/netfilter/nf_conntrack_core.c
@@ -1184,6 +1184,7 @@ __nf_conntrack_alloc(struct net *net,
 	 * SLAB_TYPESAFE_BY_RCU.
 	 */
 	ct = kmem_cache_alloc(nf_conntrack_cachep, gfp);
+	kmemleak_not_leak(ct);
 	if (ct == NULL)
 		goto out;
 
