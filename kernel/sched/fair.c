9640947f761285df92b85e702a28e0705975128c
Rick Yiu <rickyiu@google.com>
2020-03-25 21:41:10 +0800

    sched: separate capacity margin for boosted tasks
    With the introduction of placement hint patch, boosted tasks will not
    scheduled from big cores. We tune capacity margin to let important
    boosted tasks get scheduled on big cores. However, the capacity margin
    affects all group of tasks, so that non-boosted tasks get more chances
    to be scheduled on big cores, too. This could be solved by separating
    capacity margin for boosted tasks.
    
    Bug: 147785606
    Test: margin set correctly
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I2b02e138e36a6844afbc1ade60fe86a001814b30

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -201,6 +201,14 @@ unsigned int sched_capacity_margin_up[NR_CPUS] = {
 			[0 ... NR_CPUS-1] = 1280}; /* ~20% margin */
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
 			[0 ... NR_CPUS-1] = 1280}; /* ~20% margin */
+unsigned int sysctl_sched_capacity_margin_up_boosted[MAX_MARGIN_LEVELS] = {
+			[0 ... MAX_MARGIN_LEVELS-1] = 1280}; /* ~20% margin */
+unsigned int sysctl_sched_capacity_margin_down_boosted[MAX_MARGIN_LEVELS] = {
+			[0 ... MAX_MARGIN_LEVELS-1] = 1280}; /* ~20% margin */
+unsigned int sched_capacity_margin_up_boosted[NR_CPUS] = {
+			[0 ... NR_CPUS-1] = 1280}; /* ~20% margin */
+unsigned int sched_capacity_margin_down_boosted[NR_CPUS] = {
+			[0 ... NR_CPUS-1] = 1280}; /* ~20% margin */
 
 #ifdef CONFIG_SCHED_WALT
 /* 1ms default for 20ms window size scaled to 1024 */
@@ -7384,9 +7392,13 @@ static inline bool task_fits_capacity(struct task_struct *p,
 	unsigned int margin;
 
 	if (capacity_orig_of(task_cpu(p)) > capacity_orig_of(cpu))
-		margin = sched_capacity_margin_down[task_cpu(p)];
+		margin = schedtune_task_boost(p) > 0 ?
+			sched_capacity_margin_down_boosted[task_cpu(p)] :
+			sched_capacity_margin_down[task_cpu(p)];
 	else
-		margin = sched_capacity_margin_up[task_cpu(p)];
+		margin = schedtune_task_boost(p) > 0 ?
+			sched_capacity_margin_up_boosted[task_cpu(p)] :
+			sched_capacity_margin_up[task_cpu(p)];
 
 	return capacity * 1024 > boosted_task_util(p) * margin;
 }
