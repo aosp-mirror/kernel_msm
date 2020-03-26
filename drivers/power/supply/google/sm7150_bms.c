818659b3350108442a7456748d2937b90345110d
Wasb Liu <wasbliu@google.com>
2020-03-26 15:33:47 +0800

    sm7150_bms: when charger online is off, return discharging.
    Bug: 152108709
    Signed-off-by: Wasb Liu <wasbliu@google.com>
    Change-Id: I74cd37852b19ee3d83fff05fc0237070ab1d676f

--- drivers/power/supply/google/sm7150_bms.c
+++ drivers/power/supply/google/sm7150_bms.c
@@ -623,18 +623,8 @@ static int sm7150_get_chg_status(const struct bms_dev *bms,
 
 	stat1 = stat1 & CHGR_BATTERY_CHARGER_STATUS_MASK;
 
-	if (!plugged) {
-		switch (stat1) {
-		case SM7150_TERMINATE_CHARGE:
-		case SM7150_INHIBIT_CHARGE:
-			ret = POWER_SUPPLY_STATUS_FULL;
-			break;
-		default:
-			ret = POWER_SUPPLY_STATUS_DISCHARGING;
-			break;
-		}
-		return ret;
-	}
+	if (!plugged)
+		ret = POWER_SUPPLY_STATUS_DISCHARGING;
 
 	switch (stat1) {
 	case SM7150_TRICKLE_CHARGE:
