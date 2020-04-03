1122875ebe78dd3ab949fc6bd38726f1a0573fd1
Will McVicker <willmcvicker@google.com>
2020-04-03 09:42:16 -0700

    GKI: redbull: create ENABLE_THINLTO and BUILD_PERF vars
    * Enables THINLTO for the build_redbull-gki.sh script
    * Adds var BUILD_PERF to enable/disable a performance build. This is
      enabled by default.
    
    To disable the performance config,
      BUILD_PERF=0 ./build_redbull-gki.sh
    
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Bug: 153164039
    Test: compile with and without BUILD_PERF set
    Change-Id: I2a50f88c71eef3443e21ba974c38f91c1f855a1b

--- build_redbull-gki.sh
+++ build_redbull-gki.sh
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0
-if [[ "${BUILD_ABI:-0}" -eq 0 ]]; then
-BUILD_BOOT_IMG=true BUILD_CONFIG=private/msm-google/build.config.gki.redbull \
-  build/build.sh "$@"
-else
-BUILD_BOOT_IMG=true BUILD_CONFIG=private/msm-google/build.config.gki.redbull \
-  build/build_abi.sh --update "$@"
+BUILD_SCRIPT="build/build.sh"
+if [[ "${BUILD_ABI:-0}" -eq 1 ]]; then
+  BUILD_SCRIPT="build/build_abi.sh --update"
 fi
+
+BUILD_BOOT_IMG=true \
+	BUILD_CONFIG=private/msm-google/build.config.gki.redbull \
+	${BUILD_SCRIPT} "$@"
