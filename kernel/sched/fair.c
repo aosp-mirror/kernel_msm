7944a3bdaf9b673d6683f4ef65f61a7b4d7d52e3
Wei Wang <wvw@google.com>
2020-04-10 17:29:24 -0700

    sched: change capacity margin down to 20% for non-boost task
    Bug: 153761742
    Test: Build
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I21289f1c920b59364b74ba2408e2ca87cf57ea49

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -197,8 +197,8 @@ unsigned int sysctl_sched_capacity_margin_up[MAX_MARGIN_LEVELS] = {
 	[0 ... MAX_MARGIN_LEVELS - 1] = 1280
 }; /* ~20% margin */
 unsigned int sysctl_sched_capacity_margin_down[MAX_MARGIN_LEVELS] = {
-	1575, 1280
-}; /* ~35 margin for big, ~20% margin for big+ */
+	[0 ... MAX_MARGIN_LEVELS - 1] = 1280
+}; /* ~20% margin */
 unsigned int sysctl_sched_capacity_margin_up_boosted[MAX_MARGIN_LEVELS] = {
 	[0 ... MAX_MARGIN_LEVELS-1] = 1280
 }; /* ~20% margin */
@@ -211,8 +211,8 @@ unsigned int sched_capacity_margin_up[NR_CPUS] = {
 	1280, 1280, 1280, 1280, 1280, 1280, 1280, 1078
 }; /* ~20% margin for small and big, 5% for big+ */
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
-	1280, 1280, 1280, 1280, 1575, 1575, 1575, 1280
-}; /* not used for small cores, ~35% margin for big, ~20% margin for big+ */
+	[0 ... NR_CPUS-1] = 1280
+}; /* ~20% margin */
 unsigned int sched_capacity_margin_up_boosted[NR_CPUS] = {
 	1280, 1280, 1280, 1280, 1280, 1280, 1280, 1078
 }; /* ~20% margin for small and big, 5% for big+ */
