f6797620aa7ed870e33038e55b0a86f639f93659
Martin Liu <liumartin@google.com>
2020-04-12 17:52:25 +0800

    Revert "mm, oom: check LMK enabled for pagefault_out_of_memory()"
    This reverts commit eba0983ef4034b4c7239304a3e93576ce857d607.
    
    Reason for revert: remove customized code
    Bug: 140544941
    Test: boot
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: Id619e29a238d902a0a3f45b5f17a731001c89911

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -1197,10 +1197,6 @@ void pagefault_out_of_memory(void)
 		.order = 0,
 	};
 
-	if (IS_ENABLED(CONFIG_HAVE_LOW_MEMORY_KILLER) ||
-	    IS_ENABLED(CONFIG_HAVE_USERSPACE_LOW_MEMORY_KILLER))
-		return;
-
 	if (mem_cgroup_oom_synchronize(true))
 		return;
 
