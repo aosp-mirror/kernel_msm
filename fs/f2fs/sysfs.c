86048baa3a0ea0da1a3fcaacd515a4e920802104
Jaegeuk Kim <jaegeuk@google.com>
2020-04-13 08:21:12 -0700

    Merge remote-tracking branch 'aosp/upstream-f2fs-stable-linux-4.14.y' into android-4.14-stable (v5.7-rc1)
    * aosp/upstream-f2fs-stable-linux-4.14.y:
      f2fs: keep inline_data when compression conversion
      f2fs: fix to disable compression on directory
      f2fs: add missing CONFIG_F2FS_FS_COMPRESSION
      f2fs: switch discard_policy.timeout to bool type
      f2fs: fix to verify tpage before releasing in f2fs_free_dic()
      f2fs: show compression in statx
      f2fs: clean up dic->tpages assignment
      f2fs: compress: support zstd compress algorithm
      f2fs: compress: add .{init,destroy}_decompress_ctx callback
      f2fs: compress: fix to call missing destroy_compress_ctx()
      f2fs: change default compression algorithm
      f2fs: clean up {cic,dic}.ref handling
      f2fs: fix to use f2fs_readpage_limit() in f2fs_read_multi_pages()
      f2fs: xattr.h: Make stub helpers inline
      f2fs: fix to avoid double unlock
      f2fs: fix potential .flags overflow on 32bit architecture
      f2fs: fix NULL pointer dereference in f2fs_verity_work()
      f2fs: fix to clear PG_error if fsverity failed
      f2fs: don't call fscrypt_get_encryption_info() explicitly in f2fs_tmpfile()
      f2fs: don't trigger data flush in foreground operation
      f2fs: fix NULL pointer dereference in f2fs_write_begin()
      f2fs: clean up f2fs_may_encrypt()
      f2fs: fix to avoid potential deadlock
      f2fs: don't change inode status under page lock
      f2fs: fix potential deadlock on compressed quota file
      f2fs: delete DIO read lock
      f2fs: don't mark compressed inode dirty during f2fs_iget()
      f2fs: fix to account compressed blocks in f2fs_compressed_blocks()
      f2fs: xattr.h: Replace zero-length array with flexible-array member
      f2fs: fix to update f2fs_super_block fields under sb_lock
      f2fs: Add a new CP flag to help fsck fix resize SPO issues
      f2fs: Fix mount failure due to SPO after a successful online resize FS
      f2fs: use kmem_cache pool during inline xattr lookups
      f2fs: skip migration only when BG_GC is called
      f2fs: fix to show tracepoint correctly
      f2fs: avoid __GFP_NOFAIL in f2fs_bio_alloc
      f2fs: introduce F2FS_IOC_GET_COMPRESS_BLOCKS
      f2fs: fix to avoid triggering IO in write path
      f2fs: add prefix for f2fs slab cache name
      f2fs: introduce DEFAULT_IO_TIMEOUT
      f2fs: skip GC when section is full
      f2fs: add migration count iff migration happens
      f2fs: clean up bggc mount option
      f2fs: clean up lfs/adaptive mount option
      f2fs: fix to show norecovery mount option
      f2fs: clean up parameter of macro XATTR_SIZE()
      f2fs: clean up codes with {f2fs_,}data_blkaddr()
      f2fs: show mounted time
      f2fs: Use scnprintf() for avoiding potential buffer overflow
      f2fs: allow to clear F2FS_COMPR_FL flag
      f2fs: fix to check dirty pages during compressed inode conversion
      f2fs: fix to account compressed inode correctly
      f2fs: fix wrong check on F2FS_IOC_FSSETXATTR
      f2fs: fix to avoid use-after-free in f2fs_write_multi_pages()
      f2fs: fix to avoid using uninitialized variable
      f2fs: fix inconsistent comments
      f2fs: remove i_sem lock coverage in f2fs_setxattr()
      f2fs: cover last_disk_size update with spinlock
      f2fs: fix to check i_compr_blocks correctly
      f2fs: fix to avoid potential deadlock
      f2fs: add missing function name in kernel message
      f2fs: recycle unused compress_data.chksum feild
      f2fs: fix to avoid NULL pointer dereference
      f2fs: fix leaking uninitialized memory in compressed clusters
      f2fs: fix the panic in do_checkpoint()
      f2fs: fix to wait all node page writeback
      mm/swapfile.c: move inode_lock out of claim_swapfile
      fscrypt: don't evict dirty inodes after removing key
    
     Conflicts:
    	fs/f2fs/file.c
    
    Bug: 151226003
    Change-Id: I86ee3579255cf2f37cf1a1d00ee4af90e973242a
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -109,47 +109,47 @@ static ssize_t features_show(struct f2fs_attr *a,
 		return sprintf(buf, "0\n");
 
 	if (f2fs_sb_has_encrypt(sbi))
-		len += snprintf(buf, PAGE_SIZE - len, "%s",
+		len += scnprintf(buf, PAGE_SIZE - len, "%s",
 						"encryption");
 	if (f2fs_sb_has_blkzoned(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "blkzoned");
 	if (f2fs_sb_has_extra_attr(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "extra_attr");
 	if (f2fs_sb_has_project_quota(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "projquota");
 	if (f2fs_sb_has_inode_chksum(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "inode_checksum");
 	if (f2fs_sb_has_flexible_inline_xattr(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "flexible_inline_xattr");
 	if (f2fs_sb_has_quota_ino(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "quota_ino");
 	if (f2fs_sb_has_inode_crtime(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "inode_crtime");
 	if (f2fs_sb_has_lost_found(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "lost_found");
 	if (f2fs_sb_has_verity(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "verity");
 	if (f2fs_sb_has_sb_chksum(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "sb_checksum");
 	if (f2fs_sb_has_casefold(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "casefold");
 	if (f2fs_sb_has_compression(sbi))
-		len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+		len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "compression");
-	len += snprintf(buf + len, PAGE_SIZE - len, "%s%s",
+	len += scnprintf(buf + len, PAGE_SIZE - len, "%s%s",
 				len ? ", " : "", "pin_file");
-	len += snprintf(buf + len, PAGE_SIZE - len, "\n");
+	len += scnprintf(buf + len, PAGE_SIZE - len, "\n");
 	return len;
 }
 
@@ -239,16 +239,16 @@ static ssize_t f2fs_sbi_show(struct f2fs_attr *a,
 		int hot_count = sbi->raw_super->hot_ext_count;
 		int len = 0, i;
 
-		len += snprintf(buf + len, PAGE_SIZE - len,
+		len += scnprintf(buf + len, PAGE_SIZE - len,
 						"cold file extension:\n");
 		for (i = 0; i < cold_count; i++)
-			len += snprintf(buf + len, PAGE_SIZE - len, "%s\n",
+			len += scnprintf(buf + len, PAGE_SIZE - len, "%s\n",
 								extlist[i]);
 
-		len += snprintf(buf + len, PAGE_SIZE - len,
+		len += scnprintf(buf + len, PAGE_SIZE - len,
 						"hot file extension:\n");
 		for (i = cold_count; i < cold_count + hot_count; i++)
-			len += snprintf(buf + len, PAGE_SIZE - len, "%s\n",
+			len += scnprintf(buf + len, PAGE_SIZE - len, "%s\n",
 								extlist[i]);
 		return len;
 	}
@@ -580,7 +580,9 @@ F2FS_FEATURE_RO_ATTR(verity, FEAT_VERITY);
 #endif
 F2FS_FEATURE_RO_ATTR(sb_checksum, FEAT_SB_CHECKSUM);
 F2FS_FEATURE_RO_ATTR(casefold, FEAT_CASEFOLD);
+#ifdef CONFIG_F2FS_FS_COMPRESSION
 F2FS_FEATURE_RO_ATTR(compression, FEAT_COMPRESSION);
+#endif
 
 #define ATTR_LIST(name) (&f2fs_attr_##name.attr)
 static struct attribute *f2fs_attrs[] = {
@@ -661,7 +663,9 @@ static struct attribute *f2fs_feat_attrs[] = {
 #endif
 	ATTR_LIST(sb_checksum),
 	ATTR_LIST(casefold),
+#ifdef CONFIG_F2FS_FS_COMPRESSION
 	ATTR_LIST(compression),
+#endif
 	NULL,
 };
 
