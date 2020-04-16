474861186e92a1958bf9cd5116e60cda634ed162
Gururaj Pandurangi <panduran@codeaurora.org>
2020-04-15 19:02:39 -0700

    qcacld-3.0: Fix while condition in rrm_fill_beacon_ies()
    In function rrm_fill_beacon_ies, do while loop is checked
    for BcnNumIes if it is greater than IE length 0. Fix the
    check to be greater than 2 as the first two bytes is IE
    header(element ID and IE length fields both 1 byte each.)
    
    Bug: 155654263
    Change-Id: I11e5de481cd49a22acafee938fbe73f839f5b0e4
    CRs-Fixed: 2626729

--- drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c
+++ drivers/staging/qcacld-3.0/core/mac/src/pe/rrm/rrm_api.c
@@ -1,8 +1,5 @@
 /*
- * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
- *
- * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
- *
+ * Copyright (c) 2012-2020 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -721,7 +718,7 @@ rrm_fill_beacon_ies(tpAniSirGlobal pMac,
 	*pNumIes += sizeof(uint16_t);
 	pIes += sizeof(uint16_t);
 
-	while (BcnNumIes > 0) {
+	while (BcnNumIes >= 2) {
 		len = *(pBcnIes + 1);
 		len += 2;       /* element id + length. */
 		pe_debug("EID = %d, len = %d total = %d",
