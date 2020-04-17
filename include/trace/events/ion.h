a12be688e07204f949b1998b178717934a30885e
Ioannis Ilkos <ilkos@google.com>
2020-04-17 16:31:00 +0100

    ANDROID: add ion_stat tracepoint
    Emitted an event whenever ion buffers are created or freed.
    This enables tracking ion memory utilization changes, as well as
    individual buffer allocations.
    
    This was inspired by the pixel kernel patches by timmurray@ and
    carmenjackson@.
    
    Cherry-picked from ACK 4.19 (original patch aosp/1288725). At this point
    the kernels have diverged. Pixel defines custom ion tracepoints in
    include/trace/events/ion.h which do not exist upstream. ACK defines
    ion tracepoints in the ion driver.
    
    Test: build
    Bug: 154302786
    Bug: 153676730
    Change-Id: I86ca2d447af2e13a88d76348a74246aa89e11361
    Signed-off-by: Ioannis Ilkos <ilkos@google.com>

--- include/trace/events/ion.h
+++ include/trace/events/ion.h
@@ -415,6 +415,40 @@ DEFINE_EVENT(ion_cp_secure_buffer, ion_cp_secure_buffer_end,
 		      unsigned long flags),
 
 	     TP_ARGS(heap_name, len, flags));
+
+#ifndef __ION_PTR_TO_HASHVAL
+static unsigned int __maybe_unused __ion_ptr_to_hash(const void *ptr)
+{
+	unsigned long hashval;
+
+	if (ptr_to_hashval(ptr, &hashval))
+		return 0;
+
+	/* The hashed value is only 32-bit */
+	return (unsigned int)hashval;
+}
+
+#define __ION_PTR_TO_HASHVAL
+#endif
+
+TRACE_EVENT(ion_stat,
+	    TP_PROTO(const void *addr, long len,
+		     unsigned long total_allocated),
+	    TP_ARGS(addr, len, total_allocated),
+	    TP_STRUCT__entry(__field(unsigned int, buffer_id)
+		__field(long, len)
+		__field(unsigned long, total_allocated)
+	    ),
+	    TP_fast_assign(__entry->buffer_id = __ion_ptr_to_hash(addr);
+		__entry->len = len;
+		__entry->total_allocated = total_allocated;
+	    ),
+	    TP_printk("buffer_id=%u len=%ldB total_allocated=%ldB",
+		      __entry->buffer_id,
+		      __entry->len,
+		      __entry->total_allocated)
+	    );
+
 #endif /* _TRACE_ION_H */
 
 #include <trace/define_trace.h>
