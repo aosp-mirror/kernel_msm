970c4fcb4d8cf252c7e25a807c155fba0987ef01
Nick Desaulniers <ndesaulniers@google.com>
2020-05-18 10:11:29 -0700

    msm: camera: icp: fix -Wunsequenced
    Fixes:
    drivers/media/platform/msm/camera_floral/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c:3481:17:
    error: multiple unsequenced modifications to 'num_cmd_buf'
    [-Werror,-Wunsequenced]
    num_cmd_buf-- : 0;
               ^
    drivers/media/platform/msm/camera_floral/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c:3505:17:
    error: multiple unsequenced modifications to 'num_cmd_buf'
    [-Werror,-Wunsequenced]
    num_cmd_buf-- : 0;
               ^
    
    Bug: 155426751
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Change-Id: I0bdcf704fdce60d103143843ed9890330650b84e

--- drivers/media/platform/msm/camera_floral/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
+++ drivers/media/platform/msm/camera_floral/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
@@ -3477,8 +3477,8 @@ static int cam_icp_mgr_process_cmd_desc(struct cam_icp_hw_mgr *hw_mgr,
 			if (rc) {
 				CAM_ERR(CAM_ICP, "get cmd buf failed %x",
 					hw_mgr->iommu_hdl);
-				num_cmd_buf = (num_cmd_buf > 0) ?
-					num_cmd_buf-- : 0;
+				if (num_cmd_buf > 0)
+					--num_cmd_buf;
 				goto rel_cmd_buf;
 			}
 			*fw_cmd_buf_iova_addr = addr;
@@ -3501,8 +3501,8 @@ static int cam_icp_mgr_process_cmd_desc(struct cam_icp_hw_mgr *hw_mgr,
 				CAM_ERR(CAM_ICP, "get cmd buf failed %x",
 					hw_mgr->iommu_hdl);
 				*fw_cmd_buf_iova_addr = 0;
-				num_cmd_buf = (num_cmd_buf > 0) ?
-					num_cmd_buf-- : 0;
+				if (num_cmd_buf > 0)
+					--num_cmd_buf;
 				goto rel_cmd_buf;
 			}
 			if ((len <= cmd_desc[i].offset) ||
