0afb6a175d8ce242eec0879410ad546b2ed5d334
Leo Liou <leoliou@google.com>
2020-05-19 17:35:36 +0800

    scsi: ufs: reject query requests when device shutdown
    The panic comes from new sysfs query requests when device has entered
    shutdown flow. At that time, device is in power off mode and
    cannot respond to any query command. This fix aims to introduce a
    rule to block new requests from sysfs under this use case. So add a
    status checking on those query functions.
    
    The callstack:
    Core 6 PC: el1_error+40
    Core 6 LR: ufshcd_exec_dev_cmd+798
    
     el1_error+0x40
     ufshcd_query_descriptor_retry+0x370
     ufshcd_read_desc_param+0x160
     specification_version_show+0x54
     dev_attr_show+0x48
     sysfs_kf_seq_show+0xb8
     kernfs_seq_show+0x48
     seq_read+0x25c
     kernfs_fop_read+0xb8
     __vfs_read+0x50
     vfs_read+0xbc
     ksys_read+0x78
     __arm64_sys_read+0x1c
     el0_svc_common+0xb4
     el0_svc_handler+0x60
     el0_svc+0x8
    
    Bug: 156176862
    Bug: 156956960
    Change-Id: I6906f0673d7ace6350a7f06d8445cd93cb0ccbee
    Signed-off-by: Leo Liou <leoliou@google.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4243,7 +4243,7 @@ int ufshcd_query_flag_retry(struct ufs_hba *hba,
 
 	for (retries = 0; retries < QUERY_REQ_RETRIES; retries++) {
 		ret = ufshcd_query_flag(hba, opcode, idn, flag_res);
-		if (ret)
+		if (ret && ret != -ENODEV)
 			dev_dbg(hba->dev,
 				"%s: failed with error %d, retries %d\n",
 				__func__, ret, retries);
@@ -4276,6 +4276,9 @@ int ufshcd_query_flag(struct ufs_hba *hba, enum query_opcode opcode,
 	int timeout = QUERY_REQ_TIMEOUT;
 
 	BUG_ON(!hba);
+
+	if (ufshcd_is_shutdown_ongoing(hba))
+		return -ENODEV;
 
 	ufshcd_hold_all(hba);
 	mutex_lock(&hba->dev_cmd.lock);
@@ -4345,6 +4348,9 @@ int ufshcd_query_attr(struct ufs_hba *hba, enum query_opcode opcode,
 
 	BUG_ON(!hba);
 
+	if (ufshcd_is_shutdown_ongoing(hba))
+		return -ENODEV;
+
 	ufshcd_hold_all(hba);
 	if (!attr_val) {
 		dev_err(hba->dev, "%s: attribute value required for opcode 0x%x\n",
@@ -4413,7 +4419,7 @@ int ufshcd_query_attr_retry(struct ufs_hba *hba,
 	 for (retries = QUERY_REQ_RETRIES; retries > 0; retries--) {
 		ret = ufshcd_query_attr(hba, opcode, idn, index,
 						selector, attr_val);
-		if (ret)
+		if (ret && ret != -ENODEV)
 			dev_dbg(hba->dev, "%s: failed with error %d, retries %d\n",
 				__func__, ret, retries);
 		else
@@ -4436,6 +4442,9 @@ static int __ufshcd_query_descriptor(struct ufs_hba *hba,
 	int err;
 
 	BUG_ON(!hba);
+
+	if (ufshcd_is_shutdown_ongoing(hba))
+		return -ENODEV;
 
 	ufshcd_hold_all(hba);
 	if (!desc_buf) {
@@ -4520,7 +4529,7 @@ int ufshcd_query_descriptor_retry(struct ufs_hba *hba,
 		err = __ufshcd_query_descriptor(hba, opcode, idn, index,
 						selector, desc_buf, buf_len);
 		up_read(&hba->query_lock);
-		if (!err || err == -EINVAL)
+		if (!err || err == -EINVAL || err == -ENODEV)
 			break;
 	}
 
