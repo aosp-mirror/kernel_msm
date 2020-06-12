648bde63ad1437b64255f8a7930da9ef187c185d
Kyle Lin <kylelin@google.com>
2020-06-12 11:21:22 +0800

    clk: qcom: osm: avoid freq run over max_freq
    Setting max freq which is not in the given cpu's freq table, the
    target_freq is the same with max freq. When selecting the freq, it uses
    CPUFREQ_RELATION_L, so the frequency round to the next. This is
    reasonable for normal cases but the frequency should not run over the
    max setting.
    
    Bug: 158134249
    Test: build and set max/min freq to check
    Change-Id: I804454225c10b7a023b323c58e77b7bba1ec3576
    Signed-off-by: Kyle Lin <kylelin@google.com>

--- drivers/clk/qcom/clk-cpu-osm.c
+++ drivers/clk/qcom/clk-cpu-osm.c
@@ -608,9 +608,12 @@ static unsigned int
 osm_cpufreq_fast_switch(struct cpufreq_policy *policy, unsigned int target_freq)
 {
 	int index;
+	unsigned int relation;
 
-	index = cpufreq_frequency_table_target(policy, target_freq,
-							CPUFREQ_RELATION_L);
+	relation = target_freq < policy->max ? CPUFREQ_RELATION_L :
+					       CPUFREQ_RELATION_H;
+
+	index = cpufreq_frequency_table_target(policy, target_freq, relation);
 	if (index < 0)
 		return 0;
 
