ebdb2416d0897e713168a2786de45b6ec32b5c80
Will McVicker <willmcvicker@google.com>
2020-07-01 16:53:44 -0700

    GKI: remove the gki build config
    The vintf config is now GKI compliant and is used as the official
    redbull user build kernel. So,
    
    * Remove the gki and abi configs.
    * Update the build_redbull-gki.sh script to use the vintf config
    * Add THINLTO flag for GKI dev purposes to the vintf config
    
    Bug: 159400292
    Change-Id: Ifa0028700c34599d3e737e7c03649e8ad1ca6d0e
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- build_redbull-gki.sh
+++ build_redbull-gki.sh
@@ -1,13 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0
 BUILD_SCRIPT="build/build.sh"
-CONFIG="private/msm-google/build.config.gki.redbull"
-
 if [[ "${BUILD_ABI:-0}" -eq 1 ]]; then
 	BUILD_SCRIPT="build/build_abi.sh --update"
-	CONFIG="private/msm-google/build.config.redbull.vintf"
 fi
 
 BUILD_BOOT_IMG=true \
 	ENABLE_THINLTO=1 \
-	BUILD_CONFIG=${CONFIG} \
+	BUILD_CONFIG="private/msm-google/build.config.redbull.vintf" \
 	${BUILD_SCRIPT} "$@"
