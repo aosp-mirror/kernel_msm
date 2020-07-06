158b3d1f7108fa9d49f70bac63c66d9134e0284d
Will McVicker <willmcvicker@google.com>
2020-07-06 14:24:27 -0700

    GKI: clk: qcom: debugcc: fix clk_measure register failure
    For builtin drivers, the debugfs nodes are created in clk_debug_init()
    by the late_initcall. For modules, the debugfs nodes will be created
    when the clk_hw is registered during probe. The clk_measure debugfs node
    in particular needs the clk_measure pointer registered before
    registering the associated clk_hw so that the core clock driver can
    setup the debugfs node for it during registration.
    
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Bug: 160389301
    Test: boot redfin with userdebug, ls /d/clk/*/clk_measure
    Change-Id: I2ae8bc98b209d1269445d680ed9ae230a8893977

--- drivers/clk/qcom/debugcc-lito.c
+++ drivers/clk/qcom/debugcc-lito.c
@@ -852,6 +852,12 @@ static int clk_debug_lito_probe(struct platform_device *pdev)
 		}
 	}
 
+	ret = clk_debug_measure_register(&gcc_debug_mux.hw);
+	if (ret) {
+		dev_err(&pdev->dev, "Could not register Measure clock\n");
+		return ret;
+	}
+
 	for (i = 0; i < ARRAY_SIZE(debugcc_lito_hws); i++) {
 		clk = devm_clk_register(&pdev->dev, debugcc_lito_hws[i]);
 		if (IS_ERR(clk)) {
@@ -860,10 +866,6 @@ static int clk_debug_lito_probe(struct platform_device *pdev)
 			return PTR_ERR(clk);
 		}
 	}
-
-	ret = clk_debug_measure_register(&gcc_debug_mux.hw);
-	if (ret)
-		dev_err(&pdev->dev, "Could not register Measure clock\n");
 
 	return ret;
 }
