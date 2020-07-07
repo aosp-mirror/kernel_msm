ba864347477a35115087bc8e973a9e46a19ea5d1
Ajit Kumar <kajit@codeaurora.org>
2020-07-07 16:11:50 +0530

    soc: qcom: bgcom: Use shared_ee property for spi
    Use shared_ee property for bg spi. Call pm runtime
    APIs to suspend the spi driver.
    
    Change-Id: I91701306fa78a7ee7c7b347f6ff0bf8b7345b3dd
    Signed-off-by: Ajit Kumar <kajit@codeaurora.org>

--- drivers/soc/qcom/bgcom_spi.c
+++ drivers/soc/qcom/bgcom_spi.c
@@ -277,7 +277,9 @@ static int bgcom_transfer(void *handle, uint8_t *tx_buf,
 		tx_xfer->rx_buf = rx_buf;
 
 	tx_xfer->len = txn_len;
+	pm_runtime_get_sync(spi->master->dev.parent);
 	ret = spi_sync(spi, &bg_spi->msg1);
+	pm_runtime_put_sync_suspend(spi->master->dev.parent);
 	mutex_unlock(&bg_spi->xfer_mutex);
 
 	if (ret)
