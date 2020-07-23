09465dc42ed8ac2590437739d47f688460ac49cb
Leo Liou <leoliou@google.com>
2020-07-23 20:10:45 +0800

    scsi: ufs: fixup the filter condition of scsi query command
    Commit 575ee2aeead8d45 ("scsi: ufs: reject query requests when device
    shutdown") fixup the filter condition and make sure
    ufshcd_disable_auto_bkops() can be run on shutdown flow.
    
    sd 0:0:0:0: [sda] Synchronizing SCSI cache
    ufshcd-qcom 1d84000.ufshc: ufshcd_query_attr_retry: query attribute, idn 13, failed with error -19 after 3 retires
    ufshcd-qcom 1d84000.ufshc: ufshcd_disable_auto_bkops: failed to enable exception event -19
    ...
    Call trace:
     ufshcd_disable_auto_bkops+0x80/0x200
     ufshcd_suspend+0x230/0x5ac
     ufshcd_shutdown+0x198/0x1cc
     ufshcd_pltfrm_shutdown+0x14/0x20
     platform_drv_shutdown+0x20/0x2c
     device_shutdown+0x194/0x1f4
     kernel_restart+0x40/0x108
     SyS_reboot+0x188/0x258
     el0_svc_naked+0x34/0x38
    ---[ end trace aee04f719bcaa1da ]---
    Synchronous External Abort: synchronous external abort (0x96000010) at 0xffffff800c804048
    Internal error: : 96000010 [#1] PREEMPT SMP
    
    Bug: 159699522
    Bug: 160202537
    Bug: 161014938
    Test: run generic/010 for testing, off-mode-charge, reboot-test
    Change-Id: I7fafd4f7e0061ed6ee9d6cb919f2a7495ced8c0f
    Signed-off-by: Leo Liou <leoliou@google.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4495,7 +4495,7 @@ int ufshcd_query_flag(struct ufs_hba *hba, enum query_opcode opcode,
 
 	BUG_ON(!hba);
 
-	if (ufshcd_is_shutdown_ongoing(hba))
+	if (ufshcd_is_shutdown_ongoing(hba) && ufshcd_is_ufs_dev_poweroff(hba))
 		return -ENODEV;
 
 	ufshcd_hold_all(hba);
@@ -4566,7 +4566,7 @@ int ufshcd_query_attr(struct ufs_hba *hba, enum query_opcode opcode,
 
 	BUG_ON(!hba);
 
-	if (ufshcd_is_shutdown_ongoing(hba))
+	if (ufshcd_is_shutdown_ongoing(hba) && ufshcd_is_ufs_dev_poweroff(hba))
 		return -ENODEV;
 
 	ufshcd_hold_all(hba);
@@ -4661,7 +4661,7 @@ static int __ufshcd_query_descriptor(struct ufs_hba *hba,
 
 	BUG_ON(!hba);
 
-	if (ufshcd_is_shutdown_ongoing(hba))
+	if (ufshcd_is_shutdown_ongoing(hba) && ufshcd_is_ufs_dev_poweroff(hba))
 		return -ENODEV;
 
 	ufshcd_hold_all(hba);
