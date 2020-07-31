86c69f124e3ac3a023d533cb451f66e1d278b5b2
Puma Hsu <pumahsu@google.com>
2020-07-31 23:53:35 +0800

    smb5-lib: Add property to enable/disable cc toggle
    While disabling cc by setting power role to none, pd_engine
    would try to set it back to DRP because there is a disconnect.
    Add POWER_SUPPLY_PROP_CC_TOGGLE_ENABLE to force control the
    typec cc toggling.
    
    - Disable typec port:
      $echo 0 > /sys/class/power_supply/usb/cc_toggle_enable
    - Enable typec port:
      $echo 1 > /sys/class/power_supply/usb/cc_toggle_enable
    
    Bug: 156695568
    Test: Device can disable/enable cc toggling.
    Signed-off-by: Puma Hsu <pumahsu@google.com>
    Change-Id: I11121a5ff31749dcc5ae4f97b5488d105fb08c57

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -765,6 +765,7 @@ static enum power_supply_property smb5_usb_props[] = {
 	POWER_SUPPLY_PROP_INPUT_CURRENT_MAX,
 	POWER_SUPPLY_PROP_MOISTURE_DETECTION_ENABLE,
 	POWER_SUPPLY_PROP_MOISTURE_DETECTED,
+	POWER_SUPPLY_PROP_CC_TOGGLE_ENABLE,
 	POWER_SUPPLY_PROP_HVDCP_OPTI_ALLOWED,
 	POWER_SUPPLY_PROP_QC_OPTI_DISABLE,
 	POWER_SUPPLY_PROP_VOLTAGE_VPH,
@@ -789,6 +790,9 @@ static int smb5_usb_get_prop(struct power_supply *psy,
 	switch (psp) {
 	case POWER_SUPPLY_PROP_MOISTURE_DETECTION_ENABLE:
 		val->intval = chg->moisture_detection_enabled ? 1 : 0;
+		break;
+	case POWER_SUPPLY_PROP_CC_TOGGLE_ENABLE:
+		val->intval = chg->cc_toggle_enable ? 1 : 0;
 		break;
 	case POWER_SUPPLY_PROP_PRESENT:
 		rc = smblib_get_prop_usb_present(chg, val);
@@ -1001,6 +1005,10 @@ static int smb5_usb_set_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_MOISTURE_DETECTION_ENABLE:
 		rc = enable_moisture_detection(chg, val->intval == 1);
 		break;
+	case POWER_SUPPLY_PROP_CC_TOGGLE_ENABLE:
+		chg->cc_toggle_enable = val->intval;
+		rc = smblib_set_prop_typec_power_role(chg, val);
+		break;
 	case POWER_SUPPLY_PROP_PD_CURRENT_MAX:
 		rc = smblib_set_prop_pd_current_max(chg, val);
 		break;
@@ -1126,6 +1134,7 @@ static int smb5_usb_prop_is_writeable(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_DEAD_BATTERY:
 	case POWER_SUPPLY_PROP_ADAPTER_CC_MODE:
 	case POWER_SUPPLY_PROP_APSD_RERUN:
+	case POWER_SUPPLY_PROP_CC_TOGGLE_ENABLE:
 		return 1;
 	default:
 		break;
@@ -3669,6 +3678,8 @@ static int smb5_probe(struct platform_device *pdev)
 	chg->otg_present = false;
 	chg->main_fcc_max = -EINVAL;
 	chg->moisture_detection_enabled = true;
+	chg->cc_toggle_enable = true;
+
 	mutex_init(&chg->adc_lock);
 
 	chg->regmap = dev_get_regmap(chg->dev->parent, NULL);
