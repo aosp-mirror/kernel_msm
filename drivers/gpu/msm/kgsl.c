a98d4f91a7d268fe3f647cbeec16240f7d7acddf
Mohammed Mirza Mandayappurath Manzoor <mmandaya@codeaurora.org>
2020-09-02 12:25:34 -0700

    msm: kgsl: Check if mem entry to destroy is mapped
    If  mem entry is not mapped, the pagetable would be NULL and could
    cause a crash. Extract device structure only if the pagetable is not NULL.
    
    Bug: 166942279
    
    Change-Id: I7919f97128edc816bf47ed280e52b76c7559ae4b
    Signed-off-by: Mohammed Mirza Mandayappurath Manzoor <mmandaya@codeaurora.org>
    (cherry picked from commit 20b3a54fee8333105c8969822f1d3d6b76baef3f)

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -320,9 +320,14 @@ static void kgsl_destroy_ion(struct kgsl_device *device,
 
 static void mem_entry_destroy(struct kgsl_mem_entry *entry)
 {
-	struct kgsl_device *device =
-				KGSL_MMU_DEVICE(entry->memdesc.pagetable->mmu);
+	struct kgsl_device *device = NULL;
 	unsigned int memtype;
+
+	if (entry == NULL)
+		return;
+
+	if (entry->memdesc.pagetable != NULL)
+		device = KGSL_MMU_DEVICE(entry->memdesc.pagetable->mmu);
 
 	/* pull out the memtype before the flags get cleared */
 	memtype = kgsl_memdesc_usermem_type(&entry->memdesc);
