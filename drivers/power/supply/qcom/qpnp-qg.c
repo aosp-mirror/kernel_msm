9b66e6c8e6bec5e6a5832d626e27715c67b79019
Jack Wu <wjack@google.com>
2020-09-09 20:18:01 +0800

    qpnp-qg: fix msoc stuck after PD vbus change(5V/9V)
    There was a short "not charging" status in this issue.
    After this short period, msoc did not increase because
    QC did not get the notification to update status.
    
    Fix it by adding sm7150_bms to get charger psy event change.
    
    Bug: 167989585
    Bug: 167893601
    Signed-off-by: Jack Wu <wjack@google.com>
    Signed-off-by: Ken Tsou <kentsou@google.com>
    Change-Id: Iec09b807853b95bc46d3456d368acb3ad42bc0dc
    (cherry picked from commit 316b4d729e1e7c0acbd3303d72328b1150d12842)

--- drivers/power/supply/qcom/qpnp-qg.c
+++ drivers/power/supply/qcom/qpnp-qg.c
@@ -2790,6 +2790,7 @@ static int qg_notifier_cb(struct notifier_block *nb,
 		return NOTIFY_OK;
 
 	if ((strcmp(psy->desc->name, "battery") == 0)
+		|| (strcmp(psy->desc->name, "sm7150_bms") == 0)
 		|| (strcmp(psy->desc->name, "parallel") == 0)
 		|| (strcmp(psy->desc->name, "usb") == 0)
 		|| (strcmp(psy->desc->name, "dc") == 0)) {
