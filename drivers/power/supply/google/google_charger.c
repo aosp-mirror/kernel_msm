3fed922e8eb98e8417f9ad933b8ac3de04a578b2
Jenny Ho <hsiufangho@google.com>
2020-11-09 16:53:22 +0800

    google_charger: align type between power_supply_type_text and psy_usb_type_str
    Bug: 172131727
    Signed-off-by: Jenny Ho <hsiufangho@google.com>
    Change-Id: Ic0e4a11f8ba63189a355fc4266076801888dfd3d

--- drivers/power/supply/google/google_charger.c
+++ drivers/power/supply/google/google_charger.c
@@ -339,7 +339,7 @@ static char *psy_usb_type_str[] = {
 	"USB_DCP", "USB_CDP", "USB_ACA", "USB_C",
 	"USB_PD", "USB_PD_DRP", "BrickID",
 	"USB_HVDCP", "USB_HVDCP_3", "USB_HVDCP_3P5", "Wireless", "USB_FLOAT",
-	"BMS", "Parallel", "Main", "Wipower", "USB_C_UFP", "USB_C_DFP",
+	"BMS", "Parallel", "Main", "USB_C_UFP", "USB_C_DFP",
 	"Charge_Pump",
 };
 
