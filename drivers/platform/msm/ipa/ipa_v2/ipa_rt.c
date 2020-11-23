4ba6e8450d8c1bc1a8491bb78a2fd365b386e660
Praveen Kurapati <pkurapat@codeaurora.org>
2020-11-23 14:09:07 +0530

    msm: ipa3: Add check to validate rule_cnt
    Add proper check to validate table rule count
    which may lead to overflow error.
    
    Change-Id: I9bdcafcaae4e4cff1b901929c8dc6ae804f85642
    Signed-off-by: Praveen Kurapati <pkurapat@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v2/ipa_rt.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa_rt.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2018, 2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1088,7 +1088,10 @@ static int __ipa_add_rt_rule(enum ipa_ip_type ip, const char *name,
 		list_add_tail(&entry->link, &tbl->head_rt_rule_list);
 	else
 		list_add(&entry->link, &tbl->head_rt_rule_list);
-	tbl->rule_cnt++;
+	if (tbl->rule_cnt < IPA_RULE_CNT_MAX)
+		tbl->rule_cnt++;
+	else
+		return -EINVAL;
 	if (entry->hdr)
 		entry->hdr->ref_cnt++;
 	else if (entry->proc_ctx)
