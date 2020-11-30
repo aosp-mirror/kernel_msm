c1ca17ddb9de9137dc1ee1201873d864dcb0eb89
Lucas Wei <lucaswei@google.com>
2020-11-30 17:29:19 +0800

    Revert "mm/vmalloc.c: fix __purge_vmap_area_lazy takes long time"
    This reverts commit ae7c5d15baa642b277d138bf0ea7b42d83f8bbcf.
    
    Reason for revert: vendor customzied patch
    Bug: 173092118
    Signed-off-by: Lucas Wei <lucaswei@google.com>
    Change-Id: Iefe1d36ecb7341a26463a1863ea69e5a24631fa0

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -670,7 +670,6 @@ static bool __purge_vmap_area_lazy(unsigned long start, unsigned long end)
 	struct vmap_area *va;
 	struct vmap_area *n_va;
 	bool do_free = false;
-	unsigned long flush_all_threshold = VMALLOC_END - VMALLOC_START;
 
 	lockdep_assert_held(&vmap_purge_lock);
 
@@ -686,10 +685,7 @@ static bool __purge_vmap_area_lazy(unsigned long start, unsigned long end)
 	if (!do_free)
 		return false;
 
-	if (end - start <= flush_all_threshold)
-		flush_tlb_kernel_range(start, end);
-	else
-		flush_tlb_all();
+	flush_tlb_kernel_range(start, end);
 
 	spin_lock(&vmap_area_lock);
 	llist_for_each_entry_safe(va, n_va, valist, purge_list) {
