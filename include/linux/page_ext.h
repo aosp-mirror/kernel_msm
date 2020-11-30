5f9b966a82f65176761bca813f8012da796787bd
Lucas Wei <lucaswei@google.com>
2020-11-30 17:21:27 +0800

    Revert "mm: fix the page_owner initializing issue for arm32"
    This reverts commit 8d228fbbce34bcb79d2ece6b828b628bd9b80d7c.
    
    Reason for revert: vendor customzied patch
    Bug: 173092118
    Signed-off-by: Lucas Wei <lucaswei@google.com>
    Change-Id: I67e47f530f23f2115dd0b7ff369c61210dd4f046

--- include/linux/page_ext.h
+++ include/linux/page_ext.h
@@ -43,12 +43,8 @@ static inline void page_ext_init_flatmem(void)
 {
 }
 extern void page_ext_init(void);
-static inline void page_ext_init_flatmem_late(void)
-{
-}
 #else
 extern void page_ext_init_flatmem(void);
-extern void page_ext_init_flatmem_late(void);
 static inline void page_ext_init(void)
 {
 }
@@ -69,10 +65,6 @@ static inline struct page_ext *lookup_page_ext(const struct page *page)
 }
 
 static inline void page_ext_init(void)
-{
-}
-
-static inline void page_ext_init_flatmem_late(void)
 {
 }
 
