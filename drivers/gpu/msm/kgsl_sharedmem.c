b8cbca6a09200b312fb85e7cc0c943f6f681e225
Jordan Crouse <jcrouse@codeaurora.org>
2020-12-16 12:01:47 -0700

    msm: kgsl: Stop using memdesc->usermem
    It shouldn't be illegal for memory descriptors to have multiple virtual
    memory mappings under certain circumstances. If that is the case
    tracking a single usermem address for each memdesc no longer makes much
    sense. Get rid of the memdesc->usermem member and use an atomic counter
    to track mappings instead.
    
    Bug:178809945
    Change-Id: Ic0dedbad31bafcd1019ccc8e68657cb7e3c72727
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Harshitha Sai Neelati <hsaine@codeaurora.org>
    Signed-off-by: linjoey <linjoey@google.com>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -471,8 +471,6 @@ static int kgsl_page_alloc_vmfault(struct kgsl_memdesc *memdesc,
 		get_page(page);
 		vmf->page = page;
 
-		atomic_long_add(PAGE_SIZE, &memdesc->mapsize);
-
 		return 0;
 	}
 
@@ -647,8 +645,6 @@ static int kgsl_contiguous_vmfault(struct kgsl_memdesc *memdesc,
 		return VM_FAULT_OOM;
 	else if (ret == -EFAULT)
 		return VM_FAULT_SIGBUS;
-
-	atomic_long_add(PAGE_SIZE, &memdesc->mapsize);
 
 	return VM_FAULT_NOPAGE;
 }
