3b023152a4e12b9f62495c3c9ddce3bf36a5063d
Ken Tsou <kentsou@google.com>
2020-12-29 13:44:01 +0800

    google_charger: fix work re-scheduling failure in alarm_handler
    schedule_delayed_work() did not over-write the delay time of pending work.
    Fix it by using mod_delayed_work instead.
    
    Bug: 175172404
    Change-Id: Ibf36853dc482f4ac5dad9b40a271da9142f76c81
    Signed-off-by: Ken Tsou <kentsou@google.com>
    (cherry picked from commit fd83f59e8059a679cac99b142771d64dcd5874b1)

--- drivers/power/supply/google/google_charger.c
+++ drivers/power/supply/google/google_charger.c
@@ -300,7 +300,7 @@ google_chg_alarm_handler(struct alarm *alarm, ktime_t time)
 
 	__pm_stay_awake(&chg_drv->chg_ws);
 
-	schedule_delayed_work(&chg_drv->chg_work, 0);
+	reschedule_chg_work(chg_drv);
 
 	return ALARMTIMER_NORESTART;
 }
