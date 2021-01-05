154137adfadf9957015762ae3564e2dc8da09637
yixuanjiang <yixuanjiang@google.com>
2021-01-05 17:58:04 +0800

    asoc: codec: rt5514: handle i2c error case if receive irq watchdog
    Modify rt5514 watchdog flow, no need dump reg via i2c
    if i2c control has problem.
    
    Bug: 176542726
    Signed-off-by: yixuanjiang <yixuanjiang@google.com>
    Change-Id: I095e91a6fe1c95e92ce4d7d351f7e3131241fbb4

--- sound/soc/codecs/rt5514-spi.c
+++ sound/soc/codecs/rt5514-spi.c
@@ -334,9 +334,15 @@ char *rt5514_codec_hwinfo(void)
 static bool rt5514_watchdog_dbg_info(struct rt5514_dsp *rt5514_dsp)
 {
 	RT5514_DBGBUF_MEM dbgbuf;
-	unsigned int i, val[5];
+	unsigned int i, val[5], ret = 0;
 
-	regmap_read(rt5514_g_i2c_regmap, 0x18002f04, &val[0]);
+	ret = regmap_read(rt5514_g_i2c_regmap, 0x18002f04, &val[0]);
+
+	if (ret) {
+		dev_err(rt5514_dsp->dev,
+			"Failed to i2c read %d\n", ret);
+		return true;
+	}
 
 	if (!(val[0] & 0x2))
 		return false;
