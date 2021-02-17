234b0f65d6a3e527c0f54c07946184f734cf0874
Edgar Arriaga García <edgararriaga@google.com>
2021-02-17 00:40:56 +0000

    Revert "BACKPORT: mm/madvise: pass mm to do_madvise"
    Revert submission 1840387-backport-proc-madvise-v2
    
    Reason for revert: Since we are putting it in 5.4 ACK kernel and we have the framework code that fallback to procfs for older kernels, we discussed that it was not necessary to maintain it for 4.14 since there is no extra value added as new kernels will be using 5.4+ anyways.
    
    Reverted Changes:
    I3624a8b06:FROMLIST: mm/madvise: replace ptrace attach requir...
    I49b1a581d:UPSTREAM: mm/madvise: remove racy mm ownership che...
    I8ae135a79:UPSTREAM: mm: validate pmd after splitting
    I1efd9d7d7:Revert "mm: abort per-process reclaim"
    Id1f7df48d:FROMLIST: mm/madvise: fix memory leak from process...
    I9383bebe4:BACKPORT: pid: move pidfd_get_pid() to pid.c
    Icfff940ab:BACKPORT: mm/madvise: introduce process_madvise() ...
    I11b99220a:BACKPORT: mm/madvise: pass mm to do_madvise
    I9e300af00:UPSTREAM: mm: check that mm is still valid in madv...
    I125cdc841:UPSTREAM: mm: factor out common parts between MADV...
    Id400fe311:UPSTREAM: mm: introduce MADV_PAGEOUT
    I18d197e4b:UPSTREAM: mm: do not allow MADV_PAGEOUT for CoW pa...
    I8f0f9d54e:BACKPORT: mm: introduce MADV_COLD
    Id938f02c5:BACKPORT: pagewalk: separate function pointers fro...
    Icc0a67859:UPSTREAM: mm: split out a new pagewalk.h header fr...
    Ic8a6834b3:BACKPORT: pidfd: add P_PIDFD to waitid()
    I6200af248:Revert "mm: per-process reclaim"
    Ibeabf2538:Revert "mm: introduce deactivate_page"
    I510535eff:Revert "mm: perproc-reclaim: do not discarding fil...
    I8efddbaa1:Revert "mm: perproc-reclaim: do not scanning anony...
    Ifae69b33d:Revert "floral: enable CONFIG_PROCESS_RECLAIM"
    
    Change-Id: I36ed889c5c6e23ec5e082f9dadc530aa3c262c0e

--- mm/madvise.c
+++ mm/madvise.c
@@ -763,8 +763,6 @@ static long madvise_dontneed_free(struct vm_area_struct *vma,
 				  unsigned long start, unsigned long end,
 				  int behavior)
 {
-	struct mm_struct *mm = vma->vm_mm;
-
 	*prev = vma;
 	if (!can_madv_lru_vma(vma))
 		return -EINVAL;
@@ -772,8 +770,8 @@ static long madvise_dontneed_free(struct vm_area_struct *vma,
 	if (!userfaultfd_remove(vma, start, end)) {
 		*prev = NULL; /* mmap_sem has been dropped, prev is stale */
 
-		down_read(&mm->mmap_sem);
-		vma = find_vma(mm, start);
+		down_read(&current->mm->mmap_sem);
+		vma = find_vma(current->mm, start);
 		if (!vma)
 			return -ENOMEM;
 		if (start < vma->vm_start) {
@@ -827,7 +825,6 @@ static long madvise_remove(struct vm_area_struct *vma,
 	loff_t offset;
 	int error;
 	struct file *f;
-	struct mm_struct *mm = vma->vm_mm;
 
 	*prev = NULL;	/* tell sys_madvise we drop mmap_sem */
 
@@ -855,13 +852,13 @@ static long madvise_remove(struct vm_area_struct *vma,
 	get_file(f);
 	if (userfaultfd_remove(vma, start, end)) {
 		/* mmap_sem was not released by userfaultfd_remove() */
-		up_read(&mm->mmap_sem);
+		up_read(&current->mm->mmap_sem);
 	}
 	error = vfs_fallocate(f,
 				FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE,
 				offset, end - start);
 	fput(f);
-	down_read(&mm->mmap_sem);
+	down_read(&current->mm->mmap_sem);
 	return error;
 }
 
@@ -1044,7 +1041,7 @@ madvise_behavior_valid(int behavior)
  *  -EBADF  - map exists, but area maps something that isn't a file.
  *  -EAGAIN - a kernel resource was temporarily unavailable.
  */
-int do_madvise(struct mm_struct *mm, unsigned long start, size_t len_in, int behavior)
+SYSCALL_DEFINE3(madvise, unsigned long, start, size_t, len_in, int, behavior)
 {
 	unsigned long end, tmp;
 	struct vm_area_struct *vma, *prev;
@@ -1082,7 +1079,7 @@ int do_madvise(struct mm_struct *mm, unsigned long start, size_t len_in, int beh
 
 	write = madvise_need_mmap_write(behavior);
 	if (write) {
-		if (down_write_killable(&mm->mmap_sem))
+		if (down_write_killable(&current->mm->mmap_sem))
 			return -EINTR;
 
 		/*
@@ -1097,12 +1094,12 @@ int do_madvise(struct mm_struct *mm, unsigned long start, size_t len_in, int beh
 		 * but for now we have the mmget_still_valid()
 		 * model.
 		 */
-		if (!mmget_still_valid(mm)) {
-			up_write(&mm->mmap_sem);
+		if (!mmget_still_valid(current->mm)) {
+			up_write(&current->mm->mmap_sem);
 			return -EINTR;
 		}
 	} else {
-		down_read(&mm->mmap_sem);
+		down_read(&current->mm->mmap_sem);
 	}
 
 	/*
@@ -1110,7 +1107,7 @@ int do_madvise(struct mm_struct *mm, unsigned long start, size_t len_in, int beh
 	 * ranges, just ignore them, but return -ENOMEM at the end.
 	 * - different from the way of handling in mlock etc.
 	 */
-	vma = find_vma_prev(mm, start, &prev);
+	vma = find_vma_prev(current->mm, start, &prev);
 	if (vma && start > vma->vm_start)
 		prev = vma;
 
@@ -1147,19 +1144,14 @@ int do_madvise(struct mm_struct *mm, unsigned long start, size_t len_in, int beh
 		if (prev)
 			vma = prev->vm_next;
 		else	/* madvise_remove dropped mmap_sem */
-			vma = find_vma(mm, start);
+			vma = find_vma(current->mm, start);
 	}
 out:
 	blk_finish_plug(&plug);
 	if (write)
-		up_write(&mm->mmap_sem);
+		up_write(&current->mm->mmap_sem);
 	else
-		up_read(&mm->mmap_sem);
+		up_read(&current->mm->mmap_sem);
 
 	return error;
-}
-
-SYSCALL_DEFINE3(madvise, unsigned long, start, size_t, len_in, int, behavior)
-{
-	return do_madvise(current->mm, start, len_in, behavior);
 }
