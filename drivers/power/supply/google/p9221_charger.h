88ae0c2627f63a7c93b4760c60a2c5904387b60c
yihsiangpeng <yihsiangpeng@google.com>
2021-03-30 19:57:07 +0800

    p9221_charger: use txid to identify devices that implement DD
    Solve an issue with 3rd party chargers using Google PTMC
    
    Bug: 183183208
    Signed-off-by: yihsiangpeng <yihsiangpeng@google.com>
    Change-Id: I90113a1c48d8d51e1fd17c6a2f6a70c5b2ad7437
    Signed-off-by: AleX Pelosi <apelosi@google.com>
    (cherry picked from commit b47a3997747d84727938170a20ddb6214601b8cb)

--- drivers/power/supply/google/p9221_charger.h
+++ drivers/power/supply/google/p9221_charger.h
@@ -241,6 +241,10 @@
 #define ACCESSORY_TYPE_MASK			0x7
 #define ACCESSORY_TYPE_LOW_POWER_TX		BIT(2)
 
+#define TXID_TYPE_MASK			0xFF000000 /* bit[24-31] */
+#define TXID_TYPE_SHIFT			24
+#define TXID_DD_TYPE			0xE0
+
 enum p9221_align_mfg_chk_state {
 	ALIGN_MFG_FAILED = -1,
 	ALIGN_MFG_CHECKING,
