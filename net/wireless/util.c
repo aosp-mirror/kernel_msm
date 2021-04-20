bb9ea88e00819d10ea046a7d5304097463e6f4c6
JohnnLee <johnnlee@google.com>
2021-04-20 16:47:19 +0800

    Revert "cfg80211: adapt to new channelization of the 6GHz band"
    This reverts commit caa449d9738732e96753ba22fb9fa1d5c5b062d2.
    
    Bug: 185310516
    Signed-off-by: JohnnLee <johnnlee@google.com>
    Change-Id: I6d7179fbe0038812ff27416c20bff054b5eb46a2

--- net/wireless/util.c
+++ net/wireless/util.c
@@ -88,11 +88,9 @@ int ieee80211_channel_to_frequency(int chan, enum nl80211_band band)
 			return 5000 + chan * 5;
 		break;
 	case NL80211_BAND_6GHZ:
-		/* see 802.11ax D6.1 27.3.23.2 */
-		if (chan == 2)
-			return MHZ_TO_KHZ(5935);
+		/* see 802.11ax D4.1 27.3.22.2 */
 		if (chan <= 253)
-			return MHZ_TO_KHZ(5950 + chan * 5);
+			return MHZ_TO_KHZ(5940 + chan * 5);
 		break;
 	case NL80211_BAND_60GHZ:
 		if (chan < 5)
