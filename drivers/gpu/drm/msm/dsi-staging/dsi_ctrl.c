6032f250bef3fa467c46f936062ffd244b43845c
Ravikanth Tuniki <rtunik@codeaurora.org>
2021-05-17 14:41:41 +0800

    disp: msm: add check for buffer length before copy
    Length of the buffer to be copied is checked
    against both source and destination buffer lengths
    before copying. This ensures that there is  no
    buffer overflow while reading as well as writing.
    
    Bug: 184564101
    Change-Id: I19173aad48274e5a5169c0136863bbdd9bb5fef0
    Signed-off-by: raylinhsu <raylinhsu@google.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_ctrl.c
@@ -118,6 +118,9 @@ static ssize_t debugfs_state_info_read(struct file *file,
 			dsi_ctrl->clk_freq.pix_clk_rate,
 			dsi_ctrl->clk_freq.esc_clk_rate);
 
+	if (len > count)
+		len = count;
+
 	len = min_t(size_t, len, SZ_4K);
 	if (copy_to_user(buff, buf, len)) {
 		kfree(buf);
@@ -172,6 +175,9 @@ static ssize_t debugfs_reg_dump_read(struct file *file,
 		kfree(buf);
 		return rc;
 	}
+
+	if (len > count)
+		len = count;
 
 	len = min_t(size_t, len, SZ_4K);
 	if (copy_to_user(buff, buf, len)) {
