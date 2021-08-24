6bcc3d8b547b5b8ae194a10e0cd6bb389545e80d
Jenny Ho <hsiufangho@google.com>
2021-08-25 05:09:00 +0800

    google_battery: do not block status in dry_run mode
    Bug: 197600928
    Signed-off-by: Jenny Ho <hsiufangho@google.com>
    Change-Id: I45de3979eb898a57fa27912015b81ad2aa0313ff

--- drivers/power/supply/google/google_battery.c
+++ drivers/power/supply/google/google_battery.c
@@ -4864,7 +4864,8 @@ static int gbatt_get_status(struct batt_drv *batt_drv,
 
 	/* ->buck_enabled = 1, from here ownward device is connected */
 
-	if (batt_drv->batt_health == POWER_SUPPLY_HEALTH_OVERHEAT) {
+	if (batt_drv->batt_health == POWER_SUPPLY_HEALTH_OVERHEAT &&
+	    gbms_temp_defend_dry_run(false, false)) {
 		val->intval = POWER_SUPPLY_STATUS_NOT_CHARGING;
 		return 0;
 	}
