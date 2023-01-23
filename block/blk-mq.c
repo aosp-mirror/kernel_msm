d763380ca28ea43bdb16110180708f78e06e9ec5
Bart Van Assche <bvanassche@google.com>
2023-01-23 11:22:56 -0800

    BACKPORT: UPSTREAM: blk-mq: Introduce the BLK_MQ_F_NO_SCHED_BY_DEFAULT flag
    elevator_get_default() uses the following algorithm to select an I/O
    scheduler from inside add_disk():
    - In case of a single hardware queue or sharing hardware queues across
      multiple request queues (BLK_MQ_F_TAG_HCTX_SHARED), use mq-deadline.
    - Otherwise, use 'none'.
    
    This is a good choice for most but not for all block drivers. Make it
    possible to override the selection of mq-deadline with a new flag,
    namely BLK_MQ_F_NO_SCHED_BY_DEFAULT.
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Bug: 265546875
    (cherry picked from commit 90b7198001f23ea37d3b46dc631bdaa2357a20b1)
    Change-Id: I4fb658957c193f350e74bdb5876c20a8f628fcb1
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2464,7 +2464,8 @@ struct request_queue *blk_mq_init_allocated_queue(struct blk_mq_tag_set *set,
 	blk_mq_add_queue_tag_set(set, q);
 	blk_mq_map_swqueue(q);
 
-	if (!(set->flags & BLK_MQ_F_NO_SCHED)) {
+	if (!(set->flags &
+	      (BLK_MQ_F_NO_SCHED | BLK_MQ_F_NO_SCHED_BY_DEFAULT))) {
 		int ret;
 
 		ret = blk_mq_sched_init(q);
