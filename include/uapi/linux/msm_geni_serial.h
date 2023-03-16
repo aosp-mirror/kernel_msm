c4d36c0eb3ee913d7ae9216ede921664ee968be6
Ben Fennema <fennema@google.com>
2023-03-15 17:07:55 -0700

    Switch GENI_SERIAL get/put pm ioctl to use an existing compat ioctl
    With 32-bit userspace/64-bit kernel, the compat ioctl interface only
    passes through a limited number of specific ioctls to the underlying
    serial driver. Replace qcoms ioctl #'s with two that will be passed
    through.
    
    Bug: 269147645
    Change-Id: Ice48addf7e5dbd7c2a228fa0c8dc35c33d957702
    Signed-off-by: Ben Fennema <fennema@google.com>

--- include/uapi/linux/msm_geni_serial.h
+++ include/uapi/linux/msm_geni_serial.h
@@ -10,8 +10,8 @@
 /* IOCTLS used by BT clients to control UART power state */
 
 #define MSM_GENI_SERIAL_TIOCFAULT	0x54EC  /* Uart fault */
-#define MSM_GENI_SERIAL_TIOCPMGET	0x54ED	/* PM get */
-#define MSM_GENI_SERIAL_TIOCPMPUT	0x54EE	/* PM put */
+#define MSM_GENI_SERIAL_TIOCPMGET	_IOR('t', 55, int)	/* PM get */ /* PPPIOCGCHAN */
+#define MSM_GENI_SERIAL_TIOCPMPUT	_IOR('t', 86, int)	/* PM put */ /* PPPIOCGUNIT */
 #define MSM_GENI_SERIAL_TIOCPMACT	0x54EF	/* PM is active */
 
 #endif /* __UAPI_LINUX_MSM_GENI_SERIAL_H */
