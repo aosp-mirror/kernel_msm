f981735b06656af8ac8fb1ce13f1f7dd8a168b74
Rishi Sikka <rishisikka@google.com>
2023-06-26 12:41:43 +0000

    pinctrl: Allow option to unreserve monaco qup02
    Add a config option that allows the monaco qup02 pins to be unreserved
    from the monaco pinctrl driver. This allows use of these gpios for gpio
    function or for qup02 function from HLOS.
    
    Bug: 288864071
    Test: Set the flag in config and build, gpioset gpio9 works
    Change-Id: I6b940e1c076efb0a191d2c32f5ba159fac83f665
    Signed-Off-By: Rishi Sikka <rishisikka@google.com>

--- drivers/pinctrl/qcom/pinctrl-monaco.c
+++ drivers/pinctrl/qcom/pinctrl-monaco.c
@@ -1596,7 +1596,10 @@ static struct pinctrl_qup monaco_qup_regs[] = {
 };
 
 static const int monaco_reserved_gpios[] = {
-	0, 1, 2, 3, 9, 111, -1
+#ifndef CONFIG_PINCTRL_MONACO_ACCESS_QUP02
+	0, 1, 2, 3, 9,
+#endif
+	111, -1
 };
 
 static const struct msm_gpio_wakeirq_map monaco_mpm_map[] = {
