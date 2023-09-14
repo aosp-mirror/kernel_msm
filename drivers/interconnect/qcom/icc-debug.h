cf77a2c112387afb77d8444c80746a5ee241bb05
Raviteja Laggyshetty <quic_rlaggysh@quicinc.com>
2023-09-14 10:52:32 +0530

    interconnect: qcom: snapshot of icc-debug module
    icc-debug module snapshot from msm-5.15 branch.
    Add a debug library that provides QCOM-specific interconnect debug
    features. This includes support for printing all enabled interconnect
    votes to console when entering suspend.
    commit 8dfec586eb2a ("interconnect: qcom: Add debug library").
    
    Change-Id: I61302f95c94176f833bd1c88ddc76b88121b3f79
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>
    Signed-off-by: Raviteja Laggyshetty <quic_rlaggysh@quicinc.com>

---
+++ drivers/interconnect/qcom/icc-debug.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2021, The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved. */
+
+#ifndef __QCOM_ICC_DEBUG_H__
+#define __QCOM_ICC_DEBUG_H__
+
+#include <linux/interconnect-provider.h>
+
+int qcom_icc_debug_register(struct icc_provider *provider);
+int qcom_icc_debug_unregister(struct icc_provider *provider);
+
+#endif
