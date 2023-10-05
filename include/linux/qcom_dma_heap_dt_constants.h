6961a7ff1e7fd127a92596c81af8ec9a436dfd7c
Sayali Patil <quic_sayapati@quicinc.com>
2023-10-05 16:30:44 +0530

    dma-heap: qcom: Add a snapshot of the dma-buf heaps drivers
    Add a snapshot of the dma-buf heaps drivers from msm-5.15 as of
    commit f9d8782a9af5 ("drivers: soc: qcom: Fix dependencies on WALT").
    
    Change-Id: Ib6b01165654d74df592acf7d9e6d889f1c29b458
    Signed-off-by: Sayali Patil <quic_sayapati@quicinc.com>

---
+++ include/linux/qcom_dma_heap_dt_constants.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+#ifndef _QCOM_DMA_HEAP_DT_CONSTATS_H
+#define _QCOM_DMA_HEAP_DT_CONSTATS_H
+
+#define HEAP_TYPE_SECURE_CARVEOUT 0
+#define HEAP_TYPE_CARVEOUT 1
+#define HEAP_TYPE_CMA 2
+#define HEAP_TYPE_TUI_CARVEOUT 3
+
+#endif /* _QCOM_DMA_HEAP_DT_CONSTATS_H */
