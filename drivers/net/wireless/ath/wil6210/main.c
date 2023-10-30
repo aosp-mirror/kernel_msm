86f93cf4267e92c3b9e449fcb0d93b917f73c0c9
Subrat Dash <quic_sdash@quicinc.com>
2023-10-30 15:51:39 +0530

    wil6210: check integrity of received AMSDU packets
    Check integrity of received AMSDU packet, 802.11ad QoS spec requires
    that AMSDU frame contains only MSDUs whose destination address (DA)
    and sender address (SA) parameter values map to the same receiver
    address (RA) and transmitter address (TA) values.
    wil6210 Talyn HW does not check this before it cuts the AMSDU frame
    into multiple received MSDU packets.
    Adding checks to wil6210 driver to enforce spec compliance behavior
    by checking all AMSDU sub frames if it complies with the following:
    1- On AP, check packet SA is its client mac address, and on Client
       check that the DA is the local mac address. If not drop the packet
    2- if AMSDU sub frame was dropped on item 1, drop all next sub frame
       of the AMSDU by checking it has same sn/tid.
    
    This patch drops all WDS frames before it checks valid AMSDU, WDS
    includes supporting MAC header with 4 addresses which is not supported
    yet, moreover, WDS implies different validity checks on AMSDU frame.
    
    Change-Id: I71a39f95c034f05023e0e7ae3ffb5d2b4f8c6b24
    Signed-off-by: Ahmad Masri <amasri@codeaurora.org>
    Signed-off-by: Visweswara Tanuku <quic_vtanuku@quicinc.com>
    Signed-off-by: Subrat Dash <quic_sdash@quicinc.com>

--- drivers/net/wireless/ath/wil6210/main.c
+++ drivers/net/wireless/ath/wil6210/main.c
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2012-2017 Qualcomm Atheros, Inc.
  * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/moduleparam.h>
@@ -296,6 +297,7 @@ __acquires(&sta->tid_rx_lock) __releases(&sta->tid_rx_lock)
 	/* statistics */
 	memset(&sta->stats, 0, sizeof(sta->stats));
 	sta->stats.tx_latency_min_us = U32_MAX;
+	wil_sta_info_amsdu_init(sta);
 }
 
 static void _wil6210_disconnect_complete(struct wil6210_vif *vif,
@@ -680,6 +682,13 @@ void wil_bcast_fini_all(struct wil6210_priv *wil)
 		if (vif)
 			wil_bcast_fini(vif);
 	}
+}
+
+void wil_sta_info_amsdu_init(struct wil_sta_info *sta)
+{
+	sta->amsdu_drop_sn = -1;
+	sta->amsdu_drop_tid = -1;
+	sta->amsdu_drop = 0;
 }
 
 int wil_priv_init(struct wil6210_priv *wil)
